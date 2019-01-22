d132424bca3ed8a6abe21873bf9709aeaabc9adc
leeci <webrsb@gmail.com>
2019-01-22 22:47:03 +0800

    images

---
+++ assets/Unique_Unit.php
@@ -0,0 +1,401 @@
+<!DOCTYPE html>
+<html lang="zh-tw">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Aoetw</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="http://getbootstrap.com/assets/css/docs.min.css" rel="stylesheet">
+    <link href="http://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+
+    <!-- Custom styles for this template -->
+    <link href="jumbotron.css" rel="stylesheet">
+    <style>
+        body {
+            padding-top: 0px;
+            font-family: "微軟正黑體";
+            background-color: #f5f5f5;
+            font-size: 18px;
+        }
+
+        .unit h3 {
+            text-align: center;
+        }
+
+        .unit a {
+            color: #fff;
+        }
+
+        .unititle {
+            text-align: center;
+            padding: 10px;
+        }
+
+        .unitcontent {
+
+            padding: 10px;
+        }
+
+.col-md-3{
+  margin-top: 10px;
+}
+                    </style>
+
+
+
+
+
+</head>
+
+<body>
+    <?php include("head.php"); ?>
+
+
+
+    <div class="container" style="margin-top:50px">
+        <!-- Example row of columns -->
+        <h1>特殊單位</h1>
+        <h3></h3>
+        <div class="row">
+            <div class="col-md-12">
+                <p>大多數的國家只有一種特殊單位</p>
+            </div>
+        </div>
+
+        <h3>概要</h3>
+            <div class="hrn1"></div>
+            <div class="n1">
+<ul>
+<li>遊戲中每個文明最少有一個特殊單位，只能在城堡時代於城堡訓練，但有八個文明有第二種特殊單位，並且可以在城堡以外的地方訓練。</li>
+
+<li>特殊單位大概三為三種，城堡的與非城堡的還有文明共享的，詳細可以看下方表格。</li>
+
+<li>特殊單位提供了遊戲中更多元的打法，可以與同盟互相配合，或是搭配地圖地形選擇不同的文明兵種搭配，為遊戲增加更多的樂趣。</li>
+
+<li>但值得注意的是，雖然日本武士對於所以的特殊單位(包含他自己)有加成，但對帝王駱駝騎兵與帝王戰茅兵是沒有加成的。</li>
+</ul>
+          </div>      
+
+
+
+        <div class="row">
+            <div class="col-md-12">
+            <h3><a href="">城堡</a></h3>  
+            <div class="hrn1"></div>
+                <table class="table-bordered" width="100%">
+                    <tr>
+                        <td class="tunitcc" style="width: 15%;">兵種</td>
+                        <td class="tunitcc" style="width: 15%;">文明</td>
+                        <td class="tunitcc" style="width: 70%;">概要</td>
+                       
+                    </tr>
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/LongbowmanIcon.jpg" alt=""><a href="Longbowman.php">長弓兵</a></td>
+                        <td class="tunitc4">不列顛</td>
+                        <td class="tunitc6">遠距離弓兵，攻擊高。</td>
+                        
+                    </tr>
+                    
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/CataphractIcon.jpg" alt=""><a href="Cataphract.php">聖騎兵</a></td>
+                        <td class="tunitc4">拜占庭</td>
+                        <td class="tunitc6">反步兵的騎兵，但訓練與研發昂貴。</td>
+                       
+                    </tr>     
+                    
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/WBRSK.jpg" alt=""><a href="Woad_Raider.php">松藍武士</a></td>
+                        <td class="tunitc4">賽爾特</td>
+                        <td class="tunitc6">移動相當快速的步兵。</td>
+                       
+                    </tr>
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/ChuKoNuIcon.jpg" alt=""><a href="Chu_Ko_Nu.php">連弩兵</a></td>
+                        <td class="tunitc4">中國</td>
+                        <td class="tunitc6">攻擊高的弓兵，但射速慢。</td>
+                     
+                    </tr>
+  
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/TAXEM.jpg" alt=""><a href="Throwing_Axeman.php">擲斧兵</a></td>
+                        <td class="tunitc4">法蘭克</td>
+                        <td class="tunitc6">擁有遠距離攻擊的近戰步兵單位。</td>
+                        
+                    </tr>
+                   
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/HuskarlIcon.jpg" alt=""><a href="Huskarl.php">哥德衛隊</a></td>
+                        <td class="tunitc4">哥德</td>
+                        <td class="tunitc6">廉價、移動快速的反弓兵的步兵單位。</td>
+                   
+                    </tr>
+                                  
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/SMURI.jpg" alt=""><a href="Samurai.php">日本武士</a></td>
+                        <td class="tunitc4">日本</td>
+                        <td class="tunitc6">攻擊速度高，對特殊單位有額外的加成。</td>
+                
+                    </tr>
+  
+  <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/MangudaiIcon.jpg" alt=""><a href="Mangudai.php">蒙古突騎</a></td>
+                        <td class="tunitc4">蒙古</td>
+                        <td class="tunitc6">射速、移動速度快的馬弓騎兵。</td>
+                      
+                    </tr>
+  
+ 
+                     <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/WarElephantIcon.jpg" alt=""><a href="War_Elephant.php">戰象</a></td>
+                        <td class="tunitc4">波斯</td>
+                       <td class="tunitc6">破壞力十足但移動緩慢的騎兵單位。</td>
+                       
+                    </tr>
+  
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/Saracenmamluk_icon.png" alt=""><a href="Mameluke.php">奴隸兵</a></td>
+                        <td class="tunitc4">薩拉森</td>
+                       <td class="tunitc6">造價昂貴，但反騎兵且具有遠程攻擊的近戰駱駝騎兵單位。</td>
+                      
+                    </tr>
+   
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/Crusade_avatar.png" alt=""><a href="Teutonic_Knight.php">條頓武士</a></td>
+                        <td class="tunitc4">條頓</td>
+                        <td class="tunitc6">攻擊、防禦與血量優異但行動非常緩慢的步兵。</td>
+                   
+                    </tr>
+                  
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/JanissaryIcon.jpg" alt=""><a href="Janissary.php">土耳其火槍兵</a></td>
+                        <td class="tunitc4">土耳其</td>
+                        <td class="tunitc6">高攻擊，射程極遠的徒步火槍單位。</td>
+                    
+                    </tr>
+                    
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/VBRSK.jpg" alt=""><a href="Berserk.php">狂戰士</a></td>
+                        <td class="tunitc4">維京</td>
+                       <td class="tunitc6">可自行回復生命值，且研發特殊科技後可以反馬。</td>
+                   
+                    </tr>
+                   
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/JaguarWarriorIcon.jpg" alt=""><a href="Jaguar_Warrior.php">豹勇士</a></td>
+                        <td class="tunitc4">阿茲特克</td>
+                        <td class="tunitc6">對於其他步兵有額外的加成的步兵單位。</td>
+                  
+                    </tr>
+  
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/TarkanIcon.jpg" alt=""><a href="Tarkan.php">韃靼騎兵</a></td>
+                        <td class="tunitc4">匈奴</td>
+                        <td class="tunitc6">遠防與血量高，對於建築物有額外加成的騎兵。</td>
+                
+                    </tr>
+                    
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/WarWagonIcon.jpg" alt=""><a href="War_Wagon.php">馬戰車</a></td>
+                        <td class="tunitc4">韓國</td>
+                       <td class="tunitc6">高遠防與血量的馬弓騎兵類。</td>
+                       
+                    </tr>
+                     
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/TurtleShipIcon.jpg" alt=""><a href="Turtle_Ship.php">龜甲船</a></td>
+                        <td class="tunitc4">韓國</td>
+                       <td class="tunitc6">碼頭中訓練，攻擊非常高。</td>
+                 
+                    </tr>
+                   
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/PlumedArcherIcon.jpg" alt=""><a href="Plumed_Archer.php">羽毛箭射手</a></td>
+                        <td class="tunitc4">馬雅</td>
+                        <td class="tunitc6">便宜、血量高，移動速度快的弓兵單位。</td>
+                 
+                    </tr>
+                   
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/ConquistadorIcon.jpg" alt=""><a href="Conquistador.php">征服者</a></td>
+                        <td class="tunitc4">西班牙</td>
+                        <td class="tunitc6">移動速度快，高攻擊，裝甲高的火槍騎兵單位。</td>
+                  
+                    </tr>
+                    
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/MissionaryIcon.jpg" alt=""><a href="Missionary.php">傳教士</a></td>
+                        <td class="tunitc4">西班牙</td>
+                        <td class="tunitc6">修道院中生產，無法拿遺跡，且治療與招降能力皆不足僧侶，但移動速度快。</td>
+                   
+                    </tr>
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/Kamayuk_Icon.jpg" alt=""><a href="Kamayuk.php">槍隊長</a></td>
+                        <td class="tunitc4">印加</td>
+                        <td class="tunitc6">具有一格遠程距離的反騎兵的步兵單位。</td>
+                  
+                    </tr>
+                    
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/Slingericon.png" alt=""><a href="Slinger.php">投石手</a></td>
+                        <td class="tunitc4">印加</td>
+                        <td class="tunitc6">於射箭場訓練，城堡時代即可訓練的反步兵單位。本質上是弩兵。</td>
+            
+                    </tr>
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/ElephantArcherIcon.jpg" alt=""><a href="Elephant_Archer.php">象弓騎兵</a></td>
+                        <td class="tunitc4">印度</td>
+                       <td class="tunitc6">高血量高裝甲的遠程大象。</td>
+               
+                    </tr>
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/ImperialCamelIcon.jpg" alt=""><a href="Imperial_Camel.php">帝王駱駝騎兵</a></td>
+                        <td class="tunitc4">印度</td>
+                        <td class="tunitc6">帝王時代於馬廄中生產，重裝駱駝騎兵的升級，反馬單位。</td>
+                 
+                    </tr>
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/New_Genoese_Crossbowman.png" alt=""><a href="Genoese_Crossbowman.php">熱那亞弩手</a></td>
+                        <td class="tunitc4">義大利</td>
+                        <td class="tunitc6">反騎兵的重裝弩兵，移動速度緩慢，生精銳之前射速慢。</td>
+                
+                    </tr>
+                   
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/CondottieroNewIcon.jpg" alt=""><a href="Condottiero.php">傭兵</a></td>
+                        <td class="tunitc4">義大利</td>
+                        <td class="tunitc6">只要是義大利或其同盟於帝王時代在兵營中可生產，移動速度快的反火槍單位。</td>
+                   
+                    </tr>
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/Magyar_Huszar_icon.jpg" alt=""><a href="Magyar_Huszar.php">馬札爾驃騎兵</a></td>
+                        <td class="tunitc4">馬札爾</td>
+                        <td class="tunitc6">便宜速度快的輕騎兵單位。</td>
+                  
+                    </tr>
+  
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/New_Boyar.png" alt=""><a href="Boyar.php">貴族鐵騎</a></td>
+                        <td class="tunitc4">斯拉夫</td>
+                        <td class="tunitc6">重裝甲重型騎兵。</td>
+                
+                    </tr>
+                    
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/CamelArcherIcon.jpg" alt=""><a href="Camel_Archer.php">駱駝弓騎兵</a></td>
+                        <td class="tunitc4">柏柏</td>
+                        <td class="tunitc6">反馬弓騎兵的遠程駱駝。</td>
+                        
+                    </tr>
+                    
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/ShotelIcon.png" alt=""><a href="Shotel_Warrior.php">彎刀勇士</a></td>
+                        <td class="tunitc4">衣索比亞</td>
+                        <td class="tunitc6">血量防禦低，但移動快速高攻擊的步兵，且不便宜。</td>
+                     
+                    </tr>
+                    
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/GbetoIcon.png" alt=""><a href="Gbeto.php">飛刀女獵手</a></td>
+                        <td class="tunitc4">城堡</td>
+                       <td class="tunitc6">移動快速的遠程步兵單位，攻擊高但射速慢。</td>
+                       
+                    </tr>
+                    
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/OrganGunIcon.jpg" alt=""><a href="Organ_Gun.php">手風琴砲</a></td>
+                        <td class="tunitc4">葡萄牙</td>
+                        <td class="tunitc6">高攻擊的火藥單位。</td>
+                       
+                    </tr>
+                   
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/CaravelIcon2.jpg" alt=""><a href="Caravel.php">卡拉維爾戰船</a></td>
+                        <td class="tunitc4">葡萄牙</td>
+                        <td class="tunitc6">碼頭生產，具有貫穿功能的戰船。</td>
+                       
+                    </tr>
+                    
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/Arambaiicon.png" alt=""><a href="Arambai.php">飛鏢騎兵</a></td>
+                        <td class="tunitc4">緬甸</td>
+                        <td class="tunitc6">移動速度與發射速度快，且攻擊高的遠程騎兵。</td>
+                        
+                    </tr>
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/Ballistaelephanticon.png" alt=""><a href="Ballista_Elephant.php">弩砲象</a></td>
+                        <td class="tunitc4">高棉</td>
+                        <td class="tunitc6">象板弩砲，高防禦高血量，且可用弩砲銷毀樹木。</td>
+                       
+                    </tr>
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/Karambitwarrioricon.png" alt=""><a href="Karambit_Warrior.php">爪刀勇士</a></td>
+                        <td class="tunitc4">馬來</td>
+                        <td class="tunitc6">廉價，移動快速的步兵，且只佔0.5人口。</td>
+                       
+                    </tr>
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/Rattanarchericon.png" alt=""><a href="Rattan_Archer.php">藤甲弓兵</a></td>
+                        <td class="tunitc4">越南</td>
+                        <td class="tunitc6">移動快速高防禦的反射手的弓兵。</td>
+                       
+                    </tr>
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/Imperialskirmishericon.png" alt=""><a href="Imperial_Skirmisher.php">帝王戰茅兵</a></td>
+                        <td class="tunitc4">越南</td>
+                        <td class="tunitc6">越南及其同盟於帝王時代可於射箭場訓練，為精銳戰茅兵的再升級。</td>
+                      
+                    </tr>
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/GenitourIcon.jpg" alt=""><a href="GenitourIcon.php">標槍騎兵</a></td>
+                        <td class="tunitc4">柏柏</td>
+                        <td class="tunitc6">越南及其同盟於城堡時代可於射箭場訓練，反弓兵的騎馬矛兵。</td>
+                  
+                    </tr>
+                    <tr>
+                        <td class="tunitc6"><img style="margin: 2px;" src="img/LongboatIcon.jpg" alt=""><a href="Caravel.php">維京大戰船</a></td>
+                        <td class="tunitc4">維京</td>
+                        <td class="tunitc6">城堡時代於碼頭生產，可一次發出多支箭的戰船。</td>
+                       
+                    </tr>
+  
+                </table>
+
+            </div>
+            </div>
+            <hr>
+
+
+        </div>
+        <!-- /container -->
+
+
+        <?php include("foot.php"); ?>
+
+
+        <!-- Bootstrap core JavaScript
+    ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+        <script src="../assets/js/ie10-viewport-bug-workaround.js"></script>
+</body>
+
+</html>
