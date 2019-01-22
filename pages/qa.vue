5597096ea267031b9be68bffb33c76f0777cea89
leeci <webrsb@gmail.com>
2019-01-23 01:30:34 +0800

    fixed qa page

--- pages/qa.vue
+++ pages/qa.vue
@@ -1,6 +1,7 @@
 <template>
     <!-- Main jumbotron for a primary marketing message or call to action
     -->
+    <div>
     <div class="jumbotron">
       <div class="container">
          <h1 class="display-3">Aoetw</h1> 
@@ -11,10 +12,9 @@
       <!-- Example row of columns -->
       <h3>1.系統相關-購買前</h3> 
       <div class="row">
-        <div class="col-md-12"> <a class="btn btn-primary" role="button" data-toggle="collapse" href="#collapseExample"
-          aria-expanded="false" aria-controls="collapseExample">1-1.世紀帝國2目前到底有幾個資料片？</a> 
-          <div
-          class="collapse" id="collapseExample">
+        <div class="col-md-12">
+          <b-btn v-b-toggle.a-1 class="m-1" variant="primary">1-1.世紀帝國2目前到底有幾個資料片？</b-btn>
+          <b-collapse id="a-1">
             <div class="well">
               <table class="table-bordered table">
                 <tbody>
@@ -72,52 +72,48 @@
                 </tbody>
               </table>
             </div>
-        </div>
+        </b-collapse>
       </div>
     </div>
     <div class="row zo">
-      <div class="col-md-12"> <a class="btn btn-primary" role="button" data-toggle="collapse" href="#collapseExample2"
-        aria-expanded="false" aria-controls="collapseExample">1-2.世紀帝國2 HD版要如何購買？怎麼玩？</a> 
-        <div
-        class="collapse" id="collapseExample2">
-          <div class="well">
-            <p>必須使用信用卡購買</p>
-            <p><a href="http://store.steampowered.com/app/221380/">HD主程式 (舊版帝王+征服者) 原價468</a>
-            </p>
-            <h3></h3>
-            <p><a href="http://store.steampowered.com/bundle/2149/">世紀帝國2HD全集HD+3個資料片 組合包價格1040</a>
-            </p>
-          </div>
+      <div class="col-md-12">
+      <b-btn v-b-toggle.a-2 class="m-1" variant="primary">1-2.世紀帝國2 HD版要如何購買？怎麼玩？</b-btn>
+          <b-collapse id="a-2">
+            <div class="well">
+              <p>必須使用信用卡購買</p>
+              <p><a href="http://store.steampowered.com/app/221380/">HD主程式 (舊版帝王+征服者) 原價468</a>
+              </p>
+              <h3></h3>
+              <p><a href="http://store.steampowered.com/bundle/2149/">世紀帝國2HD全集HD+3個資料片 組合包價格1040</a>
+              </p>
+            </div>
+          </b-collapse>
       </div>
     </div>
-    </div>
     <div class="row zo">
-      <div class="col-md-12"> <a class="btn btn-primary" role="button" data-toggle="collapse" href="#collapseExample1-3"
-        aria-expanded="false" aria-controls="collapseExample">1-3.沒有信用卡怎麼購買？怎麼用超商付款</a> 
-        <div
-        class="collapse" id="collapseExample1-3">
+      <div class="col-md-12">
+        <b-btn v-b-toggle.a-3 class="m-1" variant="primary">1-3.沒有信用卡怎麼購買？怎麼用超商付款</b-btn>
+        <b-collapse id="a-3">
           <div class="well">可以使用Steam點數卡購買，使用超商繳費，連結如下
             <p><a target="blank" href="https://cdkey.com.tw">閃電發卡</a>
             </p>
           </div>
-      </div>
+      </b-collapse>
     </div>
     </div>
     <div class="row zo">
-      <div class="col-md-12"> <a class="btn btn-primary" role="button" data-toggle="collapse" href="#collapseExample1-4"
-        aria-expanded="false" aria-controls="collapseExample">1-4.只要購買主程式就好了嗎?還是要連同三種資料片一起買，版本才不會有問題?</a> 
-        <div
-        class="collapse" id="collapseExample1-4">
+      <div class="col-md-12">
+        <b-btn v-b-toggle.a-4 class="m-1" variant="primary">1-4.只要購買主程式就好了嗎?還是要連同三種資料片一起買，版本才不會有問題?</b-btn>
+        <b-collapse id="a-4">
           <div class="well">可以只買主程式，且HD主程式與資料片全部都可互連。但如果只買主程式，則沒辦法用新文明，沒差多少錢，建議全買。</div>
-      </div>
+      </b-collapse>
     </div>
     </div>
     <h3>2.系統相關-購買後</h3> 
     <div class="row zo">
-      <div class="col-md-12"> <a class="btn btn-primary" role="button" data-toggle="collapse" href="#collapseExample2-1"
-        aria-expanded="false" aria-controls="collapseExample">2-1.遊戲有沒有中文？如何繁中化?</a> 
-        <div
-        class="collapse" id="collapseExample2-1">
+      <div class="col-md-12">
+        <b-btn v-b-toggle.b-1 class="m-1" variant="primary">2-1.遊戲有沒有中文？如何繁中化?</b-btn>
+        <b-collapse id="b-1">
           <div class="well">
             <p>1.請將遊戲語言轉換至簡體中文（收藏庫>AOE II>右鍵>內容>語言）</p>
             <p>2.<a href="http://steamcommunity.com/sharedfiles/filedetails/?id=466768643">請訂閱插件</a>
@@ -127,66 +123,48 @@
             <p>若不是，請在遊戲中的「選項」中選擇「繁體中文」。詳細內容<a href="https://forum.gamer.com.tw/C.php?bsn=01122&snA=12498&tnum=125">請參考</a>
             </p>
           </div>
-      </div>
+      </b-collapse>
     </div>
     </div>
     <div class="row zo">
-      <div class="col-md-12"> <a class="btn btn-primary" role="button" data-toggle="collapse" href="#collapseExample2-2"
-        aria-expanded="false" aria-controls="collapseExample">2-2.怎麼更改遊戲解析度?</a> 
-        <div
-        class="collapse" id="collapseExample2-2">
+      <div class="col-md-12">
+        <b-btn v-b-toggle.b-2 class="m-1" variant="primary">2-2.怎麼更改遊戲解析度?</b-btn>
+        <b-collapse id="b-2">
           <div class="well">桌面解析度=遊戲解析度，你是筆電的話只能說gg
             <p> <a href="https://www.ptt.cc/bbs/AOE/M.1511792119.A.275.html">相關討論</a>
             </p>
           </div>
-      </div>
+      </b-collapse>
     </div>
     </div>
     <div class="row zo">
-      <div class="col-md-12"> <a class="btn btn-primary" role="button" data-toggle="collapse" href="#collapseExample2-3"
-        aria-expanded="false" aria-controls="collapseExample">2-3.連線出現握手狀態圖示，無法開始遊戲</a> 
-        <div
-        class="collapse" id="collapseExample2-3">
+      <div class="col-md-12">
+        <b-btn v-b-toggle.b-3 class="m-1" variant="primary">2-3.連線出現握手狀態圖示，無法開始遊戲</b-btn>
+        <b-collapse id="b-3">
           <div class="well">重進房應該會改善，若經常性發生，可能是虛擬IP問題 設定port forwarding或是DMZ可能改善
             <p><a href="http://kaedeknowledge.blogspot.tw/2017/06/Age-of-Empires-II-HD-Edition-fix-handshaking.html?m=1"
               target="blank">請參考:握手排除</a>
             </p>
           </div>
-      </div>
+      </b-collapse>
     </div>
     </div>
     <div class="row zo">
-      <div class="col-md-12"> <a class="btn btn-primary" role="button" data-toggle="collapse" href="#collapseExample2-4"
-        aria-expanded="false" aria-controls="collapseExample">2-4.我的版本不會自動更新</a> 
-        <div class="collapse" id="collapseExample2-4">
+      <div class="col-md-12">
+        <b-btn v-b-toggle.b-4 class="m-1" variant="primary">2-4.我的版本不會自動更新</b-btn>
+        <b-collapse id="b-4">
           <div class="well">參考下圖設定，若下方選項選這個，開著steam且遊戲不開就會自動更新了
             <img src="https://i.imgur.com/KbnO3Tll.jpg"
             alt="">
           </div>
-      </div>
-    </div>
-    </div>
-    <div class="row zo">
-      <div class="col-md-12"> <a class="btn btn-primary" role="button" data-toggle="collapse" href="#collapseExample2-5"
-        aria-expanded="false" aria-controls="collapseExample">2-5.遊戲內無法輸入中文</a> 
-        <div class="collapse" id="collapseExample2-5">
-          <div class="well">
-            <p>win+空白或是ctrl+空白鍵可切換輸入法   <a href="https://www.ptt.cc/bbs/Windows/M.1438952591.A.AE7.html">詳細解法</a></p>
-    
-            <p>遊戲內請參考下圖設定</p>
-            <img src="https://i.imgur.com/NSGNtfQ.jpg"
-            alt="">
-       
-          </div>
-      </div>
+      </b-collapse>
     </div>
     </div>
     <h3>3.遊戲相關</h3> 
     <div class="row zo">
-      <div class="col-md-12"> <a class="btn btn-success" role="button" data-toggle="collapse" href="#collapseExample3-1"
-        aria-expanded="false" aria-controls="collapseExample">3-1.遊戲的村民閒置驚嘆號、小樹、格子怎麼用？</a> 
-        <div
-        class="collapse" id="collapseExample3-1">
+      <div class="col-md-12">
+        <b-btn v-b-toggle.c-1 class="m-1" variant="success">3-1.遊戲的村民閒置驚嘆號、小樹、格子怎麼用？</b-btn>
+        <b-collapse id="c-1">
           <div class="well">
             <p>這是steam工作坊的套件，目的是更方便使用者</p>
             <p>使用方式:登入訂閱，遊戲重開一次即可</p>
@@ -210,14 +188,13 @@
               </li>
             </ul>
           </div>
-      </div>
+      </b-collapse>
     </div>
     </div>
     <div class="row zo">
-      <div class="col-md-12"> <a class="btn btn-success" role="button" data-toggle="collapse" href="#collapseExample3-2"
-        aria-expanded="false" aria-controls="collapseExample">3-2.有推薦的強AI嗎?</a> 
-        <div
-        class="collapse" id="collapseExample3-2">
+      <div class="col-md-12">
+        <b-btn v-b-toggle.c-2 class="m-1" variant="success">3-2.有推薦的強AI嗎?</b-btn>
+        <b-collapse id="c-2">
           <div class="well">
             <p>難度依序由簡單至難:</p>
             <ul>
@@ -239,16 +216,40 @@
               </li>
             </ul>
           </div>
-      </div>
+      </b-collapse>
     </div>
     </div>
     <div class="row zo">
-      <div class="col-md-12"> <a class="btn btn-success" role="button" data-toggle="collapse" href="#collapseExample2-3"
-        aria-expanded="false" aria-controls="collapseExample">1-2.世紀帝國2 HD版要如何購買？怎麼玩？</a> 
-        <div
-        class="collapse" id="collapseExample2-3">
+      <div class="col-md-12">
+        <b-btn v-b-toggle.c-3 class="m-1" variant="success">1-2.世紀帝國2 HD版要如何購買？怎麼玩？</b-btn>
+        <b-collapse id="c-3">
           <div class="well">.</div>
-      </div>
+      </b-collapse>
+    </div>
+    </div>
+    <h3>4.遊戲戰術相關</h3> 
+    <div class="row zo">
+      <div class="col-md-12">
+        <b-btn v-b-toggle.d-1 class="m-1" variant="warning">4-1.為什麼看你們打都不撿遺跡?</b-btn>
+        <b-collapse id="d-1">
+          <div class="well">
+            <p>今天要跟大家講一個生香油錢的故事，說到世紀帝國的遺跡，就一個長得很像獎杯，遊戲開始時散落在地圖各地，只有僧侶可以搬運，而當他被放置到修道院時，他就會以每2秒1黃金的速度生產黃金所以說呢，你遺跡越多黃金就越多啦，那到底為什麼實戰中大家都不撿遺跡呢？</p>
+            <h4>成本上來說</h4>
+            <p>首先來算算撿遺跡的成本，修道院175木頭，僧侶100黃金，在城堡初期的大戰中，一兵一卒都是非常珍貴的，這175木頭和100黃金，我大可拿來做其他事情，例如起城鎮中心爆經濟，或是拿去生軍事單位，對戰局的影響都遠超過撿遺跡。</p>
+            <h4>控制力來說</h4>
+            <p>其次是控制力，城堡初期是相當耗費控制力的，你真的以每個玩家都是怪力，有四隻手在控兵嗎？我要控馬還要控經濟，如果我打弩兵那耗費的控制力將會更多。你要蓋修道院，還要生僧侶，生完僧侶他不是ai阿，你還要手控去搬遺跡，而遺跡也不是你家隔壁鄰居老王這麼近阿，有的十萬八千里遠最後你還要把般著遺跡的僧侶拉回來，這中間耗費多少控制力，一樣道理，我把那些控制力去控馬多殺敵人幾隻村民，去爆經濟對於初期的戰局，我能獲得更大的回報。</p>
+            <h4>功能面來說</h4>
+            <p>撇除前面的原因，老子就是有錢蓋了修道院和出了僧侶，那為什麼你還是不撿遺跡，僧侶這麼好用，當然出來直接上戰場阿，僧侶可招降可補血，非常好用，對於一隻軍隊的幫助非常大，舉個例子好了，五隻駱駝配五隻征服者，明顯的是征服者可以虐殺駱駝，但配五隻駱駝一隻僧侶打六隻征服者那情況就相當不一樣，僧侶號稱最強防守兵種，我吃飽閒閒沒事去外面撿遺跡，那就是空閒，僧侶走的又慢，一來一往都把戰局給賣了，所以回歸到最原始的功能面，僧侶就該做他該做的事情，招降和補血，行有餘力等戰局穩定時再來做他額外的事情吧</p>
+          </div>
+      </b-collapse>
+    </div>
+    </div>
+    <div class="row zo">
+      <div class="col-md-12">
+        <b-btn v-b-toggle.d-2 class="m-1" variant="warning">4-2.被偷豬影響有多大?</b-btn>
+        <b-collapse id="d-2">
+          <div class="well">表面上被偷豬不計耗損，損失340肉，而對方賺340肉，但實際上靠趕鹿早點種田等方式，其實損失沒有帳面上如此大，高手即使被偷豬升級也不會被影響。但偷豬還有隱形的損失，且隱形的損失遠大於帳面上，譬如說偷豬的那一方多吃一隻豬，等於斥侯趕了2隻半的鹿，代表他不用提早種田，有多餘的木頭可以做更多的事情，被偷豬的那一方斥侯得在家趕鹿，所以沒有辦法探情報，對方如果打裝甲塔還是前置等兇猛的打法，沒辦法提早做準備，這是最虧的。</div>
+      </b-collapse>
     </div>
     </div>
     <hr>
@@ -256,7 +257,7 @@
       <p>此頁面感謝qaws68（伊森）整理<a href="https://www.ptt.cc/bbs/AOE/M.1495194620.A.384.html">AOE常見問題</a>完成</p>
     </blockquote>
     </div>
-    
+  </div>
 </template>
 <script>
 export default {
