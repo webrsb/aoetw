5278b69179451f276aa1a1d243aa191acfd411cf
leeci <webrsb@gmail.com>
2019-01-24 21:05:59 +0800

    use CircleCI for auto upload to server

---
+++ .circleci/deploy.js
@@ -0,0 +1,24 @@
+var FtpDeploy = require('ftp-deploy');
+var ftpDeploy = new FtpDeploy();
+ 
+var config = {
+    user: process.env.FTPUSERNAME,
+    password: process.env.FTPPASS,
+    host: process.env.FTPHOST,
+    port: 21,
+    localRoot: __dirname + "/../dist/",
+    remoteRoot: "/public_html/next",
+    include: ['*']
+}
+    
+ftpDeploy.deploy(config, function(err) {
+    if (err) console.log(err)
+    else console.log('finished');
+});
+
+ftpDeploy.on('uploading', function(data) {
+    console.log(data.filename)
+    data.totalFilesCount;       // total file count being transferred
+    data.transferredFileCount; // number of files transferred
+    data.filename;             // partial path with filename being uploaded
+});
--- nuxt.config.js
+++ nuxt.config.js
@@ -58,11 +58,6 @@ module.exports = {
     /*
     ** You can extend webpack config here
     */
-    extend(config, ctx) {
-      return Object.assign({}, config, {
-        devtool: 'source-map'
-      })
-    }
   },
   router: {
     base: "/next"
