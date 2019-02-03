7e8a55f48244c3c88521a9ca3768fc5005e3d8b9
bookthief108 <47220722+bookthief108@users.noreply.github.com>
2019-02-04 00:06:39 +0800

    Update Manipur_Cavalry.vue
    加上"/"

--- pages/techs/Manipur_Cavalry.vue
+++ pages/techs/Manipur_Cavalry.vue
@@ -1,172 +1,103 @@
 <template>
-  <!-- Main jumbotron for a primary marketing message or call to action
-    -->
-  <div class="container">
-    <!-- Example row of columns -->
-    <div class="row padtop">
-      <div class="col-md-12">
-        <h1>曼尼普爾騎兵</h1>
-        <div class="hrn1"></div>
-      </div>
-      <div class="col-md-8">
-        <div class="n1">
-          <p>
-            曼尼普爾騎兵是<nuxt-link to="/civs/Burmese">緬甸</nuxt-link
-            >的特殊科技，可於<nuxt-link to="/techs/age/Imperial_Age"
-              >帝王時代</nuxt-link
-            >在
-            <nuxt-link to="/building/Castle">城堡</nuxt-link
-            >研發。研發後<nuxt-link to="/units/Cavalry_Units"
-              >騎兵單位</nuxt-link
-            >和<nuxt-link to="/units/Arambai">飛鏢騎兵</nuxt-link>對
-            <nuxt-link to="/armor/Armor_Class_Building">建築物</nuxt-link
-            >和<nuxt-link to="/armor/Armor_Class_standard_building"
-              >標準建築</nuxt-link
-            >+3攻擊。
-          </p>
+    <div class="container">      <div class="row" style=" margin-top: 50px;">
+        <div class="col-md-12">
+           <h1>曼尼普爾騎兵</h1>
+          <div class="hrn1"></div>
         </div>
-        <h3>戰術要點</h3>
-        <div class="hrn1"></div>
-        <div class="n1">
-          <ul>
-            <li>
-              緬甸缺少<nuxt-link to="/techs/Siege_Engineers"
-                >攻城工程師</nuxt-link
-              >科技和<nuxt-link to="/units/Siege_Ram">重型衝撞車</nuxt-link
-              >。曼尼普爾騎兵可以增加
-              <nuxt-link to="/units/Battle_Elephant">矛象伕</nuxt-link
-              >對建築物的攻擊力以補足上述缺點。
-            </li>
-          </ul>
+        <div class="col-md-8">
+          <div class="n1">
+            <p>曼尼普爾騎兵是緬甸的特殊科技，可於帝王時代在城堡研發。研發後<nuxt-link to="/Cavalry_Units">騎兵</nuxt-link>和<nuxt-link to="/Arambai">飛鏢騎兵</nuxt-link>對建築物+3攻擊。</p>
+          </div>
+           <h3>戰術要點</h3>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ul>
+              <li>緬甸缺少<nuxt-link to="/Siege_Engineers">攻城工程師</nuxt-link>科技和<nuxt-link to="/Siege_Ram">重型衝撞車</nuxt-link>。曼尼普爾騎兵可以增加<nuxt-link to="/Battle_Elephant">矛象伕</nuxt-link>對建築物的攻擊力以補足上述缺點。</li>
+            </ul>
+          </div>
+           <h3>團隊加成</h3> 
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ul>
+              <li><nuxt-link to="/Berbers">柏柏</nuxt-link>：<nuxt-link to="/Kasbah">聚居堡壘</nuxt-link>能夠加速曼尼普爾騎兵的研究時間(快25%)。</li>
+            </ul>
+          </div>
+           <h3>豆知識</h3> 
+          <div class="hrn1"></div>
+          <p>曼尼普爾現今於印度境內，而曼尼普爾在血緣和文化上與中國有很大的淵源。曼尼普爾王室通常被認為來自於中國的唐朝。曼尼普爾民間一直維持著對龍的崇拜。在十八世紀，當時的國王因為個人偏好強迫曼尼普爾人皈依印度教，但遇到強大的阻力，印度教也從未真正成為曼尼普爾的民間信仰，對龍的崇拜依然以各種形式在曼尼普爾保存下來。</p>
         </div>
-        <h3>團隊加成</h3>
-        <div class="hrn1"></div>
-        <div class="n1">
-          <ul>
-            <li>
-              <nuxt-link to="/civs/Berbers">柏柏</nuxt-link>：<nuxt-link
-                to="/techs/Kasbah"
-                >聚居堡壘</nuxt-link
-              >能夠加速曼尼普爾騎兵的研究時間(快25%)。
-            </li>
-          </ul>
-        </div>
-        <h3>更新日誌</h3>
-        <div class="hrn1"></div>
-        <div class="n1">
-          <h3>
-            <em><nuxt-link to="/games/aor">王者崛起</nuxt-link></em>
-          </h3>
-          <ul>
-            <li>
-              曼尼普爾騎兵一開始只對<nuxt-link to="/armor/Armor_Class_Building"
-                >建築物</nuxt-link
-              >攻擊+6。5.3改版後對<nuxt-link to="/armor/Armor_Class_Building"
-                >建築物</nuxt-link
-              >和
-              <nuxt-link to="/armor/Armor_Class_standard_building"
-                >標準建築</nuxt-link
-              >都有加成，這讓建築學不能完全抵消曼尼普爾騎兵的效果。
-            </li>
-          </ul>
-        </div>
-        <h3>趣聞</h3>
-        <div class="hrn1"></div>
-        <p>
-          <a href="https://en.wikipedia.org/wiki/Manipur">曼尼普爾</a
-          >現今於印度境內，而曼尼普爾在血緣和文化上與中國有很大的淵源。曼尼普爾王室通常被認為來自於中國的唐朝。曼尼普爾民間一直維持著對龍的崇拜。在十八世紀，當時的國王
-          <a href="http://tinyurl.com/y9cq7s5x">帕巴</a
-          >把印度教列為國教，而對龍的崇拜依然以各種形式在曼尼普爾保存下來。歷史上曼尼普爾並不屬於緬甸，反而多次與緬甸王國交戰。
-        </p>
-      </div>
-      <div class="col-md-4">
-        <table class="table-bordered" width="100%">
-          <tr>
-            <td class="unit" colspan="2" style="background-color:#31b0d5;">
-              <div class="unititle">
-                <nuxt-link to="">曼尼普爾騎兵</nuxt-link>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td colspan="2">
-              <div class="unititle">
-                <img src="~assets/Unique-tech.jpg" width="45" alt="" />
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td class="unit" colspan="2" style="background-color:#31b0d5;">
-              <div class="unititle2">
-                <nuxt-link to="">科技介紹</nuxt-link>
-              </div>
-            </td>
-          </tr>
-          <tr>
+        <div class="col-md-4">
+          <table class="table-bordered" width="100%">
+            <tr>
+              <td class="unit" colspan="2" style=" background-color:#31b0d5;">
+                <div class="unititle"><nuxt-link to="">曼尼普爾騎兵</nuxt-link>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <div class="unititle">
+                  <img src="~assets/Unique-tech.jpg" width="45" alt="" />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td class="unit" colspan="2" style=" background-color:#31b0d5;">
+                <div class="unititle2"><nuxt-link to="">科技介紹</nuxt-link>
+                </div>
+              </td>
+            </tr>
+            <tr>
             <td class="unit3">文明</td>
-            <td class="unit4">
-              <nuxt-link to="/civs/Burmese">緬甸</nuxt-link>
-            </td>
-          </tr>
-          <tr>
-            <td class="unit3">時代</td>
-            <td class="unit4">
-              <img
-                class="gg"
-                src="~assets/Imperial-age-research.jpg"
-                alt=""
-              /><nuxt-link to="/techs/age/Imperial_Age">帝王時代</nuxt-link>
-            </td>
-          </tr>
-          <tr>
-            <td class="unit3">研發於</td>
-            <td class="unit4">
-              <img class="gg" src="~assets/Castleicon.png" alt="" /><nuxt-link
-                to="/building/Castle"
-                >城堡</nuxt-link
-              >
-            </td>
-          </tr>
-          <tr>
-            <td class="unit3">研發時間</td>
-            <td class="unit4">40秒</td>
-          </tr>
-          <tr>
-            <td class="unit" colspan="2" style="background-color:#31b0d5;">
-              <div class="unititle2"><nuxt-link to="">成本</nuxt-link></div>
-            </td>
-          </tr>
-          <tr>
-            <td class="unit3">食物</td>
-            <td class="unit4">650</td>
-          </tr>
-          <tr>
-            <td class="unit3">黃金</td>
-            <td class="unit4">400</td>
-          </tr>
-          <tr>
-            <td class="unit" colspan="2" style="background-color:#31b0d5;">
-              <div class="unititle2"><nuxt-link to="">影響</nuxt-link></div>
-            </td>
-          </tr>
-          <tr>
-            <td class="unit3">影響</td>
-            <td class="unit4">騎兵和飛鏢騎兵對建築物和標準建築+3攻擊。</td>
-          </tr>
-        </table>
-
-        <div class="thanks">感謝! 此頁面由賜齡協助編輯</div>
+            <td class="unit4"><nuxt-link to="/Burmese">緬甸</nuxt-link></td>
+            </tr>
+            <tr>
+              <td class="unit3">時代</td>
+              <td class="unit4"><img class="gg" src="~assets/Imperial-age-research.jpg" alt=""><nuxt-link to="/techs/age/Imperial_Age">帝王時代</nuxt-link></td>
+            </tr>
+            <tr>
+              <td class="unit3">研發於</td>
+              <td class="unit4"><img class="gg" src="~assets/Castleicon.png" width="45" alt=""><nuxt-link to="/building/Castle">城堡</nuxt-link></td>
+            </tr>
+            <tr>
+              <td class="unit3">研發時間</td>
+              <td class="unit4">40秒</td>
+            </tr>
+            <tr>
+              <td class="unit" colspan="2" style=" background-color:#31b0d5;">
+                <div class="unititle2"><nuxt-link to="">成本</nuxt-link>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td class="unit3">食物</td>
+              <td class="unit4">650</td>
+            </tr>
+            <tr>
+              <td class="unit3">黃金</td>
+              <td class="unit4">400</td>
+            </tr>
+            <tr>
+              <td class="unit" colspan="2" style=" background-color:#31b0d5;">
+                <div class="unititle2"><nuxt-link to="">影響</nuxt-link>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td class="unit3">影響</td>
+              <td class="unit4">騎兵和飛鏢騎兵對建築物+3攻擊。</td>
+            </tr>
+          </table>
+          <div class="thanks">感謝! 此頁面由賜齡協助編輯</div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12"></div>
       </div>
     </div>
-    <div class="row">
-      <div class="col-md-12"></div>
-    </div>
-  </div>
-</template>
+    
+    </template>
 <script>
-export default {
-  layout: 'alltech',
-  components: {}
-}
+export default {layout: 'foot',  components: {}}
 </script>
 <style></style>
