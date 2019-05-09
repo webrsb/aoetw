385fdd965b443f3689b4893746a5dca7d55620e1
bookthief108 <47220722+bookthief108@users.noreply.github.com>
2019-05-09 21:48:43 +0800

    # This is a combination of 2 commits. # This is the 1st commit message:
    Create Canoe.vue
    # This is the commit message #2:
    
    Create Priest.vue

---
+++ pages/units/edmap/Canoe.vue
@@ -0,0 +1,160 @@
+<template>
+  <div class="container">
+    <div class="row padtop">
+      <div class="col-md-12">
+        <h1>獨木舟</h1>
+        <div class="hrn1"></div>
+      </div>
+      <div class="col-md-8">
+        <div class="n1">
+          <p>
+            <b>獨木舟</b>不能被玩家建造。獨木舟曾在黃金國戰役與法蘭西斯科．阿爾梅達戰役中多次出現，
+            同時也能在地圖編輯器中找到。史前的美洲文明會使用獨木舟，之後非洲的原住民也短暫使用過獨木舟。
+          </p>
+          <p>
+            出現戰役:
+          <ul>
+              <li>失落的帝國:在黃金國戰役中兩個著名的場景分別是在"分離告別"伊瑪拉部落的獨木舟，以及"食人族"
+              中洛斯內格羅斯居民用獨木舟積極守護他們在亞馬遜河的地盤。</li>
+              <li>非洲王朝:法蘭西斯科．阿爾梅達的"非洲雄獅"戰役中關契斯人用飛刀女獵手、矛兵和一些獨木舟守護著
+              他們的自然資源。</li>
+              <li>拉惹的崛起:勃印曩"猛蛇出洞"戰役中獨木舟位於將蒲甘城一分為二的河的東側，靠近東吁勇士和龍船的地方。</li>
+          </ul>
+          
+          </p>
+        <h3>圖庫</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <div class="row">
+            <div class="col-md-4">
+              <gallery
+                ><img class="imgn hg" src="~assets/Canoegroup.png" alt=""
+              /></gallery>
+              <p><em>一群獨木舟。</em></p>
+            </div>
+            
+            </div>
+            
+          </div>
+      </div>  
+</div>
+      <div class="col-md-4">
+        <table class="table-bordered" width="100%">
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#3c763d;">
+              <div class="unititle2">獨木舟</div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <div class="unititle">
+                <img src="~assets/Canoeicon.png" width="45" alt="" />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#3c763d;">
+              <div class="unititle2">單位介紹</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">類型</td>
+            <td class="unit4">
+              <nuxt-link to="/units/Naval_Vessels">船</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">首次出現</td>
+            <td class="unit4">
+              <nuxt-link to="/techs/games/aof">失落的帝國</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#3c763d;">
+              <div class="unititle2">數據</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">
+              <nuxt-link to="/elements/Hit_points">血量</nuxt-link>
+            </td>
+            <td class="unit4">70</td>
+          </tr>
+          <tr>
+            <td class="unit3">攻擊</td>
+            <td class="unit4">7(遠攻)</td>
+          </tr>
+          <tr>
+            <td class="unit3">傷害加成</td>
+            <td class="unit4">
+              +9對<nuxt-link to="/armor/Armor_Class_Ship">船</nuxt-link
+              >
+              <br />+7對<nuxt-link to="/armor/Armor_Class_Building"
+                >建築</nuxt-link
+              >
+              <br />+4對<nuxt-link to="/armor/Armor_Class_Ram"
+                >衝撞車</nuxt-link
+              >
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">
+              <nuxt-link to="/elements/Rate_of_Fire">射速</nuxt-link>
+            </td>
+            <td class="unit4">3.05秒/次</td>
+          </tr>
+          <tr>
+            <td class="unit3">
+              <nuxt-link to="/elements/Frame_delay">開火間隔</nuxt-link>
+            </td>
+            <td class="unit4">0</td>
+          </tr>
+          <tr>
+            <td class="unit3">攻擊距離</td>
+            <td class="unit4">5</td>
+          </tr>
+          <tr>
+            <td class="unit3">準確率</td>
+            <td class="unit4">100%</td>
+          </tr>
+          <tr>
+            <td class="unit3">投射物速度</td>
+            <td class="unit4">6</td>
+          </tr>
+          <tr>
+            <td class="unit3"><nuxt-link to="/armor">近防</nuxt-link></td>
+            <td class="unit4">0</td>
+          </tr>
+          <tr>
+            <td class="unit3"><nuxt-link to="/armor">遠防</nuxt-link></td>
+            <td class="unit4">6</td>
+          </tr>
+          <tr>
+            <td class="unit3"><nuxt-link to="/armor">防禦類型</nuxt-link></td>
+            <td class="unit4">
+              <nuxt-link to="/armor/Armor_Class_Ship">船</nuxt-link><br>
+              <nuxt-link to="/armor/Armor_Class_Unique_Unit">特殊單位</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">跑速</td>
+            <td class="unit4">15</td>
+          </tr>
+          <tr>
+            <td class="unit3">視野</td>
+            <td class="unit4">8</td>
+          </tr>
+        </table>
+
+        <div class="thanks">感謝! 此頁面由賜齡協助編輯</div>
+      </div>
+
+
+</template>
+<script>
+export default {
+  layout: 'allunit',
+  components: {}
+}
+</script>
+<style></style>
---
+++ pages/units/edmap/Priest.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="container">
+    <div class="row padtop">
+      <div class="col-md-12">
+        <h1>神父</h1>
+        <div class="hrn1"></div>
+      </div>
+      <div class="col-md-8">
+        <div class="n1">
+          <p>
+            <b>神父</b>是個變種僧侶單位，首次出現是在世紀帝國I，後來在<nuxt-link to="/games/af">世紀帝國II:非洲王朝</nuxt-link
+            >》與羅馬士兵和重劍士一起回歸。神父出現在法蘭西斯科．阿爾梅達的"葡屬印度"等一些戰役中，也能在地圖編輯器找到。神父不能在一般遊戲
+            中被玩家訓練。
+          </p>
+        </div>
+        <h3>趣聞</h3>
+        <div class="hrn1"></div>
+        <ul>
+            <li>當神父拿起遺跡後會變成玩家所屬文明的僧侶模樣。這同樣會發生在美洲僧侶或僧侶英雄上，例如英雄伊滿</li>
+            <li>神父頭像依然使用原本僧侶的頭像。</li>
+            <li>遊戲中僧侶招降聲音的運作方式是根據遊戲中首先播放的聲音來選擇全局的招降聲音。 例如，如果神父是第一個
+              吟唱的人，那麼所有的僧侶單位，包括一般常見的僧侶，將在之後的遊戲部分使用“Wololo”作為招降聲音。 但是，如果
+            一個普通的僧侶（或僧侶類型的任何其他單位）首先使用正常的招降聲音，那麼神父將使用常規的聲音進行招降。</li>
+        </ul>
+
+        <h3>圖庫</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <div class="row">
+            <div class="col-md-4">
+              <gallery
+                ><img
+                  class="imgn hg"
+                  width="250"
+                  src="~assets/Priest.png"
+                  alt=""
+              /></gallery>
+              <p><em>一群神父。</em></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4">
+        <table class="table-bordered" width="100%">
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#3c763d;;">
+              <div class="unititle"><nuxt-link to="">神父</nuxt-link></div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <div class="unititle">
+                <img src="~assets/MonkIcon.jpg" width="45" alt="" />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#3c763d;;">
+              <div class="unititle2">
+                <nuxt-link to="">單位介紹</nuxt-link>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">類型</td>
+            <td class="unit4">
+              <nuxt-link to="/units/Monk">僧侶</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">首次出現</td>
+            <td class="unit4">
+              <nuxt-link to="/techs/games/af">非洲王朝</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#3c763d;;">
+              <div class="unititle2">數據</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">
+              <nuxt-link to="/elements/Hit_points">血量</nuxt-link>
+            </td>
+            <td class="unit4">30</td>
+          </tr>
+          <tr>
+            <td class="unit3">
+              <nuxt-link to="/elements/Rate_of_Fire">信仰回復速度</nuxt-link>
+            </td>
+            <td class="unit4">62</td>
+          </tr>
+          <tr>
+            <td class="unit3"><nuxt-link to="/armor">近防</nuxt-link></td>
+            <td class="unit4">0</td>
+          </tr>
+          <tr>
+            <td class="unit3"><nuxt-link to="/armor">遠防</nuxt-link></td>
+            <td class="unit4">0</td>
+          </tr>
+          <tr>
+            <td class="unit3">防禦</td>
+            <td class="unit4">
+              <nuxt-link to="/armor/Armor_Class_Monk">僧侶</nuxt-link
+              >
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">跑速</td>
+            <td class="unit4">0.7</td>
+          </tr>
+          <tr>
+            <td class="unit3"><nuxt-link to="/los.php">視野</nuxt-link></td>
+            <td class="unit4">11</td>
+          </tr>
+        </table>
+
+        <div class="thanks">感謝! 此頁面由賜齡協助編輯</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'alledmap',
+  components: {}
+}
+</script>
+<style></style>
