d8c48845359cd8fdf939e220828902a7d9137efb
bookthief108 <47220722+bookthief108@users.noreply.github.com>
2019-05-09 20:02:33 +0800

    Create Amazon_Archer.vue
    This pull request resolve #93

---
+++ pages/units/edmap/Amazon_Archer.vue
@@ -0,0 +1,153 @@
+<template>
+  <div class="container">
+    <div class="row padtop">
+      <div class="col-md-12">
+        <h1>亞馬遜弓箭手</h1>
+        <div class="hrn1"></div>
+      </div>
+      <div class="col-md-8">
+        <div class="n1">
+          <p>
+            <b>亞馬遜弓箭手</b
+            >出現在《<nuxt-link to="/games/aof">世紀帝國II:失落的帝國</nuxt-link
+            >》版本中的黃金國戰役第三關:亞馬遜族，也能在地圖編輯器找到。亞馬遜弓箭手不能在一般遊戲中被訓練。
+          </p>
+        </div>
+
+        <h3>圖庫</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <div class="row">
+            <div class="col-md-4">
+              <gallery
+                ><img
+                  class="imgn hg"
+                  width="250"
+                  src="~assets/Amazonarcher.png"
+                  alt=""
+              /></gallery>
+              <p><em>一群亞馬遜弓箭手。</em></p>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4">
+        <table class="table-bordered" width="100%">
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#3c763d;;">
+              <div class="unititle"><nuxt-link to="">亞馬遜弓箭手</nuxt-link></div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <div class="unititle">
+                <img src="~assets/Amazonarchericon.png" width="45" alt="" />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#3c763d;;">
+              <div class="unititle2">
+                <nuxt-link to="">單位介紹</nuxt-link>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">類型</td>
+            <td class="unit4">
+              <nuxt-link to="/units/Archer_Units">射手</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">首次出現</td>
+            <td class="unit4">
+              <nuxt-link to="/techs/games/aof">失落的帝國</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#3c763d;;">
+              <div class="unititle2">數據</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">
+              <nuxt-link to="/elements/Hit_points">血量</nuxt-link>
+            </td>
+            <td class="unit4">45</td>
+          </tr>
+          <tr>
+            <td class="unit3">攻擊</td>
+            <td class="unit4">5(遠距)</td>
+          </tr>
+          <tr>
+            <td class="unit3">攻擊加成</td>
+            <td class="unit4">
+              +1對<nuxt-link to="/armor/Armor_Class_Infantry"
+                >步兵</nuxt-link
+              >
+              <br />+2對<nuxt-link to="/armor/Armor_Class_Spearman"
+                >槍兵</nuxt-link
+              >
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">
+              <nuxt-link to="/elements/Rate_of_Fire">攻擊速度</nuxt-link>
+            </td>
+            <td class="unit4">1.93</td>
+          </tr>
+          <tr>
+            <td class="unit3">攻擊間隔</td>
+            <td class="unit4">5</td>
+          </tr>
+          <tr>
+            <td class="unit3">射程</td>
+            <td class="unit4">4</td>
+          </tr>
+          <tr>
+            <td class="unit3">命中率</td>
+            <td class="unit4">80%</td>
+          </tr>
+          <tr>
+            <td class="unit3">彈道速度</td>
+            <td class="unit4">7</td>
+          </tr>
+          <tr>
+            <td class="unit3"><nuxt-link to="/armor">近防</nuxt-link></td>
+            <td class="unit4">0</td>
+          </tr>
+          <tr>
+            <td class="unit3"><nuxt-link to="/armor">遠防</nuxt-link></td>
+            <td class="unit4">1</td>
+          </tr>
+          <tr>
+            <td class="unit3">防禦</td>
+            <td class="unit4">
+              <nuxt-link to="/armor/Armor_Class_Archer">射手</nuxt-link
+              >
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">跑速</td>
+            <td class="unit4">1.1</td>
+          </tr>
+          <tr>
+            <td class="unit3"><nuxt-link to="/los.php">視野</nuxt-link></td>
+            <td class="unit4">6</td>
+          </tr>
+        </table>
+
+        <div class="thanks">感謝! 此頁面由賜齡協助編輯</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'alledmap',
+  components: {}
+}
+</script>
+<style></style>
