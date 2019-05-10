4379f2658723d75553cee5125f45691b34acd6ff
bookthief108 <47220722+bookthief108@users.noreply.github.com>
2019-05-10 12:17:27 +0800

    Create Alfred_the_Alpaca.vue

---
+++ pages/units/edmap/Alfred_the_Alpaca.vue
@@ -0,0 +1,159 @@
+<template>
+  <div class="container">
+    <div class="row padtop">
+      <div class="col-md-12">
+        <h1>武裝羊駝</h1>
+        <div class="hrn1"></div>
+      </div>
+      <div class="col-md-8">
+        <div class="n1">
+          <p>
+            <b>武裝羊駝</b>的外觀與羊駝相似，但體型較小，沒有馬鞍，戴著金色的皇冠，而且有說話的能力。
+            它的移動速度中等（與大多數步兵相當）並使用頭槌攻擊。因為它具有非常高的護甲和踐踏傷害，武裝
+            羊駝在與任何單位的近戰中幾乎立於不敗之地。然而，武裝羊駝很怕打帶跑戰術與對付移動快速的遠程
+            單位，因為它缺乏速度和遠程攻擊。武裝羊駝也很怕城堡和火炮塔的攻擊。
+          </p>
+        </div>
+
+        <h3>圖庫</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <div class="row">
+            <div class="col-md-4">
+              <gallery
+                ><img
+                  class="imgn hg"
+                  width="250"
+                  src="~assets/Alfred_the_alpaca.png"
+                  alt=""
+              /></gallery>
+              <p><em>一群武裝羊駝。</em></p>
+            </div>
+            <div class="col-md-4">
+              <gallery
+                ><img
+                  class="imgn hg"
+                  width="250"
+                  src="~assets/Llama_alfred.jpg"
+                  alt=""
+              /></gallery>
+              <p><em>一般羊駝(上)與武裝羊駝(下)。</em></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4">
+        <table class="table-bordered" width="100%">
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#3c763d;;">
+              <div class="unititle2">武裝羊駝</div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <div class="unititle">
+                <img src="~assets/Alfred_icon.jpg" width="45" alt="" />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#3c763d;;">
+              <div class="unititle2">
+                <nuxt-link to="">單位介紹</nuxt-link>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">類型</td>
+            <td class="unit4">
+              <nuxt-link to="">騎兵</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">首次出現</td>
+            <td class="unit4">
+              <nuxt-link to="/techs/games/aof">失落的帝國</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">訓練於</td>
+            <td class="unit4">
+              <img class="gg" src="~assets/Towncentericon.png" alt="" /><nuxt-link
+                to="/building/Town_Center"
+                >城鎮中心</nuxt-link
+              >
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">作弊密碼</td>
+            <td class="unit4">alpaca simulator</td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#3c763d;;">
+              <div class="unititle2">數據</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">
+              <nuxt-link to="/elements/Hit_points">血量</nuxt-link>
+            </td>
+            <td class="unit4">120</td>
+          </tr>
+          <tr>
+            <td class="unit3">攻擊</td>
+            <td class="unit4">120(近戰)</td>
+          </tr>
+          <tr>
+            <td class="unit3">攻擊加成</td>
+            <td class="unit4">
+              +100對<nuxt-link to="/armor/Armor_Class_Standard_Building">標準建築物</nuxt-link><br />
+              +100對<nuxt-link to="/armor/Armor_Class_stone_defense">石頭建築</nuxt-link
+              >
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">濺涉傷害範圍</td>
+            <td class="unit4">1</td>
+          </tr>
+          <tr>
+            <td class="unit3">
+              <nuxt-link to="/elements/Rate_of_Fire">攻擊速度</nuxt-link>
+            </td>
+            <td class="unit4">2.03</td>
+          </tr>
+          <tr>
+            <td class="unit3"><nuxt-link to="/armor">近防</nuxt-link></td>
+            <td class="unit4">20</td>
+          </tr>
+          <tr>
+            <td class="unit3"><nuxt-link to="/armor">遠防</nuxt-link></td>
+            <td class="unit4">20</td>
+          </tr>
+          <tr>
+            <td class="unit3">防禦</td>
+            <td class="unit4">無
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">跑速</td>
+            <td class="unit4">1</td>
+          </tr>
+          <tr>
+            <td class="unit3"><nuxt-link to="/los.php">視野</nuxt-link></td>
+            <td class="unit4">6</td>
+          </tr>
+        </table>
+
+        <div class="thanks">感謝! 此頁面由賜齡協助編輯</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'alledmap',
+  components: {}
+}
+</script>
+<style></style>
