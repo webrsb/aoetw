e4a8039f369db37f188ce3f1411c1c0b64b1da4e
bookthief108 <47220722+bookthief108@users.noreply.github.com>
2019-05-11 00:00:44 +0800

    Create Francisco_de_Almeidaa.vue

---
+++ pages/batt/Francisco_de_Almeidaa.vue
@@ -0,0 +1,164 @@
+<template>
+  <div class="container">
+    <div class="row padtop">
+      <div class="col-md-12">
+        <h1>法蘭西斯科．德．阿爾梅達</h1>
+        <div class="hrn1"></div>
+      </div>
+      <div class="col-md-8">
+        <div class="spw">
+          <em
+            >這篇文章是說關於法蘭西斯科．德．阿爾梅達戰役。主要敘事的人是法蘭西斯科．德．阿爾梅達的副官
+            <nuxt-link to="/units/hero/Gonzalo_Pizarro">岡薩洛-皮薩羅</nuxt-link></em
+          >
+        </div>
+        <div class="n1">
+          <div class="ien">
+            <em>
+              在歐洲的邊緣地區，葡萄牙人經歷了幾個世紀以來對抗摩爾人和對手基督教王國的戰爭，全國上下處在一種微薄而不確定的存在。
+              現在，大膽的探險家已經從印度回來，並給予葡萄牙希望在舊世界無法實現的榮耀和財富。你能領導一支葡萄牙艦隊到東方，
+              建立一個橫跨三大洲和兩大洋的帝國嗎？一個新的世界在等著你！
+            </em>
+            <p style="text-align: right;margin-top: 10px;">
+              -遊戲中對於法蘭西斯科．德．阿爾梅達戰役的介紹
+            </p>
+          </div>
+          <p>
+            <b>法蘭西斯科·德·亞美達</b> （葡萄牙語：Francisco de Almeida,，1450年－1510年3月1日）是葡萄牙貴族、探險家。
+            在印度洋擴大葡萄牙的勢力，擔任葡屬印度的初代「印度副王」。
+          </p>
+          <p>
+            1450年，生於里斯本。葡萄牙勢力進入印度後，擔任印度方面的司令官。<br>
+            1505年，被任命為3年任期的「印度副王」。<br>
+            1509年，擊敗馬木路克蘇丹國等勢力，加速葡萄牙帝國在印度的擴張。<br>
+            1510年，歸國途中，為了補給水源登陸好望角，在桌灣被科伊科伊人殺害。
+          </p>
+        </div>
+        <h3>英雄</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <p>
+            在遊戲戰役中<nuxt-link to="/units/hero/Gonzalo_Pizarro">岡薩洛-皮薩羅</nuxt-link>是主要英雄，
+            外表是<nuxt-link to="/units/Conquistador">征服者</nuxt-link>，能在地圖編輯器中找到他，他不會被<nuxt-link
+              to="/elements/Conversion">招降</nuxt-link>，能夠自動恢復HP。
+          </p>
+        </div>
+        <h3>關卡</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <div class="row">
+            <div class="col-md-12">
+              <p>
+                法蘭西斯科．德．阿爾梅達由5個關卡組成。玩家扮演的是葡萄人，顏色是藍色的。
+              </p>
+            </div>
+            <div class="col-md-6">
+              <ol>
+                <li><nuxt-link to="/batt/The_Old_World">舊世界</nuxt-link>
+                </li>
+                <li>
+                  <nuxt-link to="/batt/Lion_of_Africa">非洲雄獅</nuxt-link>
+                </li>
+                <li>
+                  <nuxt-link to="/batt/Ruins_of_Empires">帝國的廢墟</nuxt-link>
+                </li>
+                <li>
+                  <nuxt-link to="/batt/Estado_da_India">葡屬印度</nuxt-link>
+                </li>
+                <li>
+                  <nuxt-link to="/batt/A_Sons_Blood">兒子的血仇</nuxt-link>
+                </li>
+              </ol>
+            </div>
+            <div class="col-md-6">
+              <img src="~assets/Francisco_de_Almeida_map.png" width="300" alt="" />
+              <p>法蘭西斯科．德．阿爾梅達戰役地圖</p>
+            </div>
+          </div>
+          <h3>芝麻小事</h3>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <p>法蘭西斯科．德．阿爾梅達的其他資訊</p>
+            <ul></ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <table class="table-bordered" width="100%">
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#375d96;">
+              <div class="unititle" style="color: #fff;">法蘭西斯科．德．阿爾梅達</div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <div class="unititle">
+                <img
+                  src="~assets/Retrato_de_D._Francisco_de_Almeida_(após_1545)_-_Autor_desconhecido.png"
+                  width="300"
+                  alt=""
+                />
+                <p><em>本系列主角 法蘭西斯科．德．阿爾梅達</em></p>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#375d96;">
+              <div class="unititle2" style="color: #fff;">劇情介紹</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">遊戲版本</td>
+            <td class="unit4">
+              <nuxt-link to="/games/ak">非洲王國</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3"><nuxt-link to="/civs">文明</nuxt-link></td>
+            <td class="unit4">
+              <nuxt-link to="/civs/Portuguese">葡萄牙</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">英雄</td>
+            <td class="unit4">
+              <nuxt-link to="/units/hero/Gonzalo_Pizarro">岡薩洛-皮薩羅</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">關卡</td>
+            <td class="unit4">5</td>
+          </tr>
+        </table>
+
+        <div class="thanks">感謝! 此頁面由賜齡協助編輯</div>
+        <div class="wiki" style="margin-top: 5px;">
+          <div style="float: left;">
+            <img src="~assets/Wikipedia-logo.png" width="50" height="46" />
+          </div>
+          <div style="margin-left: 60px;">
+            維基百科上的法蘭西斯科．德．阿爾梅達
+            <div style="margin-left: 10px;">
+              <i
+                ><b
+                  ><a
+                    href="https://en.wikipedia.org/wiki/Francisco_de_Almeida"
+                    target="_blank"
+                    >法蘭西斯科．德．阿爾梅達</a
+                  ></b
+                ></i
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'allbatt',
+  components: {}
+}
+</script>
+<style></style>
