48cc2baadfc8f7cc3981e7d5313c6975809ede3e
bookthief108 <47220722+bookthief108@users.noreply.github.com>
2019-05-11 21:19:27 +0800

    Create A_Sons_Blood.vue

---
+++ pages/batt/A_Sons_Blood.vue
@@ -0,0 +1,343 @@
+<template>
+  <div class="container">
+    <div class="row" style="margin-top: 50px;">
+      <div class="col-md-12">
+        <h1>兒子的血仇</h1>
+        <div class="hrn1"></div>
+      </div>
+      <div class="col-md-8">
+        <div class="n1">
+          <p>
+            <b>兒子的血仇</b>為<nuxt-link to="/batt/Francisco_de_Almeidaa"
+              >法蘭西斯科．德．阿爾梅達</nuxt-link
+            >的第五關劇情關卡。
+          </p>
+          <p>
+            <b>法蘭西斯科·阿爾梅達</b>被年輕國王瑪努爾任命為<nuxt-link
+              to="/civs/Indians"
+              >印度</nuxt-link
+            >總督，這件事讓整個<nuxt-link to="/civs/Portuguese"
+              >葡萄牙</nuxt-link
+            >宮廷極為震撼，法蘭西斯科是先王胡安的愛將，而新加冕的國王則是胡安的堂弟。
+          </p>
+          <p>
+            <b>法蘭西斯科</b
+            >與他的兒子<b>洛倫索</b>聚集了一批船員航行到印度，沿途經歷了海盜、暴風雨、瘟疫。在繞過非洲好望角後，終於他們第一次到達斯瓦希里海岸，有三座堅固堡壘守衛的富饒水上貿易要道。
+          </p>
+          <p>
+            長期航行疲憊不堪的他們，希望能和當地基爾瓦統治者做些貿易並借落腳的地方，但篡位的統治者拒絕了。法蘭西斯科找到了一位名叫<b>穆罕默德伊本盧肯艾丁</b>的流亡基爾瓦王子，他擁有一支反叛軍隊，伺機想奪回他的王位。
+          </p>
+          <p>
+            為了報復現任統治者的不友善，<b>洛倫索</b>決定協助這位流亡的非洲王子，並想出一條妙計，奪下斯瓦希里海岸的財富。
+          </p>
+        </div>
+        <h2>玩家</h2>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <h3>基本資訊</h3>
+          <table class="table-bordered" width="100%">
+            <tr>
+              <td class="tc1" width="15%">起始時代：</td>
+              <td class="tc2" width="15%">
+                <nuxt-link to="/techs/age/Castle_Age">城堡時代</nuxt-link>
+              </td>
+              <td class="tc1" width="15%">起始資源：</td>
+              <td class="tc2">
+                300<nuxt-link to="/resource/Wood">木頭</nuxt-link>，
+                300<nuxt-link to="/resource/Food">食物</nuxt-link>，
+                300<nuxt-link to="/resource/Gold">黃金</nuxt-link>，
+                0<nuxt-link to="/resource/Stone">石頭</nuxt-link
+                >
+              </td>
+              <td class="tc1" width="15%">人口上限：</td>
+              <td class="tc2" width="5%">150</td>
+            </tr>
+          </table>
+
+          <br />
+          <h3>AI</h3>
+          <table class="table-bordered" width="100%">
+            <tr>
+              <td class="tc4" colspan="4" style="background: #2980b9;">
+                玩家本身
+              </td>
+            </tr>
+            <tr>
+              <td class="tc6">
+                <div class="p1">玩家1</div>
+              </td>
+              <td class="tc6" colspan="4">主角（藍色）葡萄牙</td>
+            </tr>
+            <tr>
+              <td class="tc4" colspan="4" style="background: #27ae60;">同盟</td>
+            </tr>
+            <tr>
+              <td class="tc6" width="15%">
+                <div class="p4">玩家2</div>
+              </td>
+              <td class="tc6" width="35%">
+                古吉拉特商人（黃色）<nuxt-link to="/civs/Persians">波斯</nuxt-link>
+              </td>
+            </tr>
+            <tr>
+              <td class="tc4" colspan="4" style="background:#ea6153;">同盟→中立→敵對</td>
+            </tr>
+            <tr>
+            <td class="tc6" width="15%">
+                <div class="p2">玩家3</div>
+              </td>
+              <td class="tc6" width="35%">
+                阿方索艦隊（紅色）<nuxt-link to="/civs/Portuguese"
+                  >衣索比亞</nuxt-link
+                >
+              </td>
+            </tr>
+            <tr>
+              <td class="tc4" colspan="4" style="background:#ea6153;">同盟→敵對</td>
+            </tr>
+            <tr>
+            <td class="tc6" width="15%">
+                <div class="p8">玩家4</div>
+              </td>
+              <td class="tc6" width="35%">敵軍首領（橘色）波斯</td>
+             <td class="tc6" width="15%">
+                <div class="p3">玩家5</div>
+              </td>
+              <td class="tc6" width="35%">
+                奧斯曼人（綠色）<nuxt-link to="/civs/Turks"
+                  >土耳其</nuxt-link>
+              </td>
+              <td class="tc6" width="15%">
+                <div class="p5">玩家6</div>
+              </td>
+              <td class="tc6" width="35%">馬木留克（水藍色）<nuxt-link to="/civs/Saracens"
+                  >薩拉森</nuxt-link>
+              </td>
+              <td class="tc6" width="15%">
+                <div class="p6">玩家7</div>
+              </td>
+              <td class="tc6" width="35%">第烏人（紫色）波斯</td>
+            </tr>
+          </table>
+
+          <br />
+          <h3>開場：</h3>
+          <p>
+            <nuxt-link to="/units/Halbedier">戟兵</nuxt-link>*3、
+            <nuxt-link to="/units/Arbalest">強弩兵</nuxt-link>*3、
+            <nuxt-link to="/units/Monk">僧侶</nuxt-link>*2、
+            <nuxt-link to="/units/Missionary">傳教士</nuxt-link>*1、
+            <nuxt-link to="/units/Capped_Ram">裝甲衝撞車</nuxt-link>*1、
+            <nuxt-link to="/units/Villager">村民</nuxt-link>*9、
+            <nuxt-link to="/units/Light_Cavalry">輕騎兵</nuxt-link>*1。
+          </p>
+          
+          <br />
+          <h2>主要任務</h2>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ul>
+              <li>招降10個阿爾布科克人。</li>
+              <li>殺死少於15個阿爾布科克人</li>
+              <li>殺死少於10個阿爾布科克人</li>
+              <li>殺死少於6個阿爾布科克人</li>
+              <li>招降阿方索‧阿爾布科克。</li>
+              <li>摧毀第烏人的世界奇觀。</li>
+            <p>選擇以下一項完成以獲得勝利</p>
+              <li>建造並守衛一個世界奇觀。</li>
+              <li>打敗馬木留克奴隸兵人，奧斯曼人和第烏人。</li>
+            </ul>
+          </div>
+          <br />
+          <h2>劇情共通主要任務</h2>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ul>
+              <li>
+                摧毀基爾瓦（紅）<nuxt-link to="/building/Castle">城堡</nuxt-link
+                >。
+              </li>
+              <li>
+                摧毀蒙巴薩（黃）<nuxt-link to="/building/Wonder"
+                  >世界奇觀</nuxt-link
+                >。
+              </li>
+            </ul>
+          </div>
+          <br />
+          <h2>次要任務</h2>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ul>
+              <li>擊沉桑給巴爾的槳帆戰船並摧毀他們的火焰塔。</li>
+              <li>將班圖從斯瓦西里貿易站裡清除。</li>
+              <li>找到姆塔帕的金礦。</li>
+              <li>攻擊桑給巴爾的貿易商船並奪取他的貨物。</li>
+              <li>摧毀班圖草原營地。</li>
+            </ul>
+          </div>
+          <br />
+          <h2>任務提示</h2>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ol>
+              <li>
+                你的人口上限是150，初始為城堡時代，此關卡可升至<nuxt-link
+                  to="/techs/age/Imperial_Age">帝王時代</nuxt-link>。
+              </li>
+              <li>
+                當你招降葡萄牙人時，不要忽略生產新單位。你需要就緒一旦葡萄牙人都被招降，你就要跟馬木留克奴隸兵人，奧斯曼人和第烏人作戰。
+              </li>
+              <li>
+                摧毀馬木留克奴隸兵港灣大商站是首要任務。他們是馬木留克奴隸兵人國庫收入的主要來源。
+              </li>
+              <li>
+                西方的卡提瓦半島有富足的金礦。
+              </li>
+            </ol>
+          </div>
+        </div>
+        <br />
+        <h2>斥候回報</h2>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <ul>
+            <li>
+              法蘭西斯科‧德‧阿爾梅達到達了印度西北的內陸城鎮。在他為了他兒子洛倫索的死報仇雪恨之前，他不會讓出總督的位置。
+              阿方索‧阿爾布科克（紅色，葡萄牙人）帶著薩繆爾國王的一封任命信著陸了，他正信心滿滿地準備頂替法蘭西斯科成為新總督。
+              他出了名的專橫霸道，這使得他的人很容易被金錢和佈道所動搖。阿爾布科克遠征軍是由征服者，戟兵，強弩兵和火槍兵組成。
+              你要小心不要給葡萄牙同伴帶來沉重的傷亡，因為之後你會需要他們幫你一起對抗第烏。
+            </li>
+            <li>
+              第烏（紫色，波斯人）是在西方的卡提瓦島半島的南海岸外的一座島城，它將會是接下來的主戰場。
+              第烏城由馬利克阿雅姿統治，他是一位皈依了伊斯蘭教的俄國人，第烏人的主力軍是戰象，重裝駱駝和一些受穆斯林伊瑪目激勵而參戰的聖騎士。
+              由於威尼斯工匠的協助，馬利克阿雅姿在城內建造了火砲塔。
+            </li>
+            <li>
+              第烏人還得到奧斯曼蘇丹（綠色，土耳其人）大軍的援助。這是一支由土耳其皇家親兵，火砲和突厥重騎（全覆式重裝騎兵）組成的先鋒隊，
+              他們駐紮在蘇拉特城周圍，與葡萄牙人只隔著康坎河。
+            </li>
+            <li>
+              阿米爾侯賽因阿爾庫爾迪指揮埃及馬木留克奴隸兵人（青色，薩拉森人）。他的艦隊基地在卡姆巴特海灣的北面，第烏人的北面。
+              馬木留克奴隸兵軍隊包括奴隸騎兵，馬弓騎兵和攻城武器組成，還有一小支由義大利派遣過來的僱傭兵援軍。
+              威尼斯人還用貿易港灣大商站來獲取資源供馬木留克奴隸兵人招兵買馬。摧毀這些港灣大商站就會摧毀馬木留克奴隸兵人。
+            </li>
+            <li>
+              最終，古吉拉特商人（黃色，波斯人）在西方乾旱的和富有黃金的卡提瓦島半島有貿易市集。對於這些精明的商人來說，
+              只選擇一邊站隊是沒有利可圖的，因為他們急於和葡萄牙做交易。
+            </li>
+          </ul>
+          <h2>玩家攻略</h2>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ol>
+              <li>
+                。
+              </li>
+              <li>
+                。
+              </li>
+            </ol>
+          </div>
+          <h2>細節</h2>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ul>
+              <li>
+                目標要求你殺死所有三個敵人領導人（包括M馬利克·艾亞茲）。 然而，實際上，馬利克·艾亞茲在戰鬥中倖存下來並且只被抓住，
+                之後一直住在他的莊園，直到他去世。
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <table class="table-bordered" width="100%">
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#375d96;">
+              <div class="unititle2">
+                <nuxt-link to="/batt/A_Sons_Blood">兒子的血仇</nuxt-link>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <div class="unititle"><img src="~assets/A_Sons_Blood.pg" alt="" /></div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#375d96;">
+              <div class="unititle2">
+                <nuxt-link to="/batt">戰役</nuxt-link>資訊
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">版本</td>
+            <td class="unit4">
+              <nuxt-link to="/games/ak">非洲王朝</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">戰役</td>
+            <td class="unit4">
+              <nuxt-link to="/batt/Francisco_de_Almeidaa">法蘭西斯科．德．阿爾梅達</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3"><nuxt-link to="/civs">文明</nuxt-link></td>
+            <td class="unit4">
+              <nuxt-link to="/civs/Portuguese">葡萄人</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#375d96;">
+              <div class="unititle2">戰役關卡編號</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">章節</td>
+            <td class="unit4">
+              <nuxt-link to="/games/ak">非洲王朝</nuxt-link>第三章之五
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">上一關</td>
+            <td class="unit4">
+              <nuxt-link to="/batt/Estado_da_India">葡屬印度</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">下一關</td>
+            <td class="unit4">
+              N/A
+            </td>
+          </tr>
+        </table>
+
+        <div class="thanks">感謝！此頁面由裕暉協助編輯。</div>
+        <div class="wiki" style="margin-top: 5px;">
+          <div style="float: left;">
+            <img src="~assets/Wikipedia-logo.png" width="50" height="46" />
+          </div>
+          <div style="margin-left: 60px;">
+            維基百科上的<i
+              ><b
+                ><a href="http://t.cn/EqFPNxP" target="_blank"
+                  >法蘭西斯科·阿爾梅達</a
+                ></b
+              ></i
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'allbatt',
+  components: {}
+}
+</script>
+<style></style>
