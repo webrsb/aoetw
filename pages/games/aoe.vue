aeb3f8e47a31ca624c3c293daf8fca1f8571142d
bookthief108 <47220722+bookthief108@users.noreply.github.com>
2019-06-29 17:16:16 +0800

    Create aoe.vue

---
+++ pages/games/aoe.vue
@@ -0,0 +1,219 @@
+<template>
+  <div class="container">
+    <div class="row padtop">
+      <div class="col-md-12">
+        <h1>世紀帝國II：決定版</h1>
+        <div class="hrn1"></div>
+      </div>
+      <div class="col-md-8">
+        <div class="n1">
+          <p>
+            <b>《世紀帝國II：決定版》</b
+            >，譯名很多又稱<b>最後可汗</b>，是世紀帝國II第五張資料片，也是最新的資料片，將在2019年秋季發行。本資料片新增了四個新<nuxt-link
+              to="/civs"
+              >文明</nuxt-link
+            >、4K高畫質及完全重製的畫面即音軌。<br>
+            原文網址請點這:<a href="https://ageofempires.fandom.com/wiki/Age_of_Empires_II:_Definitive_Edition">請點我</a><br>
+            官方預告請點這:<a href="https://www.youtube.com/watch?v=ZOgBVR21pWg">請點我</a>
+          </p>
+         
+        </div>
+        <h2>新增內容</h2>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <ol>
+            <li>
+              世紀帝國HD版及所有擴充版都包含在決定版中。
+            </li>
+            <li>
+              新圖像與效果，包括建築物倒塌時的動畫。
+            </li>
+            <li>
+              音軌重製。
+            </li>
+            <li>
+              有提供老玩家們切換的選項(未知)。
+            </li>
+            <li>
+              農田自動補種系統。
+            </li>
+            <li>
+              生產中的所有單位都會顯示在視窗上方中間的Bar上。
+            </li>
+            <li>
+              訓練單位和科技研發可以在同一建築裡同時進行。
+            </li>
+            <li>
+              科技研發的進度條會顯示出來。正在研發科技的建築會顯示出視覺特效（尚未完成）
+            </li>
+            <li>
+              可放大縮小畫面。
+            </li>
+            <li>
+              挑戰戰役教導玩家如何誘殺野豬或設定建築清單。
+            </li>
+            <li>
+              多人遊戲有新的伺服器。
+            </li>
+            <li>
+              現在有不作弊的AI，並且設立新的電腦難度:極端。
+            </li>
+            <li>
+              在頂端的資源欄下方會顯示各種資源的工作村民數量。
+            </li>
+            <li>
+              改進及重新設計舊戰役，某些文明現在被HD版文明（例如義大利人）替換更新。
+            </li>
+            <li>
+              拜占庭人和西班牙人的建築物現在使用歐洲地中海的模板。
+            </li>
+            <li>
+              常用的模組，如矮樹叢模組及網格模組依然可用。
+            </li>
+            <li>
+              可能可以透過Age of Empires web修改模組。
+            </li>
+            <li>
+              新的單位和物體，並重新設計一些舊的（p.e“海上圍牆”）。
+            </li>
+            <li>
+              改善觀眾模式和多人遊戲模式以及串流功能。
+            </li>
+            <li>
+              在Steam和Windows Store上發布，支援兩個平台共同對戰遊玩。
+            </li>
+          </ol>
+        </div>
+        
+        <h3>戰役</h3>
+        <p>
+          <em>主要文章:<nuxt-link to="/batt">戰役</nuxt-link></em>
+        </p>
+        <ul>
+          <li>
+          <nuxt-link to="/batt/">尚未發布</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/batt/">尚未發布</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/batt/">尚未發布</nuxt-link>
+          </li>
+          
+        </ul>
+        
+      </div>
+        <h2>新增元素</h2>
+        <div class="hrn1"></div>
+        <h3>文明</h3>
+        <div class="n1">
+          <ul>
+            <li>
+              <strong><nuxt-link to="/civs/Bulgarians">保加利亞</nuxt-link>:</strong
+              >。
+            </li>
+            <li>
+              <strong><nuxt-link to="/civs/Lithuanians">立陶宛</nuxt-link>:</strong
+              >。
+            </li>
+            <li>
+              <strong><nuxt-link to="/civs/Cumans">庫曼</nuxt-link>:</strong
+              >。
+            </li>
+            <li>
+              <strong><nuxt-link to="/civs/Tatars">韃靼</nuxt-link>:</strong
+              >。
+            </li>
+          </ul>
+        </div>
+        
+        <h3>戰役</h3>
+        <p>
+          <em>主要文章:<nuxt-link to="/batt">戰役</nuxt-link></em>
+        </p>
+        <ul>
+          <li>
+          <nuxt-link to="/batt/">最後的可汗</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/batt/">尚未發布</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/batt/">尚未發布</nuxt-link>
+          </li>
+          
+        </ul>
+        
+      </div>
+      </div>
+      <div class="col-md-4">
+        <table class="table-bordered" width="100%">
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#356a98">
+              <div class="unititle2">
+                世紀帝國II：決定版
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <div class="unititle">
+                <img width="100%" src="~assets/Age-of-empires-ii_de_keyart.jpg" alt="" />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#356a98">
+              <div class="unititle2" style="color: #fff;">遊戲介紹</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">開發商</td>
+            <td class="unit4">Tantalus <br />Forgotten Empires<br />Wicked Witch</td>
+          </tr>
+          <tr>
+            <td class="unit3">發行</td>
+            <td class="unit4">微軟</td>
+          </tr>
+          <tr>
+            <td class="unit3">遊玩平台</td>
+            <td class="unit4">Windows 10</td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#356a98;">
+              <div class="unititle2" style="color: #fff;">發行日期</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">steam版本</td>
+            <td class="unit4">2019年秋季</td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#356a98;">
+              <div class="unititle2" style="color: #fff;">前作與擴充</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">擴充</td>
+            <td class="unit4">
+              <nuxt-link to="/games/aok">帝王世紀</nuxt-link>
+              <br /><nuxt-link to="/games/aoc">征服者入侵</nuxt-link>
+              <br /><nuxt-link to="/games/aof">失落的帝國</nuxt-link>
+              <br /><nuxt-link to="/games/ak">非洲王國</nuxt-link>
+              <br /><nuxt-link to="/games/aor">王者崛起</nuxt-link>
+            </td>
+          </tr>
+        </table>
+
+        <div class="thanks">感謝! 此頁面由賜齡協助編輯</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'foot',
+  components: {}
+}
+</script>
+<style></style>
