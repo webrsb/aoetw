697f9c89abae76bdbd6984725d45ccea2f624590
bookthief108 <47220722+bookthief108@users.noreply.github.com>
2019-07-24 00:25:25 +0800

    Create Bapheus_1302.vue
    待補

---
+++ pages/batt/Bapheus_1302.vue
@@ -0,0 +1,345 @@
+<template>
+  <div class="container">
+    <div class="row padtop">
+      <div class="col-md-12">
+        <h1>巴菲烏斯(1302)</h1>
+        <div class="hrn1"></div>
+      </div>
+      <div class="col-md-8">
+        <div class="n1">
+          <p>
+            <b>巴菲烏斯(1302)</b>是《世紀帝國II:失落的帝國》中失落的帝國戰役的其中一個戰役。該戰役描述鄂圖曼帝國的崛起。地圖簡略的繪出現在土耳其
+            領土的西半部。玩家跟所有同盟都是使用土耳其人。
+          </p>
+        </div>
+        <h2>玩家</h2>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <h3>基本資訊</h3>
+          <table class="table-bordered" width="100%">
+            <tr>
+              <td class="tc1" width="14%">起始時代：</td>
+              <td class="tc2" width="15%">
+                <nuxt-link to="/techs/age/Feudal_Age">封建時代</nuxt-link>
+              </td>
+              <td class="tc1" width="14%">起始資源：</td>
+              <td class="tc2">
+                <nuxt-link to="/resource/Wood">木頭</nuxt-link>0，<nuxt-link
+                  to="/resource/Food"
+                  >食物</nuxt-link
+                >0，<nuxt-link to="/resource/Gold">黃金</nuxt-link
+                >0，<nuxt-link to="/resource/Stone">石頭</nuxt-link>0
+              </td>
+              <td class="tc1" width="14%">人口上限：</td>
+              <td class="tc2" width="5%">150</td>
+            </tr>
+          </table>
+          
+          <h3>AI</h3>
+        <table class="table-bordered" width="100%">
+          <tr>
+            <td class="tc4" colspan="4" style="background: #2980b9;">
+              玩家本身
+            </td>
+          </tr>
+          <tr>
+            <td class="tc6">
+              <div class="p4"></div>
+              玩家1：
+            </td>
+            <td class="tc6" colspan="3">
+              <nuxt-link to="/civs/Persians">波斯</nuxt-link>（黃色）
+            </td>
+          </tr>
+
+          <tr>
+            <td class="tc4" colspan="4" style="background: #27ae60;">同盟</td>
+          </tr>
+          <tr>
+            <td class="tc6" width="15%">
+              <div class="p8"></div>
+              玩家2：
+            </td>
+            <td class="tc6" width="35%">波斯人（橘色）波斯</td>
+            <td class="tc6" width="15%">
+              <div class="p1"></div>
+              玩家3：
+            </td>
+            <td class="tc6" width="35%">呼羅珊人（藍色）印度</td>
+          </tr>
+          
+          <tr>
+            <td class="tc4" colspan="4" style="background:#ea6153;">敵對</td>
+          </tr>
+          <tr>
+            <td class="tc6">
+              <div class="p3"></div>
+              玩家4：
+            </td>
+            <td class="tc6">白匈奴人（綠色）蒙古</td>
+          </tr>
+          <tr>
+            <td class="tc6">
+              <div class="p5"></div>
+              玩家5：
+            </td>
+            <td class="tc6">突厥人（青色）土耳其</td>
+          </tr>
+          <tr>
+            <td class="tc6">
+              <div class="p6"></div>
+              玩家6：
+            </td>
+            <td class="tc6">白匈奴突襲兵（紫色）匈奴</td>
+          </tr>
+        </table>
+
+
+<h3>開場</h3>
+<li>霍斯勞X1，象弓騎兵X3，輕騎兵X1，村民X3</li>
+
+          <h3>主要任務</h3>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ol>
+              <li>
+                霍斯勞必須存活。
+              </li>
+              <li>
+                依著指引到能安置你基地的地點。
+              </li>
+              <li>
+                升級到帝王時代並等待你的大軍到來。
+              </li>
+              <li>
+                擊敗白匈奴人。
+              </li>
+              <li>
+                擊敗突厥人。
+              </li>
+            </ol>
+          </div>
+           <h3>選擇性任務</h3>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ul>
+              <li>
+                保護波斯村莊。
+              </li>
+              <li>
+                五分鐘之內付給白匈奴人400黃金。
+              </li>
+              <li>
+                五分鐘之內付給白匈奴人600黃金(要先觸發上一條400黃金)。
+              </li>
+              <li>
+                五分鐘之內付給白匈奴人1000黃金(要先觸發上一條600黃金)。
+              </li>
+              <li>
+                派一位使臣到突厥人那締結同盟。
+              </li>
+              <li>
+                摧毀絲路上的所有敵方箭塔並用新的箭塔取代他們。
+              </li>
+              <li>
+                給聖火廟10個村民。
+              </li>
+              <li>
+                給呼羅珊1000木材換取他們的漁獲供給。
+              </li>
+              <li>
+                你可以點擊主帳篷來訓練10個薩瓦蘭重騎兵(需要1500黃金)。
+              </li>
+            </ul>
+          </div>
+          <h3>任務提示</h3>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ul>
+              <li>
+                保護當地村莊，這樣確保他們能源源不斷地進貢。
+              </li>
+              <li>
+                你不一定非得聽白匈奴人的話乖乖進貢，但是在早期你最好不要得罪這些白匈奴人。
+              </li>
+              <li>
+                和突厥人結盟能讓你更放心地和白匈奴人抗衡。
+              </li>
+              <li>
+                如果你快沒黃金了，你可以依靠和絲路另一端的貿易戰做交易來獲取暴利。
+              </li>
+              <li>
+                這些平原被一些馬弓騎兵所佔據，你最好的克制方法就是用戟兵和戰矛兵互相配合。這種組合的性價比很高。波斯人有強大的騎兵單位，你要好好利用。
+                從目前的狀況來看戰象對你的用處不大。
+              </li>
+              <li>
+                薩瓦蘭重騎兵是波斯特產的一種能自行療傷的重騎兵單位。他們價格不斐但物超所值。
+              </li>
+            </ul>
+          </div>
+          <h3>斥候</h3>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ul>
+              <li>
+                西邊有幾座波斯村莊，他們幾乎沒有任何防禦工事。
+              </li>
+              <li>
+                北邊是突厥人部落的地盤。
+              </li>
+              <li>
+                東邊有幾座白匈奴人的防禦工事，有重兵把守。
+              </li>
+              <li>
+                在波斯影響力的邊緣地帶有一座拜火教的聖火廟。
+              </li>
+              <li>
+                絲路是一條從中國到拜占庭的貿易大道，這裡的貿易利潤非常高。
+              </li>
+            </ul>
+          </div>
+          
+          <h3>攻略</h3>
+          <div class="hrn1"></div>
+          <ul>
+            <li>
+              一開始你應該盡快建立一個小鎮。保護盟軍村莊不被襲擊，以確保你繼續從他們那裡獲得資源並進入帝王時代。
+              在每個淺灘都建造城牆或木牆阻擋襲擊者的猛攻。建議用一些弩炮戰船來控制河流，並攻擊試圖越過河流的襲擊者和白匈奴人。
+              白色匈奴人會多次要求進貢，只要你沒有體面的軍隊，繼續付錢給他們會是個好主意。稍微忍耐一下吧。
+            </li>
+            <li>
+              如果金礦耗盡了，有很多方法可以在這張地圖上獲得黃金。在波斯人的勢力邊緣，有一座祭拜瑣羅亞斯特的火神殿。
+              給他們10名村民您將獲得他們的僧侶和遺物。有四座紫色箭塔需要破壞，並用你的箭塔做替換。
+              通過這樣做，您每分鐘還可以獲得額外100金幣，您還可以安全地到東北與呼羅珊市場交易，為您提供更多的黃金。
+              靠近突厥人營地的湖的北部還有一個藍色的碼頭，在那裡您可以與之交易以獲得更多的黃金。所以對於黃金玩家是不需要擔心的。
+            </li>
+            
+            <li>
+              在寺廟的南面有一個有遊俠，兩個精銳聖騎兵和幾個帳篷的營地。
+              這是一個訓練薩瓦蘭重騎兵的營地。用1500黃金並點擊主帳篷將獲得10個薩瓦蘭重騎兵。
+              非常建議訓練這些重騎兵並搭配其他騎兵，特別是戰象，然後向白匈奴宣戰。
+            </li>
+            <li>
+              一旦你進入帝王時代就會向白匈奴宣戰，你必須打敗他們。注意他們的蒙古突騎和匈牙利輕騎兵組合。
+              他們還會訓練戰矛兵，僧侶和一些巨型投石機。
+              對抗它們的最好方法是訓練重裝駱駝騎兵，戟兵和戰矛兵搭配一些重型騎兵（你也可以購買薩瓦蘭重騎兵，一種重裝且能自行回血的騎兵）。
+              或者，如果你從上面提到的任務中獲得了大量的黃金，那麼用大規模的薩瓦蘭重騎兵軍隊和戰象的輾過白匈奴也是一個很好的選擇，
+              因為白匈奴人的部隊並不像重型波斯騎兵部隊那樣有精良的裝備。在他們的基地附近蓋幾個城堡也能幫助你對抗白匈奴人。
+              
+            </li>
+            <li>你也可以與突厥人結盟。突厥人擁有出色的重裝馬弓騎兵和匈牙利輕騎兵，他們會幫助你分散敵人的注意力並襲擊敵人的城鎮。
+              白匈奴人只會嘗試重建他們毀壞的基本軍事建築，如馬厩和射箭場，但不會嘗試重建他們的城堡或修道院。
+              因此，城堡應該是玩家巨型投石機的主要目標，因為白匈奴人不會再重建它們。你可以試圖偷溜一些爆炸桶炸毀地圖以東的修道院，
+              但這應該在你對白匈奴宣戰後立即進行。一旦你摧毀了他們所有的城堡和他們的軍隊，白匈奴將投降（立即研究間諜，因為它只需要200金！）。
+              然而，突厥人這時會背叛你，但他們並不像白匈奴那樣強大，而且很容易被擊敗。一旦他們的基地被摧毀，你就贏得這個戰役！
+            
+
+              
+            </li>
+          </ul>
+          <h3>遊戲內細節</h3>
+          <div class="hrn1"></div>
+          <ul>
+            <li>
+            </li>
+          </ul>
+          <h3>芝麻小事</h3>
+          <div class="hrn1"></div>
+          <ul>
+            <li></li>
+          </ul>
+        </div>  
+      </div>
+      <div class="col-md-4">
+        <table class="table-bordered" width="100%">
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#375d96;">
+              <div class="unititle2">巴菲烏斯(1302)</div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <div class="unititle"><img src="~assets/Bapheus_map.png" alt="" /></div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#375d96;">
+              <div class="unititle2">戰役資訊</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">版本</td>
+            <td class="unit4">
+              <nuxt-link to="/games/aof">失落的帝國</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">戰役</td>
+            <td class="unit4">
+              <nuxt-link to="/batt/Battles_of_the_Forgotten"
+                >失落的帝國的戰役</nuxt-link
+              >
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">文明</td>
+            <td class="unit4">
+              <nuxt-link to="/civs/Turks">土耳其</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#375d96;">
+              <div class="unititle2">戰役關卡編號</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">章節</td>
+            <td class="unit4">
+              <nuxt-link to="/games/aof">失落的帝國</nuxt-link>第七章之八
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">上一關</td>
+            <td class="unit4"><nuxt-link to="/batt/Cyprus_1191"
+                >塞普勒斯(1191)</nuxt-link
+              >
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">下一關</td>
+            <td class="unit4">N/A
+            </td>
+          </tr>
+        </table>
+
+        <div class="thanks">感謝！此頁面由賜齡、裕暉協助編輯。</div>
+        <div class="wiki" style="margin-top: 5px;">
+          <div style="float: left;">
+            <img src="~assets/Wikipedia-logo.png" width="50" height="46" />
+          </div>
+          <div style="margin-left: 60px;">
+            維基百科布哈拉戰役
+            <div style="margin-left: 10px;">
+              <em
+                ><strong
+                  ><a
+                    href="https://zh.wikipedia.org/wiki/%E5%B8%83%E5%93%88%E6%8B%89%E4%B9%8B%E6%88%B0"
+                    target="_blank"
+                    >布哈拉戰役</a
+                  ></strong
+                ></em
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'allbatt',
+  components: {}
+}
+</script>
+<style></style>
