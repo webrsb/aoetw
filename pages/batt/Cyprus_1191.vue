faeb66e066a58137c507d02dfaf96cc7ff97d1b7
bookthief108 <47220722+bookthief108@users.noreply.github.com>
2019-08-01 23:59:12 +0800

    Create Cyprus_1191.vue

---
+++ pages/batt/Cyprus_1191.vue
@@ -0,0 +1,372 @@
+<template>
+  <div class="container">
+    <div class="row padtop">
+      <div class="col-md-12">
+        <h1>塞普勒斯(1191)</h1>
+        <div class="hrn1"></div>
+      </div>
+      <div class="col-md-8">
+        <div class="n1">
+          <p>
+            <b><nuxt-link to="/batt/Bukhara_557">布哈拉</nuxt-link>之戰</b
+            >是《<nuxt-link to="/games/aof">世紀帝國II：失落的帝國</nuxt-link
+            >》中<nuxt-link to="/batt/Battles_of_the_Forgotten"
+              >失落的帝國的戰役</nuxt-link
+            >的其中一個戰役。該<nuxt-link to="/batt">戰役</nuxt-link
+            >是根據現實事件布哈拉之戰改編而成。
+          </p>
+        </div>
+        <h2>玩家</h2>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <h3>基本資訊</h3>
+          <table class="table-bordered" width="100%">
+            <tr>
+              <td class="tc1" width="14%">起始時代：</td>
+              <td class="tc2" width="15%">
+                <nuxt-link to="/techs/age/Feudal_Age">封建時代</nuxt-link>
+              </td>
+              <td class="tc1" width="14%">起始資源：</td>
+              <td class="tc2">
+                無
+              </td>
+              <td class="tc1" width="14%">人口上限：</td>
+              <td class="tc2" width="5%">150</td>
+            </tr>
+          </table>
+
+          <h3>AI</h3>
+          <table class="table-bordered" width="100%">
+            <tr>
+              <td class="tc4" colspan="4" style="background: #2980b9;">
+                玩家本身
+              </td>
+            </tr>
+            <tr>
+              <td class="tc6">
+                <div class="p4"></div>
+                玩家1
+              </td>
+              <td class="tc6" colspan="3">
+                <nuxt-link to="/civs/Persians">波斯</nuxt-link>（黃色）
+              </td>
+            </tr>
+
+            <tr>
+              <td class="tc4" colspan="4" style="background: #27ae60;">同盟</td>
+            </tr>
+            <tr>
+              <td class="tc6" width="15%">
+                <div class="p8"></div>
+                玩家2
+              </td>
+              <td class="tc6" width="35%">
+                <nuxt-link to="/civs/Persians">波斯人</nuxt-link
+                >（橘色）<nuxt-link to="/civs/Persians">波斯</nuxt-link>
+              </td>
+              <td class="tc6" width="15%">
+                <div class="p1"></div>
+                玩家3
+              </td>
+              <td class="tc6" width="35%">
+                呼羅珊人（藍色）<nuxt-link to="/civs/Indians">印度</nuxt-link>
+              </td>
+            </tr>
+
+            <tr>
+              <td class="tc4" colspan="4" style="background:#ea6153;">敵對</td>
+            </tr>
+            <tr>
+              <td class="tc6">
+                <div class="p3"></div>
+                玩家4
+              </td>
+              <td class="tc6">
+                白匈奴人（綠色）<nuxt-link to="/civs/Mongols">蒙古</nuxt-link>
+              </td>
+            </tr>
+            <tr>
+              <td class="tc6">
+                <div class="p5"></div>
+                玩家5
+              </td>
+              <td class="tc6">
+                突厥人（青色）<nuxt-link to="/civs/Turks">土耳其</nuxt-link>
+              </td>
+            </tr>
+            <tr>
+              <td class="tc6">
+                <div class="p6"></div>
+                玩家6
+              </td>
+              <td class="tc6">白匈奴突襲兵（紫色）匈奴</td>
+            </tr>
+          </table>
+
+          <h3>開場</h3>
+          <li>
+            <nuxt-link to="/units/hero/Khosrau">霍斯勞</nuxt-link>X1，<nuxt-link
+              to="/units/Elephant_Archer"
+              >象弓騎兵</nuxt-link
+            >X3，<nuxt-link to="/units/Light_Cavalry">輕騎兵</nuxt-link
+            >X1，<nuxt-link to="/units/Villager">村民</nuxt-link>X3
+          </li>
+
+          <h3>主要任務</h3>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ol>
+              <li>
+                理查必須存活。
+              </li>
+              <li>
+                在西西里登陸並把軍隊派到安營點。
+              </li>
+              <li>
+                
+              </li>
+              <li>
+                。
+              </li>
+              <li>
+                。
+              </li>
+            </ol>
+          </div>
+          <h3>選擇性任務</h3>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ul>
+              <li>
+                保護波斯村莊。
+              </li>
+              <li>
+                五分鐘之內付給白匈奴人400<nuxt-link to="/resource/Gold"
+                  >黃金</nuxt-link
+                >。
+              </li>
+              <li>
+                五分鐘之內付給白匈奴人600黃金(要先觸發上一條400黃金)。
+              </li>
+              <li>
+                五分鐘之內付給白匈奴人1000黃金(要先觸發上一條600黃金)。
+              </li>
+              <li>
+                派一位使臣到突厥人那締結同盟。
+              </li>
+              <li>
+                摧毀絲路上的所有敵方<nuxt-link to="/building/Watch_Tower"
+                  >箭塔</nuxt-link
+                >並用新的箭塔取代他們。
+              </li>
+              <li>
+                給聖火廟10個村民。
+              </li>
+              <li>
+                給呼羅珊1000<nuxt-link to="/resource/Wood">木材</nuxt-link
+                >換取他們的漁獲供給。
+              </li>
+              <li>
+                你可以點擊主帳篷來訓練10個薩瓦蘭重騎兵(需要1500黃金)。
+              </li>
+            </ul>
+          </div>
+          <h3>任務提示</h3>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ul>
+              <li>
+                如果你想佔領一座城池歸為己用的話，別忘了你一開始破壞得越多，最後留下來給你自己的越少。
+              </li>
+              <li>
+                有機會的話多用僧侶替你的軍隊療傷。
+              </li>
+              <li>
+                斥侯一片地域最有效的方法就是把你的單位都分散出去搜索。
+              </li>
+              <li>
+                當你在墨西拿城整軍備戰時，你可以根據自己的需要用市集來買賣各種資源。
+              </li>
+              <li>
+                一開始所有人都是你的同盟，但是別被短暫的和平蒙蔽了雙眼，其實戰火隨時會被點燃...。
+              </li>
+            </ul>
+          </div>
+          <h3>斥候</h3>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ul>
+              <li>
+                獅心王理查(紅色)正在發動他的第三次十字軍東征。首先，理查得先去和他交好的西西里王公們溝通戰備。你的征途開始於西西里島北邊的艦隊裡。
+              </li>
+              <li>
+                西西里的國王坦克雷德(綠色)居住在西邊的城池巴勒莫裡。她是你目前的同盟，也會主持你和王公們的見面會。  
+              </li>
+              <li>
+                菲利浦奧古斯都(藍色)是理查的老對手，但是這一次你們因為共同的目的而走到了一起。他已經在墨西拿城旁邊的西西里安營紮寨了。菲利浦倒不是以英勇善戰出名，
+                但是他的詭計多端，深謀遠慮卻是人人皆知的。
+              </li>
+              <li>
+                墨西拿城(黃色)是西西里島上的一座城池。有高牆防護，和四通八達的海港。他們是你的同盟，但別去挑戰他們的底線。
+              </li>
+              <li>
+                利馬索港(青藍色)是賽普勒斯的一座港口。賽普勒斯位於地圖東邊角。
+              </li>
+              <li>
+                伊薩克．科穆寧(青藍色)是賽普勒斯的國王。他一般居住在王國中部戒備森嚴的尼古西亞城。她是你的盟友，但他一向不怎麼喜歡西歐的皇族勢力。
+              </li>
+              <li>
+                賽普勒斯(青藍色)裡的所有平民及軍隊都聽從伊薩克．科穆寧的領導。
+              </li>
+            </ul>
+          </div>
+
+          <h3>攻略</h3>
+          <div class="hrn1"></div>
+          <ul>
+            <li>
+              一開始你應該盡快建立一個小鎮。保護盟軍村莊不被襲擊，以確保你繼續從他們那裡獲得資源並進入帝王時代。在每個淺灘都建造<nuxt-link
+                to="/building/Stone_Wall"
+                >城牆</nuxt-link
+              >或<nuxt-link to="/building/Palisade_Wall">木牆</nuxt-link
+              >阻擋襲擊者的猛攻。建議用一些<nuxt-link to="/units/War_Galley"
+                >弩砲戰船</nuxt-link
+              >來控制河流，並攻擊試圖越過河流的襲擊者和白匈奴人。白色匈奴人會多次要求進貢，只要你沒有體面的軍隊，繼續付錢給他們會是個好主意。稍微忍耐一下吧。
+            </li>
+            <li>
+              如果金礦耗盡了，有很多方法可以在這張地圖上獲得黃金。在波斯人的勢力邊緣，有一座祭拜瑣羅亞斯特的火神殿。給他們10名村民您將獲得他們的<nuxt-link
+                to="/units/Monk"
+                >僧侶</nuxt-link
+              >和遺物。有四座紫色箭塔需要破壞，並用你的箭塔做替換。通過這樣做，您每分鐘還可以獲得額外100金幣，您還可以安全地到東北與呼羅珊市場交易，為您提供更多的黃金。靠近突厥人營地的湖的北部還有一個藍色的<nuxt-link
+                to="/building/Dock"
+                >碼頭</nuxt-link
+              >，在那裡您可以與之交易以獲得更多的黃金。所以對於黃金玩家是不需要擔心的。
+            </li>
+            <li>
+              在寺廟的南面有一個有<nuxt-link to="/units/Paladin">遊俠</nuxt-link
+              >，兩個<nuxt-link to="/units/Cataphract">精銳聖騎兵</nuxt-link
+              >和幾個帳篷的營地。這是一個訓練薩瓦蘭重騎兵的營地。用1500黃金並點擊主帳篷將獲得10個薩瓦蘭重騎兵。非常建議訓練這些重騎兵並搭配其他<nuxt-link
+                to="/units/Cavalry_Units"
+                >騎兵</nuxt-link
+              >，特別是戰象，然後向白匈奴宣戰。
+            </li>
+            <li>
+              一旦你進入帝王時代就會向白匈奴宣戰，你必須打敗他們。注意他們的<nuxt-link
+                to="/units/Mangudai"
+                >蒙古突騎</nuxt-link
+              >和<nuxt-link to="/units/Hussar">匈牙利輕騎兵</nuxt-link
+              >組合。他們還會訓練戰矛兵，僧侶和一些<nuxt-link
+                to="/units/Trebuchet"
+                >巨型投石機</nuxt-link
+              >。對抗它們的最好方法是訓練<nuxt-link to="/units/Heavy_Camel"
+                >重裝駱駝騎兵</nuxt-link
+              >，戟兵和戰矛兵搭配一些重型騎兵（你也可以購買薩瓦蘭重騎兵，一種重裝且能自行<nuxt-link
+                to="/Regeneration"
+                >回血</nuxt-link
+              >的騎兵）。或者，如果你從上面提到的任務中獲得了大量的黃金，那麼用大規模的薩瓦蘭重騎兵軍隊和戰象的輾過白匈奴也是一個很好的選擇，因為白匈奴人的部隊並不像重型波斯騎兵部隊那樣有精良的裝備。在他們的基地附近蓋幾個<nuxt-link
+                to="/building/Castle"
+                >城堡</nuxt-link
+              >也能幫助你對抗白匈奴人。
+            </li>
+            <li>
+              你也可以與突厥人結盟。突厥人擁有出色的<nuxt-link
+                to="/units/Heavy_Cavalry_Archer"
+                >重裝馬弓騎兵</nuxt-link
+              >和匈牙利輕騎兵，他們會幫助你分散敵人的注意力並襲擊敵人的城鎮。白匈奴人只會嘗試重建他們毀壞的基本軍事<nuxt-link
+                to="/building"
+                >建築</nuxt-link
+              >，如馬厩和<nuxt-link to="/building/Archery_Range"
+                >射箭場</nuxt-link
+              >，但不會嘗試重建他們的城堡或<nuxt-link to="/building/Monastery"
+                >修道院</nuxt-link
+              >。因此，城堡應該是玩家巨型投石機的主要目標，因為白匈奴人不會再重建它們。你可以試圖偷溜一些<nuxt-link
+                to="/units/Petard"
+                >爆炸桶</nuxt-link
+              >炸毀地圖以東的修道院，但這應該在你對白匈奴宣戰後立即進行。一旦你摧毀了他們所有的城堡和他們的軍隊，白匈奴將投降（立即研究<nuxt-link
+                to="/techs/Spies"
+                >間諜</nuxt-link
+              >，因為它只需要200金！）。然而，突厥人這時會背叛你，但他們並不像白匈奴那樣強大，而且很容易被擊敗。一旦他們的基地被摧毀，你就贏得這個戰役！
+            </li>
+          </ul>
+          
+        </div>
+      </div>
+      <div class="col-md-4">
+        <table class="table-bordered" width="100%">
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#375d96;">
+              <div class="unititle2">塞普勒斯(1191)</div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <div class="unititle">
+                <img src="~assets/Cyprus_map.png" alt="" />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#375d96;">
+              <div class="unititle2">戰役資訊</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">版本</td>
+            <td class="unit4">
+              <nuxt-link to="/games/aof">失落的帝國</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">戰役</td>
+            <td class="unit4">
+              <nuxt-link to="/batt/Battles_of_the_Forgotten"
+                >失落的帝國的戰役</nuxt-link
+              >
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3"><nuxt-link to="/civs">文明</nuxt-link></td>
+            <td class="unit4">
+              <nuxt-link to="/civs/Britons">不列顛</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#375d96;">
+              <div class="unititle2">戰役關卡編號</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">章節</td>
+            <td class="unit4">
+              <nuxt-link to="/games/aof">失落的帝國</nuxt-link>第七章之七
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">上一關</td>
+            <td class="unit4">
+               <nuxt-link to="/batt/Kurikara_1183">礪波山(1183)</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">下一關</td>
+            <td class="unit4">
+              <nuxt-link to="/batt/Bapheus_1302">巴菲烏斯(1302)</nuxt-link>
+            </td>
+          </tr>
+        </table>
+
+        <div class="thanks">感謝！此頁面由賜齡、裕暉協助編輯。</div>
+        
+          
+        </div>
+      </div>
+    </div>
+  
+</template>
+<script>
+export default {
+  layout: 'allbatt',
+  components: {}
+}
+</script>
+<style></style>
