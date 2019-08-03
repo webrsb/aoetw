cc5e3ff7801107e5de222f8e63adf7e97d6e94af
bookthief108 <47220722+bookthief108@users.noreply.github.com>
2019-08-03 15:50:54 +0800

    Update Cyprus_1191.vue

--- pages/batt/Cyprus_1191.vue
+++ pages/batt/Cyprus_1191.vue
@@ -24,14 +24,14 @@
             <tr>
               <td class="tc1" width="14%">起始時代：</td>
               <td class="tc2" width="15%">
-                <nuxt-link to="/techs/age/Feudal_Age">封建時代</nuxt-link>
+                <nuxt-link to="/techs/age/Castle_Age">城堡時代</nuxt-link>
               </td>
               <td class="tc1" width="14%">起始資源：</td>
               <td class="tc2">
                 無
               </td>
               <td class="tc1" width="14%">人口上限：</td>
-              <td class="tc2" width="5%">150</td>
+              <td class="tc2" width="5%">200</td>
             </tr>
           </table>
 
@@ -44,10 +44,10 @@
             </tr>
             <tr>
               <td class="tc6">
-                <div class="p4"></div>
+                <div class="p2"></div>
                 玩家1
               </td>
-              <td class="tc6" colspan="3">波斯（黃色）
+              <td class="tc6" colspan="3">不列顛（紅色）
               </td>
             </tr>
 
@@ -59,51 +59,63 @@
                 <div class="p8"></div>
                 玩家2
               </td>
-              <td class="tc6" width="35%">波斯人（橘色）波斯
+              <td class="tc6" width="35%">英軍（橘色）不列顛
               </td>
               <td class="tc6" width="15%">
                 <div class="p1"></div>
                 玩家3
               </td>
-              <td class="tc6" width="35%">呼羅珊人（藍色）印度
+              <td class="tc6" width="35%">菲利浦．奧古斯都（藍色）法蘭克
               </td>
-            </tr>
-
-            <tr>
-              <td class="tc4" colspan="4" style="background:#ea6153;">敵對</td>
             </tr>
             <tr>
               <td class="tc6">
                 <div class="p3"></div>
                 玩家4
               </td>
-              <td class="tc6">白匈奴人（綠色）蒙古
+              <td class="tc6">西西里國王坦克雷德（綠色）義大利
               </td>
             </tr>
+            <tr>
+              <td class="tc4" colspan="4" style="background:#ea6153;">同盟→敵對→同盟</td>
+            </tr>
+            
             <tr>
               <td class="tc6">
-                <div class="p5"></div>
+                <div class="p4"></div>
                 玩家5
               </td>
-              <td class="tc6">突厥人（青色）土耳其
+              <td class="tc6">墨西拿人（黃色）義大利
               </td>
-            </tr>
-            <tr>
+            
               <td class="tc6">
                 <div class="p6"></div>
                 玩家6
               </td>
-              <td class="tc6">白匈奴突襲兵（紫色）匈奴</td>
+              <td class="tc6">C（紫色）拜占庭</td>
+            </tr>
+            <tr>
+              <td class="tc4" colspan="4" style="background:#ea6153;">同盟→敵對</td>
+            </tr>
+            
+            <tr>
+              <td class="tc6">
+                <div class="p7"></div>
+                玩家5
+              </td>
+              <td class="tc6">賽普勒斯人（灰色）義大利
+              </td>
+              <td class="tc6">
+                <div class="p5"></div>
+                玩家6
+              </td>
+              <td class="tc6">利馬索港（青色）拜占庭</td>
             </tr>
           </table>
 
           <h3>開場</h3>
           <li>
-            <nuxt-link to="/units/hero/Khosrau">霍斯勞</nuxt-link>X1，<nuxt-link
-              to="/units/Elephant_Archer"
-              >象弓騎兵</nuxt-link
-            >X3，<nuxt-link to="/units/Light_Cavalry">輕騎兵</nuxt-link
-            >X1，<nuxt-link to="/units/Villager">村民</nuxt-link>X3
+             獅心王理查X、長弓兵X10、投石車X2、衝撞車X3、弩兵X5、騎士X9、長槍兵X10、長劍兵X10
           </li>
 
           <h3>主要任務</h3>
@@ -114,7 +126,7 @@
                 理查必須存活。
               </li>
               <li>
-                瓊那必須存活。(得到瓊那後)
+                瓊那必須存活。(得到瓊那到上船之前)
               </li>
             </ul>
             <ol>
@@ -126,7 +138,7 @@
                 讓理查去會見其他的王公。
               </li>
               <li>
-                把理查和瓊那帶回營地，瓊那跟理查都必須存活。
+                把理查和瓊那帶回營地。
               </li>
               <li>
                 摧毀墨西拿城所有的倉庫(貿易工坊)來佔領墨西拿城(共五座倉庫)。
@@ -148,49 +160,9 @@
               <li>
                 打敗賽普勒斯軍隊。
               </li>
-              <li>
-                。
-              </li>
             </ol>
           </div>
-          <h3>選擇性任務</h3>
-          <div class="hrn1"></div>
-          <div class="n1">
-            <ul>
-              <li>
-                保護波斯村莊。
-              </li>
-              <li>
-                五分鐘之內付給白匈奴人400<nuxt-link to="/resource/Gold"
-                  >黃金</nuxt-link
-                >。
-              </li>
-              <li>
-                五分鐘之內付給白匈奴人600黃金(要先觸發上一條400黃金)。
-              </li>
-              <li>
-                五分鐘之內付給白匈奴人1000黃金(要先觸發上一條600黃金)。
-              </li>
-              <li>
-                派一位使臣到突厥人那締結同盟。
-              </li>
-              <li>
-                摧毀絲路上的所有敵方<nuxt-link to="/building/Watch_Tower"
-                  >箭塔</nuxt-link
-                >並用新的箭塔取代他們。
-              </li>
-              <li>
-                給聖火廟10個村民。
-              </li>
-              <li>
-                給呼羅珊1000<nuxt-link to="/resource/Wood">木材</nuxt-link
-                >換取他們的漁獲供給。
-              </li>
-              <li>
-                你可以點擊主帳篷來訓練10個薩瓦蘭重騎兵(需要1500黃金)。
-              </li>
-            </ul>
-          </div>
+          
           <h3>任務提示</h3>
           <div class="hrn1"></div>
           <div class="n1">
@@ -245,67 +217,29 @@
           <div class="hrn1"></div>
           <ul>
             <li>
-              一開始你應該盡快建立一個小鎮。保護盟軍村莊不被襲擊，以確保你繼續從他們那裡獲得資源並進入帝王時代。在每個淺灘都建造<nuxt-link
-                to="/building/Stone_Wall"
-                >城牆</nuxt-link
-              >或<nuxt-link to="/building/Palisade_Wall">木牆</nuxt-link
-              >阻擋襲擊者的猛攻。建議用一些<nuxt-link to="/units/War_Galley"
-                >弩砲戰船</nuxt-link
-              >來控制河流，並攻擊試圖越過河流的襲擊者和白匈奴人。白色匈奴人會多次要求進貢，只要你沒有體面的軍隊，繼續付錢給他們會是個好主意。稍微忍耐一下吧。
+              玩家一開始在海上有五艘運輸船。向下在西西里島海岸登陸並把軍隊帶往駐紮地。然後玩家會失去軍隊控制權只剩理查一人。理查表示自己要去跟其他王公商討東征大計。這時可以先探索墨西拿城因為待會要破懷城內的五座倉庫(貿易工廠)
+              將理查帶到坦克雷德的城堡前與他會面(綠色)。經過一番折騰辯論後後玩家會獲得瓊那的控制權。把瓊那跟理查帶回原本軍隊處。
             </li>
             <li>
-              如果金礦耗盡了，有很多方法可以在這張地圖上獲得黃金。在波斯人的勢力邊緣，有一座祭拜瑣羅亞斯特的火神殿。給他們10名村民您將獲得他們的<nuxt-link
-                to="/units/Monk"
-                >僧侶</nuxt-link
-              >和遺物。有四座紫色箭塔需要破壞，並用你的箭塔做替換。通過這樣做，您每分鐘還可以獲得額外100金幣，您還可以安全地到東北與呼羅珊市場交易，為您提供更多的黃金。靠近突厥人營地的湖的北部還有一個藍色的<nuxt-link
-                to="/building/Dock"
-                >碼頭</nuxt-link
-              >，在那裡您可以與之交易以獲得更多的黃金。所以對於黃金玩家是不需要擔心的。
+              一回到紮營處玩家會取回軍隊控制權，這時墨西拿城與坦克雷德會變成與玩家敵對。任務變成破壞五座墨西拿城的倉庫(貿易工場)。如果剛剛有先探索那任務會變得簡單許多。將步兵放進衝撞車並用衝撞車破壞城門，
+              用弩兵跟長弓兵掩護衝撞車完成他們的工作。當理查靠近軍事建築物(馬廄、軍營、攻城器製造所、射箭場)時一些會變成玩家的，破壞工廠時墨西拿城會給玩家一些資源。
+              玩家可以用這些資源建造軍隊幫忙打下工廠。破壞最後一座工廠後，墨西拿城會歸降玩家。
             </li>
             <li>
-              在寺廟的南面有一個有<nuxt-link to="/units/Paladin">遊俠</nuxt-link
-              >，兩個<nuxt-link to="/units/Cataphract">精銳聖騎兵</nuxt-link
-              >和幾個帳篷的營地。這是一個訓練薩瓦蘭重騎兵的營地。用1500黃金並點擊主帳篷將獲得10個薩瓦蘭重騎兵。非常建議訓練這些重騎兵並搭配其他<nuxt-link
-                to="/units/Cavalry_Units"
-                >騎兵</nuxt-link
-              >，特別是戰象，然後向白匈奴宣戰。
+              下個目標是渡海，菲利浦．奧古斯都會給玩家一輛貿易馬車(名稱是瓊那的財產)，把理查、瓊那還有貿易馬車帶到紅色旗幟處登船。這時玩家可以繼續創建軍隊或是用貿易馬車到坦克雷德的市場貿易，
+              用黃金交易需要的資源並研發技術升級軍隊。當然玩家也可以直接用原本的軍隊直接上船。集滿50單位上船後點擊坦克雷德的城堡並目送理查的艦隊出航。
             </li>
             <li>
-              一旦你進入帝王時代就會向白匈奴宣戰，你必須打敗他們。注意他們的<nuxt-link
-                to="/units/Mangudai"
-                >蒙古突騎</nuxt-link
-              >和<nuxt-link to="/units/Hussar">匈牙利輕騎兵</nuxt-link
-              >組合。他們還會訓練戰矛兵，僧侶和一些<nuxt-link
-                to="/units/Trebuchet"
-                >巨型投石機</nuxt-link
-              >。對抗它們的最好方法是訓練<nuxt-link to="/units/Heavy_Camel"
-                >重裝駱駝騎兵</nuxt-link
-              >，戟兵和戰矛兵搭配一些重型騎兵（你也可以購買薩瓦蘭重騎兵，一種重裝且能自行<nuxt-link
-                to="/Regeneration"
-                >回血</nuxt-link
-              >的騎兵）。或者，如果你從上面提到的任務中獲得了大量的黃金，那麼用大規模的薩瓦蘭重騎兵軍隊和戰象的輾過白匈奴也是一個很好的選擇，因為白匈奴人的部隊並不像重型波斯騎兵部隊那樣有精良的裝備。在他們的基地附近蓋幾個<nuxt-link
-                to="/building/Castle"
-                >城堡</nuxt-link
-              >也能幫助你對抗白匈奴人。
+              過場動畫後(理查的艦隊遇到暴風雨然後船隻四散)玩家先找尋四散在海上自己的船隻。記得往左上找到利馬索港的時候會觸發劇情，獅心王發現伊薩克．科穆寧(青藍色)綁架了
+              妹妹的船艦並要求贖金。只要觸發劇情就馬上走人否則玩家會被攻擊。現在任務變成佔領賽普勒斯城的兩座城堡。只要佔領兩座城堡賽普勒斯城的所有東西(包括軍隊)就會歸順玩家。
+              玩家有兩條路可選，一條是從剛剛的港口讓國王直接搶灘上岸佔領第一座城堡，之後往第二座城堡過去然後靠近城門，當敵方單位為了打你通過城門時趁機溜進去佔領第二座城堡。
+              另外一個是從左邊的陸地上岸，用衝撞車摧毀城門然後同樣地讓理查佔領城堡。理查只要靠近城堡就能佔領它們。
             </li>
             <li>
-              你也可以與突厥人結盟。突厥人擁有出色的<nuxt-link
-                to="/units/Heavy_Cavalry_Archer"
-                >重裝馬弓騎兵</nuxt-link
-              >和匈牙利輕騎兵，他們會幫助你分散敵人的注意力並襲擊敵人的城鎮。白匈奴人只會嘗試重建他們毀壞的基本軍事<nuxt-link
-                to="/building"
-                >建築</nuxt-link
-              >，如馬厩和<nuxt-link to="/building/Archery_Range"
-                >射箭場</nuxt-link
-              >，但不會嘗試重建他們的城堡或<nuxt-link to="/building/Monastery"
-                >修道院</nuxt-link
-              >。因此，城堡應該是玩家巨型投石機的主要目標，因為白匈奴人不會再重建它們。你可以試圖偷溜一些<nuxt-link
-                to="/units/Petard"
-                >爆炸桶</nuxt-link
-              >炸毀地圖以東的修道院，但這應該在你對白匈奴宣戰後立即進行。一旦你摧毀了他們所有的城堡和他們的軍隊，白匈奴將投降（立即研究<nuxt-link
-                to="/techs/Spies"
-                >間諜</nuxt-link
-              >，因為它只需要200金！）。然而，突厥人這時會背叛你，但他們並不像白匈奴那樣強大，而且很容易被擊敗。一旦他們的基地被摧毀，你就贏得這個戰役！
+              現在你佔領了利馬索港，盡快地發展經濟及訓練部隊。你最後的敵人是賽普勒斯和伊薩克．科穆寧。賽普勒斯有聖騎兵、重裝長槍兵、戰矛兵、衝撞車和巨型投石機的龐大軍隊。
+              如果時間拖得太久它們會變得異常強大(演進到帝王時代而且單位都會變成最強階段)，盡快升級到帝王時代並升級所有需要的科技，用長槍兵對付聖騎兵，騎兵對付戰矛兵，
+              長弓兵加投石車掃射所有視野內的敵軍。你會需要攻城武器摧毀敵人的城堡跟箭塔。伊薩克．科穆寧則有遊俠、戰矛兵、聖騎兵、匈牙利輕騎兵、劍兵勇士、強弩兵，
+              你的軍隊面對它們不會有太多的阻礙，尤其你有很多精銳長弓兵的話。一旦伊薩克．科穆寧最後的城堡被摧毀，理查及妹妹瓊那會走出來並宣布自己成為賽普勒斯的第一任國王，玩家便贏得這個戰役！
             </li>
           </ul>
           
