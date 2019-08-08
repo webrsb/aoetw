1ef2eef6e8b3886a365e3ee7313b2da60e45adc4
bookthief108 <47220722+bookthief108@users.noreply.github.com>
2019-08-08 19:30:19 +0800

    Create Baria.vue

---
+++ pages/batt/Baria.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="container">
+    <div class="row padtop">
+      <div class="col-md-12">
+        <h1>巴里</h1>
+        <div class="hrn1"></div>
+      </div>
+      <div class="col-md-8">
+        <div class="n1">
+          <div class="ien">
+            <em>羅馬滅亡四百年之後，義大利仍是一個群雄割據之地。做為南部的主要海港城，巴里是拜占庭人、歌德人、諾曼人和穆斯林人等的兵家必爭之地。
+            這個戰役講述了一個普通的拜占庭軍人家族在一場場爭奪巴里的戰爭中英勇的表現，和最後成為顯赫家世的傳奇故事。要知道巴里城的爭奪關乎的不僅僅是
+            一座城池，更是整個南義大利的安危</em>
+            <p style="text-align: right;margin-top: 10px;">
+              -遊戲中對於巴里戰役的介紹
+            </p>
+          </div>
+          <p>
+            《世紀帝國II:失落的帝國》中的巴里戰役共有三個場景，描繪了與意大利巴里城市同名的幾個歷史事件。 該戰役以虛構的希臘諾蒂卡斯家族為中心，
+            而“諾蒂卡斯”這個名字則表示其祖先曾是水手。
+
+
+        <h3>英雄</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <p>
+             安德烈亞斯．諾蒂卡斯是這個故事的敘述者，講述了他父親邁克爾．諾蒂卡斯講述的第一和第二場景的故事。 
+            第一個場景的主角帕尼奧斯．諾蒂卡斯是安德烈亞斯的曾祖父，也是邁克爾的祖父。
+          </p>
+        </div>
+        
+        </div>
+        <h3>關卡</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <div class="row">
+            <div class="col-md-12">
+              <p>
+                巴里戰役由3個關卡組成。玩家扮演的是拜占庭人，顏色是藍色的。
+              </p>
+            </div>
+            <div class="col-md-6">
+              <ol>
+                <li>
+                  <nuxt-link to="/batt/Arrival_at_Bari"
+                    >抵達巴里</nuxt-link
+                  >(帕尼奧斯．諾蒂卡斯)
+                </li>
+                <li>
+                  <nuxt-link to="/batt/The_Rebellion_of_Melus"
+                    >邁勒斯叛變</nuxt-link
+                  >(邁克爾．諾蒂卡斯)
+                </li>
+                <li>
+                  <nuxt-link to="/batt/The_Great_Siege">四面楚歌</nuxt-link>(安德烈亞斯．諾蒂卡斯)
+                </li>
+                
+              </ol>
+            </div>
+            <div class="col-md-6">
+              <img src="~assets/Bari.png" width="300" alt="" />
+              <p>巴里戰役地圖</p>
+            </div>
+          </div>
+          <h3>芝麻小事</h3>
+          <div class="hrn1"></div>
+          <div class="n1">
+            
+            <ul>
+              <li>
+                戰役中的三個主角在技術上都是英雄拉海爾，只是擁有三個不同名字。
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <table class="table-bordered" width="100%">
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#375d96;">
+              <div class="unititle" style="color: #fff;">巴里</div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <div class="unititle">
+                <img src="~assets/cover600.jpg" width="300" alt="" />
+                <p><em>本系列戰役發生地 義大利巴里</em></p>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#375d96;">
+              <div class="unititle2" style="color: #fff;">劇情介紹</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">遊戲版本</td>
+            <td class="unit4">
+              <nuxt-link to="/games/aof">失落的帝國</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3"><nuxt-link to="/civs">文明</nuxt-link></td>
+            <td class="unit4">拜占庭</td>
+          </tr>
+          <tr>
+            <td class="unit3"><nuxt-link to="/units/hero">英雄</nuxt-link></td>
+            <td class="unit4">帕尼奧斯．諾蒂卡斯<br>邁克爾．諾蒂卡斯<br>安德烈亞斯．諾蒂卡斯
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">關卡</td>
+            <td class="unit4">3</td>
+          </tr>
+        </table>
+
+        <div class="thanks">感謝! 此頁面由裕暉、賜齡協助編輯</div>
+        <div class="wiki" style="margin-top: 5px;">
+          <div style="float: left;">
+            <img src="~assets/Wikipedia-logo.png" width="50" height="46" />
+          </div>
+          <div style="margin-left: 60px;">
+            維基百科上的巴里
+            <div style="margin-left: 10px;">
+              <i
+                ><b
+                  ><a
+                    href="https://en.wikipedia.org/wiki/Bari"
+                    target="_blank"
+                    >巴里</a
+                  ></b
+                ></i
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+</template>
+<script>
+export default {
+  layout: 'allbatt',
+  components: {}
+}
+</script>
+<style></style>
