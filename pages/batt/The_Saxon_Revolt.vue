f414181005bfe399fc6b3a9b79ee107ca054efaf
bookthief108 <47220722+bookthief108@users.noreply.github.com>
2019-08-08 20:52:12 +0800

    Create The_Saxon_Revolt.vue

---
+++ pages/batt/The_Saxon_Revolt.vue
@@ -0,0 +1,335 @@
+<template>
+  <div class="container">
+    <div class="row padtop">
+      <div class="col-md-12">
+        <h1>撒克遜起義</h1>
+        <div class="hrn1"></div>
+      </div>
+      <div class="col-md-8">
+        <div class="n1">
+          <p>
+            <b>開城</b
+            >是《<nuxt-link to="/games/aof">世紀帝國II：失落的帝國</nuxt-link
+            >》中<nuxt-link to="/batt/Battles_of_the_Forgotten"
+              >失落的帝國的戰役</nuxt-link
+            >的其中一個戰役。該<nuxt-link to="/batt">戰役</nuxt-link
+            >描述獅心王理查在第三次十字軍東征之前佔領西西里島與賽普勒斯的事蹟。
+          </p>
+        </div>
+        <h2>玩家</h2>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <h3>基本資訊</h3>
+          <table class="table-bordered" width="100%">
+            <tr>
+              <td class="tc1" width="14%">起始時代：</td>
+              <td class="tc2" width="15%">
+                <nuxt-link to="/techs/age/Castle_Age">城堡時代</nuxt-link>
+              </td>
+              <td class="tc1" width="14%">起始資源：</td>
+              <td class="tc2">
+                無
+              </td>
+              <td class="tc1" width="14%">人口上限：</td>
+              <td class="tc2" width="5%">200</td>
+            </tr>
+          </table>
+
+          <h3>AI</h3>
+          <table class="table-bordered" width="100%">
+            <tr>
+              <td class="tc4" colspan="4" style="background: #2980b9;">
+                玩家本身
+              </td>
+            </tr>
+            <tr>
+              <td class="tc6">
+                <div class="p2"></div>
+                玩家1
+              </td>
+              <td class="tc6" colspan="3">不列顛（紅色）
+              </td>
+            </tr>
+
+            <tr>
+              <td class="tc4" colspan="4" style="background: #27ae60;">同盟</td>
+            </tr>
+            <tr>
+              <td class="tc6" width="15%">
+                <div class="p8"></div>
+                玩家2
+              </td>
+              <td class="tc6" width="35%">英軍（橘色）不列顛
+              </td>
+              <td class="tc6" width="15%">
+                <div class="p1"></div>
+                玩家3
+              </td>
+              <td class="tc6" width="35%">菲利浦．奧古斯都（藍色）法蘭克
+              </td>
+            </tr>
+            <tr>
+              <td class="tc6">
+                <div class="p3"></div>
+                玩家4
+              </td>
+              <td class="tc6">西西里國王坦克雷德（綠色）義大利
+              </td>
+            </tr>
+            <tr>
+              <td class="tc4" colspan="4" style="background:#ea6153;">同盟→敵對→同盟</td>
+            </tr>
+            
+            <tr>
+              <td class="tc6">
+                <div class="p4"></div>
+                玩家5
+              </td>
+              <td class="tc6">墨西拿人（黃色）義大利
+              </td>
+            
+              <td class="tc6">
+                <div class="p6"></div>
+                玩家6
+              </td>
+              <td class="tc6">（紫色）拜占庭</td>
+            </tr>
+            <tr>
+              <td class="tc4" colspan="4" style="background:#ea6153;">同盟→敵對</td>
+            </tr>
+            
+            <tr>
+              <td class="tc6">
+                <div class="p7"></div>
+                玩家5
+              </td>
+              <td class="tc6">賽普勒斯人（灰色）義大利
+              </td>
+              <td class="tc6">
+                <div class="p5"></div>
+                玩家6
+              </td>
+              <td class="tc6">利馬索港（青色）拜占庭</td>
+            </tr>
+          </table>
+
+          <h3>開場</h3>
+          <li>
+             獅心王理查X、長弓兵X10、投石車X2、衝撞車X3、弩兵X5、騎士X9、長槍兵X10、長劍兵X10
+          </li>
+
+          <h3>主要任務</h3>
+          <div class="hrn1"></div>
+          <div class="n1">
+             <ul>
+              <li>
+                理查必須存活。
+              </li>
+              <li>
+                瓊那必須存活。(得到瓊那到上船之前)
+              </li>
+            </ul>
+            <ol>
+              
+              <li>
+                在西西里登陸並把軍隊派到安營點。
+              </li>
+              <li>
+                讓理查去會見其他的王公。
+              </li>
+              <li>
+                把理查和瓊那帶回營地。
+              </li>
+              <li>
+                摧毀墨西拿城所有的倉庫(貿易工坊)來佔領墨西拿城(共五座倉庫)。
+              </li>
+              <li>
+                把理查、瓊那還有她的財產都帶到紅旗處的運輸船那。
+              </li>
+              <li>
+                把你想派去東征的軍隊都派上五支運輸船。包括英雄在內你一共可以運50個單位，
+                別忘了帶上能幫你攻城拔寨的武器，而且先讓單位坐進衝撞車再把衝撞車運上運輸船的話可以節省空間。
+                如果你的艦隊已經就緒要準備揚帆啟程的話就點擊坦克雷德在地圖西邊的城堡(綠色)。
+              </li>
+              <li>
+                找到艦隊其餘的船艦。
+              </li>
+              <li>
+                登陸利馬索港並奪下她的兩座城堡。想奪下城堡你只需要把理查帶到它旁邊去。
+              </li>
+              <li>
+                打敗賽普勒斯軍隊。
+              </li>
+            </ol>
+          </div>
+          
+          <h3>任務提示</h3>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ul>
+              <li>
+                如果你想佔領一座城池歸為己用的話，別忘了你一開始破壞得越多，最後留下來給你自己的越少。
+              </li>
+              <li>
+                有機會的話多用僧侶替你的軍隊療傷。
+              </li>
+              <li>
+                斥侯一片地域最有效的方法就是把你的單位都分散出去搜索。
+              </li>
+              <li>
+                當你在墨西拿城整軍備戰時，你可以根據自己的需要用市集來買賣各種資源。
+              </li>
+              <li>
+                一開始所有人都是你的同盟，但是別被短暫的和平蒙蔽了雙眼，其實戰火隨時會被點燃...。
+              </li>
+            </ul>
+          </div>
+          <h3>斥候</h3>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ul>
+              <li>
+                獅心王理查(紅色)正在發動他的第三次十字軍東征。首先，理查得先去和他交好的西西里王公們溝通戰備。你的征途開始於西西里島北邊的艦隊裡。
+              </li>
+              <li>
+                西西里的國王坦克雷德(綠色)居住在西邊的城池巴勒莫裡。她是你目前的同盟，也會主持你和王公們的見面會。  
+              </li>
+              <li>
+                菲利浦奧古斯都(藍色)是理查的老對手，但是這一次你們因為共同的目的而走到了一起。他已經在墨西拿城旁邊的西西里安營紮寨了。菲利浦倒不是以英勇善戰出名，
+                但是他的詭計多端，深謀遠慮卻是人人皆知的。
+              </li>
+              <li>
+                墨西拿城(黃色)是西西里島上的一座城池。有高牆防護，和四通八達的海港。他們是你的同盟，但別去挑戰他們的底線。
+              </li>
+              <li>
+                利馬索港(青藍色)是賽普勒斯的一座港口。賽普勒斯位於地圖東邊角。
+              </li>
+              <li>
+                伊薩克．科穆寧(青藍色)是賽普勒斯的國王。他一般居住在王國中部戒備森嚴的尼古西亞城。她是你的盟友，但他一向不怎麼喜歡西歐的皇族勢力。
+              </li>
+              <li>
+                賽普勒斯(青藍色)裡的所有平民及軍隊都聽從伊薩克．科穆寧的領導。
+              </li>
+            </ul>
+          </div>
+
+          <h3>攻略</h3>
+          <div class="hrn1"></div>
+          <ul>
+            <li>
+              玩家一開始在海上有五艘運輸船。向下在西西里島海岸登陸並把軍隊帶往駐紮地。然後玩家會失去軍隊控制權只剩理查一人。理查表示自己要去跟其他王公商討東征大計。這時可以先探索墨西拿城因為待會要破懷城內的五座倉庫(貿易工廠)
+              將理查帶到坦克雷德的城堡前與他會面(綠色)。經過一番折騰辯論後後玩家會獲得瓊那的控制權。把瓊那跟理查帶回原本軍隊處。
+            </li>
+            <li>
+              一回到紮營處玩家會取回軍隊控制權，這時墨西拿城與坦克雷德會變成與玩家敵對。任務變成破壞五座墨西拿城的倉庫(貿易工場)。如果剛剛有先探索那任務會變得簡單許多。將步兵放進衝撞車並用衝撞車破壞城門，
+              用弩兵跟長弓兵掩護衝撞車完成他們的工作。當理查靠近軍事建築物(馬廄、軍營、攻城器製造所、射箭場)時一些會變成玩家的，破壞工廠時墨西拿城會給玩家一些資源。
+              玩家可以用這些資源建造軍隊幫忙打下工廠。破壞最後一座工廠後，墨西拿城會歸降玩家。
+            </li>
+            <li>
+              下個目標是渡海，菲利浦．奧古斯都會給玩家一輛貿易馬車(名稱是瓊那的財產)，把理查、瓊那還有貿易馬車帶到紅色旗幟處登船。這時玩家可以繼續創建軍隊或是用貿易馬車到坦克雷德的市場貿易，
+              用黃金交易需要的資源並研發技術升級軍隊。當然玩家也可以直接用原本的軍隊直接上船。集滿50單位上船後點擊坦克雷德的城堡並目送理查的艦隊出航。
+            </li>
+            <li>
+              這裡要注意的點是瓊那上的那艘船在後面劇情會被抓住，所以裡面的軍隊也會被鎖在利馬索港的運輸船裡(僧侶除外，會待在運輸船附近的岸邊)。直到劇情觸發前玩家都
+              所以玩家要謹慎選擇自己的軍隊上哪艘運輸船。
+            </li>
+            <li>
+              過場動畫後(理查的艦隊遇到暴風雨然後船隻四散)玩家先找尋四散在海上自己的船隻。記得往左上找到利馬索港的時候會觸發劇情，獅心王發現伊薩克．科穆寧(青藍色)綁架了
+              妹妹的船艦並要求贖金。只要觸發劇情就馬上走人否則玩家會被攻擊。現在任務變成佔領賽普勒斯城的兩座城堡。只要佔領兩座城堡賽普勒斯城的所有東西(包括軍隊)就會歸順玩家。
+              玩家有兩條路可選，一條是從剛剛的港口讓國王直接搶灘上岸佔領第一座城堡，之後往第二座城堡過去然後靠近城門，當敵方單位為了打你通過城門時趁機溜進去佔領第二座城堡。
+              另外一個是從左邊的陸地上岸，用衝撞車摧毀城門然後同樣地讓理查佔領城堡。理查只要靠近城堡就能佔領它們。
+            </li>
+            <li>
+              現在你佔領了利馬索港，盡快地發展經濟及訓練部隊。你最後的敵人是賽普勒斯和伊薩克．科穆寧。賽普勒斯有聖騎兵、重裝長槍兵、戰矛兵、衝撞車和巨型投石機的龐大軍隊。
+              如果時間拖得太久它們會變得異常強大(演進到帝王時代而且單位都會變成最強階段)，盡快升級到帝王時代並升級所有需要的科技，用長槍兵對付聖騎兵，騎兵對付戰矛兵，
+              長弓兵加投石車掃射所有視野內的敵軍。你會需要攻城武器摧毀敵人的城堡跟箭塔。伊薩克．科穆寧則有遊俠、戰矛兵、聖騎兵、匈牙利輕騎兵、劍兵勇士、強弩兵，
+              你的軍隊面對它們不會有太多的阻礙，尤其你有很多精銳長弓兵的話。一旦伊薩克．科穆寧最後的城堡被摧毀，伊薩克及瓊那會走出來然後玩家便贏得這個戰役！
+            </li>
+          </ul>
+          
+        </div>
+        <h3>芝麻小事</h3>
+          <div class="hrn1"></div>
+          <ul>
+            <li>玩家可以研發火炮戰船科技(研發化學後)，但是卻不能訓練火炮戰船，所以只是浪費資源!</li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <table class="table-bordered" width="100%">
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#375d96;">
+              <div class="unititle2">撒克遜起義</div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <div class="unititle">
+                <img src="~assets/Saxonrevolt_map.png" alt="" />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#375d96;">
+              <div class="unititle2">戰役資訊</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">版本</td>
+            <td class="unit4">
+              <nuxt-link to="/games/aof">失落的帝國</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">戰役</td>
+            <td class="unit4">
+              <nuxt-link to="/batt/Battles_of_the_Forgotten"
+                >失落的帝國的戰役</nuxt-link
+              >
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3"><nuxt-link to="/civs">文明</nuxt-link></td>
+            <td class="unit4">c韓國
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#375d96;">
+              <div class="unititle2">戰役關卡編號</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">章節</td>
+            <td class="unit4">
+              <nuxt-link to="/games/aof">失落的帝國</nuxt-link>第七章額外關卡(需下載)
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">上一關</td>
+            <td class="unit4">
+               <nuxt-link to="/batt/Langshan_Jiang_919">狼山江(919)</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">下一關</td>
+            <td class="unit4">
+              <nuxt-link to="/batt/Kurikara_1183">礪波山(1183)</nuxt-link>
+            </td>
+          </tr>
+        </table>
+
+        <div class="thanks">感謝！此頁面由賜齡、裕暉協助編輯。</div>
+        
+          
+        </div>
+      </div>
+    </div>
+  
+</template>
+<script>
+export default {
+  layout: 'allbatt',
+  components: {}
+}
+</script>
+<style></style>
