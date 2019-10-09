470d9ba3112bda52ce962399b02d89fa95e7ad88
bookthief108 <47220722+bookthief108@users.noreply.github.com>
2019-10-09 21:12:29 +0800

    Create The_Saxon_Revolt.vue

---
+++ pages/batt/The_Saxon_Revolt.vue
@@ -0,0 +1,416 @@
+<template>
+  <div class="container">
+    <div class="row padtop">
+      <div class="col-md-12">
+        <h1><nuxt-link to="/batt/The_Saxon_Revolt">撒克遜起義</nuxt-link></h1>
+        <div class="hrn1"></div>
+      </div>
+      <div class="col-md-8">
+        <div class="n1">
+          <p>
+            <b>撒克遜起義</b>（德語原稱：Anno 782-Sachsenaufstand）是<nuxt-link
+              to="/batt/Battles_of_the_Forgotten"
+              >失落的帝國戰役</nuxt-link
+            >的隱藏戰役。該戰役的歷史背景是法蘭克國王查理曼大帝與撒克遜公爵維杜金德之間的戰爭。更具體地說，該場景涵蓋了
+            782年在Süntel的戰役和Verden的大屠殺。玩家控制法蘭克人對陣撒克遜人及其盟友。
+          </p>
+        </div>
+        <h2>玩家</h2>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <h3>基本資訊</h3>
+          <table class="table-bordered" width="100%">
+            <tr>
+              <td class="tc1" width="14%">起始時代：</td>
+              <td class="tc2" width="14%">
+                <nuxt-link to="/techs/age/Castle_Age">城堡時代</nuxt-link>
+              </td>
+              <td class="tc1" width="14%">起始資源：</td>
+              <td class="tc2">無</td>
+              <td class="tc1" width="14%">人口上限：</td>
+              <td class="tc2" width="5%">200</td>
+            </tr>
+          </table>
+
+          <h3>AI</h3>
+          <table class="table-bordered" width="100%">
+            <tr>
+              <td class="tc4" colspan="4" style="background: #2980b9;">
+                玩家本身
+              </td>
+            </tr>
+            <tr>
+              <td class="tc6">
+                <div class="p1"></div>
+                玩家1
+              </td>
+              <td class="tc6" colspan="3">查理曼大帝（藍色）法蘭克</td>
+            </tr>
+            <tr>
+              <td class="tc4" colspan="4" style="background: #27ae60;">同盟</td>
+            </tr>
+            <tr>
+              <td class="tc6" width="12%">
+                <div class="p4"></div>
+                玩家2
+              </td>
+              <td class="tc6" width="38%">Verden（黃色）法蘭克</td>
+              <td class="tc6" width="12%">
+                <div class="p4"></div>
+                玩家3
+              </td>
+              <td class="tc6" width="38%">Loyal Saxons（黃色）不列顛</td>
+            </tr>
+            
+            <tr>
+              <td class="tc4" colspan="4" style="background:#ea6153;">敵對</td>
+            </tr>
+            <tr>
+              <td class="tc6">
+                <div class="p3"></div>
+                玩家4
+              </td>
+              <td class="tc6">Engern（墨綠色）哥德</td>
+              <td class="tc6">
+                <div class="p5"></div>
+                玩家5
+              </td>
+              <td class="tc6">Saxony's Nobility（青藍色）條頓</td>
+            </tr>
+            <tr>
+              <td class="tc6">
+                <div class="p2"></div>
+                玩家6
+              </td>
+              <td class="tc6">Westphalians（紅色）不列顛</td>
+              <td class="tc6">
+                <div class="p8"></div>
+                玩家7
+              </td>
+              <td class="tc6">Frisians（橘色）不列顛</td>
+            </tr>
+          </table>
+
+          <div class="col-md-12">
+            <br />
+            <dl class="dl-horizontal">
+              <dt>開場：</dt>
+              <dd>
+              <nuxt-link to="">蓋佐斯爾立</nuxt-link>，
+              <nuxt-link to="/units/Two-Handed_Swordsman">雙手劍兵</nuxt-link>x9，
+              <nuxt-link to="/units/Pikeman">重裝長槍兵</nuxt-link>x6，
+              <nuxt-link to="/units/Knight">騎士/nuxt-link>x6，
+              <nuxt-link to="/units/Cavalier">騎兵</nuxt-link>x1，
+              <nuxt-link to="/units/Paladin">遊俠</nuxt-link>x5，
+              <nuxt-link to="/units/Elite_Skirmisher">戰矛兵</nuxt-link>x3，
+              <nuxt-link to="/units/Skirmisher">矛兵</nuxt-link>x4。
+              </dd>
+              <dt>大地之母單位：</dt>
+              <dd>無。</dd>
+            </dl>
+          </div>
+          <h3>主要任務</h3>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ol>
+              <li>查理曼大帝必須生存。</li>
+              <li>齊格弗里德必須生存，直到他處於盟友的安全保護之下。</li>
+              <li>您必須將3件遺失的遺跡歸還給您的修道院。</li>              
+            </ol>
+          </div>
+          <h3>次要任務</h3>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ol>
+              <li>
+                把鎌倉主城運送過來的糧草車安全送往礪波山城內的宮殿，每輛糧草車的貨物可換1,000黃金。
+              </li>
+              <li>
+                殺死平清房還有他的護衛並奪取燈塔（紀念碑），這樣你的船可以毫髮無傷地通過岩岸。
+              </li>
+              <li>帶一個軍事單位去和北条氏進行同盟談判。</li>
+              <li>
+                帶兩台<nuxt-link to="/units/Trebuchet">投石機</nuxt-link
+                >至北条氏的皇宮以換取他們的同盟。
+              </li>
+            </ol>
+          </div>
+          <h3>任務提示</h3>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ul>
+              <li>
+                通往礪波山地區的關隘大道上已布滿了平家軍，你最好直奔城池而去。
+              </li>
+              <li>
+                平氏軍隊並不知道糧草已經被送往城邦了，但是他們很快會發起瘋狂的進攻。
+              </li>
+              <li>這片海域對你極具戰略意義，但出海口被礁石封鎖住了...</li>
+              <li>
+                由於平氏軍閥經把所有的當地人都招募進軍隊了，他們找不到勞役來為他們建造和修理建築。
+              </li>
+              <li>
+                這場<nuxt-link to="/batt">戰役</nuxt-link
+                >發生在黑火藥武器尚未發明的朝代。
+              </li>
+            </ul>
+          </div>
+          <h3>斥候</h3>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ul>
+              <li>
+                源賴朝已經往礪波山（紫色）派遣了一支援軍（藍色），以協防這片兵家必爭之地，礪波山城高牆聳立，是源賴朝的領地，目前由他的堂弟木曾義仲駐守。
+              </li>
+              <li>
+                北条氏（黃色）在東邊有一座城池，由他們的首領北条時政所掌管。北条時政痛恨平家，只希望在破解礪波山之圍之後加入<nuxt-link
+                  to="/units/hero/Minamoto"
+                  >源氏</nuxt-link
+                >勢力。
+              </li>
+              <li>
+                平家軍（橘色）是你早期的主要對手。想要給礪波山解圍的話，你必須把他們前置的<nuxt-link
+                  to="/building/Siege_Workshop"
+                  >攻城器製造所</nuxt-link
+                >摧毀，注意還有三座軍營在守衛河道。
+              </li>
+              <li>
+                平氏守衛（綠色）在城外四處巡邏，他們的<nuxt-link
+                  to="/units/Villager"
+                  >村民</nuxt-link
+                >為平家提供了糧草供應。
+              </li>
+              <li>
+                平氏的主城京都（青綠色）固若金湯，由大量防衛塔和<nuxt-link
+                  to="/building/Castle"
+                  >城堡</nuxt-link
+                >守衛著，從未被攻破過，倘若<nuxt-link to="/building/Stone_Wall"
+                  >城牆</nuxt-link
+                >被攻破，大批的敵軍就會衝去防守。
+              </li>
+              <li>
+                平家軍的首領是一幫兇殘的武將（紅色），尤其是他們的主公平清盛本人。擒賊擒王，想要取勝，你得想方設法幹掉敵人那些帶頭的武將。
+              </li>
+            </ul>
+          </div>
+          <h3>攻略</h3>
+          <div class="hrn1"></div>
+          <ul>
+            <li>
+              遊戲一開始有十幾個軍事單位與必須保護的貿易車，帶領他們以最快速度進入木曾義仲的城寨，若碰到敵軍則盡量以騎兵引誘，其他單位砍殺的方式解決，能夠減少傷亡。
+            </li>
+            <li>
+              木曾義仲的城寨裡面有不少軍隊，而礦藏都在城外，把軍隊部署在外面以保護你的村民。
+            </li>
+            <li>
+              城寨東邊有一座農場，但是沒有<nuxt-link to="/building/Gate"
+                >城門</nuxt-link
+              >，補上城門好安然採集<nuxt-link to="/resource/Food"
+                >食物</nuxt-link
+              >。
+            </li>
+            <li>
+              先挖取<nuxt-link to="/resource/Stone">石頭</nuxt-link
+              >來蓋城堡，城堡蓋在離礦藏近的地方，或是直接蓋在第一個平家軍軍營的門口，利用城堡來壓制他們。
+            </li>
+            <li>
+              敵人大部分以步兵為主，<nuxt-link to="/units/Cavalry_Archer"
+                >馬弓騎兵</nuxt-link
+              >能夠有效防衛，但是別忘了針對<nuxt-link to="/units/Battering_Ram"
+                >衝撞車</nuxt-link
+              >生產一些步兵。
+            </li>
+            <li>
+              敵人不會生產投石機，所以城堡加上數個<nuxt-link
+                to="/building/Watch_Tower"
+                >箭塔</nuxt-link
+              >，再搭配十幾個<nuxt-link to="/units/Spearman">長槍兵</nuxt-link
+              >就足以防守。
+            </li>
+            <li>
+              木曾義仲基地南方有個金礦地，清除第一個前線基地之後可以在這裡建築城堡防守。
+            </li>
+            <li>
+              北条氏可以協助你清理地圖上的平家守衛，兩台投石機是很好的投資。
+            </li>
+            <li>
+              必須把四個平家軍的前線基地拆除（所有軍事建築），還有五位平家將軍殺死，才能觸發源氏劇情。
+            </li>
+            <li>
+              五位敵方將軍分別是：靠最近的平通盛，南邊的平經正，中央基地城堡前的平知盛，靠近西方基地的平忠度，以及北方與平家守衛一同佔領燈塔的平清房。
+            </li>
+            <li>
+              海戰不是必要的，若要使用海戰來殲滅平家軍的前線基地，則必須先殺死平清房奪取燈塔。
+            </li>
+            <li>
+              源氏帶來的軍隊是很強大的騎兵，擁有8/6的防禦，12的<nuxt-link
+                to="/elements/Attack"
+                >攻擊力</nuxt-link
+              >，還有250的變態<nuxt-link to="/elements/Hit_points"
+                >生命值</nuxt-link
+              >與自然<nuxt-link to="/elements/Regeneration">回血</nuxt-link
+              >。只要他們沒有死，都可以加入下一場戰鬥。
+            </li>
+            <li>京都的防守非常厚實，善用投石機。</li>
+            <li>
+              <nuxt-link to="/units/Naval_Vessels">海軍</nuxt-link
+              >無法傷到京都，注意投資的量。
+            </li>
+          </ul>
+          <h3>遊戲內細節</h3>
+          <div class="hrn1"></div>
+          <ul>
+            <li>
+              遊戲中的＂燈塔＂看起來就是<nuxt-link to="/civs/Koreans"
+                >韓國</nuxt-link
+              >的<nuxt-link to="/building/Wonder">世界奇觀</nuxt-link>。
+            </li>
+            <li>
+              除了京都之外的敵方勢力都不能進入<nuxt-link
+                to="/techs/age/Imperial_Age"
+                >帝王時代</nuxt-link
+              >。
+            </li>
+            <li>
+              電腦很笨，不會出<nuxt-link to="/units/Mangonel">投石車</nuxt-link
+              >。
+            </li>
+            <li>所有宮殿看起來都一樣，但是真正的金閣寺只有在奈良。</li>
+            <li>
+              很神奇的，源氏帶來的軍隊沒有中文名稱，英文名稱是Elite Mounted
+              Samurai，這是很奇怪的名稱，因為遊戲內沒有Mounted
+              Samurai這種單位，而Elite是精銳的意思。
+            </li>
+            <li>
+              木曾義仲把城寨與兵權交給玩家之後似乎就去睡覺了（從地圖上消失）。
+            </li>
+            <li>
+              你可以吸引平清盛遠離京都裡面的紅色世界奇觀，並用你的部隊靠近，屆時世界奇觀會變成你的藍色。
+            </li>
+            <li>北条通常從他們基地的南方出兵，而那裏是最沒有效率的。</li>
+            <li>地圖上的狼全都是恐狼。</li>
+          </ul>
+          <h3>歷史</h3>
+          <div class="hrn1"></div>
+          <ul>
+            <li>
+              該戰役不是由Ensemble Studios編寫的，而是由設計者安德烈亞斯·馬歇德（更名為aMa）編寫的戰役，
+              他根據戰役的自述文件於2000年5月28日完成了戰役。
+            </li>
+            <li>該戰役是2000年微軟舉行的查里曼戰役競賽的獲勝者，也是將其添加到《世紀帝國II：征服者入侵》擴充版的原因。
+            儘管它已包含在遊戲文件中，但在戰役菜單中看不到也無法遊玩，以防止侵犯版權。 因此遊玩該戰役的唯一方法是將戰役
+            文件從光碟的“ Goodies”文件夾移動到遊戲的戰役文件夾。</li>
+            <li>
+              即使該戰役最初只有德語內容和旁白，相應國家/地區也可以從首次發售的擴充版遊戲光碟中獲得其他語言的版本。
+              除了不同的語言版本外，還存在“帝王時代”（.scn），“征服者”（.scx）和“帝國時代II” HD（.aoe2scenario）場景的單獨版本。
+              安德烈亞斯·馬歇德（Andreas Marscheider）本人建議使用《世紀帝國II:帝王世紀》的原始版本，因為那是根據該版本的遊戲所設計的戰役。
+            </li>            
+            
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <table class="table-bordered" width="100%">
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#375d96;">
+              <div class="unititle2">
+                撒克遜起義
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <div class="unititle">
+                <img src="~assets/Saxonrevolt_map.png" alt="" />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#375d96;">
+              <div class="unititle2">情景信息</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">版本</td>
+            <td class="unit4">
+              <nuxt-link to="/games/aok">帝王世紀</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">戰役</td>
+            <td class="unit4"><nuxt-link to="/games/aok">帝王世紀</nuxt-link>戰役擴充版
+              >
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3"><nuxt-link to="/civs">文明</nuxt-link></td>
+            <td class="unit4">
+              <nuxt-link to="/civs/Franks">法蘭克</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#375d96;">
+              <div class="unititle2">
+                戰役關卡編號
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">章節</td>
+            <td class="unit4">
+              <nuxt-link to="/games/aok">帝王世紀</nuxt-link>戰役擴充版
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">上一關</td>
+            <td class="unit4">
+              <nuxt-link to="/batt/">N/A</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">下一關</td>
+            <td class="unit4">
+              <nuxt-link to="/batt/">N/A</nuxt-link>
+            </td>
+          </tr>
+        </table>
+
+        <div class="thanks">感謝！此頁面由賜齡協助編輯。</div>
+
+        <div class="wiki" style="margin-top: 5px;">
+          <div style="float: left;">
+            <img src="~assets/Wikipedia-logo.png" width="50" height="46" />
+          </div>
+          <div style="margin-left: 60px;">
+            維基百科上的<em
+                ><strong
+                  ><a href="https://en.wikipedia.org/wiki/Massacre_of_Verden"
+                    >費爾登大屠殺</a
+                  ></strong
+                ></em
+              >
+            </div>
+            <div style="margin-left: 60px;">
+            維基百科上的<em
+                ><strong
+                  ><a href="https://en.wikipedia.org/wiki/Battle_of_S%C3%BCntel"
+                    ></a
+                  >辛特爾之戰</strong
+                ></em
+              >
+            </div>
+               </div>       
+            </div>
+          </div>
+        </div>
+      
+    
+
+</template>
+<script>
+export default {
+  layout: 'allbatt',
+  components: {}
+}
+</script>
+<style></style>
