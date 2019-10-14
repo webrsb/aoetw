8f5282846df2e0d409cbd84011d508c0e5158b86
bookthief108 <47220722+bookthief108@users.noreply.github.com>
2019-10-14 20:09:26 +0800

    Create de_qa.vue

---
+++ pages/de_qa.vue
@@ -0,0 +1,300 @@
+<template>
+  <div>
+    <div class="jumbotron">
+      <div class="container">
+        <h1 class="display-3">Aoetw</h1>
+        <p>《世紀帝國II:決定版》疑難雜症專區</p>
+      </div>
+    </div>
+    <div class="container">
+      
+      <h3>以下列出最常被問的決定版問題</h3>
+      <div class="row">
+        <div class="col-md-12">
+          <b-btn v-b-toggle.a-1 class="m-1" variant="primary"
+            >1.世紀帝國II目前到底有幾個資料片？</b-btn
+          >
+          <b-collapse id="a-1">
+            <div class="well">
+              <table class="table-bordered table">
+                <tbody>
+                  <tr>
+                    <th>資料片名稱</th>
+                    <th>發布日期</th>
+                    <th>新增文明</th>
+                    <th>簡稱</th>
+                  </tr>
+                  <tr>
+                    <td>
+                      《<nuxt-link
+                        to="/games/aoc"
+                        title="世紀帝國II：征服者入侵"
+                        >世紀帝國II：征服者入侵</nuxt-link
+                      >》
+                    </td>
+                    <td>2000年8月24日</td>
+                    <td>
+                      <nuxt-link to="/civs/Spanish" title="西班牙帝國"
+                        >西班牙</nuxt-link
+                      >，<nuxt-link
+                        to="/civs/Aztecs"
+                        class="mw-redirect"
+                        title="阿茲特克"
+                        >阿茲特克</nuxt-link
+                      >，<nuxt-link
+                        to="/civs/Mayans"
+                        class="mw-redirect"
+                        title="馬雅文明"
+                        >馬雅</nuxt-link
+                      >，<nuxt-link to="/civs/Huns" title="匈人">匈奴</nuxt-link
+                      >，<nuxt-link to="/civs/Koreans" title="李氏朝鮮"
+                        >韓國</nuxt-link
+                      >
+                    </td>
+                    <td>(HD版主程式)(舊版帝王+征服者)</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      《<nuxt-link
+                        to="/games/aof"
+                        title="世紀帝國II：失落的帝國"
+                        >世紀帝國II：失落的帝國</nuxt-link
+                      >》
+                    </td>
+                    <td>2013年11月7日</td>
+                    <td>
+                      <nuxt-link
+                        to="/civs/Indians"
+                        class="mw-redirect"
+                        title="印度歷史"
+                        >印度</nuxt-link
+                      >，<nuxt-link to="/civs/Italians" title="義大利歷史"
+                        >義大利</nuxt-link
+                      >，<nuxt-link
+                        to="/civs/Incas"
+                        class="mw-redirect"
+                        title="印加"
+                        >印加</nuxt-link
+                      >，<nuxt-link to="/civs/Magyars" title="匈牙利歷史"
+                        >馬扎爾</nuxt-link
+                      >，<nuxt-link
+                        to="/civs/Slavs"
+                        class="mw-redirect"
+                        title="斯拉夫"
+                        >斯拉夫</nuxt-link
+                      >
+                    </td>
+                    <td>(HD版資料片1)</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      《<nuxt-link to="/games/ak" title="世紀世紀II：非洲王國"
+                        >世紀帝國II：非洲王國</nuxt-link
+                      >》
+                    </td>
+                    <td>2015年11月5日</td>
+                    <td>
+                      <nuxt-link
+                        to="/civs/Berbers"
+                        class="mw-redirect"
+                        title="柏柏"
+                        >柏柏</nuxt-link
+                      >，<nuxt-link
+                        to="/civs/Malians"
+                        class="mw-redirect"
+                        title="馬利"
+                        >馬利</nuxt-link
+                      >，<nuxt-link
+                        to="/civs/Ethiopians"
+                        class="mw-redirect"
+                        title="衣索比亞"
+                        >衣索比亞</nuxt-link
+                      >，<nuxt-link to="/civs/Portuguese" title="葡萄牙帝國"
+                        >葡萄牙</nuxt-link
+                      >
+                    </td>
+                    <td>(HD版資料片2)</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      《<nuxt-link to="/games/aor"
+                        >世紀帝國II：王者崛起</nuxt-link
+                      >》
+                    </td>
+                    <td>2016年12月20日</td>
+                    <td>
+                      <nuxt-link
+                        to="/civs/Burmese"
+                        class="mw-redirect"
+                        title="緬甸"
+                        >緬甸</nuxt-link
+                      >，<nuxt-link to="/civs/Vietnamese" title="越南"
+                        >越南</nuxt-link
+                      >，<nuxt-link
+                        to="/civs/Khmer"
+                        class="mw-disambig"
+                        title="高棉"
+                        >高棉</nuxt-link
+                      >，<nuxt-link
+                        to="/civs/Malay"
+                        class="mw-disambig"
+                        title="馬來"
+                        >馬來</nuxt-link
+                      >
+                    </td>
+                    <td>(HD版資料片3)</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      《<nuxt-link to="/games/de">
+                        世紀帝國II：決定版 </nuxt-link
+                      >》
+                    </td>
+                    <td>2019年11月15日</td>
+                    <td><nuxt-link
+                        to="/civs/Bulgarians"
+                        class="mw-redirect"
+                        title="保加利亞"
+                        >保加利亞</nuxt-link
+                      >，<nuxt-link to="/civs/Lithuanians" title="立陶宛"
+                        >立陶宛</nuxt-link
+                      >，<nuxt-link
+                        to="/civs/Cumans"
+                        class="mw-disambig"
+                        title="庫曼"
+                        >庫曼</nuxt-link
+                      >，<nuxt-link
+                        to="/civs/Tatars"
+                        class="mw-disambig"
+                        title="韃靼"
+                        >韃靼</nuxt-link
+                      >
+                    </td>
+                    <td>(決定版)</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </b-collapse>
+        </div>
+      </div>
+      <div class="row zo">
+        <div class="col-md-12">
+          <b-btn v-b-toggle.a-2 class="m-1" variant="primary"
+            >2.世紀帝國II DE版要如何購買？我該買哪個組合包？</b-btn
+          >
+          <b-collapse id="a-2">
+            <div class="well">
+              <p>必須使用信用卡購買</p>
+              <p>以下皆為11/15前預購優惠, 11/15上市後會不會變回原價不知道</p>
+              <p>
+                Q1:是否已擁有世紀帝國一代：決定版或想一起購買世紀帝國一代：決定版?<br>
+              
+                是:    
+                <a
+                  href=" https://store.steampowered.com/bundle/11831/  "
+                  >(內含世紀帝國一代：決定版& 世紀帝國二代：決定版, 若已擁有其中一項會自動扣除)</a><br>             
+             
+                否:接Q2
+              </p>
+              <h3></h3>
+              <p>
+              Q2:是否已擁有Age of Empires II (2013)(HD版主程式)或想一起購買Age of Empires II (2013)(HD版主程式)?<br>
+                 是: <a href="https://store.steampowered.com/bundle/11830/"
+                  target="_blank">內含世紀帝國二代：決定版 & Age of Empires II (2013)(HD版主程式, 若已擁有其中一項會自動扣除)</a>
+              <br>              
+                  否:<a href="https://store.steampowered.com/app/813780/"></a><a href="https://i.imgur.com/YExX5qd.png"></a>
+                  許多有購買HD版的人 都有這個-25%的折價卷可以找人贈送給你, 就可以純購買決定版一樣有優惠, 需注意有效期限至 11/15。
+                  若找不到人贈送, 可以到<a href=" https://discord.gg/ymNuMVg"> 詢問</a> 。
+              </p>
+            </div>
+          </b-collapse>
+        </div>
+      </div>
+      <div class="row zo">
+        <div class="col-md-12">
+          <b-btn v-b-toggle.a-3 class="m-1" variant="primary"
+            >3.沒有信用卡怎麼購買？怎麼用超商付款</b-btn
+          >
+          <b-collapse id="a-3">
+            <div class="well">
+              可以使用Steam點數卡購買，使用超商繳費，連結如下
+              <p><a href="https://cdkey.com.tw" target="_blank">閃電發卡</a></p>
+            </div>
+          </b-collapse>
+        </div>
+      </div>
+      
+      <div class="row zo">
+        <div class="col-md-12">
+          <b-btn v-b-toggle.a-6 class="m-1" variant="primary"
+            >4.DE版的電腦配備需求?</b-btn
+          >
+          <b-collapse id="a-6">
+            <div class="well">
+              <h3>Windows 10 限定，有官方繁中，系統需求</h3>
+              <p>
+                最低配備:
+                				需要 64 位元的處理器及作業系統<br>
+                        作業系統: Windows 10 64bit<br>
+                        處理器: Intel Core 2 Duo or AMD Athlon 64x2 5600+<br>
+                        記憶體: 4 GB 記憶體<br>
+                        顯示卡: NVIDIA® GeForce® GT 420 or ATI™ Radeon™ HD 6850<br>
+                        DirectX: 版本：11<br>
+                        網路: 寬頻網際網路連線<br>
+                        儲存空間: 30 GB 可用空間<br>
+              </p>
+            </div>
+          </b-collapse>
+        </div>
+      </div>
+          
+      <div class="row zo">
+        <div class="col-md-12">
+          <b-btn v-b-toggle.a-6 class="m-1" variant="primary"
+            >5.DE版可以跟HD版連線嗎?</b-btn
+          >
+          <b-collapse id="a-6">
+            <div class="well">             
+              <p>
+                不行
+              </p>
+            </div>
+          </b-collapse>
+        </div>
+      </div>
+              
+      <div class="row zo">
+        <div class="col-md-12">
+          <b-btn v-b-toggle.d-4 class="m-1" variant="primary"
+            >6.是否有小樹、格線之類的工作坊?</b-btn
+          >
+          <b-collapse id="d-4">
+            <div class="well">
+              <p>
+                上市後有, 到時候會開放在 <a href="https://www.ageofempires.com/mods">工作坊</a>。
+              </p>
+            </div>
+          </b-collapse>
+        </div>
+      </div>
+      <hr />
+      <blockquote>
+        <p>
+          此頁面感謝Ethan Lee整理<a
+            href="https://www.ptt.cc/bbs/AOE/M.1495194620.A.384.html"
+            target="_blank"
+            >決定版常見問題</a
+          >完成
+        </p>
+      </blockquote>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'foot',
+  components: {}
+}
+</script>
+<style></style>
