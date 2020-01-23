99736d6a16e5f19fbf576e341cbf081321071c9a
bookthief108 <47220722+bookthief108@users.noreply.github.com>
2020-01-23 23:39:13 +0800

    Create Supplies.vue

---
+++ pages/techs/Supplies.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="container">
+    <div class="row padtop">
+      <div class="col-md-12">
+        <h1>供給</h1>
+        <div class="hrn1"></div>
+      </div>
+      <div class="col-md-8">
+        <div class="n1">
+          <p>
+            <b>供給</b>於《世紀帝國II:決定版》首次登場，可於<nuxt-link to="/techs/age/Feudal_Age">封建時代</nuxt-link>在
+            <nuxt-link to="/building/Barracks">軍營</nuxt-link>研發。研發後民兵系的食物成本會減少15。
+          </p>
+        </div>
+        <h3>文明可研發與否</h3>
+        <div class="hrn1"></div>
+        <div class="row">
+          <div class="col-md-12">
+            <div class="n1">
+              <h4>
+                <strong
+                  ><i class="fas fa-check" style="color: green;"></i
+                  ><span style="margin-left: 2px;">可研發</span></strong
+                >:除哥德外的所有文明。
+              </h4>
+              
+            </div>
+          </div>
+        </div>
+        
+        <h3>文明加成</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <ul>
+            <li>
+              <nuxt-link to="/civs/Chinese">中國</nuxt-link>：該科技在封建/城堡/帝王時代
+              >花費便宜10%/15%/20%。
+            </li>
+            <li>
+              <nuxt-link to="/civs/Slavs">斯拉夫</nuxt-link>：免費獲得供給。
+            </li>
+          </ul>
+        </div>
+        <h3>團隊加分</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <ul>
+            <li>
+              <nuxt-link to="/civs/Goths">哥德</nuxt-link
+              >：該科技研發時間快20%
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <table class="table-bordered" width="100%">
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#31b0d5;">
+              <div class="unititle2">供給</div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <div class="unititle">
+                <img src="~assets/Suplliesicon.jpg" width="45" alt="" />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#31b0d5;">
+              <div class="unititle2">
+                <nuxt-link to="/techs">科技介紹</nuxt-link>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">文明</td>
+            <td class="unit4">除哥德外所有文明</td>
+          </tr>
+          <tr>
+            <td class="unit3">時代</td>
+            <td class="unit4">
+              <img
+                class="gg"
+                src="~assets/Feudal-age-reseach.jpg"
+                alt=""
+              /><nuxt-link to="/techs/age/Feudal_Age">封建時代</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">研發於</td>
+            <td class="unit4">
+              <img
+                class="gg"
+                src="~assets/Barracks-logo.jpg"
+                alt=""
+              /><nuxt-link to="/building/Barracks">軍營</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">研發時間</td>
+            <td class="unit4">35秒</td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#31b0d5;">
+              <div class="unititle2">成本</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">
+              <nuxt-link to="/resource/Food">食物</nuxt-link>
+            </td>
+            <td class="unit4">150</td>
+          </tr>
+          <tr>
+            <td class="unit3">
+              <nuxt-link to="/resource/Gold">黃金</nuxt-link>
+            </td>
+            <td class="unit4">100</td>
+          </tr>          
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#31b0d5;">
+              <div class="unititle2">影響</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">影響</td>
+            <td class="unit4">
+              民兵系食物成本-15。
+            </td>
+          </tr>
+        </table>
+
+        <div class="thanks">感謝! 此頁面由賜齡協助編輯</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'alltech',
+  components: {}
+}
+</script>
+<style></style>
