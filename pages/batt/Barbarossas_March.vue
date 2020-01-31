fd07cc15668a6b05539919dab2897454fb5535d2
harry3305 <52451150+harry3305@users.noreply.github.com>
2020-01-31 20:44:49 +0800

    Update Barbarossas_March.vue (#278)
    * Update Barbarossas_March.vue
    
    這關幾乎沒變 只有小修關卡名
    
    * Update Barbarossas_March.vue
    
    Co-authored-by: webrsb <webrsb@gmail.com>

--- pages/batt/Barbarossas_March.vue
+++ pages/batt/Barbarossas_March.vue
@@ -2,13 +2,14 @@
   <div class="container">
     <div class="row padtop">
       <div class="col-md-12">
-        <h1>紅鬍子的行軍</h1>
+        <h1>巴巴羅薩行軍</h1>
         <div class="hrn1"></div>
       </div>
       <div class="col-md-8">
         <div class="n1">
           <p>
-            <b>紅鬍子的行軍</b>為<nuxt-link to="/batt/Barbarossa"
+            <b>巴巴羅薩行軍</b>（舊譯：<b>紅鬍子的行軍</b>）為<nuxt-link
+              to="/batt/Barbarossa"
               >巴巴羅薩戰役</nuxt-link
             >的第五關劇情關卡。
           </p>
@@ -108,7 +109,7 @@
           <tr>
             <td class="unit" colspan="2" style="background-color:#375d96;">
               <div class="unititle2">
-                <nuxt-link to="/batt/Barbarossas_March">紅鬍子的行軍</nuxt-link>
+                <nuxt-link to="/batt/Barbarossas_March">巴巴羅薩行軍</nuxt-link>
               </div>
             </td>
           </tr>
@@ -156,7 +157,7 @@
           <tr>
             <td class="unit3">上一關</td>
             <td class="unit4">
-              <nuxt-link to="/batt/The_Lombard_League">倫巴聯盟</nuxt-link>
+              <nuxt-link to="/batt/The_Lombard_League">倫巴底聯盟</nuxt-link>
             </td>
           </tr>
           <tr>
