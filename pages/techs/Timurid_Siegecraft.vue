d4078dc19af7d4ad03ffe90427bb5c6689fa8827
singsing612 <62696812+singsing612@users.noreply.github.com>
2020-03-29 15:55:55 +0800

    Create Timurid_Siegecraft.vue
    韃靼帝王時代特殊科技帖木兒攻城術

---
+++ pages/techs/Timurid_Siegecraft.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="container">
+    <div class="row padtop">
+      <div class="col-md-12">
+        <h1>帖木兒攻城術</h1>
+        <div class="hrn1"></div>
+      </div>
+      <div class="col-md-8">
+        <div class="n1">
+          <p>
+            <b>帖木兒攻城術</b>是<nuxt-link to="/civs/Tatars"
+            >韃靼</nuxt-link>的<nuxt-link to="/techs/untech">特殊科技</nuxt-link
+            >，可於<nuxt-link to="/techs/age/Imperial_Age">帝王時代</nuxt-link
+            >在<nuxt-link to="/building/Castle">城堡</nuxt-link
+            >研發。研發後<nuxt-link to="/units/Trebuchet">投石機</nuxt-link
+            >+1射程。
+          </p>
+        </div>
+        <h3>戰術要點</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <ul>
+            <li>
+              韃靼本身已經擁有<nuxt-link to="/techs/Siege_Engineers"
+            >攻城工程師</nuxt-link>，可以增加<nuxt-link to="/units/Siege_Weapons"
+            >攻城單位</nuxt-link>的射程和對建築物傷害，加上對低地建築物傷害+ 50％的文明加成，韃靼<nuxt-link to="/units/Trebuchet"
+            >投石機</nuxt-link>本身已經很強。全滿的投石機會成為遊戲中射程最遠的單位（總共18射程），投石機也可以更自由地安裝。而韃靼應先在<nuxt-link to="/building/University">大學</nuxt-link>研發攻城工程師，因為它可以給投石機+1射程，並且對建築物有高的傷害，同時也會影響其他攻城單位。
+            </li>
+          </ul>
+        </div>
+        <h3>團隊加分</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <ul>
+            <li>
+              <nuxt-link to="/civs/Berbers">柏柏</nuxt-link>：<nuxt-link
+                to="/techs/Kasbah"
+                >聚居堡壘</nuxt-link
+              >能夠加速絲綢之路的研究時間(快25%)。
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <table class="table-bordered" width="100%">
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#31b0d5;">
+              <div class="unititle2">帖木兒攻城術</div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <div class="unititle">
+                <img
+                  src="~assets/unique_tech_imperial.png"
+                  class="img-fluid"
+                  alt=""
+                />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#31b0d5;">
+              <div class="unititle2">
+                <nuxt-link to="/techs">科技介紹</nuxt-link>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3"><nuxt-link to="/civs">文明</nuxt-link></td>
+            <td class="unit4">
+              <nuxt-link to="/civs/Tatars">韃靼</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">時代</td>
+            <td class="unit4">
+              <img class="gg" src="~assets/imperial_age.png" alt="" /><nuxt-link
+                to="/techs/age/Imperial_Age"
+                >帝王時代</nuxt-link
+              >
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">研發於</td>
+            <td class="unit4">
+              <img class="gg" src="~assets/castle.png" alt="" /><nuxt-link
+                to="/building/Castle"
+                >城堡</nuxt-link
+              >
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">研發時間</td>
+            <td class="unit4">50秒</td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#31b0d5;">
+              <div class="unititle2">成本</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">
+              <nuxt-link to="/resource/Wood">木材</nuxt-link>
+            </td>
+            <td class="unit4">400</td>
+          </tr>
+          <tr>
+            <td class="unit3">
+              <nuxt-link to="/resource/Gold">黃金</nuxt-link>
+            </td>
+            <td class="unit4">500</td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#31b0d5;">
+              <div class="unititle2">影響</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">影響</td>
+            <td class="unit4">
+              <nuxt-link to="/units/Trebuchet">投石機</nuxt-link>+1射程。
+            </td>
+          </tr>
+        </table>
+
+        <div class="thanks">感謝! </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'alltech',
+  components: {}
+}
+</script>
+<style></style>
