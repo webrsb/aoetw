255c3194880567caad1f412f58cec8e1db253dfd
singsing612 <62696812+singsing612@users.noreply.github.com>
2020-04-05 21:08:00 +0800

    Update Long_Swordsman.vue
    保加利亞的協議特性需更新相關頁面

--- pages/units/Long_Swordsman.vue
+++ pages/units/Long_Swordsman.vue
@@ -177,7 +177,14 @@
                 width="30"
                 alt=""
               /><nuxt-link to="/techs/Plate_Mail_Armor">步兵板甲</nuxt-link
-              >(+1/+2)
+              >(+1/+2)<br />
+              <img
+                class="gg"
+                src="~assets/unique_tech_imperial.png"
+                width="30"
+                alt=""
+              /><nuxt-link to="/techs/Druzhina">協議</nuxt-link
+              >:步兵近戰防禦+5，<nuxt-link to="/civs/Bulgarians">保加利亞</nuxt-link>限定。<br />
             </td>
           </tr>
           <tr>
@@ -410,6 +417,10 @@
               >：長劍兵生產速度提高15％。
             </li>
             <li>
+              <nuxt-link to="/civs/Bulgarians">保加利亞</nuxt-link
+              >：長劍兵近戰防禦+5。
+            </li>
+            <li>
               <nuxt-link to="/civs/Burmese">緬甸</nuxt-link
               >：長劍兵在封建/<nuxt-link to="/techs/age/Castle_Age"
                 >城堡</nuxt-link
@@ -614,8 +625,11 @@
           </h3>
           <ul>
             <li>
+              <nuxt-link to="/civs/Bulgarians">保加利亞</nuxt-link>:引入<nuxt-link to="/techs/Bagains">協議</nuxt-link>，裝甲步兵近戰防禦+5。
+            </li>
+            <li>
               <nuxt-link to="/civs/Teutons">條頓</nuxt-link>
-              (35584更新)：長劍兵近戰防禦+1
+              (35584更新)：長劍兵近戰防禦+1。
             </li>
             <li>
               <nuxt-link to="/civs/Goths">哥德</nuxt-link
