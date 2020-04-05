1fbb21256f35336dd31e7cac80bec24a339e9278
singsing612 <62696812+singsing612@users.noreply.github.com>
2020-04-05 16:46:00 +0800

    Create Bagains.vue (#301)
    保加利亞帝王時代特殊科技協議

---
+++ pages/techs/Bagains.vue
@@ -0,0 +1,175 @@
+<template>
+  <div class="container">
+    <div class="row padtop">
+      <div class="col-md-12">
+        <h1>協議</h1>
+        <div class="hrn1"></div>
+      </div>
+      <div class="col-md-8">
+        <div class="n1">
+          <p>
+            <b>協議</b>是<nuxt-link to="/civs/Bulgarians">保加利亞</nuxt-link
+            >的<nuxt-link to="/techs/untech">特殊科技</nuxt-link
+            >，可於<nuxt-link to="/techs/age/Imperial_Age">帝王時代</nuxt-link
+            >在<nuxt-link to="/building/Castle">城堡</nuxt-link
+            >研發。研發後<nuxt-link to="/units/Militia">民兵系</nuxt-link
+            ><nuxt-link to="/armor/Armor_Class_Infantry">近程護甲</nuxt-link
+            >+5。
+          </p>
+        </div>
+        <h3>戰術要點</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <ul>
+            <li>
+              保加利亞没有<nuxt-link to="/units/Champion">劍兵勇士</nuxt-link
+              >，但這項科技使他們的<nuxt-link to="/units/Two-Handed_Swordsman"
+                >雙手劍兵</nuxt-link
+              >擁有+8<nuxt-link to="/armor/Armor_Class_Infantry">近防</nuxt-link
+              >。而這使没有<nuxt-link to="/techs/Blast_Furnace"
+                >鼓風爐</nuxt-link
+              >的<nuxt-link to="/units/Halberdier">戟兵</nuxt-link>、<nuxt-link
+                to="/units/Light_Cavalry"
+                >輕騎兵</nuxt-link
+              >、<nuxt-link to="/units/Hussar">匈牙利輕騎兵</nuxt-link
+              >和所有<nuxt-link to="/units/Pikeman">重裝長槍兵</nuxt-link
+              >、<nuxt-link to="/units/Scout_Cavalry">斥候</nuxt-link
+              >對雙手劍兵只有1點傷害，這使他們在對付垃圾兵中佔有巨大優勢。
+            </li>
+            <li>
+              擁有協議的雙手劍兵在等量上可以完勝全滿的劍兵勇士，除了<nuxt-link
+                to="/civs/Aztecs"
+                >阿茲特克</nuxt-link
+              >、<nuxt-link to="/civs/Burmese">緬甸</nuxt-link>、<nuxt-link
+                to="/civs/Teutons"
+                >條頓</nuxt-link
+              >、<nuxt-link to="/civs/Vikings">維京</nuxt-link>、<nuxt-link
+                to="/civs/Slavs"
+                >斯拉夫</nuxt-link
+              >（詳見<nuxt-link to="/techs/Druzhina">王家侍從</nuxt-link
+              >）和<nuxt-link to="/civs/Japanese">日本</nuxt-link
+              >。在科技全滿的情況下，保加利亞雙手劍兵擁有所有單位中第三高的近防，僅次於+9近防的精銳<nuxt-link
+                to="/units/Boyar"
+                >貴族鐵騎</nuxt-link
+              >和+13近防的精銳<nuxt-link to="/units/Teutonic_Knight"
+                >條頓武士</nuxt-link
+              >。
+            </li>
+            <li>
+              但是，極高的近防仍不敵會無視近防的<nuxt-link to="/units/Leitis"
+                >烈堤司</nuxt-link
+              >，還有對<nuxt-link to="/units/Infantry_Units">步兵單位</nuxt-link
+              >有額外傷害的<nuxt-link to="/units/Cataphract"
+                >拜占庭聖騎兵</nuxt-link
+              >和<nuxt-link to="/units/Jaguar_Warrior">豹勇士</nuxt-link>。
+            </li>
+          </ul>
+        </div>
+        <h3>團隊加分</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <ul>
+            <li>
+              <nuxt-link to="/civs/Berbers">柏柏</nuxt-link>：<nuxt-link
+                to="/techs/Kasbah"
+                >聚居堡壘</nuxt-link
+              >能夠加速協議的研究時間(快25%)。
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <table class="table-bordered" width="100%">
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#31b0d5;">
+              <div class="unititle2">協議</div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <div class="unititle">
+                <img
+                  src="~assets/unique_tech_imperial.png"
+                  class="img-fluid"
+                  alt=""
+                />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#31b0d5;">
+              <div class="unititle2">
+                <nuxt-link to="/techs">科技介紹</nuxt-link>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3"><nuxt-link to="/civs">文明</nuxt-link></td>
+            <td class="unit4">
+              <nuxt-link to="/civs/Bulgarians">保加利亞</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">時代</td>
+            <td class="unit4">
+              <img class="gg" src="~assets/imperial_age.png" alt="" /><nuxt-link
+                to="/techs/age/Imperial_Age"
+                >帝王時代</nuxt-link
+              >
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">研發於</td>
+            <td class="unit4">
+              <img class="gg" src="~assets/castle.png" alt="" /><nuxt-link
+                to="/building/Castle"
+                >城堡</nuxt-link
+              >
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">研發時間</td>
+            <td class="unit4">40秒</td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#31b0d5;">
+              <div class="unititle2">成本</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">
+              <nuxt-link to="/resource/Food">食物</nuxt-link>
+            </td>
+            <td class="unit4">900</td>
+          </tr>
+          <tr>
+            <td class="unit3">
+              <nuxt-link to="/resource/Gold">黃金</nuxt-link>
+            </td>
+            <td class="unit4">450</td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#31b0d5;">
+              <div class="unititle2">影響</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">影響</td>
+            <td class="unit4">
+              民兵系近程護甲+5。
+            </td>
+          </tr>
+        </table>
+
+        <div class="thanks">感謝! singsing612</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'alltech',
+  components: {}
+}
+</script>
+<style></style>
