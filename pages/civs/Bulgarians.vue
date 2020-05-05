022b13525c8ac93e09cac0e413b917c2675fb339
singsing612 <62696812+singsing612@users.noreply.github.com>
2020-05-05 17:13:01 +0800

    Create Bulgarians.vue
    新增保加利亞文明，缺圖片、缺科技樹

---
+++ pages/civs/Bulgarians.vue
@@ -0,0 +1,269 @@
+<template>
+  <div class="container">
+    <div class="row padtop">
+      <div class="col-md-12">
+        <h1>保加利亞</h1>
+        <div class="hrn1"></div>
+      </div>
+      <div class="col-md-8">
+        <div class="n1">
+          <p>
+            <b>保加利亞</b>是東歐的<nuxt-link to="/units/Infantry_Units"
+              >步兵</nuxt-link
+            >和<nuxt-link to="/units/Cavalry_Units">騎兵文明</nuxt-link
+            >，是《<nuxt-link to="/games/de">世紀帝國II：決定版</nuxt-link
+            >》新增的文明。
+          </p>
+          <p>
+            保加利亞人擁有精湛的鐵器鍛造、陶器和木匠技術，使得他們的<nuxt-link to="/building/Blacksmith">兵工廠</nuxt-link
+            >效率提升和擁有<nuxt-link to="/units/Militia">民兵</nuxt-link
+            >系免費升級。在軍事上，保加利亞軍的核心力量在於<nuxt-link to="/units/Cavalry_Units">騎兵</nuxt-link
+            >，其中包括12000到30000名重裝騎兵，在歐洲也是最強大的軍事力量之一。這在其<nuxt-link to="/units/Unique_Unit">特殊單位</nuxt-link
+            ><nuxt-link to="/units/Konnik">保加利亞騎兵</nuxt-link
+            >中足夠反映出來。
+而在<nuxt-link to="/units/Infantry_Units">步兵</nuxt-link
+            >力量中，保加利亞軍擁有良好的步兵組織，使得他們在戰鬥中能受到更大的近戰傷害，這讓他們擁有<nuxt-link to="/techs/untech">特殊科技</nuxt-link
+            ><nuxt-link to="/techs/Bagains">協議</nuxt-link
+            >，為民兵系提升<nuxt-link to="/armor">近程護甲</nuxt-link
+            >。防禦上，保加利亞人在中世紀時以堅守山口、利用地形和防禦工事多次擊敗強大的對手，使他們在建造<nuxt-link to="/building/Town_Center">城鎮中心</nuxt-link
+            >時能節省50％的<nuxt-link to="/resourses/Stone">石頭</nuxt-link
+            >，並且還可以建造小城堡<nuxt-link to="/building/Krepost">營壘</nuxt-link
+            >。
+          </p>
+          <p>
+            在科技方面，保加利亞人和附近的游牧部落把<nuxt-link to="/techs/Stirrups">馬鐙</nuxt-link
+            >傳入了歐洲，從而改變了歐洲騎兵的作戰方式，因此保加利亞擁有城堡時代的特殊科技馬鐙來提高騎兵的攻速。
+          </p>
+        </div>
+        <h3>概要</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <p>
+            保加利亞是<nuxt-link to="/units/Infantry_Units">步兵</nuxt-link
+            >和<nuxt-link to="/units/Cavalry_Units">騎兵文明</nuxt-link
+            >，因此在步兵和騎兵上都十分優秀的，也在其特殊單位中得到了充分體現(具有步兵和騎兵兩種模式)。騎兵上擁有除<nuxt-link to="/units/Paladin">遊俠</nuxt-link
+            >升級外的所有單位升級和科技，加上特殊科技<nuxt-link to="/techs/Stirrups">馬鐙</nuxt-link
+            >提高了所有騎兵攻擊速度。步兵上雖然缺乏<nuxt-link to="/units/Champion">劍兵勇士</nuxt-link
+            >升級，但<nuxt-link to="/units/Militia">民兵</nuxt-link
+            >系免費升級，而且擁有特殊科技<nuxt-link to="/techs/Bagains">協議</nuxt-link
+            >的額外<nuxt-link to="/armor">近戰裝甲</nuxt-link
+            >加成。另外，無論是步兵還是騎兵，都得益於<nuxt-link to="/building/Blacksmith">兵工廠</nuxt-link
+            >工作效益提高的團隊加分。<nuxt-link to="/units/Siege_Weapons">攻城器單位</nuxt-link
+            >上，除<nuxt-link to="/units/Bombard_Cannon">火炮</nuxt-link
+            >外擁有所有單位升級。然而，保加利亞的<nuxt-link to="/units/Archer_Units">弓兵</nuxt-link
+            >系統是最爛的一塊，弓兵連<nuxt-link to="/units/Crossbowman">弩兵</nuxt-link
+            >都無法升上，也缺乏<nuxt-link to="/techs/Ring_Archer_Armor">弓兵鏈甲</nuxt-link
+            >。其<nuxt-link to="/units/Naval_Vessels">海軍</nuxt-link
+            >和<nuxt-link to="/units/Monk">僧侶</nuxt-link
+            >也平淡無奇，但保加利亞可以建造特殊建築<nuxt-link to="/building/Krepost">營壘</nuxt-link
+            >（小城堡，可以訓練<nuxt-link to="/units/Konnik">保加利亞騎兵</nuxt-link
+            >），因此防禦上還算不錯的，但也缺乏一些重要的防禦科技。經濟上缺乏<nuxt-link to="/techs/Two-Man_Saw">雙人鋸</nuxt-link
+            >和<nuxt-link to="/techs/Guilds">公會制度</nuxt-link
+            >，但擁有<nuxt-link to="/building/Town_Center">城鎮中心</nuxt-link
+            >50%<nuxt-link to="/resourses/Stone">石頭</nuxt-link
+            >減免的文明加成。值得一提的是，沒有圍牆的保加利亞將難以應付大量的<nuxt-link to="/units/Arbalest">強弩兵</nuxt-link
+            >或<nuxt-link to="/units/Cavalry_Archer">馬弓騎兵</nuxt-link
+            >。
+          </p>
+          <h3>特殊單位</h3>
+          <li>
+            <img class="gg" src="~assets/Konnikicon.png" alt="" /><nuxt-link
+              to="/units/Konnik"
+              >保加利亞騎兵</nuxt-link
+            >：死亡後會變成步兵繼續作戰的的騎兵單位。
+          </li>
+          <h3>特殊建築</h3>
+          <li>
+            <img class="gg" src="~assets/krepost.png" alt="" /><nuxt-link
+              to="/building/Krepost"
+              >營壘</nuxt-link
+            >：軍事防禦建築。城堡的縮小版，可以訓練保加利亞騎兵。
+          </li>
+          <h3>特殊科技</h3>
+          <li>
+            <img class="gg" src="~assets/unique_tech.png" alt="" /><nuxt-link
+              to="/techs/Stirrups"
+              >馬鐙</nuxt-link
+            >：騎兵單位攻速+25%。
+          </li>
+          <li>
+            <img
+              class="gg"
+              src="~assets/unique_tech_imperial.png"
+              alt=""
+            /><nuxt-link to="/techs/Bagains">協議</nuxt-link
+            >：民兵系近程護甲+5。
+          </li>
+        </div>
+        <h3>文明加成</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <ul>
+            <li><nuxt-link to="/units/Militia">民兵</nuxt-link
+            >系升級免費</li>
+            <li>
+              <nuxt-link to="/building/Town_Center">城鎮中心</nuxt-link
+            ><nuxt-link to="/resourses/Stone">石頭</nuxt-link
+            >成本減免50%
+            </li>
+            <li><nuxt-link to="/techs/age/Castle_Age">城堡時代</nuxt-link
+            >能建造<nuxt-link to="/building/Krepost">營壘</nuxt-link
+            ></li>
+            <li>
+              <strong
+                ><nuxt-link to="/elements/Team_Bonus"
+                  >團隊加分</nuxt-link
+                ></strong
+              >:<nuxt-link to="/building/Blacksmith">兵工廠</nuxt-link
+            >工作效率+50%
+            </li>
+          </ul>
+        </div>
+        <h3>科技樹</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <em><nuxt-link to="/tree/por">保加利亞科技樹</nuxt-link></em>
+        </div>
+        <br />
+        <h3>更新日誌</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <h3>
+            <em><nuxt-link to="/games/de">決定版</nuxt-link></em>
+          </h3>
+          <ul>
+            <li>(34055更新) 協議提供+3的近程護甲提升為+5</li>
+            <li>
+              (34055更新) 下馬後的保加利亞騎兵從1.9攻速提升為2.4， (精鋭)保加利亞騎兵減至100 (120)HP。
+            </li>
+            <li>(34055更新) 下馬後的保加利亞騎兵會受到日本武士的額外傷害。</li>
+            <li>(34055更新) 營壘尺寸由4x4改為3x3，且不能被招降。</li>
+            <li>(36906更新) 無法訓練遊俠，馬鐙也能影響騎士系。</li>
+          </ul>
+        </div>
+        <h3>趣事</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <ul>
+            <li>保加利亞的文明徽章來自於希什曼家族的家徽，而希什曼家族在中世紀時統治著保加利亞第二帝國。</li>
+            <li>
+              與<nuxt-link to="/civs/Spanish">西班牙</nuxt-link
+            >一樣，保加利亞是唯二沒有弩兵的文明。
+            </li>
+            <li>
+              36906更新後，保加利亞成為了唯一被奪走升級遊俠的文明。
+            </li>
+          </ul>
+        </div>
+        <h3>圖庫</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <div class="row">
+            <div class="col-md-4">
+              <gallery
+                ><img
+                  class="imgn hg"
+                  src="~assets/Portuguese_preview.jpg"
+                  alt=""
+              /></gallery>
+              <p><em>保加利亞世界奇觀 普雷斯拉夫圓頂教堂</em></p>
+            </div>
+          </div>
+        </div>
+        <h3>歷史</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <p>
+            公元670年左右，已故的保加利亞首領的兒子阿斯帕魯克帶領着人民渡過多瑙河尋找新的牧場。隨後阿斯帕魯克在他的統治下結合了遷移過來的斯拉夫人。由於拜占庭帝國本身佔領着該地區，君士坦丁四世因此發起了幾次戰爭但都失敗，以致於被迫放棄了巴爾幹山脈以北的土地，這也標誌著保加利亞第一帝國（681-1018）的誕生。後來在保加利亞大公西蒙一世（893-927年）的統治下，保加利亞第一帝國進入了黃金時代，幾乎控制了整個巴爾幹半島。在藝術、文學、文化、經濟都取得蓬勃發展。西蒙死後，帝國因持續不斷的戰爭而衰弱，在1018年被拜占庭帝國征服。雖然後來成功驅逐了拜占庭人並成立了保加利亞第二帝國（1186-1396），國力和疆土都達到鼎盛，但在1396年後，保加利亞第二帝國被奧斯曼帝國征服，成為奧斯曼帝國的一個行省，直到500年後。
+          </p>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <table class="table-bordered" width="100%">
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#e6992c;">
+              <div class="unititle2">保加利亞</div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <div class="unititle">
+                <img src="~assets/Belem.jpg" width="250" alt="" />
+                <p><em>位於保加利亞大普雷斯拉夫的普雷斯拉夫圓頂教堂</em></p>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#e6992c;">
+              <div class="unititle2">概要</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">建築風格</td>
+            <td class="unit4">東歐</td>
+          </tr>
+          <tr>
+            <td class="unit3">分類文明</td>
+            <td class="unit4">
+              <nuxt-link to="/units/Infantry_Units">步兵</nuxt-link>與<nuxt-link
+                to="/units/Cavalry_Units"
+                >騎兵</nuxt-link
+              >
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">
+              <nuxt-link to="/units/Unique_Unit">特殊單位</nuxt-link>
+            </td>
+            <td class="unit4">
+              <img class="gg" src="~assets/Konnikicon.png" /><nuxt-link
+                to="/units/Konnik"
+                >保加利亞騎兵</nuxt-link
+              >
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">特殊建築</td>
+            <td class="unit4">
+              <img class="gg" src="~assets/krepost.png" /><nuxt-link
+                to="/building/Krepost"
+                >營壘</nuxt-link
+              >
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">首選戰略</td>
+            <td class="unit4">
+              <nuxt-link to="/ar/armstower">裝甲開局</nuxt-link
+            >、<nuxt-link to="/ar/armstar">裝甲塔</nuxt-link
+            >
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">特殊技術</td>
+            <td class="unit4">
+              <img class="gg" src="~assets/unique_tech.png" /><nuxt-link
+                to="/techs/Stirrups"
+                >馬鐙</nuxt-link
+              >
+              <br />
+              <img
+                class="gg"
+                src="~assets/unique_tech_imperial.png"
+              /><nuxt-link to="/techs/Bagains">協議</nuxt-link>
+            </td>
+          </tr>
+        </table>
+
+        <div class="thanks">此頁面由singsing612協助編輯</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'allciv',
+  components: {}
+}
+</script>
+<style></style>
