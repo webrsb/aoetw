dd07680a2161af1358dfc180615ef49d2f2a8f9b
t304077 <64843355+t304077@users.noreply.github.com>
2020-05-06 23:54:59 +0800

    Update Herbal_Medicine.vue (#364)
    DE更新

--- pages/techs/Herbal_Medicine.vue
+++ pages/techs/Herbal_Medicine.vue
@@ -8,15 +8,17 @@
       <div class="col-md-8">
         <div class="n1">
           <p>
-            <b>草藥治療</b>(2013版稱<b>草藥學</b>)於<nuxt-link
+            <b>草藥治療</b
+            >(2013版稱<b>草藥學</b>)於《世紀帝國:征服者入侵》首次登場，在<nuxt-link
               to="/techs/age/Castle_Age"
               >城堡時代</nuxt-link
-            >在<nuxt-link to="/building/Monastery">修道院</nuxt-link
-            >研發後可讓駐紮單位的自癒速度提升300%。<nuxt-link
+            >於<nuxt-link to="/building/Monastery">修道院</nuxt-link
+            >研發後可讓駐紮單位的自癒速度提升500%。<nuxt-link
               to="/building/Watch_Tower"
               >箭塔</nuxt-link
-            >中每分鐘回24HP，<nuxt-link to="/building/Castle">城堡</nuxt-link
-            >則是每分鐘回48HP
+            >中每分鐘回36HP，<nuxt-link to="/building/Castle">城堡</nuxt-link
+            >則是每分鐘回72HP。
+            玩家駐紮在同盟的城堡時必須該同盟的城堡有研發草藥治療才能讓建築物中的玩家軍隊加速自癒速度，而非關玩家是否有研發草藥治療。
           </p>
           <p>
             草藥治療可顯著提升防禦部隊的機動性，讓受傷單位駐紮到城堡或箭塔中治療，同時給與火力掩護，治療完後又可重新再出戰
@@ -33,44 +35,37 @@
                   ><span style="margin-left: 2px;">可研發</span></strong
                 >
               </h4>
-              <nuxt-link to="/civs/Aztecs">阿茲特克</nuxt-link>、<nuxt-link
-                to="/civs/Berbers"
-                >柏柏</nuxt-link
-              >、<nuxt-link to="/civs/Britons">不列顛</nuxt-link>、<nuxt-link
-                to="/civs/Burmese"
-                >緬甸</nuxt-link
-              >、<nuxt-link to="/civs/Celts">塞爾特</nuxt-link>、<nuxt-link
-                to="/civs/Chinese"
-                >中國</nuxt-link
-              >、<nuxt-link to="/civs/Ethiopians">衣索比亞</nuxt-link
-              >、<nuxt-link to="/civs/Franks">法蘭克</nuxt-link>、<nuxt-link
-                to="/civs/Goths"
-                >哥德</nuxt-link
-              >、<nuxt-link to="/civs/Incas">印加</nuxt-link>、<nuxt-link
-                to="/civs/Indians"
-                >印度</nuxt-link
-              >、<nuxt-link to="/civs/Italians">義大利</nuxt-link>、<nuxt-link
-                to="/civs/Japanese"
-                >日本</nuxt-link
-              >、<nuxt-link to="/civs/Khmer">高棉</nuxt-link>、<nuxt-link
-                to="/civs/Koreans"
-                >朝鮮</nuxt-link
-              >、<nuxt-link to="/civs/Magyars">馬扎爾</nuxt-link>、<nuxt-link
-                to="/civs/Malay"
-                >馬來</nuxt-link
-              >、<nuxt-link to="/civs/Malians">馬利</nuxt-link>、<nuxt-link
-                to="/civs/Mayans"
-                >馬雅</nuxt-link
-              >、<nuxt-link to="/civs/Mongols">蒙古</nuxt-link>、<nuxt-link
-                to="/civs/Persians"
-                >波斯</nuxt-link
-              >、<nuxt-link to="/civs/Portuguese">葡萄牙</nuxt-link>、<nuxt-link
-                to="/civs/Saracens"
-                >薩拉森</nuxt-link
-              >、<nuxt-link to="/civs/Slavs">斯拉夫</nuxt-link>、<nuxt-link
-                to="/civs/Spanish"
-                >西班牙</nuxt-link
-              >、<nuxt-link to="/civs/Vietnamese">越南</nuxt-link>。
+              <nuxt-link to="/civs/Aztecs">阿茲特克</nuxt-link>、
+              <nuxt-link to="/civs/Berbers">柏柏</nuxt-link>、
+              <nuxt-link to="/civs/Britons">不列顛</nuxt-link>、
+              <nuxt-link to="/civs/Bulgarians">保加利亞</nuxt-link>、
+              <nuxt-link to="/civs/Burmese">緬甸</nuxt-link>、
+              <nuxt-link to="/civs/Celts">塞爾特</nuxt-link>、
+              <nuxt-link to="/civs/Chinese">中國</nuxt-link>、
+              <nuxt-link to="/civs/Cumans">庫曼</nuxt-link>、
+              <nuxt-link to="/civs/Ethiopians">衣索比亞</nuxt-link>、
+              <nuxt-link to="/civs/Franks">法蘭克</nuxt-link>、
+              <nuxt-link to="/civs/Goths">哥德</nuxt-link>、
+              <nuxt-link to="/civs/Incas">印加</nuxt-link>、
+              <nuxt-link to="/civs/Indians">印度</nuxt-link>、
+              <nuxt-link to="/civs/Italians">義大利</nuxt-link>、
+              <nuxt-link to="/civs/Japanese">日本</nuxt-link>、
+              <nuxt-link to="/civs/Khmer">高棉</nuxt-link>、
+              <nuxt-link to="/civs/Koreans">韓國</nuxt-link>、
+              <nuxt-link to="/civs/Lithuanians">立陶宛</nuxt-link>、
+              <nuxt-link to="/civs/Magyars">馬扎爾</nuxt-link>、
+              <nuxt-link to="/civs/Malay">馬來</nuxt-link>、
+              <nuxt-link to="/civs/Malians">馬利</nuxt-link>、
+              <nuxt-link to="/civs/Mayans">馬雅</nuxt-link>、
+              <nuxt-link to="/civs/Mongols">蒙古</nuxt-link>、
+              <nuxt-link to="/civs/Persians">波斯</nuxt-link>、
+              <nuxt-link to="/civs/Portuguese">葡萄牙</nuxt-link>、
+              <nuxt-link to="/civs/Saracens">薩拉森</nuxt-link>、
+              <nuxt-link to="/civs/Slavs">斯拉夫</nuxt-link>、
+              <nuxt-link to="/civs/Spanish">西班牙</nuxt-link>、
+              <nuxt-link to="/civs/Tatars">韃靼</nuxt-link>、
+              <nuxt-link to="/civs/Teutons">條頓</nuxt-link>、
+              <nuxt-link to="/civs/Vietnamese">越南</nuxt-link>。
             </div>
           </div>
         </div>
@@ -83,13 +78,10 @@
                   ><span style="margin-left: 2px;">不可研發</span></strong
                 >
               </h4>
-              <nuxt-link to="/civs/Byzantines">拜占庭</nuxt-link>、<nuxt-link
-                to="/civs/Huns"
-                >匈人</nuxt-link
-              >、<nuxt-link to="/civs/Teutons">條頓</nuxt-link>、<nuxt-link
-                to="/civs/Turks"
-                >土耳其</nuxt-link
-              >、<nuxt-link to="/civs/Vikings">維京</nuxt-link>。
+              <nuxt-link to="/civs/Byzantines">拜占庭</nuxt-link>、
+              <nuxt-link to="/civs/Huns">匈奴</nuxt-link>、
+              <nuxt-link to="/civs/Turks">土耳其</nuxt-link>、
+              <nuxt-link to="/civs/Vikings">維京</nuxt-link>。
             </div>
           </div>
         </div>
@@ -114,26 +106,42 @@
               >/<nuxt-link to="/techs/age/Imperial_Age">帝王時代</nuxt-link
               >各便宜15%/20%。
             </li>
+            <li>
+              <nuxt-link to="/civs/Teutons">條頓</nuxt-link>：免費獲得草藥治療。
+            </li>
           </ul>
-        </div>
-        <h3>歷史</h3>
-        <div class="hrn1"></div>
-        <div class="n1">
-          <p>
-            “雖然古典社會推動解剖學和生理學的研究，但在18世紀和19世紀的科學和工業革命之前，醫學是建立在實踐知識基礎之上的一種不精確的科學，首先是藉由口頭傳統和書面記錄傳遞經驗上累積的治療方法和程序。治療的主要媒介是草藥，通過實驗發現了具有療效的草藥化合物的應用，在中世紀的城鎮或村莊中草藥專家的出現對居民是一種福音。”
-          </p>
         </div>
         <h3>更新日誌</h3>
         <div class="hrn1"></div>
         <div class="n1">
           <h3>
+            <em><nuxt-link to="/games/aoc">征服者入侵</nuxt-link></em>
+          </h3>
+          <ul>
+            <li>草藥治療增加受傷部隊在建築物中300%自癒速度。</li>
+            <li>
+              <nuxt-link to="/civs/Teutons">條頓</nuxt-link>：沒有草藥治療。
+            </li>
+          </ul>
+          <h3>
             <em><nuxt-link to="/games/de">決定版</nuxt-link></em>
           </h3>
           <ul>
+            <li>草藥治療增加受傷部隊在建築物中500%自癒速度。</li>
             <li>
-              回復力由4倍提升為6倍
+              <nuxt-link to="/civs/Teutons">條頓</nuxt-link
+              >：免費獲得草藥治療當作文明加成。
             </li>
           </ul>
+          <h3>歷史</h3>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <p>
+              “雖然古典社會推動解剖學和生理學的研究，但在18世紀和19世紀的科學和工業革命之前，醫學是建立在實踐知識基礎之上的一種不精確的科學，
+              首先是藉由口頭傳統和書面記錄傳遞經驗上累積的治療方法和程序。治療的主要媒介是草藥，通過實驗發現了具有療效的草藥化合物的應用，
+              在中世紀的城鎮或村莊中草藥專家的出現對居民是一種福音。”
+            </p>
+          </div>
         </div>
       </div>
       <div class="col-md-4">
@@ -146,11 +154,7 @@
           <tr>
             <td colspan="2">
               <div class="unititle">
-                <img
-                  src="~assets/Herbalmedicine.png"
-                  class="img-fluid"
-                  alt=""
-                />
+                <img src="~assets/Herbalmedicine.png" class="img-fluid" alt="" />
               </div>
             </td>
           </tr>
