eef67e72599a8d0f764a24637661ad1132797401
t304077 <64843355+t304077@users.noreply.github.com>
2020-05-06 22:26:49 +0800

    Update Faith.vue
    (cherry picked from commit 8166abcd72ca8dbf05a4a3cc7ca6795049fbbbf4)

--- pages/techs/Faith.vue
+++ pages/techs/Faith.vue
@@ -16,9 +16,16 @@
             >的抗<nuxt-link to="/elements/Conversion">招降</nuxt-link>能力
           </p>
           <p>
+            忠誠信仰能將招降所需的最短時間增加兩秒，最大時間增加約四秒，從而增加所有單位的抗招降能力。
+            僧侶每秒完成招降的機會也降低為67％（這增加了平均轉化時間）。
+          </p>
+          <p>
             雖然此<nuxt-link to="/techs">科技</nuxt-link
-            >不便宜，但如果對方是打<nuxt-link to="/units/Monk">僧侶</nuxt-link
+            >不便宜(花金又花食物)，但如果對方是打<nuxt-link to="/units/Monk">僧侶</nuxt-link
             >爆，那就有研發的必要。
+          </p>
+          <p>
+            條頓人的文明加成給他們自己的所有單位和隊友的單位有更強的抗招降能力，並且也能研發中程信仰。條頓人是可說是抗招降能力最高的文明。
           </p>
         </div>
         <h3>文明可研發與否</h3>
@@ -32,49 +39,38 @@
                   ><span style="margin-left: 2px;">可研發</span></strong
                 >
               </h4>
-              <nuxt-link to="/civs/Aztecs">阿茲特克</nuxt-link>、<nuxt-link
-                to="/civs/Berbers"
-                >柏柏</nuxt-link
-              >、<nuxt-link to="/civs/Britons">不列顛</nuxt-link>、<nuxt-link
-                to="/civs/Burmese"
-                >緬甸</nuxt-link
-              >、<nuxt-link to="/civs/Byzantines">拜占庭</nuxt-link>、<nuxt-link
-                to="/civs/Celts"
-                >塞爾特</nuxt-link
-              >、<nuxt-link to="/civs/Chinese">中國</nuxt-link>、<nuxt-link
-                to="/civs/Ethiopians"
-                >衣索比亞</nuxt-link
-              >、<nuxt-link to="/civs/Franks">法蘭克</nuxt-link>、<nuxt-link
-                to="/civs/Goths"
-                >哥德</nuxt-link
-              >、<nuxt-link to="/civs/Huns">匈人</nuxt-link>、<nuxt-link
-                to="/civs/Incas"
-                >印加</nuxt-link
-              >、<nuxt-link to="/civs/Indians">印度</nuxt-link>、<nuxt-link
-                to="/civs/Italians"
-                >義大利</nuxt-link
-              >、<nuxt-link to="/civs/Japanese">日本</nuxt-link>、<nuxt-link
-                to="/civs/Koreans"
-                >朝鮮</nuxt-link
-              >、<nuxt-link to="/civs/Malay">馬來</nuxt-link>、<nuxt-link
-                to="/civs/Malians"
-                >馬利</nuxt-link
-              >、<nuxt-link to="/civs/Mayans">馬雅</nuxt-link>、<nuxt-link
-                to="/civs/Mongols"
-                >蒙古</nuxt-link
-              >、<nuxt-link to="/civs/Persians">波斯</nuxt-link>、<nuxt-link
-                to="/civs/Portuguese"
-                >葡萄牙</nuxt-link
-              >、<nuxt-link to="/civs/Saracens">薩拉森</nuxt-link>、<nuxt-link
-                to="/civs/Spanish"
-                >西班牙</nuxt-link
-              >、<nuxt-link to="/civs/Teutons">條頓</nuxt-link>、<nuxt-link
-                to="/civs/Turks"
-                >土耳其</nuxt-link
-              >、<nuxt-link to="/civs/Vietnamese">越南</nuxt-link>、<nuxt-link
-                to="/civs/Vikings"
-                >維京</nuxt-link
-              >。
+              <nuxt-link to="/civs/Aztecs">阿茲特克</nuxt-link>、
+<nuxt-link to="/civs/Berbers">柏柏</nuxt-link >、
+<nuxt-link to="/civs/Britons">不列顛</nuxt-link>、
+<nuxt-link to="/civs/Burmese">緬甸</nuxt-link>、
+<nuxt-link to="/civs/Byzantines">拜占庭</nuxt-link>、
+<nuxt-link to="/civs/Celts">塞爾特</nuxt-link>、
+<nuxt-link to="/civs/Chinese">中國</nuxt-link>、
+<nuxt-link to="/civs/Cumans">庫曼</nuxt-link>、
+<nuxt-link to="/civs/Ethiopians">衣索比亞</nuxt-link>、
+<nuxt-link to="/civs/Franks">法蘭克</nuxt-link>、
+<nuxt-link to="/civs/Goths">哥德</nuxt-link>、
+<nuxt-link to="/civs/Huns">匈奴</nuxt-link>、
+<nuxt-link to="/civs/Incas">印加</nuxt-link>、
+<nuxt-link to="/civs/Indians">印度</nuxt-link>、
+<nuxt-link to="/civs/Italians">義大利</nuxt-link>、
+<nuxt-link to="/civs/Japanese">日本</nuxt-link>、
+<nuxt-link to="/civs/Khmer">高棉</nuxt-link>、
+<nuxt-link to="/civs/Koreans">朝鮮</nuxt-link>、
+<nuxt-link to="/civs/Lithuanians">立陶宛</nuxt-link>、
+<nuxt-link to="/civs/Malay">馬來</nuxt-link>、
+<nuxt-link to="/civs/Malians">馬利</nuxt-link>、
+<nuxt-link to="/civs/Mayans">馬雅</nuxt-link>、
+<nuxt-link to="/civs/Mongols">蒙古</nuxt-link>、
+<nuxt-link to="/civs/Persians">波斯</nuxt-link>、
+<nuxt-link to="/civs/Portuguese">葡萄牙</nuxt-link>、
+<nuxt-link to="/civs/Saracens">薩拉森</nuxt-link>、
+<nuxt-link to="/civs/Slavs">斯拉夫</nuxt-link>、
+<nuxt-link to="/civs/Spanish">西班牙</nuxt-lin>、
+<nuxt-link to="/civs/Teutons">條頓</nuxt-lin>、
+<nuxt-link to="/civs/Turks">土耳其</nuxt-lin>、
+<nuxt-link to="/civs/Vietnamese">越南</nuxt-link>、
+<nuxt-link to="/civs/Viking">維京</nuxt-link>。
             </div>
           </div>
         </div>
@@ -87,10 +83,9 @@
                   ><span style="margin-left: 2px;">不可研發</span></strong
                 >
               </h4>
-              <nuxt-link to="/civs/Khmer">高棉</nuxt-link>、<nuxt-link
-                to="/civs/Magyars"
-                >馬扎爾</nuxt-link
-              >、<nuxt-link to="/civs/Slavs">斯拉夫</nuxt-link>。
+              <nuxt-link to="/civs/Bulgarians">保加利亞</nuxt-link>、
+<nuxt-link to="/civs/Magyars">馬扎爾</nuxt-link>、
+<nuxt-link to="/civs/Tatars">韃靼</nuxt-lin>。
             </div>
           </div>
         </div>
@@ -103,7 +98,7 @@
               >：研發該科技給與僧侶+5HP。
             </li>
             <li>
-              <nuxt-link to="/civs/Burmese">緬甸</nuxt-link>：科技便宜50%。
+              <nuxt-link to="/civs/Burmese">緬甸</nuxt-link>：該科技便宜50%。
             </li>
             <li>
               <nuxt-link to="/civs/Chinese">中國</nuxt-link>：科技在<nuxt-link
@@ -111,6 +106,38 @@
                 >帝王時代</nuxt-link
               >便宜20%。
             </li>
+          </ul>
+        </div>
+        <h3>團隊加成</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <ul>
+            <li>
+              <nuxt-link to="/civs/Lithuanians">立陶宛</nuxt-link>：忠誠信仰研發速度快20%。
+            </li>
+          </ul>
+        </div>
+        <h3>更新日誌</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <h3>
+            <em><nuxt-link to="/games/aof">失落的帝國</nuxt-link></em>
+          </h3>
+          <ul>
+            <li><nuxt-link to="/civs/Slavs">斯拉夫</nuxt-link>:無法研發忠誠信仰。</li>
+          </ul>
+          <h3>
+            <em><nuxt-link to="/games/aor">非洲王朝</nuxt-link></em>
+          </h3>
+          <ul>
+            <li><nuxt-link to="/civs/Khmer">高棉</nuxt-link>:無法研發忠誠信仰。</li>
+          </ul>
+          <h3>
+            <em><nuxt-link to="/games/de">決定版</nuxt-link></em>
+          </h3>
+          <ul>
+            <li><nuxt-link to="/civs/Slavs">斯拉夫</nuxt-link>:可以研發忠誠信仰。</li>
+            <li><nuxt-link to="/civs/Khmer">高棉</nuxt-link>:可以研發忠誠信仰。</li>
           </ul>
         </div>
         <h3>歷史</h3>
@@ -149,7 +176,7 @@
           <tr>
             <td class="unit3">時代</td>
             <td class="unit4">
-              <img class="gg" src="~assets/imperial_age.png" alt="" /><nuxt-link
+              <img class="gg" src="~assets/ImperialageDE.png" alt="" /><nuxt-link
                 to="/techs/age/Imperial_Age"
                 >帝王時代</nuxt-link
               >
