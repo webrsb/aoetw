acb93d36057bac8b60a7d0f68728a4d6c463fd3c
webrsb <webrsb@gmail.com>
2020-05-07 00:15:31 +0800

    format

--- pages/techs/Faith.vue
+++ pages/techs/Faith.vue
@@ -21,7 +21,8 @@
           </p>
           <p>
             雖然此<nuxt-link to="/techs">科技</nuxt-link
-            >不便宜(花金又花食物)，但如果對方是打<nuxt-link to="/units/Monk">僧侶</nuxt-link
+            >不便宜(花金又花食物)，但如果對方是打<nuxt-link to="/units/Monk"
+              >僧侶</nuxt-link
             >爆，那就有研發的必要。
           </p>
           <p>
@@ -40,37 +41,37 @@
                 >
               </h4>
               <nuxt-link to="/civs/Aztecs">阿茲特克</nuxt-link>、
-<nuxt-link to="/civs/Berbers">柏柏</nuxt-link >、
-<nuxt-link to="/civs/Britons">不列顛</nuxt-link>、
-<nuxt-link to="/civs/Burmese">緬甸</nuxt-link>、
-<nuxt-link to="/civs/Byzantines">拜占庭</nuxt-link>、
-<nuxt-link to="/civs/Celts">塞爾特</nuxt-link>、
-<nuxt-link to="/civs/Chinese">中國</nuxt-link>、
-<nuxt-link to="/civs/Cumans">庫曼</nuxt-link>、
-<nuxt-link to="/civs/Ethiopians">衣索比亞</nuxt-link>、
-<nuxt-link to="/civs/Franks">法蘭克</nuxt-link>、
-<nuxt-link to="/civs/Goths">哥德</nuxt-link>、
-<nuxt-link to="/civs/Huns">匈奴</nuxt-link>、
-<nuxt-link to="/civs/Incas">印加</nuxt-link>、
-<nuxt-link to="/civs/Indians">印度</nuxt-link>、
-<nuxt-link to="/civs/Italians">義大利</nuxt-link>、
-<nuxt-link to="/civs/Japanese">日本</nuxt-link>、
-<nuxt-link to="/civs/Khmer">高棉</nuxt-link>、
-<nuxt-link to="/civs/Koreans">朝鮮</nuxt-link>、
-<nuxt-link to="/civs/Lithuanians">立陶宛</nuxt-link>、
-<nuxt-link to="/civs/Malay">馬來</nuxt-link>、
-<nuxt-link to="/civs/Malians">馬利</nuxt-link>、
-<nuxt-link to="/civs/Mayans">馬雅</nuxt-link>、
-<nuxt-link to="/civs/Mongols">蒙古</nuxt-link>、
-<nuxt-link to="/civs/Persians">波斯</nuxt-link>、
-<nuxt-link to="/civs/Portuguese">葡萄牙</nuxt-link>、
-<nuxt-link to="/civs/Saracens">薩拉森</nuxt-link>、
-<nuxt-link to="/civs/Slavs">斯拉夫</nuxt-link>、
-<nuxt-link to="/civs/Spanish">西班牙</nuxt-lin>、
-<nuxt-link to="/civs/Teutons">條頓</nuxt-lin>、
-<nuxt-link to="/civs/Turks">土耳其</nuxt-lin>、
-<nuxt-link to="/civs/Vietnamese">越南</nuxt-link>、
-<nuxt-link to="/civs/Viking">維京</nuxt-link>。
+              <nuxt-link to="/civs/Berbers">柏柏</nuxt-link>、
+              <nuxt-link to="/civs/Britons">不列顛</nuxt-link>、
+              <nuxt-link to="/civs/Burmese">緬甸</nuxt-link>、
+              <nuxt-link to="/civs/Byzantines">拜占庭</nuxt-link>、
+              <nuxt-link to="/civs/Celts">塞爾特</nuxt-link>、
+              <nuxt-link to="/civs/Chinese">中國</nuxt-link>、
+              <nuxt-link to="/civs/Cumans">庫曼</nuxt-link>、
+              <nuxt-link to="/civs/Ethiopians">衣索比亞</nuxt-link>、
+              <nuxt-link to="/civs/Franks">法蘭克</nuxt-link>、
+              <nuxt-link to="/civs/Goths">哥德</nuxt-link>、
+              <nuxt-link to="/civs/Huns">匈奴</nuxt-link>、
+              <nuxt-link to="/civs/Incas">印加</nuxt-link>、
+              <nuxt-link to="/civs/Indians">印度</nuxt-link>、
+              <nuxt-link to="/civs/Italians">義大利</nuxt-link>、
+              <nuxt-link to="/civs/Japanese">日本</nuxt-link>、
+              <nuxt-link to="/civs/Khmer">高棉</nuxt-link>、
+              <nuxt-link to="/civs/Koreans">朝鮮</nuxt-link>、
+              <nuxt-link to="/civs/Lithuanians">立陶宛</nuxt-link>、
+              <nuxt-link to="/civs/Malay">馬來</nuxt-link>、
+              <nuxt-link to="/civs/Malians">馬利</nuxt-link>、
+              <nuxt-link to="/civs/Mayans">馬雅</nuxt-link>、
+              <nuxt-link to="/civs/Mongols">蒙古</nuxt-link>、
+              <nuxt-link to="/civs/Persians">波斯</nuxt-link>、
+              <nuxt-link to="/civs/Portuguese">葡萄牙</nuxt-link>、
+              <nuxt-link to="/civs/Saracens">薩拉森</nuxt-link>、
+              <nuxt-link to="/civs/Slavs">斯拉夫</nuxt-link>、
+              <nuxt-link to="/civs/Spanish">西班牙</nuxt-link>、
+              <nuxt-link to="/civs/Teutons">條頓</nuxt-link>、
+              <nuxt-link to="/civs/Turks">土耳其</nuxt-link>、
+              <nuxt-link to="/civs/Vietnamese">越南</nuxt-link>、
+              <nuxt-link to="/civs/Viking">維京</nuxt-link>。
             </div>
           </div>
         </div>
@@ -84,8 +85,8 @@
                 >
               </h4>
               <nuxt-link to="/civs/Bulgarians">保加利亞</nuxt-link>、
-<nuxt-link to="/civs/Magyars">馬扎爾</nuxt-link>、
-<nuxt-link to="/civs/Tatars">韃靼</nuxt-lin>。
+              <nuxt-link to="/civs/Magyars">馬扎爾</nuxt-link>、
+              <nuxt-link to="/civs/Tatars">韃靼</nuxt-link>。
             </div>
           </div>
         </div>
@@ -113,7 +114,8 @@
         <div class="n1">
           <ul>
             <li>
-              <nuxt-link to="/civs/Lithuanians">立陶宛</nuxt-link>：忠誠信仰研發速度快20%。
+              <nuxt-link to="/civs/Lithuanians">立陶宛</nuxt-link
+              >：忠誠信仰研發速度快20%。
             </li>
           </ul>
         </div>
@@ -124,20 +126,28 @@
             <em><nuxt-link to="/games/aof">失落的帝國</nuxt-link></em>
           </h3>
           <ul>
-            <li><nuxt-link to="/civs/Slavs">斯拉夫</nuxt-link>:無法研發忠誠信仰。</li>
+            <li>
+              <nuxt-link to="/civs/Slavs">斯拉夫</nuxt-link>:無法研發忠誠信仰。
+            </li>
           </ul>
           <h3>
             <em><nuxt-link to="/games/aor">非洲王朝</nuxt-link></em>
           </h3>
           <ul>
-            <li><nuxt-link to="/civs/Khmer">高棉</nuxt-link>:無法研發忠誠信仰。</li>
+            <li>
+              <nuxt-link to="/civs/Khmer">高棉</nuxt-link>:無法研發忠誠信仰。
+            </li>
           </ul>
           <h3>
             <em><nuxt-link to="/games/de">決定版</nuxt-link></em>
           </h3>
           <ul>
-            <li><nuxt-link to="/civs/Slavs">斯拉夫</nuxt-link>:可以研發忠誠信仰。</li>
-            <li><nuxt-link to="/civs/Khmer">高棉</nuxt-link>:可以研發忠誠信仰。</li>
+            <li>
+              <nuxt-link to="/civs/Slavs">斯拉夫</nuxt-link>:可以研發忠誠信仰。
+            </li>
+            <li>
+              <nuxt-link to="/civs/Khmer">高棉</nuxt-link>:可以研發忠誠信仰。
+            </li>
           </ul>
         </div>
         <h3>歷史</h3>
@@ -176,7 +186,7 @@
           <tr>
             <td class="unit3">時代</td>
             <td class="unit4">
-              <img class="gg" src="~assets/ImperialageDE.png" alt="" /><nuxt-link
+              <img class="gg" src="~assets/imperial_age.png" alt="" /><nuxt-link
                 to="/techs/age/Imperial_Age"
                 >帝王時代</nuxt-link
               >
