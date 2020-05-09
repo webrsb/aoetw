7430459dd1233ba6cadcb6cbc02d5397f913717a
t304077 <64843355+t304077@users.noreply.github.com>
2020-05-09 22:38:51 +0800

    Update Armor_Class_Siege_Weapon.vue
    1.精銳火炮戰船圖片待補
    2.DE更新完成，可合併

--- pages/armor/Armor_Class_Siege_Weapon.vue
+++ pages/armor/Armor_Class_Siege_Weapon.vue
@@ -123,7 +123,7 @@
           以下單位擁有對攻城單位的額外傷害，按造殺傷力排列。
           <div class="n1">
             <div class="row">
-              <div class="col-md-4">
+              <div class="col-md-6">
                 <div style="margin:12px 0px;"><strong>+65攻擊</strong></div>
                 <ul>
                   <li>
@@ -193,7 +193,7 @@
                   </li>
                 </ul>
               </div>
-              <div class="col-md-4">
+              <div class="col-md-6">
                 <div style="margin:12px 0px;"><strong>+12攻擊</strong></div>
                 <ul>
                   <li>
