a5fc18804815fc85806faae2ecc6c894e2b75dc3
webrsb <webrsb@gmail.com>
2020-05-10 23:19:27 +0800

    format and pics

--- pages/units/animal/Javelina.vue
+++ pages/units/animal/Javelina.vue
@@ -8,7 +8,9 @@
       <div class="n1"></div>
       <div class="col-md-8">
         <p>
-          <b>爪哇野豬(Javelina)</b>在《<nuxt-link to="/games/aoc">世紀帝國II：征服者入侵</nuxt-link>》首次引入，它有75點生命值，也比鹿要難殺死得多。
+          <b>爪哇野豬(Javelina)</b>在《<nuxt-link to="/games/aoc"
+            >世紀帝國II：征服者入侵</nuxt-link
+          >》首次引入，它有75點生命值，也比鹿要難殺死得多。
           村民們必須集體出擊才能收獲這樣大的獵物，任何落單的村民都將落得死亡的下場。爪哇野豬提供340食物，比大多數可捕獵動物要多得多。
           它是野豬在功能和外觀上完全相同的美洲版本。
         </p>
@@ -20,10 +22,14 @@
               <nuxt-link to="/civs/Mongols">蒙古</nuxt-link>:打獵速度快50%。
             </li>
             <li>
-              <nuxt-link to="/civs/Mayans">馬雅</nuxt-link>:額外多獵取20%的肉量。
+              <nuxt-link to="/civs/Mayans">馬雅</nuxt-link
+              >:額外多獵取20%的肉量。
             </li>
             <li>
-              <nuxt-link to="/civs/Goths">哥德</nuxt-link>:村民打<nuxt-link to="/units/animal/Wild_Boar">野豬</nuxt-link>攻擊+5，攜帶量+15。
+              <nuxt-link to="/civs/Goths">哥德</nuxt-link>:村民打<nuxt-link
+                to="/units/animal/Wild_Boar"
+                >野豬</nuxt-link
+              >攻擊+5，攜帶量+15。
             </li>
           </ul>
         </div>
@@ -31,9 +37,14 @@
         <div class="hrn1"></div>
         <div class="n1">
           <ul>
-            <li>“ javelina”一詞源自西班牙的jabalina，意為“雌性野豬”和用來獵取野豬的矛（javelin）。</li>
-            <li>儘管在遊戲中看起來與以前版本的野豬相同，但真正的爪哇野豬更小，臉更短，下巴的尖長牙在閉上嘴時不會突出嘴外。他們吃的也比野豬更素食。
-            然而，當受傷或受到威脅時，它們仍然是危險的動物。</li>
+            <li>
+              “
+              javelina”一詞源自西班牙的jabalina，意為“雌性野豬”和用來獵取野豬的矛（javelin）。
+            </li>
+            <li>
+              儘管在遊戲中看起來與以前版本的野豬相同，但真正的爪哇野豬更小，臉更短，下巴的尖長牙在閉上嘴時不會突出嘴外。他們吃的也比野豬更素食。
+              然而，當受傷或受到威脅時，它們仍然是危險的動物。
+            </li>
           </ul>
         </div>
         <h3>圖庫</h3>
@@ -65,7 +76,7 @@
           <tr>
             <td colspan="2">
               <div class="unititle">
-                <img src="~assets/Iron-boar-icon-aoe2.png" width="80" alt="" />
+                <img src="~assets/Iron-boar-icon-aoe2.png" alt="" />
               </div>
             </td>
           </tr>
