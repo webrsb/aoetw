10ed9b199652e07c7fed4259168abca69ba8ee81
t304077 <64843355+t304077@users.noreply.github.com>
2020-05-10 19:39:51 +0800

    Update Bracer.vue (#406)
    1.更新可研發/不可研發文明
    2.強力護腕圖片待補

--- pages/techs/Bracer.vue
+++ pages/techs/Bracer.vue
@@ -2,101 +2,36 @@
   <div class="container">
     <div class="row padtop">
       <div class="col-md-12">
-        <h1>強力護腕</h1>
+        <h1>絕世好箭</h1>
         <div class="hrn1"></div>
       </div>
       <div class="col-md-8">
         <div class="n1">
           <p>
-            <b>強力護腕</b>(2013版稱<b>護腕</b>)可於<nuxt-link
-              to="/techs/age/Imperial_Age"
-              >帝王時代</nuxt-link
-            >在<nuxt-link to="/building/Blacksmith">兵工廠</nuxt-link
-            >研發，可讓<nuxt-link to="/units/Archer_Units">弓兵</nuxt-link
+            <b>絕世好箭</b>(2013版稱<b>箭羽</b>)可於<nuxt-link
+              to="/techs/age/Feudal_Age"
+              >封建時代</nuxt-link
+            >在<nuxt-link to="/building/Blacksmith">兵工廠</nuxt-link>
+            研發，研發後<nuxt-link to="/units/Archer_Units">弓兵</nuxt-link
             >、<nuxt-link to="/building/Castle">城堡</nuxt-link>、<nuxt-link
               to="/building/Watch_Tower"
               >箭塔</nuxt-link
             >、<nuxt-link to="/units/Galley">戰船</nuxt-link>和<nuxt-link
               to="/units/Longboat"
               >維京大戰船</nuxt-link
-            >+1攻擊和+1射程；<nuxt-link to="/building/Town_Center"
+            >、<nuxt-link to="/units/Elite_Caravel">精銳卡拉維爾戰船</nuxt-link
+            >、<nuxt-link to="/building/Harbor">海港</nuxt-link>、<nuxt-link
+              to="/building/Krepost"
+              >營壘</nuxt-link
+            >、<nuxt-link to="/units/Elite_Skirmisher">戰矛兵</nuxt-link
+            >、<nuxt-link to="/units/Genitour">標槍騎兵</nuxt-link>和<nuxt-link
+              to="/building/Bombard_Tower"
+              >火砲塔</nuxt-link
+            >+1攻擊和+1射程(即使最後三個不是用弓箭的)。<nuxt-link
+              to="/building/Town_Center"
               >城鎮中心</nuxt-link
-            >只+1攻擊。
+            >則是攻擊和視野各+1。
           </p>
-        </div>
-        <h3>文明可研發與否</h3>
-        <div class="hrn1"></div>
-        <div class="row">
-          <div class="col-md-12">
-            <div class="n1">
-              <h4>
-                <strong
-                  ><i class="fas fa-check" style="color: green;"></i
-                  ><span style="margin-left: 2px;">可研發</span></strong
-                >
-              </h4>
-              <nuxt-link to="/civs/Aztecs">阿茲特克</nuxt-link>、<nuxt-link
-                to="/civs/Berbers"
-                >柏柏</nuxt-link
-              >、<nuxt-link to="/civs/Britons">不列顛</nuxt-link>、<nuxt-link
-                to="/civs/Burmese"
-                >緬甸</nuxt-link
-              >、<nuxt-link to="/civs/Byzantines">拜占庭</nuxt-link>、<nuxt-link
-                to="/civs/Chinese"
-                >中國</nuxt-link
-              >、<nuxt-link to="/civs/Ethiopians">衣索比亞</nuxt-link
-              >、<nuxt-link to="/civs/Goths">哥德</nuxt-link>、<nuxt-link
-                to="/civs/Huns"
-                >匈人</nuxt-link
-              >、<nuxt-link to="/civs/Incas">印加</nuxt-link>、<nuxt-link
-                to="/civs/Indians"
-                >印度</nuxt-link
-              >、<nuxt-link to="/civs/Italians">義大利</nuxt-link>、<nuxt-link
-                to="/civs/Japanese"
-                >日本</nuxt-link
-              >、<nuxt-link to="/civs/Khmer">高棉</nuxt-link>、<nuxt-link
-                to="/civs/Koreans"
-                >朝鮮</nuxt-link
-              >、<nuxt-link to="/civs/Magyars">馬扎爾</nuxt-link>、<nuxt-link
-                to="/civs/Malay"
-                >馬來</nuxt-link
-              >、<nuxt-link to="/civs/Mayans">馬雅</nuxt-link>、<nuxt-link
-                to="/civs/Mongols"
-                >蒙古</nuxt-link
-              >、<nuxt-link to="/civs/Portuguese">葡萄牙</nuxt-link>、<nuxt-link
-                to="/civs/Saracens"
-                >薩拉森</nuxt-link
-              >、<nuxt-link to="/civs/Spanish">西班牙</nuxt-link>、<nuxt-link
-                to="/civs/Turks"
-                >土耳其</nuxt-link
-              >、<nuxt-link to="/civs/Vietnamese">越南</nuxt-link>、<nuxt-link
-                to="/civs/Vikings"
-                >維京</nuxt-link
-              >。
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-12">
-            <div class="n1">
-              <h4>
-                <strong
-                  ><i class="fas fa-times" style="color: #d9534f;"></i
-                  ><span style="margin-left: 2px;">不可研發</span></strong
-                >
-              </h4>
-              <nuxt-link to="/civs/Celts">塞爾特</nuxt-link>、<nuxt-link
-                to="/civs/Franks"
-                >法蘭克</nuxt-link
-              >、<nuxt-link to="/civs/Malians">馬利</nuxt-link>、<nuxt-link
-                to="/civs/Persians"
-                >波斯</nuxt-link
-              >、<nuxt-link to="/civs/Slavs">斯拉夫</nuxt-link>、<nuxt-link
-                to="/civs/Teutons"
-                >條頓</nuxt-link
-              >。
-            </div>
-          </div>
         </div>
         <h3>文明加成</h3>
         <div class="hrn1"></div>
@@ -106,59 +41,47 @@
               <nuxt-link to="/civs/Chinese">中國</nuxt-link>：該<nuxt-link
                 to="/techs"
                 >科技</nuxt-link
-              >在<nuxt-link to="/techs/age/Imperial_Age">帝王時代</nuxt-link
-              >花費便宜20%。
+              >在封建/<nuxt-link to="/techs/age/Castle_Age">城堡</nuxt-link
+              >/<nuxt-link to="/techs/age/Imperial_Age">帝王時代</nuxt-link
+              >花費便宜10%/15%/20%。
             </li>
             <li>
-              <nuxt-link to="/civs/Spanish">西班牙</nuxt-link
-              >：研發該科技不需要<nuxt-link to="/resource/Gold">黃金</nuxt-link
-              >。
+              <nuxt-link to="/civs/Spanish">西班牙</nuxt-link>：<nuxt-link
+                to="/building/Blacksmith"
+                >兵工廠</nuxt-link
+              >的科技花費只需<nuxt-link to="/resource/Food">食物</nuxt-link>。
             </li>
           </ul>
         </div>
-        <h3>豆知識</h3>
+        <h3>團隊加成</h3>
         <div class="hrn1"></div>
         <div class="n1">
           <ul>
             <li>
-              大多數缺乏強力護腕的<nuxt-link to="/civs">文明</nuxt-link
-              >都可以使用<nuxt-link to="/units/Paladin">遊俠</nuxt-link
-              >。<nuxt-link to="/civs/Slavs">斯拉夫人</nuxt-link>和<nuxt-link
-                to="/civs/Malians"
-                >馬利</nuxt-link
-              >都缺乏遊俠和強力護腕，不過他們分別有<nuxt-link to="/units/Boyar"
-                >貴族鐵騎</nuxt-link
-              >和<nuxt-link to="/techs/Farimba">驃悍精神</nuxt-link>做為交換。
+              <nuxt-link to="/civs/Bulgarians">保加利亞</nuxt-link
+              >：兵工廠科技研發速度加快50%。
             </li>
           </ul>
         </div>
         <h3>歷史</h3>
         <div class="hrn1"></div>
         <div class="n1">
-          <div class="ien">
-            <em
-              >“強力護腕是一個佩戴在<nuxt-link to="/units/Archer"
-                >弓箭手</nuxt-link
-              >前手上的硬皮護具，這樣不僅可以支撐手腕在多次的拉弓下不會覺得非常疲累，並保護手臂內側免受<nuxt-link
-                to="/techs/Fletching"
-                >絕世好箭</nuxt-link
-              >的磨擦。如果沒有強力護腕保護，手臂可能在多次拉弓下會肌肉斷裂，削弱弓箭手的戰鬥力以致最後脫離戰鬥狀態，對於弓箭手而言，強力護腕是一個重要的改進。“</em
-            >
-            <p style="text-align: right;margin-top: 10px;">-世紀帝國II手稿。</p>
-          </div>
+          <p>
+            在箭頭後方提供羽毛或其他風翼稱為飛羽，這項創新提高了飛行中箭頭的穩定性。絕世好箭增加了箭矢的範圍和精度。精良的箭矢對弓箭手的成功和效率性至關重要。
+          </p>
         </div>
       </div>
       <div class="col-md-4">
         <table class="table-bordered" width="100%">
           <tr>
             <td class="unit" colspan="2" style="background-color:#31b0d5;">
-              <div class="unititle2">強力護腕</div>
+              <div class="unititle2">絕世好箭</div>
             </td>
           </tr>
           <tr>
             <td colspan="2">
               <div class="unititle">
-                <img src="~assets/Bracer.jpg" class="img-fluid" alt="" />
+                <img src="~assets/fletching.png" class="img-fluid" alt="" />
               </div>
             </td>
           </tr>
@@ -170,13 +93,13 @@
             </td>
           </tr>
           <tr>
-            <td class="unit3">文明</td>
-            <td class="unit4">請看分類</td>
+            <td class="unit3"><nuxt-link to="/civs">文明</nuxt-link></td>
+            <td class="unit4">所有文明</td>
           </tr>
           <tr>
             <td class="unit3">時代</td>
             <td class="unit4">
-              <nuxt-link to="/techs/age/Imperial_Age">帝王時代</nuxt-link>
+              <nuxt-link to="/techs/age/Feudal_Age">封建時代</nuxt-link>
             </td>
           </tr>
           <tr>
@@ -186,17 +109,19 @@
             </td>
           </tr>
           <tr>
-            <td class="unit3">升級所需科技</td>
+            <td class="unit3">次一升級</td>
             <td class="unit4">
-              <img class="gg" src="~assets/bodkin_arrow.png" alt="" /><nuxt-link
-                to="/techs/Bodkin_Arrow"
-                >羽箭</nuxt-link
-              >
+              <img
+                class="gg"
+                src="~assets/bodkin_arrow.png"
+                width="45"
+                alt=""
+              /><nuxt-link to="/techs/Bodkin_Arrow">羽箭</nuxt-link>
             </td>
           </tr>
           <tr>
             <td class="unit3">研發時間</td>
-            <td class="unit4">40秒</td>
+            <td class="unit4">30秒</td>
           </tr>
           <tr>
             <td class="unit" colspan="2" style="background-color:#31b0d5;">
@@ -207,13 +132,13 @@
             <td class="unit3">
               <nuxt-link to="/resource/Food">食物</nuxt-link>
             </td>
-            <td class="unit4">300</td>
+            <td class="unit4">100</td>
           </tr>
           <tr>
             <td class="unit3">
               <nuxt-link to="/resource/Gold">黃金</nuxt-link>
             </td>
-            <td class="unit4">200</td>
+            <td class="unit4">50</td>
           </tr>
           <tr>
             <td class="unit" colspan="2" style="background-color:#31b0d5;">
@@ -223,15 +148,17 @@
           <tr>
             <td class="unit3">影響</td>
             <td class="unit4">
-              讓<nuxt-link to="/units/Archer_Units">弓兵</nuxt-link>、<nuxt-link
+              <nuxt-link to="/units/Archer_Units">弓兵</nuxt-link>、<nuxt-link
                 to="/building/Castle"
                 >城堡</nuxt-link
               >、<nuxt-link to="/building/Watch_Tower">箭塔</nuxt-link
-              >、<nuxt-link to="/units/Naval_Vessels">戰船</nuxt-link
-              >和<nuxt-link to="/units/Longboat">維京大戰船</nuxt-link
-              >+1攻擊和+1射程；<nuxt-link to="/building/Town_Center"
+              >、<nuxt-link to="/units/Galley">戰船</nuxt-link>、<nuxt-link
+                to="/units/Longboat"
+                >維京大戰船</nuxt-link
+              >、卡拉維爾戰船、海港、營壘、戰矛兵、標槍騎兵和火炮塔+1攻擊和+1射程。<nuxt-link
+                to="/building/Town_Center"
                 >城鎮中心</nuxt-link
-              >只+1攻擊。
+              >攻擊和視野各+1。
             </td>
           </tr>
         </table>
