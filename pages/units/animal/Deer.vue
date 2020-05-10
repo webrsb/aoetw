fe84b798e393bfd65805ba0211f984959d7ae867
bookthief108 <47220722+bookthief108@users.noreply.github.com>
2020-05-10 22:41:32 +0800

    Update Deer.vue (#260)
    * Update Deer.vue

--- pages/units/animal/Deer.vue
+++ pages/units/animal/Deer.vue
@@ -13,7 +13,9 @@
               >斥候</nuxt-link
             >趕回<nuxt-link to="/building/Town_Center">城鎮中心</nuxt-link
             >，是初期相當快速的<nuxt-link to="/resource/Food">食物</nuxt-link
-            >來源。<nuxt-link to="/units/Villager">村民</nuxt-link
+            >來源。鹿通常三隻以上一群，他們的模組在各方面都與世紀帝國的羚羊非常相似。<nuxt-link
+              to="/units/Villager"
+              >村民</nuxt-link
             >獵殺鹿至少要兩箭，而最少3個村民殺鹿會比較有效率。一般來說打封建開打的遊戲，並不會去<nuxt-link
               to="/units/animal/Deer"
               >鹿群</nuxt-link
@@ -31,11 +33,19 @@
             <li>
               <nuxt-link to="/civs/Mayans">馬雅</nuxt-link>:額外多獵取20%的肉量
             </li>
+            <li><nuxt-link to="/civs/Goths">哥德</nuxt-link>:獵人攜帶量+15</li>
+          </ul>
+        </div>
+        <h3>趣事</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <ul>
             <li>
-              <nuxt-link to="/civs/Goths">哥德</nuxt-link>:村民打<nuxt-link
-                to="/units/animal/Wild_Boar"
-                >野豬</nuxt-link
-              >攻擊+5，攜帶量+15
+              遊戲中的鹿都有角，然而現實中只有公鹿才有角。而且只有在每年的特定季節角才會長出來。
+            </li>
+            <li>
+              儘管許多情況下鹿是溫順的，但可以使用“場景編輯器”中的觸發事件使它們攻擊。芬特蘭撒格(1000)場景中的Bug證明了這一點，
+              該錯誤是由一隻鹿不知不覺地走進狼王歐魯先前佔領的地磚而引起的。鹿的“攻擊”看起來像是一隻吃草的瞪羚，它迅速搖晃角，但幾秒鐘的攻擊僅造成1點傷害。
             </li>
           </ul>
         </div>
@@ -52,7 +62,7 @@
           <tr>
             <td colspan="2">
               <div class="unititle">
-                <img src="~assets/Deer-icon-aoe2.jpg" width="80" alt="" />
+                <img src="~assets/Deer-icon-aoe2.png" alt="" />
               </div>
             </td>
           </tr>
@@ -63,7 +73,7 @@
           </tr>
           <tr>
             <td class="unit3">
-              <nuxt-link to="/resource/Food">食物</nuxt-link>量
+              <nuxt-link to="/resource/Food">食物量</nuxt-link>
             </td>
             <td class="unit4">140</td>
           </tr>
@@ -73,9 +83,15 @@
             </td>
             <td class="unit4">5</td>
           </tr>
+          <tr>
+            <td class="unit3">
+              建議獵人數
+            </td>
+            <td class="unit4">2~4</td>
+          </tr>
         </table>
 
-        <div class="thanks">感謝! 此頁面由眠協助編輯</div>
+        <div class="thanks">感謝! 此頁面由眠、賜齡協助編輯</div>
       </div>
     </div>
   </div>
