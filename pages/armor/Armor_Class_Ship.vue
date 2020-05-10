25f0b9837b4338e6c3f13e26bbfca1c0a51be9e0
t304077 <64843355+t304077@users.noreply.github.com>
2020-05-10 19:06:59 +0800

    Update Armor_Class_Ship.vue (#381)
    1.漁船有自己的護甲分類:漁船護甲。待新增新頁，已登記於架構圖
    2.精銳火炮戰船、海港、營壘圖片待補
    3.箭塔圖是否跟科技圖要區分或是都放大型箭塔圖就好?
    4.DE更新完成，可合併

--- pages/armor/Armor_Class_Ship.vue
+++ pages/armor/Armor_Class_Ship.vue
@@ -33,13 +33,6 @@
             <li>
               <img
                 class="gg"
-                src="~assets/FishingShipIcon.png"
-                alt=""
-              /><nuxt-link to="/units/Fishing_Ship">漁船</nuxt-link>
-            </li>
-            <li>
-              <img
-                class="gg"
                 src="~assets/TransportShipIcon.png"
                 alt=""
               /><nuxt-link to="/units/Transport_Ship">運輸船</nuxt-link>
@@ -104,37 +97,48 @@
               >5()
             </li>
             <li>
-              <img src="~assets/CannonGalleonIcon.png" alt="" /><nuxt-link
-                to="/units/Cannon_Galleon"
-                >火砲戰船</nuxt-link
-              >→ <img src="~assets/CannonGalleonIcon.png" alt="" /><nuxt-link
-                to="/units/Elite_Cannon_Galleon"
+              <img
+                class="gg"
+                src="~assets/CannonGalleonIcon.png"
+                alt=""
+              /><nuxt-link to="/units/Cannon_Galleon">火砲戰船</nuxt-link>→
+              <img
+                class="gg"
+                src="~assets/EliteCannonGalleonicon.png"
+                alt=""
+              /><nuxt-link to="/units/Elite_Cannon_Galleon"
                 >精銳火砲戰船</nuxt-link
               >
             </li>
             <li>
-              <img src="~assets/LongboatIcon.png" alt="" /><nuxt-link
+              <img class="gg" src="~assets/LongboatIcon.png" alt="" /><nuxt-link
                 to="/units/Longboat"
                 >維京大戰船</nuxt-link
-              >→ <img src="~assets/LongboatIcon.png" alt="" /><nuxt-link
+              >→
+              <img class="gg" src="~assets/LongboatIcon.png" alt="" /><nuxt-link
                 to="/units/Longboat"
                 >精銳維京大戰船</nuxt-link
               >(<nuxt-link to="/civs/Vikings">維京</nuxt-link>限定)
             </li>
             <li>
-              <img src="~assets/TurtleShipIcon.png" alt="" /><nuxt-link
-                to="/units/Turtle_Ship"
-                >龜甲船</nuxt-link
-              >(8)→ <img src="~assets/TurtleShipIcon.png" alt="" /><nuxt-link
-                to="/units/Turtle_Ship"
-                >精銳龜甲船</nuxt-link
+              <img
+                class="gg"
+                src="~assets/TurtleShipIcon.png"
+                alt=""
+              /><nuxt-link to="/units/Turtle_Ship">龜甲船</nuxt-link>(8)→
+              <img
+                class="gg"
+                src="~assets/TurtleShipIcon.png"
+                alt=""
+              /><nuxt-link to="/units/Turtle_Ship">精銳龜甲船</nuxt-link
               >(11)(<nuxt-link to="/civs/Koreans">朝鮮</nuxt-link>限定)
             </li>
             <li>
-              <img src="~assets/CaravelIcon.png" alt="" /><nuxt-link
+              <img class="gg" src="~assets/CaravelIcon.png" alt="" /><nuxt-link
                 to="/units/Caravel"
                 >卡拉維爾戰船</nuxt-link
-              >→ <img src="~assets/CaravelIcon.png" alt="" /><nuxt-link
+              >→
+              <img class="gg" src="~assets/CaravelIcon.png" alt="" /><nuxt-link
                 to="/units/Caravel"
                 >精銳卡拉維爾戰船</nuxt-link
               >(<nuxt-link to="/civs/Portuguese">葡萄牙</nuxt-link>限定)
@@ -195,9 +199,11 @@
                     /><nuxt-link to="/units/Galleon">重型弩砲戰船</nuxt-link>
                   </li>
                   <li>
-                    <img src="~assets/LongboatIcon.png" alt="" /><nuxt-link
-                      to="/units/Longboat"
-                      >精銳維京大戰船</nuxt-link
+                    <img
+                      class="gg"
+                      src="~assets/LongboatIcon.png"
+                      alt=""
+                    /><nuxt-link to="/units/Longboat">精銳維京大戰船</nuxt-link
                     >(<nuxt-link to="/civs/Vikings">維京</nuxt-link>限定)
                   </li>
                 </ul>
@@ -431,6 +437,8 @@
                   >
                 </li>
               </ul>
+            </div>
+            <div class="col-md-4">
               <div style="margin:12px 0px;"><strong>+9攻擊</strong></div>
               <ul>
                 <li>
@@ -440,8 +448,6 @@
                   >
                 </li>
               </ul>
-            </div>
-            <div class="col-md-4">
               <div style="margin:12px 0px;"><strong>+4攻擊</strong></div>
               <ul>
                 <li>
@@ -462,6 +468,15 @@
                     to="/civs/Malay"
                     >馬來</nuxt-link
                   >限定)
+                </li>
+                <li>
+                  <img
+                    class="gg"
+                    src="~assets/Kreposticon.png"
+                    width="22"
+                    alt=""
+                  /><nuxt-link to="/building/Krepost">營壘</nuxt-link
+                  >(<nuxt-link to="/civs/Bulgarians">保加利亞</nuxt-link>限定)
                 </li>
               </ul>
             </div>
