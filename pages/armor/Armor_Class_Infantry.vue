d4d38f6e960f077e3d8b1c218ce476b7197b5a67
t304077 <64843355+t304077@users.noreply.github.com>
2020-05-10 19:02:10 +0800

    Update Armor_Class_Infantry.vue (#379)
    1. 精銳槍隊長圖片待補

--- pages/armor/Armor_Class_Infantry.vue
+++ pages/armor/Armor_Class_Infantry.vue
@@ -93,33 +93,44 @@
                 class="gg"
                 src="~assets/CondottieroIcon.png"
                 alt=""
-              /><nuxt-link to="/units/Condottiero">傭兵</nuxt-link>(<nuxt-link
-                to="/civs/Italians"
-                >義大利</nuxt-link
-              >和同盟限定)
+              /><nuxt-link to="/units/Condottiero">傭兵</nuxt-link
+              >(10)(<nuxt-link to="/civs/Italians">義大利</nuxt-link>和同盟限定)
             </li>
             <li>
               <img class="gg" src="~assets/WBRSK.png" alt="" /><nuxt-link
                 to="/units/Woad_Raider"
-                >菘藍武士(精銳)</nuxt-link
+                >菘藍武士</nuxt-link
+              >→ <img class="gg" src="~assets/WBRSK.png" alt="" /><nuxt-link
+                to="/units/Woad_Raider"
+                >精銳菘藍武士</nuxt-link
               >(<nuxt-link to="/civs/Celts">塞爾特</nuxt-link>限定)
             </li>
             <li>
               <img class="gg" src="~assets/TAXEM.png" alt="" /><nuxt-link
                 to="/units/Throwing_Axeman"
-                >擲斧兵(精銳)</nuxt-link
+                >擲斧兵</nuxt-link
+              >→ <img class="gg" src="~assets/TAXEM.png" alt="" /><nuxt-link
+                to="/units/Throwing_Axeman"
+                >精銳擲斧兵</nuxt-link
               >(<nuxt-link to="/civs/Franks">法蘭克</nuxt-link>限定)
             </li>
             <li>
               <img class="gg" src="~assets/HuskarlIcon.png" alt="" /><nuxt-link
                 to="/units/Huskarl"
-                >哥德衛隊(精銳)</nuxt-link
+                >哥德衛隊</nuxt-link
+              >→
+              <img class="gg" src="~assets/HuskarlIcon.png" alt="" /><nuxt-link
+                to="/units/Huskarl"
+                >精銳哥德衛隊</nuxt-link
               >(<nuxt-link to="/civs/Goths">哥德</nuxt-link>限定)
             </li>
             <li>
               <img class="gg" src="~assets/SMURI.png" alt="" /><nuxt-link
                 to="/units/Samurai"
-                >日本武士(精銳)</nuxt-link
+                >日本武士</nuxt-link
+              >→ <img class="gg" src="~assets/SMURI.png" alt="" /><nuxt-link
+                to="/units/Samurai"
+                >精銳日本武士</nuxt-link
               >(<nuxt-link to="/civs/Japanese">日本</nuxt-link>限定)
             </li>
             <li>
@@ -127,13 +138,21 @@
                 class="gg"
                 src="~assets/Crusade_avatar.png"
                 alt=""
-              /><nuxt-link to="/units/Teutonic_Knight">條頓武士(精銳)</nuxt-link
+              /><nuxt-link to="/units/Teutonic_Knight">條頓武士</nuxt-link>→
+              <img
+                class="gg"
+                src="~assets/Crusade_avatar.png"
+                alt=""
+              /><nuxt-link to="/units/Teutonic_Knight">精銳條頓武士</nuxt-link
               >(<nuxt-link to="/civs/Teutons">條頓</nuxt-link>限定)
             </li>
             <li>
               <img class="gg" src="~assets/VBRSK.png" alt="" /><nuxt-link
                 to="/units/Berserk"
-                >狂戰士(精銳)</nuxt-link
+                >狂戰士</nuxt-link
+              >→ <img class="gg" src="~assets/VBRSK.png" alt="" /><nuxt-link
+                to="/units/Berserk"
+                >精銳狂戰士</nuxt-link
               >(<nuxt-link to="/civs/Vikings">維京</nuxt-link>限定)
             </li>
             <li>
@@ -141,25 +160,41 @@
                 class="gg"
                 src="~assets/JaguarWarriorIcon.png"
                 alt=""
-              /><nuxt-link to="/units/Jaguar_Warrior">豹勇士(精銳)</nuxt-link
+              /><nuxt-link to="/units/Jaguar_Warrior">豹勇士</nuxt-link>→
+              <img
+                class="gg"
+                src="~assets/JaguarWarriorIcon.png"
+                alt=""
+              /><nuxt-link to="/units/Jaguar_Warrior">精銳豹勇士</nuxt-link
               >(<nuxt-link to="/civs/Aztecs">阿茲特克</nuxt-link>限定)
             </li>
             <li>
               <img class="gg" src="~assets/Kamayuk_Icon.jpg" alt="" /><nuxt-link
                 to="/units/Kamayuk"
-                >槍隊長(精銳)</nuxt-link
+                >槍隊長</nuxt-link
+              >→
+              <img class="gg" src="~assets/Kamayuk_Icon.jpg" alt="" /><nuxt-link
+                to="/units/Kamayuk"
+                >精銳槍隊長</nuxt-link
               >(<nuxt-link to="/civs/Incas">印加</nuxt-link>限定)
             </li>
             <li>
               <img class="gg" src="~assets/GbetoIcon.png" alt="" /><nuxt-link
                 to="/units/Gbeto"
-                >飛刀女獵手(精銳)</nuxt-link
+                >飛刀女獵手</nuxt-link
+              >→ <img class="gg" src="~assets/GbetoIcon.png" alt="" /><nuxt-link
+                to="/units/Gbeto"
+                >精銳飛刀女獵手</nuxt-link
               >(<nuxt-link to="/civs/Malians">馬利</nuxt-link>限定)
             </li>
             <li>
               <img class="gg" src="~assets/ShotelIcon.png" alt="" /><nuxt-link
                 to="/units/Shotel_Warrior"
-                >彎刀勇士(精銳)</nuxt-link
+                >彎刀勇士</nuxt-link
+              >→
+              <img class="gg" src="~assets/ShotelIcon.png" alt="" /><nuxt-link
+                to="/units/Shotel_Warrior"
+                >精銳彎刀勇士</nuxt-link
               >(<nuxt-link to="/civs/Ethiopians">衣索比亞</nuxt-link>限定)
             </li>
             <li>
@@ -167,8 +202,12 @@
                 class="gg"
                 src="~assets/Karambitwarrioricon.png"
                 alt=""
-              /><nuxt-link to="/units/Karambit_Warrior"
-                >爪刀勇士(精銳)</nuxt-link
+              /><nuxt-link to="/units/Karambit_Warrior">爪刀勇士</nuxt-link>→
+              <img
+                class="gg"
+                src="~assets/Karambitwarrioricon.png"
+                alt=""
+              /><nuxt-link to="/units/Karambit_Warrior">精銳爪刀勇士</nuxt-link
               >(<nuxt-link to="/civs/Malay">馬來</nuxt-link>限定)
             </li>
           </ul>
@@ -191,7 +230,7 @@
                     >→
                     <img
                       class="gg"
-                      src="~assets/CannonGalleonIcon.png"
+                      src="~assets/EliteCannonGalleonicon.png"
                       alt=""
                     /><nuxt-link to="/units/Elite_Cannon_Galleon"
                       >精銳火砲戰船</nuxt-link
@@ -222,6 +261,8 @@
                     >(<nuxt-link to="/civs/Aztecs">阿茲特克</nuxt-link>限定)
                   </li>
                 </ul>
+              </div>
+              <div class="col-md-4">
                 <div style="margin:12px 0px;"><strong>+10攻擊</strong></div>
                 <ul>
                   <li>
@@ -248,8 +289,6 @@
                     >(<nuxt-link to="/civs/Aztecs">阿茲特克</nuxt-link>限定)
                   </li>
                 </ul>
-              </div>
-              <div class="col-md-4">
                 <div style="margin:12px 0px;"><strong>+9攻擊</strong></div>
                 <ul>
                   <li>
@@ -261,6 +300,8 @@
                     >(<nuxt-link to="/civs/Byzantines">拜占庭</nuxt-link>限定)
                   </li>
                 </ul>
+              </div>
+              <div class="col-md-4">
                 <div style="margin:12px 0px;"><strong>+2攻擊</strong></div>
                 <ul>
                   <li>
@@ -304,7 +345,12 @@
                 class="gg"
                 src="~assets/CataphractIcon.png"
                 alt=""
-              /><nuxt-link to="/units/Cataphract">拜占庭聖騎兵(精銳)</nuxt-link
+              /><nuxt-link to="/units/Cataphract">拜占庭聖騎兵</nuxt-link>→
+              <img
+                class="gg"
+                src="~assets/CataphractIcon.png"
+                alt=""
+              /><nuxt-link to="/units/Cataphract">精銳拜占庭聖騎兵</nuxt-link
               >(<nuxt-link to="/civs/Byzantines">拜占庭</nuxt-link>限定)
             </li>
           </ul>
