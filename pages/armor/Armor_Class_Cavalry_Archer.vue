eac20e972ffd700d4caee0468dfbb7b6fa5ea178
t304077 <64843355+t304077@users.noreply.github.com>
2020-05-10 18:55:43 +0800

    Update Armor_Class_Cavalry_Archer.vue (#377)
    1.欽察圖片待補

--- pages/armor/Armor_Class_Cavalry_Archer.vue
+++ pages/armor/Armor_Class_Cavalry_Archer.vue
@@ -42,23 +42,18 @@
               >
             </li>
             <li>
-              <img class="gg" src="~assets/GenitourIcon.png" alt="" /><nuxt-link
-                to="/units/Genitour"
-                >標槍騎兵</nuxt-link
-              >(1)→
-              <img class="gg" src="~assets/GenitourIcon.png" alt="" /><nuxt-link
-                to="/units/Genitour"
-                >精銳標槍騎兵</nuxt-link
-              >(1)(<nuxt-link to="/civs/Berbers">柏柏</nuxt-link>及其同盟限定)
-            </li>
-            <li>
               <img
                 class="gg"
-                style="margin: 2px;"
-                width="22"
                 src="~assets/MangudaiIcon.png"
+                width="22"
                 alt=""
-              /><nuxt-link to="/units/Mangudai">蒙古突騎(精銳)</nuxt-link
+              /><nuxt-link to="/units/Mangudai">蒙古突騎</nuxt-link>→
+              <img
+                class="gg"
+                src="~assets/MangudaiIcon.png"
+                width="22"
+                alt=""
+              /><nuxt-link to="/units/Mangudai">精銳蒙古突騎</nuxt-link
               >(<nuxt-link to="/civs/Mongols">蒙古</nuxt-link>限定)
             </li>
             <li>
@@ -73,11 +68,28 @@
             <li>
               <img
                 class="gg"
-                style="margin: 2px;"
+                src="~assets/ConquistadorIcon.png"
+                alt=""
+              /><nuxt-link to="/units/Conquistador">西班牙征服者</nuxt-link>→
+              <img
+                class="gg"
+                src="~assets/ConquistadorIcon.png"
+                alt=""
+              /><nuxt-link to="/units/Conquistador">精銳征服者</nuxt-link
+              >(<nuxt-link to="/civs/Spanish">西班牙</nuxt-link>限定)
+            </li>
+            <li>
+              <img
+                class="gg"
                 src="~assets/ElephantArcherIcon.png"
                 alt=""
-              /><nuxt-link to="/units/Elephant_Archer">象弓騎兵(精銳)</nuxt-link
-              >(<nuxt-link to="/civs/Indians">印度</nuxt-link>限定)
+              /><nuxt-link to="/units/Elephant_Archer">象弓騎兵</nuxt-link>(-2)→
+              <img
+                class="gg"
+                src="~assets/ElephantArcherIcon.png"
+                alt=""
+              /><nuxt-link to="/units/Elephant_Archer">精銳象弓騎兵</nuxt-link
+              >(-2)(<nuxt-link to="/civs/Indians">印度</nuxt-link>限定)
             </li>
             <li>
               <img
@@ -85,17 +97,49 @@
                 style="margin: 2px;"
                 src="~assets/CamelArcherIcon.png"
                 alt=""
-              /><nuxt-link to="/units/Camel_Archer">駱駝弓騎兵(精銳)</nuxt-link
-              >(<nuxt-link to="/civs/Berbers">柏柏</nuxt-link>限定)
-            </li>
-            <li>
+              /><nuxt-link to="/units/Camel_Archer">駱駝弓騎兵</nuxt-link>→
               <img
                 class="gg"
                 style="margin: 2px;"
-                src="~assets/Arambaiicon.png"
+                src="~assets/CamelArcherIcon.png"
                 alt=""
-              /><nuxt-link to="/units/Arambai">飛鏢騎兵(精銳)</nuxt-link
+              />精銳<nuxt-link to="/units/Camel_Archer">駱駝弓騎兵</nuxt-link
+              >(<nuxt-link to="/civs/Berbers">柏柏</nuxt-link>限定)
+            </li>
+            <li>
+              <img class="gg" src="~assets/GenitourIcon.png" alt="" /><nuxt-link
+                to="/units/Genitour"
+                >標槍騎兵</nuxt-link
+              >(1)→
+              <img
+                class="gg"
+                src="~assets/GenitourIcon.png"
+                alt=""
+              />精銳<nuxt-link to="/units/Genitour">標槍騎兵</nuxt-link
+              >(1)(<nuxt-link to="/civs/Berbers">柏柏</nuxt-link>及其同盟限定)
+            </li>
+            <li>
+              <img class="gg" src="~assets/Arambaiicon.png" alt="" /><nuxt-link
+                to="/units/Arambai"
+                >飛鏢騎兵</nuxt-link
+              >→
+              <img class="gg" src="~assets/Arambaiicon.png" alt="" /><nuxt-link
+                to="/units/Arambai"
+                >精銳飛鏢騎兵</nuxt-link
               >(<nuxt-link to="/civs/Burmese">緬甸</nuxt-link>限定)
+            </li>
+            <li>
+              <img class="gg" src="~assets/Kipchakicon.png" alt="" /><nuxt-link
+                to="/units/Kipchak"
+                >欽察</nuxt-link
+              >→
+              <img class="gg" src="~assets/Kipchakicon.png" alt="" /><nuxt-link
+                to="/units/Kipchak"
+                >精銳欽察</nuxt-link
+              >(<nuxt-link to="/civs/Cumans">庫曼</nuxt-link
+              >及庫曼研發<nuxt-link to="/civs/Cuman_Mercenaries"
+                >庫曼傭兵</nuxt-link
+              >後同盟限定)
             </li>
           </ul>
         </div>
