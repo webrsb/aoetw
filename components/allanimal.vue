e3eb01b2a240944b547dde438a401e94c52574c7
bookthief108 <47220722+bookthief108@users.noreply.github.com>
2020-05-13 23:04:35 +0800

    Update allanimal.vue (#259)
    https://ageofempires.fandom.com/wiki/Animal
    更新完畢，拿掉 Rabid_Wolf 狂暴之狼

--- components/allanimal.vue
+++ components/allanimal.vue
@@ -6,7 +6,7 @@
           <tr>
             <td class="unit" colspan="2" style="background-color:#a868b1;">
               <div class="unititle" style="color: #fff;">
-                <nuxt-link to="/units/animal">世紀帝國II的野生動物</nuxt-link>
+                <nuxt-link to="/units/animal">世紀帝國II的野生動物 </nuxt-link>
               </div>
             </td>
           </tr>
@@ -14,46 +14,83 @@
             <td class="unit3" style="width: 8%;">不可馴化可食用</td>
             <td class="unit3">
               <nuxt-link to="/units/animal/Deer">鹿</nuxt-link>•
-              <nuxt-link to="/units/animal/Iron_Boar">鐵野豬</nuxt-link>•
-              <nuxt-link to="/units/animal/Wild_Boar">野豬</nuxt-link>•
-              <nuxt-link to="/units/animal/Zebra">斑馬</nuxt-link>•
               <nuxt-link to="/units/animal/Elephant">象</nuxt-link>•
+              <nuxt-link to="/units/animal/Ibex">野山羊</nuxt-link>•
+              <nuxt-link to="/units/animal/Javelina">爪哇野豬</nuxt-link>•
               <nuxt-link to="/units/animal/Ostrich">鴕鳥</nuxt-link>•
               <nuxt-link to="/units/animal/Rhinoceros">犀牛</nuxt-link>•
-              <nuxt-link to="/units/animal/fish">魚類</nuxt-link>
-            </td>
-          </tr>
-          <tr>
-            <td class="unit3" style="width: 8%;">可馴化可食用</td>
-            <td class="unit3">
-              <nuxt-link to="/units/animal/Sheep">羊</nuxt-link>•
-              <nuxt-link to="/units/animal/Turkey">火雞</nuxt-link>•
-              <nuxt-link to="/units/animal/Llama">羊駝</nuxt-link>•
-              <nuxt-link to="/units/animal/Cow">牛</nuxt-link>•
-              <nuxt-link to="/units/animal/Goat">山羊</nuxt-link>•
-              <nuxt-link to="/units/animal/Water_Buffalo">水牛</nuxt-link>
+              <nuxt-link to="/units/animal/Wild_Boar">野豬</nuxt-link>•
+              <nuxt-link to="/units/animal/Zebra">斑馬</nuxt-link>
             </td>
           </tr>
           <tr>
             <td class="unit3" style="width: 8%;">不可食用</td>
             <td class="unit3">
-              <nuxt-link to="/units/animal/Wolf">狼</nuxt-link>•
-              <nuxt-link to="//units/animal/Jaguar">豹</nuxt-link>•
-              <nuxt-link to="/units/animal/Wild_Horse">馬</nuxt-link>•
+              <nuxt-link to="//units/animal/Bactrian_Camel">雙峰駱駝</nuxt-link>•
               <nuxt-link to="/units/animal/Bear">熊</nuxt-link>•
-              <nuxt-link to="/units/animal/Camel">駱駝</nuxt-link>•
-              <nuxt-link to="/units/animal/Donkey">驢</nuxt-link>•
-              <nuxt-link to="/units/animal/Tiger">虎</nuxt-link>•
-              <nuxt-link to="/units/animal/Lion">獅子</nuxt-link>•
+              <nuxt-link to="/units/animal/Camel">駱駝(中立)</nuxt-link>•
               <nuxt-link to="/units/animal/Crocodile">鱷魚</nuxt-link>•
-              <nuxt-link to="/units/animal/Komodo_Dragon">科莫多巨蜥</nuxt-link>
+              <nuxt-link to="/units/animal/Dire_Wolf">獵狼</nuxt-link>•
+              <nuxt-link to="/units/animal/Donkey">毛驢</nuxt-link>•
+              <nuxt-link to="/units/animal/Horse">馬</nuxt-link>•
+              <nuxt-link to="/units/animal/Iron_Boar">鐵野豬</nuxt-link>•
+              <nuxt-link to="//units/animal/Jaguar">豹</nuxt-link>• 
+              <nuxt-link to="/units/animal/Bactrian_Camel">科莫多巨蜥</nuxt-link>•
+              <nuxt-link to="/units/animal/Lion">獅子</nuxt-link>•
+              <nuxt-link to="//units/animal/Snow_Leopard">雪豹</nuxt-link>• 
+              <nuxt-link to="/units/animal/Tiger">虎</nuxt-link>•
+              <nuxt-link to="//units/animal/Wild_Bactrian_Camel">野生雙峰駱駝</nuxt-link> •
+              <nuxt-link to="//units/animal/Wild_Camel">野生駱駝</nuxt-link> • 
+              <nuxt-link to="/units/animal/Wolf">狼</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3" style="width: 8%;">可馴化可食用</td>
+            <td class="unit3">
+              <nuxt-link to="/units/animal/Cow">乳牛</nuxt-link>•
+              <nuxt-link to="/units/animal/Goat">山羊</nuxt-link>•
+              <nuxt-link to="/units/animal/Goose">鵝</nuxt-link>•
+              <nuxt-link to="/units/animal/Llama">羊駝</nuxt-link>•
+              <nuxt-link to="/units/animal/Pig">豬</nuxt-link>•
+              <nuxt-link to="/units/animal/Sheep">綿羊</nuxt-link>•
+              <nuxt-link to="/units/animal/Turkey">火雞</nuxt-link>•
+              <nuxt-link to="/units/animal/Water_Buffalo">水牛</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3" style="width: 8%;">水域可食用</td>
+            <td class="unit3">
+              <nuxt-link to="/units/animal/Box_Turtle">箱龜</nuxt-link>•
+              <nuxt-link to="/units/animal/Dolphin">海豚</nuxt-link>•
+              <nuxt-link to="/units/animal/Dorado">鬼頭刀</nuxt-link>•
+              <nuxt-link to="/units/animal/Marlin">馬林魚</nuxt-link>•
+              <nuxt-link to="/units/animal/Perch">鱸魚</nuxt-link>•
+              <nuxt-link to="/units/animal/Salmon">鮭魚</nuxt-link>•
+              <nuxt-link to="/units/animal/Shore_Fish">沿岸魚類</nuxt-link>•
+              <nuxt-link to="/units/animal/Snapper">鯛魚</nuxt-link>•
+              <nuxt-link to="/units/animal/Tuna">鮪魚</nuxt-link>
             </td>
           </tr>
           <tr>
             <td class="unit3" style="width: 8%;">裝飾動物</td>
             <td class="unit3">
+              <nuxt-link to="/units/animal/Butterflies">蝴蝶</nuxt-link>•
+              <nuxt-link to="/units/animal/Falcon">游隼</nuxt-link>•
               <nuxt-link to="/units/animal/Hawk">鷹</nuxt-link>•
-              <nuxt-link to="/units/animal/Macaw">金剛鸚鵡</nuxt-link>•<nuxt-link to="/units/animal/Vulture">禿鷹</nuxt-link>
+              <nuxt-link to="/units/animal/Macaw">金剛鸚鵡</nuxt-link>•
+              <nuxt-link to="/units/animal/Seagulls">海鷗</nuxt-link>•
+              <nuxt-link to="/units/animal/Stork">鸛</nuxt-link>•
+              <nuxt-link to="/units/animal/Stormy_Dog">風暴狗</nuxt-link>•
+              <nuxt-link to="/units/animal/Vulture">禿鷹</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3" style="width: 8%;">軍事單位</td>
+            <td class="unit3">
+              <nuxt-link to="/units/animal/Alfred_the_Alpaca">羊駝阿佛烈</nuxt-link>•
+              <nuxt-link to="/units/animal/Flaming_Camel">火焰駱駝</nuxt-link>•
+              <nuxt-link to="/units/animal/Furious_the_Monkey_Boy">狂暴猴孩</nuxt-link>•
+              <nuxt-link to="/units/animal/Penguin">企鵝</nuxt-link>
             </td>
           </tr>
         </table>
