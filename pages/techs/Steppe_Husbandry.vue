4683cdd2b109ed2161394ae7f41d530a12c97579
webrsb <webrsb@gmail.com>
2020-05-15 22:24:44 +0800

    format

--- pages/techs/Steppe_Husbandry.vue
+++ pages/techs/Steppe_Husbandry.vue
@@ -8,7 +8,16 @@
       <div class="col-md-8">
         <div class="n1">
           <p>
-            <b>大草原耕種技術</b>是<nuxt-link to="/civs/Cumans">庫曼</nuxt-link>的特殊科技，於《世紀帝國II:決定版》引進。可於城堡時代的城堡中研發，研發後斥侯系、草原騎兵和馬弓騎兵訓練速度快50%。
+            <b>大草原耕種技術</b>是<nuxt-link to="/civs/Cumans">庫曼</nuxt-link
+            >的特殊科技，於《<nuxt-link to="/games/de"
+              >世紀帝國II：決定版</nuxt-link
+            >》引進。可於<nuxt-link to="/techs/age/Castle_Age"
+              >城堡時代</nuxt-link
+            >在<nuxt-link to="/building/Castle">城堡</nuxt-link
+            >研發。研發後<nuxt-link to="/units/Scout_Cavalry">斥候系</nuxt-link
+            >、<nuxt-link to="/units/Steppe_Lancer">草原騎兵</nuxt-link
+            >和<nuxt-link to="/units/Cavalry_Archer">馬弓騎兵</nuxt-link
+            >訓練速度+50%。
           </p>
         </div>
         <h3>戰術要點</h3>
@@ -16,12 +25,30 @@
         <div class="n1">
           <ul>
             <li>
-              大草原耕種技術具有類似法蘭克特殊科技騎士精神和哥德灌注士氣的效果。讓斥侯系、草原騎兵和馬弓騎兵訓練速度快50%。
-              如果玩家想組建一支快速，具成本效益的軍隊，那麼這項技術將非常有用，特別是因為其成本以及在城堡/帝國時代所有庫曼騎兵的移動速度都快了10％/ 15％。
-              但是，大草原耕種技術不會影響欽察或標槍騎兵。
+              大草原耕種技術具有類似法蘭克特殊科技<nuxt-link
+                to="/techs/Chivalry"
+                >騎士精神</nuxt-link
+              >和哥德<nuxt-link to="/techs/Perfusion">灌注士氣</nuxt-link
+              >的效果。讓斥侯系、草原騎兵和馬弓騎兵訓練速度快50%。
+              如果玩家想組建一支快速，具成本效益的軍隊，那麼這項技術將非常有用，特別是因為其成本以及在城堡/<nuxt-link
+                to="/techs/age/Imperial_Age"
+                >帝王時代</nuxt-link
+              >所有<nuxt-link to="/civs/Cumans">庫曼</nuxt-link
+              >騎兵的移動速度都快了10％/ 15％。
+              但是，大草原耕種技術不會影響<nuxt-link to="/units/Kipchak"
+                >欽察</nuxt-link
+              >或<nuxt-link to="/units/Genitour">標槍騎兵</nuxt-link>。
             </li>
             <li>
-              在帝王時代，使用徵兵制度可以進一步提高訓練速度。如果玩家擁有不列顛或匈奴盟友的話，則可以使他們的射箭場和馬廄的工作速度分別提高20％。
+              在帝王時代，使用<nuxt-link to="/techs/Conscription"
+                >徵兵制度</nuxt-link
+              >可以進一步提高訓練速度。如果玩家擁有<nuxt-link to="/civs/Britons"
+                >不列顛</nuxt-link
+              >或<nuxt-link to="/civs/Huns">匈人</nuxt-link
+              >盟友的話，則可以使他們的<nuxt-link to="/building/Archery_Range"
+                >射箭場</nuxt-link
+              >和<nuxt-link to="/building/Stable">馬廄</nuxt-link
+              >的工作速度分別提高20％。
             </li>
           </ul>
         </div>
@@ -45,12 +72,14 @@
           </h3>
           <ul>
             <li>
-              最初大草原耕種技術不會影響草原騎兵而且訓練速度是400％（儘管科技樹錯誤顯示為80％）。36202更新後大草原耕種技術現在可以影響草原騎兵，
-              並且其效果降低到50％
+              大草原耕種技術不會影響草原騎兵而且訓練速度是400%（儘管科技樹錯誤顯示為80％）。
+            </li>
+            <li>
+              (36202更新)
+              後大草原耕種技術現在可以影響草原騎兵，並且其效果降低到50％
             </li>
           </ul>
         </div>
-
       </div>
       <div class="col-md-4">
         <table class="table-bordered" width="100%">
@@ -118,7 +147,7 @@
             </td>
             <td class="unit4">300</td>
           </tr>
-          
+
           <tr>
             <td class="unit" colspan="2" style="background-color:#31b0d5;">
               <div class="unititle2">影響</div>
@@ -126,7 +155,12 @@
           </tr>
           <tr>
             <td class="unit3">影響</td>
-            <td class="unit4">斥侯系、草原騎兵和馬弓騎兵訓練速度快50%。</td>
+            <td class="unit4">
+              <nuxt-link to="/units/Scout_Cavalry">斥侯</nuxt-link
+              >系、<nuxt-link to="/units/Steppe_Lancer">草原騎兵</nuxt-link
+              >和<nuxt-link to="/units/Cavalry_Archer">馬弓騎兵</nuxt-link
+              >訓練速度快50%。
+            </td>
           </tr>
         </table>
 
