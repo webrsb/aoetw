7e727425a27083e40fea5349bdc98bc9b2917f78
t304077 <64843355+t304077@users.noreply.github.com>
2020-05-15 22:25:05 +0800

    Update Howdah.vue (#431)
    1.新增更新日誌
    DE更新，可合併

--- pages/techs/Steppe_Husbandry.vue
+++ pages/techs/Steppe_Husbandry.vue
@@ -9,10 +9,12 @@
         <div class="n1">
           <p>
             <b>大草原耕種技術</b>是<nuxt-link to="/civs/Cumans">庫曼</nuxt-link
-            >的<nuxt-link to="/techs/untech">特殊科技</nuxt-link
-            >，可於<nuxt-link to="/techs/age/Castle_Age">城堡時代</nuxt-link
+            >的特殊科技，於《<nuxt-link to="/games/de"
+              >世紀帝國II：決定版</nuxt-link
+            >》引進。可於<nuxt-link to="/techs/age/Castle_Age"
+              >城堡時代</nuxt-link
             >在<nuxt-link to="/building/Castle">城堡</nuxt-link
-            >研發。研發後<nuxt-link to="/units/Light_Cavalry">輕騎兵</nuxt-link
+            >研發。研發後<nuxt-link to="/units/Scout_Cavalry">斥候系</nuxt-link
             >、<nuxt-link to="/units/Steppe_Lancer">草原騎兵</nuxt-link
             >和<nuxt-link to="/units/Cavalry_Archer">馬弓騎兵</nuxt-link
             >訓練速度+50%。
@@ -23,21 +25,30 @@
         <div class="n1">
           <ul>
             <li>
-              大草原耕種技術提升了庫曼的主要單位：<nuxt-link
-                to="/units/Light_Cavalry"
-                >輕騎兵</nuxt-link
-              >和<nuxt-link to="/units/Cavalry_Archer">馬弓騎兵</nuxt-link
-              >的訓練速度。
-              如果是想組建一支快速並具成本效益的軍隊的話，再配合着庫曼<nuxt-link
-                to="/units/Cavalry_Units"
-                >騎兵</nuxt-link
-              >移動速度的文明加成(騎兵速度在城堡時代/<nuxt-link
+              大草原耕種技術具有類似法蘭克特殊科技<nuxt-link
+                to="/techs/Chivalry"
+                >騎士精神</nuxt-link
+              >和哥德<nuxt-link to="/techs/Perfusion">灌注士氣</nuxt-link
+              >的效果。讓斥侯系、草原騎兵和馬弓騎兵訓練速度快50%。
+              如果玩家想組建一支快速，具成本效益的軍隊，那麼這項技術將非常有用，特別是因為其成本以及在城堡/<nuxt-link
                 to="/techs/age/Imperial_Age"
                 >帝王時代</nuxt-link
-              >提升10％/ 15％)。這項科技將非常有用，但此科技並不影響<nuxt-link
-                to="/units/Kipchaks"
+              >所有<nuxt-link to="/civs/Cumans">庫曼</nuxt-link
+              >騎兵的移動速度都快了10％/ 15％。
+              但是，大草原耕種技術不會影響<nuxt-link to="/units/Kipchak"
                 >欽察</nuxt-link
-              >和<nuxt-link to="/units/Genitour">標槍騎兵</nuxt-link>。
+              >或<nuxt-link to="/units/Genitour">標槍騎兵</nuxt-link>。
+            </li>
+            <li>
+              在帝王時代，使用<nuxt-link to="/techs/Conscription"
+                >徵兵制度</nuxt-link
+              >可以進一步提高訓練速度。如果玩家擁有<nuxt-link to="/civs/Britons"
+                >不列顛</nuxt-link
+              >或<nuxt-link to="/civs/Huns">匈人</nuxt-link
+              >盟友的話，則可以使他們的<nuxt-link to="/building/Archery_Range"
+                >射箭場</nuxt-link
+              >和<nuxt-link to="/building/Stable">馬廄</nuxt-link
+              >的工作速度分別提高20％。
             </li>
           </ul>
         </div>
@@ -50,6 +61,22 @@
                 to="/techs/Kasbah"
                 >聚居堡壘</nuxt-link
               >能夠加速大草原耕種技術的研究時間(快25%)。
+            </li>
+          </ul>
+        </div>
+        <h3>更新日誌</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <h3>
+            <em><nuxt-link to="/games/de">決定版</nuxt-link></em>
+          </h3>
+          <ul>
+            <li>
+              大草原耕種技術不會影響草原騎兵而且訓練速度是400%（儘管科技樹錯誤顯示為80％）。
+            </li>
+            <li>
+              (36202更新)
+              後大草原耕種技術現在可以影響草原騎兵，並且其效果降低到50％
             </li>
           </ul>
         </div>
@@ -112,14 +139,15 @@
             <td class="unit3">
               <nuxt-link to="/resource/Food">食物</nuxt-link>
             </td>
-            <td class="unit4">300</td>
+            <td class="unit4">200</td>
           </tr>
           <tr>
             <td class="unit3">
               <nuxt-link to="/resource/Wood">木材</nuxt-link>
             </td>
-            <td class="unit4">200</td>
+            <td class="unit4">300</td>
           </tr>
+
           <tr>
             <td class="unit" colspan="2" style="background-color:#31b0d5;">
               <div class="unititle2">影響</div>
@@ -128,15 +156,15 @@
           <tr>
             <td class="unit3">影響</td>
             <td class="unit4">
-              <nuxt-link to="/units/Light_Cavalry">輕騎兵</nuxt-link
-              >、<nuxt-link to="/units/Steppe_Lancer">草原騎兵</nuxt-link
+              <nuxt-link to="/units/Scout_Cavalry">斥侯</nuxt-link
+              >系、<nuxt-link to="/units/Steppe_Lancer">草原騎兵</nuxt-link
               >和<nuxt-link to="/units/Cavalry_Archer">馬弓騎兵</nuxt-link
-              >訓練速度+50%
+              >訓練速度快50%。
             </td>
           </tr>
         </table>
 
-        <div class="thanks">感謝! singsing612</div>
+        <div class="thanks">感謝! 此頁面由賜齡協助編輯</div>
       </div>
     </div>
   </div>
