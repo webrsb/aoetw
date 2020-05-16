20bdbe6038eee2306ddcc4df7ff250eaf60f62f7
singsing612 <62696812+singsing612@users.noreply.github.com>
2020-05-16 21:02:42 +0800

    Create Lithuanians.vue (#414)
    新增立陶宛文明，缺圖片、缺科技樹
    立陶宛科技樹連結

---
+++ pages/civs/Lithuanians.vue
@@ -0,0 +1,309 @@
+<template>
+  <div class="container">
+    <div class="row padtop">
+      <div class="col-md-12">
+        <h1><img src="~assets/lithuanians.png" />立陶宛</h1>
+        <div class="hrn1"></div>
+      </div>
+      <div class="col-md-8">
+        <div class="n1">
+          <p>
+            <b>立陶宛</b>是《<nuxt-link to="/games/de"
+              >世紀帝國II：決定版</nuxt-link
+            >》新增著重於<nuxt-link to="/units/Monk">僧侶</nuxt-link
+            >和<nuxt-link to="/units/Cavalry_Units">騎兵</nuxt-link>的東歐文明。
+          </p>
+          <p>
+            從歷史上看，立陶宛人由許多波羅的海部落組成，最終組成為一個國家-立陶宛大公國，以抵禦<nuxt-link
+              to="/civs/Teutons"
+              >條頓</nuxt-link
+            >騎士團和東<nuxt-link to="/civs/Slavs">斯拉夫</nuxt-link
+            >人的持續入侵，直到16世紀中葉與波蘭組成聯邦。
+          </p>
+          <p>
+            相對於大多數的歐洲王國，立陶宛人主要是異教徒。即使在接受基督教後，立陶宛人仍然保持著異教的文化根基，因此他們擁有<nuxt-link
+              to="/building/Monastery"
+              >修道院</nuxt-link
+            >工作效率更快的團隊加分。
+          </p>
+          <p>
+            立陶宛軍隊中，<nuxt-link to="/units/Cavalry_Units">騎兵</nuxt-link
+            >佔大多數，使他們在抵禦條頓人和斯拉夫中發揮了重要作用。因此他們擁有能無視單位<nuxt-link
+              to="/armor"
+              >裝甲</nuxt-link
+            >的<nuxt-link to="/units/Unique_Unit">特殊單位</nuxt-link
+            ><nuxt-link to="/units/Leitis">烈堤司</nuxt-link
+            >，有效對抗<nuxt-link to="/units/Teutonic_Knight"
+              >條頓武士</nuxt-link
+            >和<nuxt-link to="/units/Boyar">貴族鐵騎</nuxt-link
+            >以及每駐守一個<nuxt-link to="/elements/Relic">遺跡</nuxt-link
+            ><nuxt-link to="/units/Knight">騎士</nuxt-link>系和烈堤司<nuxt-link
+              to="/elements/Attack"
+              >攻擊力</nuxt-link
+            >就+1的文明加成。
+          </p>
+          <p>
+            波羅的海獨特的溫帶海洋性和大陸性氣候使立陶宛人得以蓬勃發展，這體現在開局<nuxt-link
+              to="/resourses/Food"
+              >食物</nuxt-link
+            >+150的文明加成中。
+          </p>
+          <p>
+            軍事上，立陶宛人以<nuxt-link to="/techs/Hill_Forts"
+              >山城堡</nuxt-link
+            >而聞名，有助於保護村莊以免受入侵者的入侵，因此他們擁有同名的特殊科技上，<nuxt-link
+              to="/building/Town_Center"
+              >城鎮中心</nuxt-link
+            >射程+3。而矛是立陶宛軍隊最常用的武器，因此立陶宛<nuxt-link
+              to="/units/Spearman"
+              >長槍兵</nuxt-link
+            >和<nuxt-link to="/units/Skirmisher">矛兵</nuxt-link
+            >提高了移動速度，以及<nuxt-link to="/techs/age/Imperial_Age"
+              >帝王時代</nuxt-link
+            >特殊科技<nuxt-link to="/techs/Tower_Shields">塔盾</nuxt-link
+            >為長槍兵系和矛兵提供額外的遠程護甲。
+          </p>
+        </div>
+        <h3>概要</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <p>
+            立陶宛擁有完整的<nuxt-link to="/units/Cavalry_Units">騎兵</nuxt-link
+            >單位升級和科技，其<nuxt-link to="/units/Unique_Unit"
+              >特殊單位</nuxt-link
+            ><nuxt-link to="/units/Leitis">烈堤司</nuxt-link
+            >攻擊時更能無視歒方<nuxt-link to="/armor">裝甲</nuxt-link
+            >。<nuxt-link to="/units/Knight">騎士</nuxt-link
+            >系和烈堤司每駐守一件<nuxt-link to="/elements/Relic">遺跡</nuxt-link
+            >，<nuxt-link to="/elements/Attack">攻擊力</nuxt-link
+            >+1（最多+4），使立陶宛騎兵十分强大，甚至超越<nuxt-link
+              to="/civs/Berbers"
+              >柏柏</nuxt-link
+            >、<nuxt-link to="/civs/Franks">法蘭克</nuxt-link>或<nuxt-link
+              to="/civs/Magyars"
+              >馬扎爾</nuxt-link
+            >等騎兵文明。此外，立陶宛也擁有完整的<nuxt-link to="/units/Monk"
+              >僧侶</nuxt-link
+            >科技，加上<nuxt-link to="/building/Monastery">修道院</nuxt-link
+            >工作效率+20%的團隊加分。然而，<nuxt-link to="/units/Infantry_Units"
+              >步兵</nuxt-link
+            >和<nuxt-link to="/units/Cavalry_Archer">馬弓騎兵</nuxt-link
+            >並不突出。步兵單位缺乏<nuxt-link to="/techs/Plate_Mail_Armor"
+              >步兵板甲</nuxt-link
+            >，而<nuxt-link to="/units/Archer_Units">遠距離單位</nuxt-link
+            >缺乏<nuxt-link to="/units/Arbalest">強弩兵</nuxt-link
+            >升級和<nuxt-link to="/techs/Parthian_Tactics">安息人戰術</nuxt-link
+            >。但是，<nuxt-link to="/units/Skirmisher">矛兵</nuxt-link
+            >系和<nuxt-link to="/units/Spearman">長槍兵</nuxt-link
+            >系的移動速度提高了10％，並受益於特殊科技<nuxt-link
+              to="/techs/Tower_Shields"
+              >塔盾</nuxt-link
+            >，遠程護甲+2。其<nuxt-link to="/units/Siege_Weapons"
+              >攻城器</nuxt-link
+            >和<nuxt-link to="/units/Naval_Vessels">海軍</nuxt-link
+            >也是平淡無奇。在防禦上只缺乏<nuxt-link to="/techs/Arrowslits"
+              >箭狹槽</nuxt-link
+            >，但擁有特殊科技<nuxt-link to="/techs/Hill_Forts">山城堡</nuxt-link
+            >，<nuxt-link to="/building/Town_Center">城鎮中心</nuxt-link
+            >射程+3。經濟上只缺乏<nuxt-link to="/techs/Gold_Shaft_Mining"
+              >採金裝置</nuxt-link
+            >，但擁有開局+150<nuxt-link to="/resourses/Food">食物</nuxt-link
+            >的文明加成，是遊戲中最好的開局之一，因此經濟上還算不錯。
+          </p>
+          <h3>特殊單位</h3>
+          <li>
+            <img class="gg" src="~assets/Leitisicon.png" alt="" /><nuxt-link
+              to="/units/Leitis"
+              >烈堤司</nuxt-link
+            >：能無視單位裝甲的<nuxt-link to="/units/Cavalry_Units"
+              >騎兵</nuxt-link
+            >單位。
+          </li>
+          <h3>特殊科技</h3>
+          <li>
+            <img class="gg" src="~assets/unique_tech.png" alt="" /><nuxt-link
+              to="/techs/Hill_Forts"
+              >山城堡</nuxt-link
+            >：<nuxt-link to="/building/Town_Center">城鎮中心</nuxt-link
+            >射程+3。
+          </li>
+          <li>
+            <img
+              class="gg"
+              src="~assets/unique_tech_imperial.png"
+              alt=""
+            /><nuxt-link to="/techs/Tower_Shields">塔盾</nuxt-link>：<nuxt-link
+              to="/units/Spearman"
+              >長槍兵</nuxt-link
+            >系和<nuxt-link to="/units/Skirmisher">矛兵</nuxt-link>系<nuxt-link
+              to="/armor"
+              >遠程護甲</nuxt-link
+            >+1。
+          </li>
+        </div>
+        <h3>文明加成</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <ul>
+            <li>開局<nuxt-link to="/resourses/Food">食物</nuxt-link>+150</li>
+            <li>
+              <nuxt-link to="/units/Spearman">長槍兵</nuxt-link>系和<nuxt-link
+                to="/units/Skirmisher"
+                >矛兵</nuxt-link
+              >系移動速度提高10%
+            </li>
+            <li>
+              每駐守一件<nuxt-link to="/elements/Relic">遺跡</nuxt-link
+              >，<nuxt-link to="/units/Knight">騎士</nuxt-link>系和<nuxt-link
+                to="/units/Leitis"
+                >烈堤司</nuxt-link
+              >攻擊力+1(最高+4)
+            </li>
+            <li>
+              <strong
+                ><nuxt-link to="/elements/Team_Bonus"
+                  >團隊加分</nuxt-link
+                ></strong
+              >:<nuxt-link to="/building/Monastery">修道院</nuxt-link
+              >工作效率+20%
+            </li>
+          </ul>
+        </div>
+        <h3>科技樹</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <em><nuxt-link to="/tree/lit">立陶宛科技樹</nuxt-link></em>
+        </div>
+        <br />
+        <h3>更新日誌</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <h3>
+            <em><nuxt-link to="/games/de">決定版</nuxt-link></em>
+          </h3>
+          <ul>
+            <li>
+              文明加成中駐守遺跡的上限為5，並且影響所有騎兵。
+            </li>
+            <li
+            i>
+              (34055更新) 駐守遺跡上限降為4且不再影響<nuxt-link
+                to="/units/Light_Cavalry"
+                >輕騎兵</nuxt-link
+              >系。
+            </li>
+          </ul>
+        </div>
+        <h3>趣事</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <ul>
+            <li>
+              保加利亞的文明徽標來自於繪有格迪米纳斯之柱的塔盾，而格迪米納斯王朝從14世紀到16世紀統治着立陶宛。
+            </li>
+            <li>
+              立陶宛是唯一無法在單一戰役中可供遊玩的文明。
+            </li>
+          </ul>
+        </div>
+        <h3>圖庫</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <div class="row">
+            <div class="col-md-4">
+              <gallery
+                ><img
+                  class="imgn hg"
+                  src="~assets/WonderLithuanians.jpg"
+                  alt=""
+              /></gallery>
+              <p><em>立陶宛世界奇觀 特拉凱城堡</em></p>
+            </div>
+          </div>
+        </div>
+        <h3>歷史</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <p>
+            立陶宛人被認為是歐洲最古老的文明之一，由許多波羅的海部落組成。十三世紀初，教皇額我略九世對異教徒的立陶宛進行十字軍東征，在一個名叫明道加斯的上等公爵的領導下，成功擊敗了基督教勢力的寶劍利沃尼亞兄弟騎士團。然而，立陶宛後來仍然昄依了基督教，消除了十字軍東征的藉口，並成為了立陶宛歷史上的第一位也是唯一的國王，但在1263年，明道加斯卻被暗殺，並開始陷入了政治動亂。數十年後，立陶宛大公格迪米納斯建立了長期國家的基礎，他利用外交手段確保了立陶宛的國際地位，並在維爾紐斯建立了新的首都，邀請西方工匠和知識分子進入首都，並建立了山間堡壘網絡來保衛邊界，因此格迪米納斯被認為是立陶宛大公國的真正建國者，並統治了兩個多世紀。維陶塔斯大帝統治時，公國達到了頂峰，後來更在格倫瓦德之戰中指揮波蘭立陶宛聯軍擊敗條頓騎士團。
+          </p>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <table class="table-bordered" width="100%">
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#e6992c;">
+              <div class="unititle2">立陶宛</div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <div class="unititle">
+                <img src="~assets/Traku-pilis_trakaimuziejus.lt_.jpg" width="250" alt="" />
+                <p><em>位於立陶宛特拉凱省的特拉凱城堡</em></p>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#e6992c;">
+              <div class="unititle2">概要</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">建築風格</td>
+            <td class="unit4">東歐</td>
+          </tr>
+          <tr>
+            <td class="unit3">分類文明</td>
+            <td class="unit4">
+              <nuxt-link to="/units/Monk">僧侶</nuxt-link>與<nuxt-link
+                to="/units/Cavalry_Units"
+                >騎兵</nuxt-link
+              >
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">
+              <nuxt-link to="/units/Unique_Unit">特殊單位</nuxt-link>
+            </td>
+            <td class="unit4">
+              <img class="gg" src="~assets/Leitisicon.png" /><nuxt-link
+                to="/units/Leitis"
+                >烈堤司</nuxt-link
+              >
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">首選戰略</td>
+            <td class="unit4">
+              <nuxt-link to="/ar/brushtof">黑快轉封</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">特殊技術</td>
+            <td class="unit4">
+              <img class="gg" src="~assets/unique_tech.png" /><nuxt-link
+                to="/techs/Hill_Forts"
+                >山城堡</nuxt-link
+              >
+              <br />
+              <img
+                class="gg"
+                src="~assets/unique_tech_imperial.png"
+              /><nuxt-link to="/techs/Tower_Shields">塔盾</nuxt-link>
+            </td>
+          </tr>
+        </table>
+
+        <div class="thanks">此頁面由singsing612協助編輯</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'allciv',
+  components: {}
+}
+</script>
+<style></style>
