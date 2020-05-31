d0d8f9d52bae73dfbe72c7e0df8fee035788409b
harry3305 <52451150+harry3305@users.noreply.github.com>
2020-05-31 19:47:30 +0800

    Update Bapheus_1302.vue
    改為更新版

--- pages/batt/Bapheus_1302.vue
+++ pages/batt/Bapheus_1302.vue
@@ -2,13 +2,13 @@
   <div class="container">
     <div class="row padtop">
       <div class="col-md-12">
-        <h1>巴菲烏斯(1302)</h1>
+        <h1>兵臨巴菲烏斯(1302)</h1>
         <div class="hrn1"></div>
       </div>
       <div class="col-md-8">
         <div class="n1">
           <p>
-            <b>巴菲烏斯(1302)</b>是《<nuxt-link to="/games/aof"
+            <b>兵臨巴菲烏斯(1302)</b>是《<nuxt-link to="/games/aof"
               >世紀帝國II：失落的帝國</nuxt-link
             >》中<nuxt-link to="/batt/Battles_of_the_Forgotten"
               >失落的帝國的戰役</nuxt-link
@@ -26,14 +26,14 @@
             <tr>
               <td class="tc1" width="14%">起始時代：</td>
               <td class="tc2" width="15%">
-                <nuxt-link to="/techs/age/Feudal_Age">封建時代</nuxt-link>
+                城堡時代
               </td>
               <td class="tc1" width="14%">起始資源：</td>
               <td class="tc2">
-                無
+                500木材/400食物/200黃金/150石頭
               </td>
               <td class="tc1" width="14%">人口上限：</td>
-              <td class="tc2" width="5%">200</td>
+              <td class="tc2" width="5%">175</td>
             </tr>
           </table>
 
@@ -46,11 +46,11 @@
             </tr>
             <tr>
               <td class="tc6">
-                <div class="p4"></div>
+                <div class="p2"></div>
                 玩家1
               </td>
               <td class="tc6" colspan="3">
-                <nuxt-link to="/civs/Turks">土耳其</nuxt-link>（綠色）
+                <nuxt-link to="/civs/Turks">土耳其</nuxt-link>（紅色）
               </td>
             </tr>
 
@@ -59,10 +59,10 @@
             </tr>
             <tr>
               <td class="tc6">
-                <div class="p2"></div>
+                <div class="p6"></div>
                 玩家2
               </td>
-              <td class="tc6">羅姆蘇丹（紅色）土耳其</td>
+              <td class="tc6">魯姆蘇丹（粉紅色）土耳其</td>
             </tr>
 
             <tr>
@@ -72,42 +72,35 @@
             </tr>
             <tr>
               <td class="tc6">
-                <div class="p1"></div>
+                <div class="p3"></div>
                 玩家3
               </td>
-              <td class="tc6">卡裡希人(藍色)土耳其</td>
+              <td class="tc6">卡里希人(綠色)土耳其</td>
             </tr>
             <tr>
               <td class="tc6">
                 <div class="p4"></div>
                 玩家4
               </td>
-              <td class="tc6">格爾米揚人(黃色)土耳其</td>
+              <td class="tc6">蓋爾米揚人(黃色)土耳其</td>
             </tr>
             <tr>
               <td class="tc6">
                 <div class="p5"></div>
                 玩家5
               </td>
-              <td class="tc6">坎達人(青色)土耳其</td>
+              <td class="tc6">桑達爾人(青色)土耳其</td>
             </tr>
             <tr>
               <td class="tc4" colspan="4" style="background:#ea6153;">敵對</td>
             </tr>
             <tr>
               <td class="tc6">
-                <div class="p7"></div>
+                <div class="p1"></div>
                 玩家6
               </td>
-              <td class="tc6">突厥流寇（灰色）土耳其</td>
-            </tr>
-            <tr>
               <td class="tc6">
-                <div class="p6"></div>
-                玩家7
-              </td>
-              <td class="tc6">
-                拜占庭帝國（紫色）<nuxt-link to="/civs/Byzantines"
+                拜占庭帝國（藍）<nuxt-link to="/civs/Byzantines"
                   >拜占庭</nuxt-link
                 >
               </td>
@@ -115,10 +108,10 @@
             <tr>
               <td class="tc6">
                 <div class="p8"></div>
-                玩家8
+                玩家7
               </td>
               <td class="tc6">
-                卡特藍軍團（橙色）<nuxt-link to="/civs/Spanish"
+                加泰羅尼亞傭兵（橙色）<nuxt-link to="/civs/Spanish"
                   >西班牙</nuxt-link
                 >
               </td>
@@ -130,7 +123,7 @@
             <nuxt-link to="/units/hero/Osman">奧斯曼</nuxt-link>X1，<nuxt-link
               to="/units/Cavalry_Archer"
               >馬弓騎兵</nuxt-link
-            >X8，<nuxt-link to="/units/Light_Cavalry">輕騎兵</nuxt-link>X4
+            >X3，匈牙利輕騎兵X3
           </li>
 
           <h3>主要任務</h3>
@@ -138,34 +131,24 @@
           <div class="n1">
             <ol>
               <li>
-                奧斯曼必須存活。
+                奧斯曼必須活著。
               </li>
               <li>
-                找到五個突厥部落並說服他們加入魯姆蘇丹國的陣營。
+                找到奧斯曼家族的領土。
               </li>
               <li>
-                讓奧斯曼回去向蘇丹王覆命。
+                打敗拜占庭人，摧毀3座拜占庭城堡。
               </li>
-              <li>
-                從拜占庭人手中重奪瑟於特城。
-              </li>
-              <li>
-                摧毀所有拜占庭城池及所有敵人。
-              </li>
+
             </ol>
           </div>
-          <h3>選擇性任務</h3>
+          <h3>次要任務</h3>
           <div class="hrn1"></div>
           <div class="n1">
             <ul>
+              <li>您可以選擇一個其他的土耳其勢力，派奧斯曼前往他們的城堡結成同盟。您有10分鐘的時間選擇結盟，每個勢力都會免費贈與您資源與科技。您也可以選擇不結盟。</li>
               <li>
-                把滿載資源的貨運車運送到瑟於特的<nuxt-link
-                  to="/building/Town_Center"
-                  >城鎮中心</nuxt-link
-                >去。
-              </li>
-              <li>
-                為奧斯曼選擇一位新娘。你只需要把奧斯曼帶到其中一位新娘的身邊即可。每個突厥勢力都會給你不同的好處，所以你要小心地做出選擇。如果15分鐘內你不做出選擇的話奧斯曼將永遠單身。
+                打敗卡里希人、蓋爾米揚人、桑達爾人。（若未與之結盟）
               </li>
             </ul>
           </div>
@@ -174,17 +157,13 @@
           <div class="n1">
             <ul>
               <li>
-                注意不是所有的首領都願意加入魯姆蘇丹國的陣營，他們甚至可能反戈加入流寇，如果那樣的話你唯有殺掉他們來招安他們部下的軍隊。
+               鄂圖曼人上限為175，同時他們還被四面八方的敵對勢力包圍，因此將疲於應付所有敵人。
               </li>
               <li>
-                從你最強大突厥勢力裡挑選新娘聯姻，因為你得消滅其他你不想和親的勢力。
+                其他土耳其勢力也許有興趣跟奧斯曼結盟，但他們彼此都是勁敵，您只能選擇一個盟友，因此要深思熟慮。
               </li>
               <li>
-                除了<nuxt-link to="/units/Janissary">土耳其火槍兵</nuxt-link
-                >之外，你不能招募任何其他的黑<nuxt-link
-                  to="/units/Gunpowder_Unit"
-                  >火藥單位</nuxt-link
-                >。
+                土耳其人可選的兵種十分豐富。是保留游牧傳統，依靠高機動力騎兵，還是放棄部落生活，創建一支由土耳其火槍兵和其他熱兵器單位所組成的軍隊？
               </li>
             </ul>
           </div>
@@ -193,130 +172,72 @@
           <div class="n1">
             <ul>
               <li>
-                奧斯曼(綠色)和他的軍隊起始於地圖東邊，和羅姆蘇丹人(紅色)一起。
+                奧斯曼(1，紅色)和他鄂圖曼士兵最初位於東邊的魯姆蘇丹國(2，紫色)。蘇丹國被蒙古人占領了，因此鄂圖曼必須向西取回屬於他的家族的領土。
               </li>
               <li>
-                拜占庭人(紫色)在地圖中間和西北角有大營。
+                拜占庭帝國(6，藍色)在西北方佔據著數座防守堅固的城市，但隨著帝國的衰落，這些城市都不再穩固。拜占庭的守城力量包括拜占庭聖騎兵、重裝長槍兵和擲矛手。據傳拜占庭還僱用了來自伊比利亞的歐洲人為他們戰鬥。
               </li>
               <li>
-                有三個突厥部落，分別是西邊的卡裡希人(藍色)，南邊的格爾米揚人(黃色)和東邊的坎達人(青色)。
+                這片區域內還有三股較小的土耳其勢力：西邊的卡里希、南邊的蓋爾米揚和東邊的桑達爾。
               </li>
               <li>
-                這片區域遍布了敵對的突厥流寇(灰色)，傳言說一支名為卡特蘭的雇傭軍團(橙色)正氣勢洶洶地從西邊殺過來。
+                卡里希(3，綠色)是唯一一個靠近水域的土耳其勢力，為了抗衡拜占庭人的海上霸權，他們造出了戰艦。他們的陸地兵種包括矛兵、馬騎弓兵、匈牙利輕騎兵和攻城單位。最近，他們將用火槍兵取代矛兵。
+              </li>
+              <li>
+                蓋爾米揚(4，黃色)十分看重游牧傳統，據說他們的騎射手從來不會射偏。他們的軍隊中包括輕騎兵、駱駝騎兵和馬騎弓兵，定期掃蕩敵人。
+              </li>
+              <li>
+                桑達爾(5，青色)已經轉向重裝騎兵和防禦工事，其水平堪與歐洲人比肩。在與敵人在戰場上正面交鋒之際，他們會充分利用自己的防守優勢。他們的擔位包括劍兵和重裝騎兵。
               </li>
             </ul>
           </div>
 
           <h3>攻略</h3>
           <div class="hrn1"></div>
-          <p>第一階段</p>
           <ul>
             <li>
-              玩家的任務是先與五個部落會面，然後再回到他們開始的地方。在玩家沒有順著故事進展遊玩前，電腦都不會開始採集或建立部隊。這意味著玩家應該利用這個機會探索全地圖並完全消滅突厥流寇。但這應該非常緩慢謹慎地進行，因為玩家只有有限數量的<nuxt-link
-                to="/units/Cavalry_Units"
-                >騎兵</nuxt-link
-              >和馬弓騎兵。盡量用奧斯曼去探查地圖，玩家也會遇到幾個加入他們軍隊的單位，單位受傷時盡量使用<nuxt-link
-                to="/building/Castle"
-                >城堡</nuxt-link
-              >駐紮<nuxt-link to="/Regeneration">回血</nuxt-link
-              >，標準難度會有<nuxt-link to="/units/Monk">僧侶</nuxt-link
-              >幫玩家治療。摧毀突厥流寇的<nuxt-link to="/building/Blacksmith"
-                >兵工廠</nuxt-link
-              >可以讓玩家升級單位的裝甲及攻擊，但這對後來的遊戲來說都不是那麼重要。
+             奧斯曼必須帶著一小匹軍隊到他的領土，陸上會有一匹拜占庭聖騎兵和重裝長槍兵，將之解決後到達領土會得到6位村民，運用這些村民建立自己的基地。
             </li>
             <li>
-              在正常的難度中，五個陣營中的一個會有一個和尚（上面的藍色），兩個將遵守命令並且協助蘇丹王，另外兩個（較低的藍色和東邊較遠的青色）將抵抗和玩家戰鬥直到他們的領導者被殺，所以玩家應該瞄準敵方過多單位受傷。一旦領導被殺其他單位將立即變為紅色（魯姆蘇丹國）。在難的難度中，會有更多陣營將與玩家抗爭。
-            </li>
+                隨後會要你在10分鐘內從三個同盟中選擇一個，只要派奧斯曼到達他們的城堡即可。如果都不選擇則會在10分鐘過後全改敵對，但將之全打敗後可以獲得成就「土耳其軟糖」。可以利用這段時間好好發展、圍一下基地，等時間快到再去預定要選擇的同盟國城堡。
+              </li>
             <li>
-              玩家還可以先計劃放置基地的位置。三個城鎮裡的一個（坎達人/卡裡希人/格爾米揚人）將成為盟友，因此隱藏在他們身後可能是一個好主意。如果選擇格爾米揚人(黃色)，基地可以設在下方角落。卡裡希人(藍色)僅受到<nuxt-link
-                to="/building/Palisade_Wall"
-                >木牆</nuxt-link
-              >的保護。坎達人(青色)小鎮背後有很多森林，有足夠的空間可以放置一個大基地，但比黃色的地點來說更難用牆擋住。玩家可能還想從各地收集<nuxt-link
-                to="/units/animal/Sheep"
-                >綿羊</nuxt-link
-              >並將它們圍起來放在一個安全的地方。
-            </li>
+                與各個國家同盟後皆會取得相關科技：與卡里希同盟會取得弩砲戰船、重型弩砲戰船、航海技術、船塢、造船匠等海軍科技；與蓋爾米揚同盟會取得羽箭、安息人戰術、重裝馬騎弓兵、采邑騎兵等弓騎兵相關科技；與桑達爾同盟會取得重裝騎士、防禦箭塔、鍊鎧甲、磚瓦技術、建築學、近射孔、垛牆等重騎兵與防禦相關科技。同時各國也會不時出兵，出兵兵種請見斥侯的部份。如果要選同盟則建議選卡里希，主要是因為使用的兵種比較強。
+              </li>
+            <li>
+                在戰役進行的過程中，我方不時會有援軍從開場地點抵達。其中包括無法訓練的阿拉伯奴隸兵。當打下第一座拜占庭的城堡時，會有一支加泰羅尼亞傭兵的軍隊攻向我方基地，同時會在我方基地的東北角開始建立基地。掃蕩時請帶攻城武器，因為會建立火砲塔鞏固基地的防禦。將之掃蕩完畢後，再將其他兩座拜占庭城堡摧毀即可完成任務。
+              </li>
+            <li>
+                拜占庭帝國的領土幾乎都在精銳火砲戰船的砲擊範圍，因此創造一群強大的海軍艦隊也許是不錯的考量。如果同盟選擇卡里希更是直接幫你研發海上相關科技，自己只要研發火砲戰船即可。
+              </li>
           </ul>
-
-          <p>第二階段</p>
+          
+                    <h3>決定版變更點</h3>
+          <div class="hrn1"></div>
           <ul>
             <li>
-              當玩家帶著奧斯曼返回蘇丹城時，拜占庭人會將外交狀態切換到敵人，因此他們現在可以被玩家攻擊了。玩家現在的任務是奪回拜占庭人在地圖中央的湖旁的城鎮中心，但在此之前玩家應該盡可能地攻擊和摧毀拜占庭人的<nuxt-link
-                to="/building"
-                >建築</nuxt-link
-              >（例如東北部沒有妥善保護的村莊）。只要玩家遠離目標的城鎮中心，遊戲都還不會啟動，電腦不會做任何事情。在奪回城鎮中心之前，請務必盡可能地利用這一點。
+            刪去和部落會面的部份，直接從建立基地、選擇同盟開始。選擇同盟之後會獲得相關科技，選擇時間由15分減為10分。
             </li>
+            <li>
+                新增成就「土耳其軟糖」：不與任一方同盟，將所有敵人全部擊敗。
+              </li>
+               
           </ul>
-
-          <p>第三階段</p>
-          <ul>
-            <li>
-              一旦城鎮中心被奪回遊戲就會開始轉動。這時要妥善保護貿易馬車，因為它們是玩家能使用的唯一資源，即在貿易馬車到達目的地之前不能建造任何<nuxt-link
-                to="/units/Villager"
-                >村民</nuxt-link
-              >。由於這是一個戰略上非常糟糕的時刻（玩家會受到各方的攻擊），玩家應該迅速收集足夠的資源並在其他地方馬上建立一個新的城鎮中心。
-            </li>
-            <li>
-              玩家不應該在魯姆蘇丹國的主要基地放置<nuxt-link to="/building"
-                >建築物</nuxt-link
-              >，因為它們那裡並不安全（魯姆蘇丹國與所有人結盟，見上文），而且不定時會有持續的增援抵達蘇丹王的城堡（<nuxt-link
-                to="/units/Hussar"
-                >匈牙利輕騎兵</nuxt-link
-              >，<nuxt-link to="/units/Imperial_Camel">帝王駱駝騎兵</nuxt-link
-              >，<nuxt-link to="/units/Mameluke">阿拉伯奴隸兵</nuxt-link
-              >，<nuxt-link to="/units/Bombard_Cannon">火砲</nuxt-link
-              >和一位拿著<nuxt-link to="/Relic">遺跡</nuxt-link
-              >的僧侶），隨著時間的推移直到遊戲結束（最終可能是數百個單位），但如果玩家將<nuxt-link
-                to="/building/House"
-                >房屋</nuxt-link
-              >放在地圖下緣，這些增援可能會被擋住。
-            </li>
-
-            <li>
-              三個土耳其城鎮將為奧斯曼提供和親的選擇，而玩家選中的那一個將成為盟友，另外兩個將變成敵對。選卡裡希人會給600肉和600木；格爾米揚人給500肉和400金；坎達人給600肉和300<nuxt-link
-                to="/resource/Stone"
-                >石頭</nuxt-link
-              >。一旦獲得新娘馬上帶著奧斯曼跟她回到城鎮中心才算完成任務。若沒有在15分鐘內選擇任一新娘或是沒將新娘帶回城鎮則三個部落都會變為敵對。
-            </li>
-            <li>
-              卡特蘭軍團會在稍後的某個點變為玩家的敵人。卡特蘭軍團的營地在地圖右邊，軍隊由<nuxt-link
-                to="/units/Halberdier"
-                >戟兵</nuxt-link
-              >、<nuxt-link to="/units/Two-Handed_Swordsman">雙手劍兵</nuxt-link
-              >、重裝騎士、輕騎兵、<nuxt-link to="/units/Capped_Ram"
-                >裝甲衝撞車</nuxt-link
-              >，<nuxt-link to="/units/Trebuchet">巨型投石機</nuxt-link
-              >組成。面對他們的時候玩家要全力以赴，<nuxt-link
-                to="/units/Mangonel"
-                >投石車</nuxt-link
-              >搭配土耳其火槍兵可以很好的消滅牠們。要消滅橘色只要派船把他們的<nuxt-link
-                to="/units/hero"
-                >英雄</nuxt-link
-              >打死即可。當所有敵人被殲滅後玩家將獲得最終勝利。
-            </li>
-            <li>
-              拜占庭帝國的領土幾乎都在精銳<nuxt-link to="/units/Cannon_Galleon"
-                >火砲戰船</nuxt-link
-              >的砲擊範圍，因此創造一群強大的<nuxt-link
-                to="/units/Naval_Vessels"
-                >海軍</nuxt-link
-              >艦隊也許是不錯的考量。
-            </li>
-          </ul>
-        </div>
+          
+        </div>        
       </div>
 
       <div class="col-md-4">
         <table class="table-bordered" width="100%">
           <tr>
             <td class="unit" colspan="2" style="background-color:#375d96;">
-              <div class="unititle2">巴菲烏斯(1302)</div>
+              <div class="unititle2">兵臨巴菲烏斯(1302)</div>
             </td>
           </tr>
           <tr>
             <td colspan="2">
               <div class="unititle">
-                <img src="~assets/Bapheus_map.png" width="100%" alt="" />
+                (決定版地圖待補)
               </div>
             </td>
           </tr>
@@ -334,9 +255,7 @@
           <tr>
             <td class="unit3">戰役</td>
             <td class="unit4">
-              <nuxt-link to="/batt/Battles_of_the_Forgotten"
-                >失落的帝國的戰役</nuxt-link
-              >
+              歷史戰役
             </td>
           </tr>
           <tr>
@@ -353,8 +272,7 @@
           <tr>
             <td class="unit3">章節</td>
             <td class="unit4">
-              <nuxt-link to="/games/aof">失落的帝國</nuxt-link>第七章之八
-            </td>
+              歷史戰役
           </tr>
           <tr>
             <td class="unit3">上一關</td>
