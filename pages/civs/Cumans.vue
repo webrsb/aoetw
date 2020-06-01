da6a6e61d8a26c42f1fb3c9dd9a2ea931da44f03
singsing612 <62696812+singsing612@users.noreply.github.com>
2020-06-01 20:56:50 +0800

    Create Cumans.vue (#446)
    新增決定版庫曼文明

---
+++ pages/civs/Cumans.vue
@@ -0,0 +1,445 @@
+<template>
+  <div class="container">
+    <div class="row padtop">
+      <div class="col-md-12">
+        <h1><img src="~assets/cumans.png" />庫曼</h1>
+        <div class="hrn1"></div>
+      </div>
+      <div class="col-md-8">
+        <div class="n1">
+          <p>
+            <b>庫曼</b>是《<nuxt-link to="/games/de"
+              >世紀帝國II：決定版</nuxt-link
+            >》新增著重於<nuxt-link to="/units/Cavalry_Units">騎兵</nuxt-link
+            >的中亞文明。
+          </p>
+          <p>
+            從歷史上看，庫曼人源自於突厥人，鼎盛時期居住在黑海和窩瓦河之間的地區，與欽察人同盟，並影響着基輔羅斯、佩切涅格人、<nuxt-link
+              to="/civs/Byzantines"
+              >拜占庭</nuxt-link
+            >帝國等鄰國的政治，直到13世紀中葉遭到<nuxt-link to="/civs/Mongols"
+              >蒙古</nuxt-link
+            >人的入侵而流亡。
+          </p>
+          <p>
+            庫曼戰士都是優秀的騎師，平常以<nuxt-link to="/units/Light_Cavalry"
+              >輕騎兵</nuxt-link
+            >作戰，並手持弓箭和其他武器。因此庫曼人可以研發<nuxt-link
+              to="/techs/untech"
+              >特殊科技</nuxt-link
+            ><nuxt-link to="/techs/Steppe_Husbandry">大草原耕種技術</nuxt-link
+            >，以提高輕騎兵、<nuxt-link to="/units/Steppe_Lancer"
+              >草原騎兵</nuxt-link
+            >和<nuxt-link to="/units/Cavalry_Archer">馬弓騎兵</nuxt-link
+            >訓練速度。此外，庫曼人還能訓練能一次多發的馬弓<nuxt-link
+              to="/units/Unique_Unit"
+              >特殊單位</nuxt-link
+            ><nuxt-link to="/units/Kipchak">欽察</nuxt-link>。
+          </p>
+          <p>
+            由於庫曼人是游牧民族，自小到大就在馬背上，因此他們的騎兵擁有出色的機動性，一旦情況不利，便能迅速遷移到另一地方。為此，庫曼騎兵的速度比其他大多數文明的騎兵快。
+          </p>
+          <p>
+            在蒙古帝國崛起前，庫曼人居住在歐亞大陸的廣闊地區，即所謂的庫曼汗國。當他們定居時，便會開始建造防禦工事。因此，庫曼人擁有<nuxt-link
+              to="/elements/Hit_points"
+              >血量</nuxt-link
+            >更高的<nuxt-link to="/building/Palisade_Wall">木牆</nuxt-link
+            >的團隊加分。
+          </p>
+          <p>
+            當庫曼汗國被蒙古人征服後，庫曼人逃至東歐並建立了新的定居點。為了體現出能在新土地上快速建立勢力的能力，庫曼人能在<nuxt-link
+              to="/techs/age/Feudal_Age"
+              >封建時代</nuxt-link
+            >建立一個額外的<nuxt-link to="/building/Town_Center"
+              >城鎮中心</nuxt-link
+            >。另外，庫曼戰士也經常向歐洲各國的統治者提供傭兵服務，因此庫曼人可以研發特殊科技<nuxt-link
+              to="/techs/Cuman_Mercenaries"
+              >庫曼傭兵</nuxt-link
+            >，使盟友能免費生產10名精銳欽察。
+          </p>
+          <p>
+            儘管庫曼人以野戰聞名，但他們也能圍攻佩列亞斯拉夫和塞薩洛尼基等軍事要塞，因此，庫曼人能在封建時代就可以建造<nuxt-link
+              to="/building/Siege_Workshop"
+              >攻城器製造所</nuxt-link
+            >和<nuxt-link to="/units/Battering_Ram">輕型衝撞車</nuxt-link
+            >，升上<nuxt-link to="/techs/age/Castle_Age">城堡時代</nuxt-link
+            >更可以直接使用<nuxt-link to="/units/Capped_Ram"
+              >裝甲衝撞車</nuxt-link
+            >。
+          </p>
+        </div>
+        <h3>概要</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <p>
+            庫曼人是<nuxt-link to="/units/Cavalry_Units">騎兵</nuxt-link
+            >文明，因此騎兵十分優秀擁有全滿的<nuxt-link to="/units/Militia"
+              >匈牙利輕騎兵</nuxt-link
+            >、<nuxt-link to="/units/Paladin">遊俠</nuxt-link>和<nuxt-link
+              to="/units/Steppe_Lancer"
+              >草原騎兵</nuxt-link
+            >，以及特殊科技<nuxt-link to="/techs/Steppe_Husbandry"
+              >大草原耕種技術</nuxt-link
+            >，使<nuxt-link to="/units/Light_Cavalry">輕騎兵</nuxt-link
+            >和草原騎兵訓練速度<abbr title="科技樹誤植為100%">+50%</abbr
+            >。此外，騎兵從<nuxt-link to="/techs/age/Feudal_Age"
+              >封建時代</nuxt-link
+            >開始每個時代速度提升5%。但奇怪的是，庫曼是唯一擁有<nuxt-link
+              to="/units/Camel"
+              >駱駝騎兵</nuxt-link
+            >但無法升級為<nuxt-link to="/units/Heavy_Camel"
+              >重裝駱駝騎兵</nuxt-link
+            >的文明。
+          </p>
+          <p>
+            庫曼的<nuxt-link to="/units/Cavalry_Archer">馬弓騎兵</nuxt-link
+            >也十分出色，擁有高攻速的<nuxt-link to="/units/Kipchak"
+              >欽察</nuxt-link
+            >以及大草原耕種技術使馬弓騎兵訓練速度+50%，但缺乏<nuxt-link
+              to="/techs/Bracer"
+              >強力護腕</nuxt-link
+            >使馬弓騎兵在<nuxt-link to="/techs/age/Imperial_Age"
+              >帝王時代</nuxt-link
+            >變得虛弱。也因為缺乏強力護腕、<nuxt-link to="/units/Arbalest"
+              >強弩兵</nuxt-link
+            >和<nuxt-link to="/units/Hand_Cannoneer">火槍兵</nuxt-link
+            >相當虛弱。<nuxt-link to="/units/Infantry_Units">步兵</nuxt-link
+            >方面，擁有完整的步兵系統，但沒有任何相關的文明加成。
+          </p>
+          <p>
+            <nuxt-link to="/units/Siege_Weapons">攻城器單位</nuxt-link
+            >上，擁有在封建時代就可以使用<nuxt-link to="/units/Battering_Ram"
+              >衝撞車</nuxt-link
+            >的大優勢，但在後期時，庫曼缺乏<nuxt-link
+              to="/techs/Siege_Engineers"
+              >攻城工程師</nuxt-link
+            >、<nuxt-link to="/units/Heavy_Scorpion">重型弩砲</nuxt-link
+            >和<nuxt-link to="/units/Bombard_Cannon">火砲</nuxt-link
+            >。而庫曼<nuxt-link to="/units/Naval_Vessels">海軍</nuxt-link
+            >是遊戲中最差的海軍之一，缺乏<nuxt-link to="/units/Cannon_Galleon"
+              >火砲戰船</nuxt-link
+            >、<nuxt-link to="/units/Heavy_Demolition_Ship"
+              >重型爆破船</nuxt-link
+            >、強力護腕、<nuxt-link to="/techs/Dry_Dock">船塢</nuxt-link
+            >和<nuxt-link to="/techs/Shipwright">造船員</nuxt-link>。<nuxt-link
+              to="/units/Monk"
+              >僧侶</nuxt-link
+            >也十分平庸，缺乏<nuxt-link to="/techs/Redemption"
+              >神聖思想</nuxt-link
+            >、<nuxt-link to="/techs/Illumination">啟示思想</nuxt-link
+            >、<nuxt-link to="/techs/Block_Printing">印刷技術</nuxt-link
+            >和<nuxt-link to="/techs/Theocracy">神權統治</nuxt-link
+            >。與<nuxt-link to="/civs/Goths">哥德</nuxt-link
+            >一樣，庫曼無法建造<nuxt-link to="/building/Stone_Wall"
+              >石牆</nuxt-link
+            >，但擁有<nuxt-link to="/building/Palisade_Wall">木牆</nuxt-link
+            ><nuxt-link to="/elements/Hit_points">血量</nuxt-link
+            >+50%的團隊加分。此外，防禦方面上，缺乏<nuxt-link
+              to="/building/Guard_Tower"
+              >防禦箭塔</nuxt-link
+            >、<nuxt-link to="/building/Bombard_Tower">火砲塔</nuxt-link
+            >、<nuxt-link to="/techs/Arrowslits">箭狹槽</nuxt-link>、<nuxt-link
+              to="/techs/Treadmill_Crane"
+              >磨坊水車</nuxt-link
+            >和<nuxt-link to="/techs/Architecture">建築學</nuxt-link
+            >。經濟方面十分穩固，只缺乏<nuxt-link to="/techs/Stone_Shaft_Mining"
+              >採石裝置</nuxt-link
+            >，但是可以在封建時代建造多一個<nuxt-link to="/building/Town_Center"
+              >城鎮中心</nuxt-link
+            >，因此可以彌補這一缺點。
+          </p>
+          <h3>特殊單位</h3>
+          <li>
+            <img class="gg" src="~assets/Kipchakicon.png" alt="" /><nuxt-link
+              to="/units/Kipchak"
+              >欽察</nuxt-link
+            >：能一次多發的<nuxt-link to="/units/Cavalry_Archer"
+              >馬弓騎兵</nuxt-link
+            >。
+          </li>
+          <h3>特殊科技</h3>
+          <li>
+            <img class="gg" src="~assets/unique_tech.png" alt="" /><nuxt-link
+              to="/techs/Steppe_Husbandry"
+              >大草原耕種技術</nuxt-link
+            >：<nuxt-link to="/units/Light_Cavalry">輕騎兵</nuxt-link
+            >、<nuxt-link to="/units/Steppe_Lancer">草原騎兵</nuxt-link
+            >和馬弓騎兵訓練速度+50%。
+          </li>
+          <li>
+            <img
+              class="gg"
+              src="~assets/unique_tech_imperial.png"
+              alt=""
+            /><nuxt-link to="/techs/Cuman_Mercenaries">庫曼傭兵</nuxt-link
+            >：隊伍成員能在<nuxt-link to="/building/Castle">城堡</nuxt-link
+            >免費生產10名精銳欽察。
+          </li>
+        </div>
+        <h3>文明加成</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <ul>
+            <li>
+              <nuxt-link to="/techs/age/Feudal_Age">封建時代</nuxt-link
+              >可以建造額外的<nuxt-link to="/building/Town_Center"
+                >城鎮中心</nuxt-link
+              >
+            </li>
+            <li>
+              封建時代可以建造<nuxt-link to="/building/Siege_Workshop"
+                >攻城器製造所</nuxt-link
+              >和<nuxt-link to="/units/Battering_Ram">輕型衝撞車</nuxt-link
+              >，<nuxt-link to="/techs/age/Castle_Age">城堡時代</nuxt-link
+              >可以使用<nuxt-link to="/units/Capped_Ram">裝甲衝撞車</nuxt-link
+              >升級
+            </li>
+            <li>
+              每個時代<nuxt-link to="/units/Cavalry_Units">騎兵</nuxt-link
+              >速度提升5%(從封建時代開始)
+            </li>
+            <li>
+              <strong
+                ><nuxt-link to="/elements/Team_Bonus"
+                  >團隊加分</nuxt-link
+                ></strong
+              >:<nuxt-link to="/building/Palisade_Wall">木牆</nuxt-link
+              ><nuxt-link to="/elements/Hit_points">血量</nuxt-link>+50%
+            </li>
+          </ul>
+        </div>
+        <h3>科技樹</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <em><nuxt-link to="/tree/cum">庫曼科技樹</nuxt-link></em>
+        </div>
+        <br />
+        <h3>更新日誌</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <h3>
+            <em><nuxt-link to="/games/de">決定版</nuxt-link></em>
+          </h3>
+          <ul>
+            <li>
+              最初可以研發<nuxt-link to="/techs/Redemption">神聖思想</nuxt-link
+              >、<nuxt-link to="/techs/Husbandry">耕種技術</nuxt-link>。
+            </li>
+            <li>
+              最初欽察有1.8的視野範圍。
+            </li>
+            <li>
+              最初，從<nuxt-link to="/techs/age/Feudal_Age">封建時代</nuxt-link
+              >開始，<nuxt-link to="/units/Cavalry_Units">騎兵</nuxt-link
+              >的移動速度提高10％。
+            </li>
+            <li>
+              (34055更新) (精鋭)<nuxt-link to="/units/Kipchak">欽察</nuxt-link
+              >有40 (45) HP和2.2的攻速。
+            </li>
+            <li>
+              (34055更新) 欽察增加視野範圍為2.2。
+            </li>
+            <li>
+              (34055更新) 精鋭欽察一次三發。
+            </li>
+            <li>
+              (34055更新) 封建時代建造<nuxt-link to="/building/Town_Center"
+                >城鎮中心</nuxt-link
+              >的時間從225秒增加到270秒。
+            </li>
+            <li>
+              (34055更新) 封建/<nuxt-link to="/techs/age/Castle_Age"
+                >城堡</nuxt-link
+              >/<nuxt-link to="/techs/age/Imperial_Age">帝王時代</nuxt-link
+              >的騎兵速度提高為5%/ 10%/ 15%。
+            </li>
+            <li>
+              (36202更新) 城堡時代能研發<nuxt-link to="/units/Capped_Ram"
+                >裝甲衝撞車</nuxt-link
+              >。
+            </li>
+            <li>
+              (36202更新) 將神聖思想及耕種技術從科技樹移除
+            </li>
+            <li>
+              (36202更新) 欽察一次三發，精鋭欽察一次四發。
+            </li>
+            <li>
+              (36202更新)
+              <nuxt-link to="/techs/Steppe_Husbandry">大草原耕種技術</nuxt-link
+              >受益於<nuxt-link to="/units/Steppe_Lancer">草原騎兵</nuxt-link
+              >，但訓練速度從+80％降為+50％。
+            </li>
+          </ul>
+        </div>
+        <h3>趣事</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <ul>
+            <li>
+              庫曼是唯一能在<nuxt-link to="/techs/age/Feudal_Age"
+                >封建時代</nuxt-link
+              >建造額外的<nuxt-link to="/building/Town_Center"
+                >城鎮中心</nuxt-link
+              >，<nuxt-link to="/building/Siege_Workshop"
+                >攻城器製造所</nuxt-link
+              >和<nuxt-link to="/units/Battering_Ram">衝撞車</nuxt-link
+              >的文明，也是唯一擁有<nuxt-link to="/units/Camel"
+                >駱駝騎兵</nuxt-link
+              >但無法升級為<nuxt-link to="/units/Heavy_Camel"
+                >重裝駱駝騎兵</nuxt-link
+              >的文明。
+            </li>
+            <li>
+              與<nuxt-link to="/civs/Goths">哥德</nuxt-link
+              >一樣，庫曼是無法建造<nuxt-link to="/building/Stone_Wall"
+                >石牆</nuxt-link
+              >的文明。而且也與<nuxt-link to="/civs/Malians">馬利</nuxt-link
+              >一樣，可以升級為<nuxt-link to="/units/Siege_Onager"
+                >重型投石車</nuxt-link
+              >但無法研究<nuxt-link to="/techs/Siege_Engineers"
+                >攻城工程師</nuxt-link
+              >。
+            </li>
+            <li>
+              庫曼人的文明徽標來自於庫曼<nuxt-link to="/units/Kipchak"
+                >欽察</nuxt-link
+              >戰士所佩戴的面具。其面具的設計相當普遍，薩珊人、塞琉古人、古典和後來的羅馬人以及15世紀的<nuxt-link
+                to="/civs/Persians"
+                >波斯</nuxt-link
+              >騎兵也佩戴過類似設計的面具。<nuxt-link
+                to="/units/Elite_Steppe_Lancer"
+                >精鋭草原騎兵</nuxt-link
+              >也佩戴著類似的面具。
+            </li>
+            <li>
+              伊斯蘭歷史學家曾說一名叫Qun的人被叫Qay的<nuxt-link
+                to="/civs/Mongols"
+                >蒙古</nuxt-link
+              >人趕出了東亞家園，後來又被表親的契丹人驅逐。
+              Qun可能是庫曼人的祖先（因為蒙古人記錄的庫曼人的縮寫為Kun）。
+            </li>
+            <li>
+              庫曼世界奇觀Sarkel要塞是由西突厥的可薩人建造的。遊戲中的世界奇觀已成廢墟，就像<nuxt-link
+                to="/civs/Huns"
+                >匈奴</nuxt-link
+              >人的世界奇觀君士坦丁凱旋門廢墟一樣，説明遊戲中的庫曼人只是佔領了波斯中亞的建築物而已，就像匈奴人佔領的中歐建築物一樣。
+            </li>
+            <li>
+              實際上，庫曼-欽察的歷史痕跡能到達花剌子模，該地區擁有豐富的突厥-波斯文化。
+            </li>
+            <li>
+              一些庫曼的AI以可薩人和佩切涅格人的統治者命名，這可能説明庫曼人不僅庫曼汗國/欽察-庫曼邦聯，而且還代表歷史上其他曾佔領東歐大草原的突厥人。
+            </li>
+            <li>
+              由於擁有與<nuxt-link to="/civs/Mongols">蒙古</nuxt-link
+              >人相同的建築風格，庫曼的修道院是根據一座清真寺設計的，但是在早期，可薩人、佩切涅格人和欽察-庫曼邦聯都不是穆斯林，而是崇拜騰格里的異教徒，在後來才歸付亞伯拉罕諸教。因此，可薩人、佩切涅格人和欽察-庫曼邦聯擁有多元的宗教，後來庫曼人更與其他種族（<nuxt-link
+                to="/civs/Magyars"
+                >馬扎爾</nuxt-link
+              >人，<nuxt-link to="/civs/Bulgarians">保加利亞</nuxt-link
+              >人，<nuxt-link to="/civs/Slavs">斯拉夫</nuxt-link
+              >人，欽察人，韃靼人，阿拉伯人等）同化，其後代不僅信仰伊斯蘭教，甚至還信仰基督教。
+            </li>
+          </ul>
+        </div>
+        <h3>圖庫</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <div class="row">
+            <div class="col-md-4">
+              <gallery
+                ><img class="imgn hg" src="~assets/AoE2DE_Cuman_Wonder.jpg" alt=""
+              /></gallery>
+              <p><em>庫曼世界奇觀 Sarkel要塞</em></p>
+            </div>
+          </div>
+        </div>
+        <h3>歷史</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <p>
+            庫曼人起源於黃河以東的突厥部落，在與契丹部落交戰後趕出家園，在10世紀向西遷移至哈薩克草原，並與另一個突厥部落欽察人合併，在12世紀和13世紀統治了當今保加利亞和哈薩克之間的廣大領土。後來向俄羅斯的基輔、巴爾幹半島和哈拉茲姆王國發起了多次戰役。最著名的是首領邦歷克（C.
+            Boniak）協助拜占庭皇帝對抗佩切涅格人對拉維尼歐的入侵，突襲了基輔附近的幾座修道院和擊敗了匈牙利國王卡爾曼。但是，庫曼人也會運用了和親和傭兵服務等外交手段以擴大對周邊國家的影響力。然而在13世紀初期，蒙古和韃靼軍隊征服了庫曼汗國。可汗忽炭（Khan
+            Kotyan）試圖通過與羅斯結盟來抵抗，但在1223年於卡利奇克河遭受慘敗，許多庫曼人向各處流亡並被當地居民吸收，但更多人是被俘虜作為奴隸出售。埃及蘇丹沙伊爾阿尤比（Al-Sahil
+            Ayyubi）購買了許多庫曼人和欽察人訓練為馬穆魯克。1250年，馬穆魯克利用自己的軍事力量奪取了埃及的政權，並建立了馬穆魯克蘇丹國。
+          </p>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <table class="table-bordered" width="100%">
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#e6992c;">
+              <div class="unititle2">庫曼</div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <div class="unititle">
+                <img
+                  src="~assets/sarkel2.jpg"
+                  width="250"
+                  alt=""
+                />
+                <p><em>位於俄羅斯羅斯托夫州的Sarkel要塞</em></p>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#e6992c;">
+              <div class="unititle2">概要</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">建築風格</td>
+            <td class="unit4">中亞</td>
+          </tr>
+          <tr>
+            <td class="unit3">分類文明</td>
+            <td class="unit4">
+              <nuxt-link to="/units/Cavalry_Units">騎兵</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">
+              <nuxt-link to="/units/Unique_Unit">特殊單位</nuxt-link>
+            </td>
+            <td class="unit4">
+              <img class="gg" src="~assets/Kipchakicon.png" /><nuxt-link
+                to="/units/Kipchak"
+                >欽察</nuxt-link
+              >
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">首選戰略</td>
+            <td class="unit4"></td>
+          </tr>
+          <tr>
+            <td class="unit3">特殊技術</td>
+            <td class="unit4">
+              <img class="gg" src="~assets/unique_tech.png" /><nuxt-link
+                to="/techs/Steppe_Husbandry"
+                >大草原耕種技術</nuxt-link
+              >
+              <br />
+              <img
+                class="gg"
+                src="~assets/unique_tech_imperial.png"
+              /><nuxt-link to="/techs/Cuman_Mercenaries">庫曼傭兵</nuxt-link>
+            </td>
+          </tr>
+        </table>
+
+        <div class="thanks">此頁面由singsing612協助編輯</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'allciv',
+  components: {}
+}
+</script>
+<style></style>
