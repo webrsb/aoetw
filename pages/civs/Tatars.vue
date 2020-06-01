16b4da213ad43ced0294010e054a4121a5561d33
webrsb <webrsb@gmail.com>
2020-06-01 21:22:42 +0800

    update

--- pages/civs/Tatars.vue
+++ pages/civs/Tatars.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row padtop">
       <div class="col-md-12">
-        <h1><img src="~assets/lithuanians.png" />韃靼</h1>
+        <h1><img src="~assets/tatars.png" />韃靼</h1>
         <div class="hrn1"></div>
       </div>
       <div class="col-md-8">
@@ -10,46 +10,66 @@
           <p>
             <b>韃靼</b>是《<nuxt-link to="/games/de"
               >世紀帝國II：決定版</nuxt-link
-            >》新增著重於<nuxt-link to="/units/Cavalry_Archer">馬弓騎兵</nuxt-link>的中亞文明。
+            >》新增著重於<nuxt-link to="/units/Cavalry_Archer"
+              >馬弓騎兵</nuxt-link
+            >的中亞文明。
           </p>
           <p>
-           從歷史上看，韃靼人源自於戈壁沙漠東北方的準<nuxt-link to="/civs/Mongols">蒙古</nuxt-link
+            從歷史上看，韃靼人源自於戈壁沙漠東北方的準<nuxt-link
+              to="/civs/Mongols"
+              >蒙古</nuxt-link
             >人。一些韃靼人離開後加入了基馬克汗國，而在原地的韃靼人則被成吉思汗征服，併入到蒙古帝國，還參與蒙古的征服，並這此過程中獲得了新土地來定居。韃靼人後來被突厥化並被吸入到講突厥語的鄰居中。遊戲中的韃靼人主要代表了突厥-蒙古後繼汗國和蒙古帝國衰落和分裂期間中建立的國家，而歐洲人通常將其稱為“Tartars”，像是金帳汗國、查加泰汗國和帖木兒帝國（這是韃靼戰役的主角）。時至今日，許多欽察語支的人仍自稱為韃靼人。
           </p>
           <p>
-           後來在草原上的韃靼人以游牧而聞名，這種生活方式主要依靠着狩獵和放牧。因此，韃靼人可以從可畜養動物身上獲得多50％的<nuxt-link to="/resource/Food">食物</nuxt-link
+            後來在草原上的韃靼人以游牧而聞名，這種生活方式主要依靠着狩獵和放牧。因此，韃靼人可以從可畜養動物身上獲得多50％的<nuxt-link
+              to="/resource/Food"
+              >食物</nuxt-link
             >。
           </p>
           <p>
-           韃靼人像其他馬上游牧民族（蒙古人、<nuxt-link to="/civs/Huns">匈奴</nuxt-link
+            韃靼人像其他馬上游牧民族（蒙古人、<nuxt-link to="/civs/Huns"
+              >匈奴</nuxt-link
             >人、烏古斯人、<nuxt-link to="/units/Cumans">庫曼</nuxt-link
             >-欽察人、早期<nuxt-link to="/civs/Magyars">馬扎爾</nuxt-link
             >人）一樣，經常採用佯裝撤退的戰術，他們假裝逃離戰場，使敵人進行追擊。當韃靼軍隊到達某個地點後，便會利用地形伏擊追擊中的敵人。因此，從更高的位置攻擊敵方單位時，韃靼單位可以造成多50％的傷害。
           </p>
           <p>
-           韃靼騎兵擅長在馬背上使用武器，尤其是弓箭。他們免費的<nuxt-link to="/techs/Parthian_Tactics">安息人戰術</nuxt-link
+            韃靼騎兵擅長在馬背上使用武器，尤其是弓箭。他們免費的<nuxt-link
+              to="/techs/Parthian_Tactics"
+              >安息人戰術</nuxt-link
             >和<nuxt-link to="/techs/Thumb_Ring">拇指環</nuxt-link
-            >反映了這一點。此外，韃靼人還可以使用<nuxt-link to="/techs/untech">特殊科技</nuxt-link
+            >反映了這一點。此外，韃靼人還可以使用<nuxt-link to="/techs/untech"
+              >特殊科技</nuxt-link
             ><nuxt-link to="/techs/Silk_Armor">絲護甲</nuxt-link
             >來加強<nuxt-link to="/units/Light_Cavalry">輕騎兵</nuxt-link
             >、<nuxt-link to="/units/Steppe_Lancer">草原騎兵</nuxt-link
             >和<nuxt-link to="/units/Cavalry_Archer">馬弓騎兵</nuxt-link
-            >的盔甲，還有馬弓騎兵視線加成的的團隊加分。
+            >的盔甲，還有馬弓騎兵<nuxt-link to="/los">視野</nuxt-link
+            >加成的的團隊加分。
           </p>
           <p>
-           蒙古帝國分裂後，韃靼軍閥在前蒙古帝國領土上建立了各種新王國或汗國。這些國家經常入侵並掠奪鄰地。因此韃靼人可以訓練<nuxt-link to="/units/Unique_Unit">特殊單位</nuxt-link
+            蒙古帝國分裂後，韃靼軍閥在前蒙古帝國領土上建立了各種新王國或汗國。這些國家經常入侵並掠奪鄰地。因此韃靼人可以訓練<nuxt-link
+              to="/units/Unique_Unit"
+              >特殊單位</nuxt-link
             ><nuxt-link to="/units/Keshik">怯薛</nuxt-link
-            >，與其他單位作戰時生產<nuxt-link to="/resource/Gold">黃金</nuxt-link
+            >，與其他單位作戰時生產<nuxt-link to="/resource/Gold"
+              >黃金</nuxt-link
             >。
           </p>
           <p>
-           前蒙古帝國領土上所建立最強大的王國就是帖木兒帝國，它會密集部署攻城武器。因此韃靼人的<nuxt-link to="/techs/Timurid_Siegecraft">帖木兒攻城術</nuxt-link
+            前蒙古帝國領土上所建立最強大的王國就是帖木兒帝國，它會密集部署攻城武器。因此韃靼人的<nuxt-link
+              to="/techs/Timurid_Siegecraft"
+              >帖木兒攻城術</nuxt-link
             >為<nuxt-link to="/units/Trebuchet">投石機</nuxt-link
             >提供了更多的射程。
           </p>
-           <p>
-           在德里戰役（1398年）中，帖木兒軍隊使用<nuxt-link to="/units/Flaming_Camel">火焰駱駝</nuxt-link
-            >來驚嚇德里的戰象，並令其踐踏自己的部隊。因此，一旦研發了帖木兒攻城術，就可啟用火焰駱駝，對大象和其他<nuxt-link to="/units/Cavalry_Units">騎兵</nuxt-link
+          <p>
+            在德里戰役（1398年）中，帖木兒軍隊使用<nuxt-link
+              to="/units/Flaming_Camel"
+              >火焰駱駝</nuxt-link
+            >來驚嚇德里的戰象，並令其踐踏自己的部隊。因此，一旦研發了帖木兒攻城術，就可啟用火焰駱駝，對大象和其他<nuxt-link
+              to="/units/Cavalry_Units"
+              >騎兵</nuxt-link
             >造成額外傷害。
           </p>
         </div>
@@ -57,62 +77,95 @@
         <div class="hrn1"></div>
         <div class="n1">
           <p>
-           韃靼人是<nuxt-link to="/units/Cavalry_Archer">馬弓騎兵</nuxt-link>文明，因此馬弓騎兵十分優秀，不僅擁有所有升級（<nuxt-link to="/techs/Parthian_Tactics">安息人戰術</nuxt-link
+            韃靼人是<nuxt-link to="/units/Cavalry_Archer">馬弓騎兵</nuxt-link
+            >文明，因此馬弓騎兵十分優秀，不僅擁有所有升級（<nuxt-link
+              to="/techs/Parthian_Tactics"
+              >安息人戰術</nuxt-link
             >和<nuxt-link to="/techs/Thumb_Ring">拇指環</nuxt-link
-            >還是免費的），而且在高地戰鬥時還會造成多50％的額外傷害（此加成適用於所有韃靼單位）和馬弓騎兵視線加成的團隊加分，以及擁有用<nuxt-link to="/techs/untech">特殊科技</nuxt-link
+            >還是免費的），而且在高地戰鬥時還會造成多50％的額外傷害（此加成適用於所有韃靼單位）和馬弓騎兵視線加成的團隊加分，以及擁有用<nuxt-link
+              to="/techs/untech"
+              >特殊科技</nuxt-link
             ><nuxt-link to="/techs/Silk_Armor">絲護甲</nuxt-link
             >，為馬弓騎兵提供了額外的<nuxt-link to="/armor">遠程護甲</nuxt-link
             >，使韃靼馬弓騎兵與<nuxt-link to="/civs/Mongols">蒙古</nuxt-link
-            >、<nuxt-link to="/civs/Huns">匈奴</nuxt-link
-            >、<nuxt-link to="/civs/Turks">土耳其</nuxt-link
-            >，<nuxt-link to="/civs/Magyars">馬扎爾</nuxt-link
-            >和<nuxt-link to="/civs/Cumans">庫曼</nuxt-link
-            >馬弓騎兵處於同一水平。韃靼雖然沒有<nuxt-link to="/units/Paladin">遊俠</nuxt-link
+            >、<nuxt-link to="/civs/Huns">匈奴</nuxt-link>、<nuxt-link
+              to="/civs/Turks"
+              >土耳其</nuxt-link
+            >，<nuxt-link to="/civs/Magyars">馬扎爾</nuxt-link>和<nuxt-link
+              to="/civs/Cumans"
+              >庫曼</nuxt-link
+            >馬弓騎兵處於同一水平。
+          </p>
+          <p>
+            韃靼雖然沒有<nuxt-link to="/units/Paladin">遊俠</nuxt-link
             >，但<nuxt-link to="/units/Cavalry_Units">騎兵</nuxt-link
-            >仍然強大，因為他們擁有重型騎兵<nuxt-link to="/units/Keshik">怯薛</nuxt-link
-            >，可以在與其他部隊作戰時產生<nuxt-link to="/resource/Gold">黃金</nuxt-link
-            >，並且也擁有其他全滿騎兵，加上<nuxt-link to="/units/Light_Cavalry">輕騎兵</nuxt-link
+            >仍然強大，因為他們擁有重型騎兵<nuxt-link to="/units/Keshik"
+              >怯薛</nuxt-link
+            >，可以在與其他部隊作戰時產生<nuxt-link to="/resource/Gold"
+              >黃金</nuxt-link
+            >，並且也擁有其他全滿騎兵，加上<nuxt-link to="/units/Light_Cavalry"
+              >輕騎兵</nuxt-link
             >、<nuxt-link to="/units/Steppe_Lancer">草原騎兵</nuxt-link
             >也受益於<nuxt-link to="/techs/Silk_Armor">絲護甲</nuxt-link
             >。雖然擁有<nuxt-link to="/units/Hand_Cannoneer">火槍兵</nuxt-link
             >，但由於缺乏<nuxt-link to="/units/Arbalest">強弩兵</nuxt-link
             >升級，<nuxt-link to="/units/Archer_Units">弓兵</nuxt-link
-            >並不出色。<nuxt-link to="/units/Infantry_Units">步兵</nuxt-link
-            >上，韃靼步兵差到無法使用，不僅缺乏<nuxt-link to="/units/Champion">劍兵勇士</nuxt-link
-            >升級，還是唯一沒有<nuxt-link to="/techs/Chain_Mail_Armor">步兵鏈甲</nuxt-link
-            >的文明。由於缺乏<nuxt-link to="/techs/Siege_Engineers">攻城工程師</nuxt-link
+            >並不出色。
+          </p>
+          <p>
+            <nuxt-link to="/units/Infantry_Units">步兵</nuxt-link
+            >方面，韃靼步兵差到無法使用，不僅缺乏<nuxt-link to="/units/Champion"
+              >劍兵勇士</nuxt-link
+            >升級，還是唯一沒有<nuxt-link to="/techs/Chain_Mail_Armor"
+              >步兵鏈甲</nuxt-link
+            >的文明。由於缺乏<nuxt-link to="/techs/Siege_Engineers"
+              >攻城工程師</nuxt-link
             >和<nuxt-link to="/units/Bombard_Cannon">火砲</nuxt-link
             >，<nuxt-link to="/units/Siege_Weapons">攻城器單位</nuxt-link
-            >並不出彩，但特殊科技<nuxt-link to="/techs/Timurid_Siegecraft">帖木兒攻城術</nuxt-link
+            >並不出彩，但特殊科技<nuxt-link to="/techs/Timurid_Siegecraft"
+              >帖木兒攻城術</nuxt-link
             >使<nuxt-link to="/units/Trebuchet">投石機</nuxt-link
-            >擁有19的射程，使韃靼投石機成為射程最長的單位。韃靼<nuxt-link to="/units/Naval_Vessels">海軍</nuxt-link
-            >是中等水平，只缺乏<nuxt-link to="/units/Heavy_Demolition_Ship">重型爆破船</nuxt-link
+            >擁有19的射程，使韃靼投石機成為射程最長的單位。韃靼<nuxt-link
+              to="/units/Naval_Vessels"
+              >海軍</nuxt-link
+            >是中等水平，只缺乏<nuxt-link to="/units/Heavy_Demolition_Ship"
+              >重型爆破船</nuxt-link
             >和<nuxt-link to="/techs/Shipwright">造船員</nuxt-link
             >。由於缺乏<nuxt-link to="/techs/Hoardings">圍牆</nuxt-link
             >，<nuxt-link to="/techs/Architecture">建築學</nuxt-link
-            >，<nuxt-link to="/building/Keep">大型箭塔</nuxt-link
-            >和<nuxt-link to="/techs/Arrowslits">箭狹槽</nuxt-link
-            >，防禦能力低下，但卻擁有<nuxt-link to="/building/Bombard_Tower">火砲塔</nuxt-link
+            >，<nuxt-link to="/building/Keep">大型箭塔</nuxt-link>和<nuxt-link
+              to="/techs/Arrowslits"
+              >箭狹槽</nuxt-link
+            >，防禦能力低下，但卻擁有<nuxt-link to="/building/Bombard_Tower"
+              >火砲塔</nuxt-link
             >。因為缺乏大量<nuxt-link to="/units/Monk">僧侶</nuxt-link
-            >科技，因此僧侶非常弱。因為可畜養動物收集的<nuxt-link to="/resource/Food">食物</nuxt-link
-            >+50%，一開始經濟十分強勁，但是由於缺乏<nuxt-link to="/techs/Two-Man_Saw">雙人鋸</nuxt-link
+            >科技，因此僧侶非常弱。因為可畜養動物收集的<nuxt-link
+              to="/resource/Food"
+              >食物</nuxt-link
+            >+50%，一開始經濟十分強勁，但是由於缺乏<nuxt-link
+              to="/techs/Two-Man_Saw"
+              >雙人鋸</nuxt-link
             >和<nuxt-link to="/techs/Stone_Shaft_Mining">採石裝置</nuxt-link
-            >，使遊戲後期經濟變得十分有限。
+            >，使遊戲後期經濟變得十分受限。
           </p>
           <h3>特殊單位</h3>
           <li>
             <img class="gg" src="~assets/Keshikicon.png" alt="" /><nuxt-link
               to="/units/Keshik"
               >怯薛</nuxt-link
-            >：與其他單位作戰就能生產<nuxt-link to="/resource/Gold">黃金</nuxt-link
-            >的<nuxt-link to="/units/Cavalry_Units">騎兵單位</nuxt-link
-            >。
+            >：與其他單位作戰就能生產<nuxt-link to="/resource/Gold"
+              >黃金</nuxt-link
+            >的<nuxt-link to="/units/Cavalry_Units">騎兵單位</nuxt-link>。
           </li>
           <li>
-            <img class="gg" src="~assets/Flaming_camel_icon.png" alt="" /><nuxt-link
-              to="/units/Flaming_Camel"
-              >火焰駱駝</nuxt-link
-            >：針對騎兵、大象的駱駝炸藥桶。
+            <img
+              class="gg"
+              src="~assets/Flaming_camel_icon.png"
+              alt=""
+            /><nuxt-link to="/units/Flaming_Camel">火焰駱駝</nuxt-link
+            >：針對騎兵、大象的駱駝<nuxt-link to="/units/Petard"
+              >爆炸桶</nuxt-link
+            >。
           </li>
           <h3>特殊科技</h3>
           <li>
@@ -121,15 +174,16 @@
               >絲護甲</nuxt-link
             >：<nuxt-link to="/units/Light_Cavalry">輕騎兵</nuxt-link
             >、<nuxt-link to="/units/Steppe_Lancer">草原騎兵</nuxt-link
-            >和<nuxt-link to="/units/Cavalry_Archer">馬弓騎兵</nuxt-link><nuxt-link to="/armor">遠程護甲</nuxt-link
-            >+1。
+            >和<nuxt-link to="/units/Cavalry_Archer">馬弓騎兵</nuxt-link
+            ><nuxt-link to="/armor">遠程護甲</nuxt-link>+1。
           </li>
           <li>
             <img
               class="gg"
               src="~assets/unique_tech_imperial.png"
               alt=""
-            /><nuxt-link to="/techs/Timurid_Siegecraft">帖木兒攻城術</nuxt-link>：<nuxt-link to="/units/Trebuchet">投石機</nuxt-link
+            /><nuxt-link to="/techs/Timurid_Siegecraft">帖木兒攻城術</nuxt-link
+            >：<nuxt-link to="/units/Trebuchet">投石機</nuxt-link
             >+2射程、可啟用火焰駱駝。
           </li>
         </div>
@@ -139,20 +193,22 @@
           <ul>
             <li>
               可畜養動物收集的<nuxt-link to="/resource/Food">食物</nuxt-link
-            >+50%
+              >+50%
             </li>
             <li>
               高地戰鬥傷害+50%
             </li>
-            <li>免費擁有<nuxt-link to="/techs/Parthian_Tactics">安息人戰術</nuxt-link
-            >和<nuxt-link to="/techs/Thumb_Ring">拇指環</nuxt-link
-            ></li>
+            <li>
+              免費擁有<nuxt-link to="/techs/Parthian_Tactics"
+                >安息人戰術</nuxt-link
+              >和<nuxt-link to="/techs/Thumb_Ring">拇指環</nuxt-link>
+            </li>
             <li>
               <strong
                 ><nuxt-link to="/elements/Team_Bonus"
                   >團隊加分</nuxt-link
                 ></strong
-              >:馬弓騎兵視野+2
+              >:馬弓騎兵<nuxt-link to="/los">視野</nuxt-link>+2
             </li>
           </ul>
         </div>
@@ -170,37 +226,43 @@
           </h3>
           <ul>
             <li>
-            最初，(精鋭)<nuxt-link to="/units/Keshik">怯薛</nuxt-link
-            >成本為50F/80G，<nuxt-link to="/elements/Attack">攻擊力</nuxt-link
-            >12(14)，<nuxt-link to="/elements/Hit_points">血量</nuxt-link
-            >100(130)，訓練時間23(20)秒。
+              最初，(精鋭)<nuxt-link to="/units/Keshik">怯薛</nuxt-link
+              >成本為50F/80G，<nuxt-link to="/elements/Attack">攻擊力</nuxt-link
+              >12(14)，<nuxt-link to="/elements/Hit_points">血量</nuxt-link
+              >100(130)，訓練時間23(20)秒。
             </li>
             <li>
-            最初，韃靼無法研發<nuxt-link to="/units/Halberdier">戟兵</nuxt-link
-            >。
+              最初，韃靼無法研發<nuxt-link to="/units/Halberdier"
+                >戟兵</nuxt-link
+              >。
             </li>
             <li>
-            (34055更新) (精鋭)怯薛成本改為50F/40G，攻擊力改為9(11)，血量改為110(140)。
+              (34055更新)
+              (精鋭)怯薛成本改為50F/40G，攻擊力改為9(11)，血量改為110(140)。
             </li>
             <li>
-            (35584更新) 戟兵被添加到科技樹中。
+              (35584更新) 戟兵被添加到科技樹中。
             </li>
             <li>
-            (36202更新) 獲得免費<nuxt-link to="/techs/Thumb_Ring">拇指環</nuxt-link
-            >的文明加成。
+              (36202更新) 獲得免費<nuxt-link to="/techs/Thumb_Ring"
+                >拇指環</nuxt-link
+              >的文明加成。
             </li>
             <li>
-            (36202更新) (精鋭)怯薛成本訓練時間改為16(14)秒。
+              (36202更新) (精鋭)怯薛成本訓練時間改為16(14)秒。
             </li>
             <li>
-            (36202更新) <nuxt-link to="/units/Steppe_Lancer">草原騎兵</nuxt-link
-            >受益於<nuxt-link to="/techs/Silk_Armor">絲護甲</nuxt-link
-            >。
+              (36202更新)
+              <nuxt-link to="/units/Steppe_Lancer">草原騎兵</nuxt-link
+              >受益於<nuxt-link to="/techs/Silk_Armor">絲護甲</nuxt-link>。
             </li>
             <li>
-            (36202更新) 研發<nuxt-link to="/techs/Timurid_Siegecraft">帖木兒攻城術</nuxt-link>後<nuxt-link to="/units/Trebuchet">投石機</nuxt-link
-            >額外多2射程，並可以訓練<nuxt-link to="/units/Flaming_Camel">火焰駱駝</nuxt-link
-            >。
+              (36202更新) 研發<nuxt-link to="/techs/Timurid_Siegecraft"
+                >帖木兒攻城術</nuxt-link
+              >後<nuxt-link to="/units/Trebuchet">投石機</nuxt-link
+              >額外多2射程，並可以訓練<nuxt-link to="/units/Flaming_Camel"
+                >火焰駱駝</nuxt-link
+              >。
             </li>
           </ul>
         </div>
@@ -212,17 +274,19 @@
               韃靼的文明徽標來自於金帳汗族使用的旗帜。然而諷刺的是，金帳汗族是帖木兒的歒人，韃靼人經常在這兩方作戰。
             </li>
             <li>
-              韃靼是唯一缺乏<nuxt-link to="/techs/Chain_Mail_Armor">步兵鏈甲</nuxt-link
-            >的文明，也是唯一擁有<nuxt-link to="/building/Bombard_Tower">火砲塔</nuxt-link
-            >但没有<nuxt-link to="/building/Keep">大型箭塔</nuxt-link
-            >的文明。
+              韃靼是唯一缺乏<nuxt-link to="/techs/Chain_Mail_Armor"
+                >步兵鏈甲</nuxt-link
+              >的文明，也是唯一擁有<nuxt-link to="/building/Bombard_Tower"
+                >火砲塔</nuxt-link
+              >但没有<nuxt-link to="/building/Keep">大型箭塔</nuxt-link>的文明。
             </li>
             <li>
               一些韃靼AI的名字是突厥汗國、喀喇汗國、烏古斯葉護國、黑羊王朝和白羊王朝的統治者。但是，這些都與韃靼人不同：突厥汗國的語言屬西伯利亞語支，鄂爾渾碑銘更含有對韃靼人的敵意；喀喇汗國的建築風格可能已被漢化；烏古斯葉護國、黑羊王朝和白羊王朝的語言屬烏古斯語支，後兩者經常與帖木兒帝國交戰；喀喇汗國人講的是葛邏祿語，直到後來中亞和西亞的韃靼人才會使用察合台葛邏祿方言。因此，遊戲中的韃靼人不僅代表了金帳汗國和帖木兒王朝，還代表了其他中亞和西亞的突厥人。
             </li>
             <li>
-             韃靼AI的名字，帖木兒和月即別，以前是<nuxt-link to="/civs/Mongols">蒙古</nuxt-link
-            >AI的名字。
+              韃靼AI的名字，帖木兒和月即別，以前是<nuxt-link to="/civs/Mongols"
+                >蒙古</nuxt-link
+              >AI的名字。
             </li>
             <li>
               歐洲編年史家在民族名字的韃靼（Tatar）中添加了一個額外的-r-，導致韃靼（Tartar）的名字變成貶義，可能與希臘神話中的地獄塔耳塔羅斯（Tartarus）有關。在蒙古統治下，其他歐洲人種學家會錯誤地將韃靼語用於蒙古人以及其他講土耳其語的民族。因此，韃靼人成為歷史稱呼韃靼利亞的同名。
@@ -235,7 +299,7 @@
           <div class="row">
             <div class="col-md-4">
               <gallery
-                ><img class="imgn hg" src="~assets/WonderLithuanians.jpg" alt=""
+                ><img class="imgn hg" src="~assets/Ulugh.jpg" alt=""
               /></gallery>
               <p><em>韃靼世界奇觀 烏魯伯格天文台</em></p>
             </div>
@@ -259,11 +323,7 @@
           <tr>
             <td colspan="2">
               <div class="unititle">
-                <img
-                  src="~assets/Traku-pilis_trakaimuziejus.lt_.jpg"
-                  width="250"
-                  alt=""
-                />
+                <img src="~assets/Amir_Timur.jpg" width="250" alt="" />
                 <p><em>帖木兒</em></p>
               </div>
             </td>
@@ -280,10 +340,7 @@
           <tr>
             <td class="unit3">分類文明</td>
             <td class="unit4">
-              <nuxt-link
-                to="/units/Cavalry_Archer"
-                >馬弓騎兵</nuxt-link
-              >
+              <nuxt-link to="/units/Cavalry_Archer">馬弓騎兵</nuxt-link>
             </td>
           </tr>
           <tr>
@@ -295,18 +352,16 @@
               >
               <br />
               <p>
-                <img class="gg" src="~assets/Flaming_camel_icon.png" /><nuxt-link
-                  to="/units/Flaming_Camel"
-                  >火焰駱駝</nuxt-link
-                >
+                <img
+                  class="gg"
+                  src="~assets/Flaming_camel_icon.png"
+                /><nuxt-link to="/units/Flaming_Camel">火焰駱駝</nuxt-link>
               </p>
             </td>
           </tr>
           <tr>
             <td class="unit3">首選戰略</td>
-            <td class="unit4">
-
-            </td>
+            <td class="unit4"></td>
           </tr>
           <tr>
             <td class="unit3">特殊技術</td>
@@ -319,7 +374,9 @@
               <img
                 class="gg"
                 src="~assets/unique_tech_imperial.png"
-              /><nuxt-link to="/techs/Timurid_Siegecraft">帖木兒攻城術</nuxt-link>
+              /><nuxt-link to="/techs/Timurid_Siegecraft"
+                >帖木兒攻城術</nuxt-link
+              >
             </td>
           </tr>
         </table>
