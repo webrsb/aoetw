f7f1b885d458f3a748588ce40eceb1e580bcb8c9
webrsb <webrsb@gmail.com>
2020-12-25 20:31:46 +0800

    format

--- pages/techs/Husbandry.vue
+++ pages/techs/Husbandry.vue
@@ -27,7 +27,7 @@
             >就必須研發耕種技術，以免被風箏。
           </p>
           <p>
-            耕種技術能讓騎兵在逃脫弓箭手的火力和以及縮小與敵人距離還有偵測上都極大的幫助。 對於像柏柏人，法蘭克人，匈人或薩拉森人這樣的以騎兵為中心的文明，研究耕種技術是絕對必要的。
+            耕種技術能讓騎兵在逃脫弓箭手的火力和以及縮小與敵人距離還有偵測上都極大的幫助。對於像柏柏人，法蘭克人，匈人或薩拉森人這樣的以騎兵為中心的文明，研究耕種技術是絕對必要的。
           </p>
         </div>
         <h3>文明可研發與否</h3>
@@ -41,35 +41,35 @@
                   ><span style="margin-left: 2px;">可研發</span></strong
                 >
               </h4>
-<nuxt-link to="/civs/Berbers">柏柏</nuxt-link>、
-<nuxt-link to="/civs/Britons">不列顛</nuxt-link>、
-<nuxt-link to="/civs/Bulgarians">保加利亞</nuxt-link>、
-<nuxt-link to="/civs/Burmese">緬甸</nuxt-link>、
-<nuxt-link to="/civs/Byzantines">拜占庭</nuxt-link>、
-<nuxt-link to="/civs/Celts">塞爾特</nuxt-link>、
-<nuxt-link to="/civs/Chinese">中國</nuxt-link>、
-<nuxt-link to="/civs/Ethiopians">衣索比亞</nuxt-link>、
-<nuxt-link to="/civs/Franks">法蘭克</nuxt-link>、
-<nuxt-link to="/civs/Goths">哥德</nuxt-link>、
-<nuxt-link to="/civs/Huns">匈人</nuxt-link>、
-<nuxt-link to="/civs/Indians">印度</nuxt-link>、
-<nuxt-link to="/civs/Italians">義大利</nuxt-link>、
-<nuxt-link to="/civs/Japanese">日本</nuxt-link>、
-<nuxt-link to="/civs/Khmer">高棉</nuxt-link>、
-<nuxt-link to="/civs/Koreans">朝鮮</nuxt-link>、
-<nuxt-link to="/civs/Lithuanians">立陶宛</nuxt-link>、
-<nuxt-link to="/civs/Magyars">馬扎爾</nuxt-link>、
-<nuxt-link to="/civs/Malay">馬來</nuxt-link>、
-<nuxt-link to="/civs/Malians">馬利</nuxt-link>、
-<nuxt-link to="/civs/Mongols">蒙古</nuxt-link>、
-<nuxt-link to="/civs/Persians">波斯</nuxt-link>、
-<nuxt-link to="/civs/Portuguese">葡萄牙</nuxt-link>、
-<nuxt-link to="/civs/Saracens">薩拉森</nuxt-link>、
-<nuxt-link to="/civs/Slavs">斯拉夫</nuxt-link>、
-<nuxt-link to="/civs/Spanish">西班牙</nuxt-link>、
-<nuxt-link to="/civs/Tatars">韃靼</nuxt-link>、
-<nuxt-link to="/civs/Turks">土耳其</nuxt-link>、
-<nuxt-link to="/civs/Vietnamese">越南</nuxt-link>。
+              <nuxt-link to="/civs/Berbers">柏柏</nuxt-link>、
+              <nuxt-link to="/civs/Britons">不列顛</nuxt-link>、
+              <nuxt-link to="/civs/Bulgarians">保加利亞</nuxt-link>、
+              <nuxt-link to="/civs/Burmese">緬甸</nuxt-link>、
+              <nuxt-link to="/civs/Byzantines">拜占庭</nuxt-link>、
+              <nuxt-link to="/civs/Celts">塞爾特</nuxt-link>、
+              <nuxt-link to="/civs/Chinese">中國</nuxt-link>、
+              <nuxt-link to="/civs/Ethiopians">衣索比亞</nuxt-link>、
+              <nuxt-link to="/civs/Franks">法蘭克</nuxt-link>、
+              <nuxt-link to="/civs/Goths">哥德</nuxt-link>、
+              <nuxt-link to="/civs/Huns">匈人</nuxt-link>、
+              <nuxt-link to="/civs/Indians">印度</nuxt-link>、
+              <nuxt-link to="/civs/Italians">義大利</nuxt-link>、
+              <nuxt-link to="/civs/Japanese">日本</nuxt-link>、
+              <nuxt-link to="/civs/Khmer">高棉</nuxt-link>、
+              <nuxt-link to="/civs/Koreans">朝鮮</nuxt-link>、
+              <nuxt-link to="/civs/Lithuanians">立陶宛</nuxt-link>、
+              <nuxt-link to="/civs/Magyars">馬扎爾</nuxt-link>、
+              <nuxt-link to="/civs/Malay">馬來</nuxt-link>、
+              <nuxt-link to="/civs/Malians">馬利</nuxt-link>、
+              <nuxt-link to="/civs/Mongols">蒙古</nuxt-link>、
+              <nuxt-link to="/civs/Persians">波斯</nuxt-link>、
+              <nuxt-link to="/civs/Portuguese">葡萄牙</nuxt-link>、
+              <nuxt-link to="/civs/Saracens">薩拉森</nuxt-link>、
+              <nuxt-link to="/civs/Slavs">斯拉夫</nuxt-link>、
+              <nuxt-link to="/civs/Spanish">西班牙</nuxt-link>、
+              <nuxt-link to="/civs/Tatars">韃靼</nuxt-link>、
+              <nuxt-link to="/civs/Turks">土耳其</nuxt-link>、
+              <nuxt-link to="/civs/Vietnamese">越南</nuxt-link>。
             </div>
           </div>
         </div>
@@ -82,12 +82,12 @@
                   ><span style="margin-left: 2px;">不可研發</span></strong
                 >
               </h4>
-<nuxt-link to="/civs/Aztecs">阿茲特克</nuxt-link>、
-<nuxt-link to="/civs/Cumans">庫曼</nuxt-link>、
-<nuxt-link to="/civs/Incas">印加</nuxt-link>、
-<nuxt-link to="/civs/Mayans">馬雅</nuxt-link>、
-<nuxt-link to="/civs/Teutons">條頓</nuxt-link>、
-<nuxt-link to="/civs/Viking">維京</nuxt-link>。
+              <nuxt-link to="/civs/Aztecs">阿茲特克</nuxt-link>、
+              <nuxt-link to="/civs/Cumans">庫曼</nuxt-link>、
+              <nuxt-link to="/civs/Incas">印加</nuxt-link>、
+              <nuxt-link to="/civs/Mayans">馬雅</nuxt-link>、
+              <nuxt-link to="/civs/Teutons">條頓</nuxt-link>、
+              <nuxt-link to="/civs/Viking">維京</nuxt-link>。
             </div>
           </div>
         </div>
@@ -134,7 +134,10 @@
             <em><nuxt-link to="/games/aoc">征服者入侵</nuxt-link></em>
           </h3>
           <ul>
-            <li><nuxt-link to="/civs/Spanish">西班牙</nuxt-link>:傳教士不受耕種技術影響。</li>
+            <li>
+              <nuxt-link to="/civs/Spanish">西班牙</nuxt-link
+              >:傳教士不受耕種技術影響。
+            </li>
           </ul>
           <h3>
             <em><nuxt-link to="/games/ak">非洲王國</nuxt-link></em>
@@ -147,15 +150,26 @@
             <em><nuxt-link to="/games/aor">王者崛起</nuxt-link></em>
           </h3>
           <ul>
-            <li><nuxt-link to="/civs/Vietnamese">越南</nuxt-link>:無法研發耕種技術。</li>
+            <li>
+              <nuxt-link to="/civs/Vietnamese">越南</nuxt-link
+              >:無法研發耕種技術。
+            </li>
           </ul>
           <h3>
             <em><nuxt-link to="/games/de">決定版</nuxt-link></em>
           </h3>
           <ul>
-            <li><nuxt-link to="/civs/Cumans">庫曼</nuxt-link>:最初能研發耕種技術；34055更新後被移除。</li>
-            <li><nuxt-link to="/civs/Spanish">西班牙</nuxt-link>:傳教士受耕種技術影響。</li>
-            <li><nuxt-link to="/civs/Vietnamese">越南</nuxt-link>:可研發耕種技術。</li>
+            <li>
+              <nuxt-link to="/civs/Cumans">庫曼</nuxt-link
+              >:最初能研發耕種技術；34055更新後被移除。
+            </li>
+            <li>
+              <nuxt-link to="/civs/Spanish">西班牙</nuxt-link
+              >:傳教士受耕種技術影響。
+            </li>
+            <li>
+              <nuxt-link to="/civs/Vietnamese">越南</nuxt-link>:可研發耕種技術。
+            </li>
           </ul>
         </div>
         <h3>歷史</h3>
