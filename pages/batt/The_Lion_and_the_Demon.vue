390421ac9818b4ddc7c063ebd208bac3250ca2a1
webrsb <webrsb@gmail.com>
2020-12-26 00:00:45 +0800

    f

--- pages/batt/The_Lion_and_the_Demon.vue
+++ pages/batt/The_Lion_and_the_Demon.vue
@@ -12,7 +12,9 @@
               >薩拉丁戰役</nuxt-link
             >的第六關劇情關卡，也是薩拉丁戰役的最後一關<nuxt-link to="/batt"
               >戰役</nuxt-link
-            >。玩家必須在阿卡城建立<nuxt-link to="/building/Wonder">世界奇觀</nuxt-link>並抵擋十字軍的攻擊。實際上阿卡城並不存在。
+            >。玩家必須在阿卡城建立<nuxt-link to="/building/Wonder"
+              >世界奇觀</nuxt-link
+            >並抵擋十字軍的攻擊。實際上阿卡城並不存在。
           </p>
         </div>
         <h2>玩家</h2>
@@ -63,8 +65,7 @@
                 玩家7
               </td>
               <td class="tc6" colspan="3">
-                <nuxt-link to="/civs/Persians">波斯</nuxt-link
-                >哨站（紫色）波斯
+                <nuxt-link to="/civs/Persians">波斯</nuxt-link>哨站（紫色）波斯
               </td>
             </tr>
             <tr>
@@ -93,7 +94,9 @@
                 玩家4
               </td>
               <td class="tc6">
-                獅心王理查 （紅色）<nuxt-link to="/civs/Britons">不列顛</nuxt-link>
+                獅心王理查 （紅色）<nuxt-link to="/civs/Britons"
+                  >不列顛</nuxt-link
+                >
               </td>
               <td class="tc6">
                 <div class="p4"></div>
@@ -160,65 +163,73 @@
             </li>
           </ul>
           <h2>攻略</h2>
-                  <div class="hrn1"></div>
-        <div class="n1">
-          <ul>
-            
-            <li>
-              雖然任務的目標是建築<nuxt-link to="/building/Wonder"
-                >世界奇觀</nuxt-link
-              >並且防禦之，但還是比較建議運用起始資源進行充足的防守。不久後波斯會運送一匹精銳戰象到我方陣地。
-            </li>
-            <li>
-              把神弓手駐進<nuxt-link to="/building/Watch_Tower">箭塔</nuxt-link
-              >裡、蓋個<nuxt-link to="/building/Archery_Range">射箭場</nuxt-link
-              >放些<nuxt-link to="/units/Crossbowman">弩兵</nuxt-link
-              >進去，然後運用<nuxt-link to="/units/Cavalry_Units"
-                >騎兵</nuxt-link
-              >阻止敵人的<nuxt-link to="/units/Siege_Weapons"
-                >攻城武器</nuxt-link
-              >。
-            </li>
-            <li>
-              法蘭克人會派<nuxt-link to="/units/Bombard_Cannon">火砲</nuxt-link
-              >、耶路撒冷會派巨投，獅心王理察最後會架起他的<nuxt-link
-                to="/units/Siege_Weapons"
-                >英雄投石機</nuxt-link
-              >：<nuxt-link to="/units/hero/God_s_Own_Sling">神之弓</nuxt-link
-              >與<nuxt-link to="/units/hero/Bad_Neighbor">壞鄰居</nuxt-link>。
-            </li>
-            <li>
-              玩家可以善用漁船賺取食物，建造一小批<nuxt-link
-                to="/units/Naval_Vessels"
-                >海軍</nuxt-link
-              >防禦熱那亞的海上攻擊。一開始會有點艱難，不過反擊的時候總會到來。此外，西邊的海上有個充滿黃金的小島，一旦玩家用完主要大陸上的黃金，這邊相當不錯。
-            </li>
-            <li>
-              你占據地圖的左側、敵人位於右側：敵人的分布，自上而下順序是熱那亞、法蘭克人、聖殿騎士、獅心王，與耶路撒冷。
-            </li>
-            <li>
-              成就「進攻即是最好的防禦」：在建造世界奇觀前打敗所有敵人。建議的攻擊順序是耶路撒冷、<nuxt-link
-                to="/units/hero/Richard_the_Lionheart"
-                >獅心王理查</nuxt-link
-              >，然後是法蘭克人和聖殿騎士、熱那亞，消滅耶路撒冷後可以於地圖的右下角發現大量的金礦。在這些金礦附近建造箭塔守衛，敵人家的礦挖完了會來搶這幾處金礦。
-            </li>
-            <li>首先要先快攻耶路撒冷，造兩台巨投和運輸船後，將巨投和一開始的兵力用運輸船從下方繞過去快攻，家裡訓練奴隸兵和匈牙利輕騎兵防禦第一波攻擊以及準備進行下一波攻勢用。再來對付理查時，敵人的精銳長弓兵和弩砲、巨投很煩人，因此要使用大匹的匈牙利輕騎兵來迎擊，然後用火砲或巨投拆掉防禦工事尤其是城堡省得一直造長弓兵。由於其他同盟很常來救，因此要準備阿拉伯奴隸兵迎擊遊俠、以及準備火槍兵迎擊條頓武士。如果能重創這兩個勢力則離勝利便不遠了，接著就把其他的勢力一一消滅即可。
-            </li>
-            <li>
-              這關卡還有一個比較簡單的玩法：將西邊島嶼上的金礦、樹木採集完畢，就可以在這裡建造世界奇觀。當敵人主要關注於你的要塞防禦，世界奇觀的倒數也就默默完成。
-            </li>
-          </ul>
-        </div>
-          </div>
-          <h2>決定版變更點</h2>
           <div class="hrn1"></div>
           <div class="n1">
-            <ol>
-              <li>人口上限由75改100，耶路撒冷種族由<nuxt-link to="/civs/Byzantines">拜占庭</nuxt-link>改成法蘭克，熱那亞種族由拜占庭改成<nuxt-link to="/civs/Italians">義大利</nuxt-link>。</li>
-              <li>新增成就「進攻即是最好的防禦」：在蓋世界奇觀之前全滅敵人。</li>
-            </ol>
-          </div>        
-        
+            <ul>
+              <li>
+                雖然任務的目標是建築<nuxt-link to="/building/Wonder"
+                  >世界奇觀</nuxt-link
+                >並且防禦之，但還是比較建議運用起始資源進行充足的防守。不久後波斯會運送一匹精銳戰象到我方陣地。
+              </li>
+              <li>
+                把神弓手駐進<nuxt-link to="/building/Watch_Tower"
+                  >箭塔</nuxt-link
+                >裡、蓋個<nuxt-link to="/building/Archery_Range"
+                  >射箭場</nuxt-link
+                >放些<nuxt-link to="/units/Crossbowman">弩兵</nuxt-link
+                >進去，然後運用<nuxt-link to="/units/Cavalry_Units"
+                  >騎兵</nuxt-link
+                >阻止敵人的<nuxt-link to="/units/Siege_Weapons"
+                  >攻城武器</nuxt-link
+                >。
+              </li>
+              <li>
+                法蘭克人會派<nuxt-link to="/units/Bombard_Cannon"
+                  >火砲</nuxt-link
+                >、耶路撒冷會派巨投，獅心王理察最後會架起他的<nuxt-link
+                  to="/units/Siege_Weapons"
+                  >英雄投石機</nuxt-link
+                >：<nuxt-link to="/units/hero/God_s_Own_Sling">神之弓</nuxt-link
+                >與<nuxt-link to="/units/hero/Bad_Neighbor">壞鄰居</nuxt-link>。
+              </li>
+              <li>
+                玩家可以善用漁船賺取食物，建造一小批<nuxt-link
+                  to="/units/Naval_Vessels"
+                  >海軍</nuxt-link
+                >防禦熱那亞的海上攻擊。一開始會有點艱難，不過反擊的時候總會到來。此外，西邊的海上有個充滿黃金的小島，一旦玩家用完主要大陸上的黃金，這邊相當不錯。
+              </li>
+              <li>
+                你占據地圖的左側、敵人位於右側：敵人的分布，自上而下順序是熱那亞、法蘭克人、聖殿騎士、獅心王，與耶路撒冷。
+              </li>
+              <li>
+                成就「進攻即是最好的防禦」：在建造世界奇觀前打敗所有敵人。建議的攻擊順序是耶路撒冷、<nuxt-link
+                  to="/units/hero/Richard_the_Lionheart"
+                  >獅心王理查</nuxt-link
+                >，然後是法蘭克人和聖殿騎士、熱那亞，消滅耶路撒冷後可以於地圖的右下角發現大量的金礦。在這些金礦附近建造箭塔守衛，敵人家的礦挖完了會來搶這幾處金礦。
+              </li>
+              <li>
+                首先要先快攻耶路撒冷，造兩台巨投和運輸船後，將巨投和一開始的兵力用運輸船從下方繞過去快攻，家裡訓練奴隸兵和匈牙利輕騎兵防禦第一波攻擊以及準備進行下一波攻勢用。再來對付理查時，敵人的精銳長弓兵和弩砲、巨投很煩人，因此要使用大匹的匈牙利輕騎兵來迎擊，然後用火砲或巨投拆掉防禦工事尤其是城堡省得一直造長弓兵。由於其他同盟很常來救，因此要準備阿拉伯奴隸兵迎擊遊俠、以及準備火槍兵迎擊條頓武士。如果能重創這兩個勢力則離勝利便不遠了，接著就把其他的勢力一一消滅即可。
+              </li>
+              <li>
+                這關卡還有一個比較簡單的玩法：將西邊島嶼上的金礦、樹木採集完畢，就可以在這裡建造世界奇觀。當敵人主要關注於你的要塞防禦，世界奇觀的倒數也就默默完成。
+              </li>
+            </ul>
+          </div>
+        </div>
+        <h2>決定版變更點</h2>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <ol>
+            <li>
+              人口上限由75改100，耶路撒冷種族由<nuxt-link to="/civs/Byzantines"
+                >拜占庭</nuxt-link
+              >改成法蘭克，熱那亞種族由拜占庭改成<nuxt-link to="/civs/Italians"
+                >義大利</nuxt-link
+              >。
+            </li>
+            <li>新增成就「進攻即是最好的防禦」：在蓋世界奇觀之前全滅敵人。</li>
+          </ol>
+        </div>
       </div>
       <div class="col-md-4">
         <table class="table-bordered" width="100%">
@@ -276,7 +287,9 @@
           </tr>
           <tr>
             <td class="unit3">上一關</td>
-            <td class="unit4"><nuxt-link to="/batt/Jihad">聖戰！</nuxt-link></td>
+            <td class="unit4">
+              <nuxt-link to="/batt/Jihad">聖戰！</nuxt-link>
+            </td>
           </tr>
           <tr>
             <td class="unit3">下一關</td>
