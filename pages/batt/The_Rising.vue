b1082a09e8ed81b18fbbd9880f1e9cc3942cf31c
harry3305 <52451150+harry3305@users.noreply.github.com>
2020-12-25 21:42:16 +0800

    Update The_Rising.vue
    修改至決定版用語及更新決定版修正部分

--- pages/batt/The_Rising.vue
+++ pages/batt/The_Rising.vue
@@ -2,13 +2,13 @@
   <div class="container">
     <div class="row padtop">
       <div class="col-md-12">
-        <h1>叛變</h1>
+        <h1>龍飛鳳起</h1>
         <div class="hrn1"></div>
       </div>
       <div class="col-md-8">
         <div class="n1">
           <p>
-            <b>叛變</b>為<nuxt-link to="/batt/Joan_of_Arc"
+            <b>龍飛鳳起</b>（舊譯：叛變）為<nuxt-link to="/batt/Joan_of_Arc"
               >聖女貞德戰役</nuxt-link
             >的第四關關卡，<nuxt-link to="/batt">戰役</nuxt-link
             >描述法軍解放3座英軍控制的城市，並讓法國王子加冕成為<nuxt-link
@@ -42,27 +42,27 @@
               <div class="p2"></div>
               玩家2
             </td>
-            <td class="tc6">
-              <nuxt-link to="/civs/Britons">不列顛</nuxt-link
-              >的皇家禁衛軍（紅色）不列顛
+            <td class="tc8">
+              特華（橙色）<nuxt-link to="/civs/Britons">不列顛</nuxt-link
+              >
             </td>
             <td class="tc6">
               <div class="p3"></div>
               玩家3
             </td>
-            <td class="tc6">沙朗（綠色）不列顛</td>
+            <td class="tc6">沙隆（綠色）不列顛</td>
           </tr>
           <tr>
             <td class="tc6">
               <div class="p4"></div>
               玩家4
             </td>
-            <td class="tc6">理姆斯（黃色）不列顛</td>
+            <td class="tc6">漢斯（黃色）不列顛</td>
             <td class="tc6">
-              <div class="p8"></div>
+              <div class="p2"></div>
               玩家5
             </td>
-            <td class="tc6">特洛伊斯（橘色）不列顛</td>
+            <td class="tc6">英格蘭警衛（橘色）不列顛</td>
           </tr>
         </table>
       </div>
@@ -71,7 +71,7 @@
           <tr>
             <td class="unit" colspan="2" style="background-color:#375d96;">
               <div class="unititle2">
-                叛變
+                龍飛鳳起
               </div>
             </td>
           </tr>
@@ -129,7 +129,7 @@
           <tr>
             <td class="unit3">下一關</td>
             <td class="unit4">
-              <nuxt-link to="/batt/The_Siege_of_Paris">巴黎攻城戰</nuxt-link>
+              <nuxt-link to="/batt/The_Siege_of_Paris">圍攻巴黎</nuxt-link>
             </td>
           </tr>
         </table>
@@ -158,7 +158,7 @@
               >黃金</nuxt-link
             >／150<nuxt-link to="/resource/Stone">石頭</nuxt-link>
           </li>
-          <li><strong>人口上限：</strong>75</li>
+          <li><strong>人口上限：</strong>125</li>
         </ul>
       </div>
       <div class="col-md-4">
@@ -223,12 +223,12 @@
       <ol>
         <li>西進以支援法國城鎮。要小心注意沿途的兩旁，貞德必須活著。</li>
         <li>
-          破壞特洛伊斯的<nuxt-link to="/building/Town_Center"
+          破壞特華的<nuxt-link to="/building/Town_Center"
             >城鎮中心</nuxt-link
           >。
         </li>
-        <li>破壞沙朗的城鎮中心。</li>
-        <li>破壞理姆斯的城鎮中心。</li>
+        <li>破壞沙隆的城鎮中心。</li>
+        <li>破壞漢斯的城鎮中心。</li>
       </ol>
     </div>
     <h2>任務提示</h2>
@@ -242,7 +242,7 @@
           >的製造上。
         </li>
         <li>
-          沙朗是被<nuxt-link to="/civs/Britons">英國</nuxt-link
+          沙隆是被<nuxt-link to="/civs/Britons">英國</nuxt-link
           >佔據的三個村莊中最近且最有價值的一個。
         </li>
         <li>
@@ -277,7 +277,7 @@
       <div class="n1">
         <ul>
           <li>
-            你一開始有一些軍隊，當你往西前進時會遇到不列顛皇家禁衛軍，有9個<nuxt-link
+            你一開始有一些軍隊，當你往西前進時會遇到英格蘭警衛，有9個<nuxt-link
               to="/units/Man-at-Arms"
               >裝甲步兵</nuxt-link
             >跟6個<nuxt-link to="/units/Crossbowman">弩兵</nuxt-link
@@ -289,10 +289,10 @@
               >箭塔</nuxt-link
             >射到，之後一路往西取得你的<nuxt-link to="/units/Villager"
               >村民</nuxt-link
-            >並開始發展經濟體。這裡要注意的是如果選擇先躲過不列顛皇家禁衛軍，之後他們還是會進攻玩家城鎮，不過只要搖響警戒鐘讓城鎮中心處理就好。
+            >並開始發展經濟體。這裡要注意的是如果選擇先躲過英格蘭警衛，之後他們還是會進攻玩家城鎮，不過只要搖響警戒鐘讓城鎮中心處理就好。
           </li>
           <li>
-            發展經濟體的時候你可以帶著原有的軍隊往沙朗前進，沙朗有座沒有防禦建築的城鎮在地圖西邊過河流穿越處的地方。他們有<nuxt-link
+            發展經濟體的時候你可以帶著原有的軍隊往沙隆前進，沙隆有座沒有防禦建築的城鎮在地圖西邊過河流穿越處的地方。他們有<nuxt-link
               to="/units/Knight"
               >騎士</nuxt-link
             >、弩砲還有弩兵，如果時間足夠的話也會訓練<nuxt-link
@@ -301,7 +301,7 @@
             >。先對付他們的騎士，記得用<nuxt-link to="/units/Monk"
               >僧侶</nuxt-link
             >幫忙<nuxt-link to="/elements/Conversion">招降</nuxt-link
-            >1~2隻騎士，之後直接解決弩砲跟弩兵，弩兵可以用貞德對付，然後開始打城鎮中心，可以用拉兵躲避(左右閃避)城鎮中心的箭還有僧侶治療被打的目標來增加其他軍隊攻擊的時間，只要城鎮中心陷落沙朗就會投降，玩家也能開採尚未使用的黃金和石材。
+            >1~2隻騎士，之後直接解決弩砲跟弩兵，弩兵可以用貞德對付，然後開始打城鎮中心，可以用拉兵躲避(左右閃避)城鎮中心的箭還有僧侶治療被打的目標來增加其他軍隊攻擊的時間，只要城鎮中心陷落沙隆就會投降，玩家也能開採尚未使用的黃金和石材。
           </li>
         </ul>
         <div class="row">
@@ -317,7 +317,7 @@
             <p>
               <em
                 >走小道（<nuxt-link to="/units/Scout_Cavalry">斥候</nuxt-link
-                >所在處）可以暫時躲過不列顛禁衛軍。</em
+                >所在處）可以暫時躲過英格蘭警衛。</em
               >
             </p>
           </div>
@@ -330,18 +330,18 @@
                 src="~assets/Joan4chalon.jpg"
                 alt=""
             /></gallery>
-            <p><em>沙朗的城鎮中心陷落。</em></p>
+            <p><em>沙隆的城鎮中心陷落。</em></p>
           </div>
         </div>
         <li>
-          下一個目標是特洛伊斯。特洛伊斯在地圖東邊。在河流穿越處有他們的<nuxt-link
+          下一個目標是特華。特華在地圖東邊。在河流穿越處有他們的<nuxt-link
             to="/building/Stone_Wall"
             >城牆</nuxt-link
           >，不過另外一側則完全沒有防備。它們會訓練<nuxt-link
             to="/units/Pikeman"
             >重裝長槍兵</nuxt-link
           >和騎士，也會訓練<nuxt-link to="/units/War_Galley">弩砲戰船</nuxt-link
-          >。玩家持續發展經濟(村民數建議到50左右，不要高於50)並在原沙朗的位置建造軍事<nuxt-link
+          >。玩家持續發展經濟(村民數建議到50左右，不要高於50)並在原沙隆的位置建造軍事<nuxt-link
             to="/building"
             >建築物</nuxt-link
           >(<nuxt-link to="/building/Stable">馬廄</nuxt-link>、<nuxt-link
@@ -356,14 +356,14 @@
           >做防禦，將貞德留在原本的基地內，開始訓練以騎士為主力的部隊，3~5台的<nuxt-link
             to="/units/Battering_Ram"
             >衝撞車</nuxt-link
-          >，並在兵工廠跟馬廄升級他們，就可以攻打特洛伊斯了，也可以升級到帝王時代升級<nuxt-link
+          >，並在兵工廠跟馬廄升級他們，就可以攻打特華了，也可以升級到帝王時代升級<nuxt-link
             to="/units/Paladin"
             >遊俠</nuxt-link
           >、<nuxt-link to="/units/Capped_Ram">裝甲衝撞車</nuxt-link
           >和<nuxt-link to="/units/Trebuchet">巨型投石機</nuxt-link
-          >。可以直接派一些村民到特洛伊斯的<nuxt-link to="/building/Market"
+          >。可以直接派一些村民到特華的<nuxt-link to="/building/Market"
             >市集</nuxt-link
-          >旁邊蓋城堡提供軍隊在攻擊時的火力掩護，記得也在前線蓋馬廄跟攻城器製造所，城鎮中心陷落後特洛伊斯也會投降。
+          >旁邊蓋城堡提供軍隊在攻擊時的火力掩護，記得也在前線蓋馬廄跟攻城器製造所，城鎮中心陷落後特華也會投降。
         </li>
         <div class="row">
           <div class="col-md-6">
@@ -376,20 +376,20 @@
             <gallery
               ><img class="imgn hg" src="~assets/Joan4troyes.jpg" alt=""
             /></gallery>
-            <p><em>特洛伊斯的城鎮中心陷落。</em></p>
+            <p><em>特華的城鎮中心陷落。</em></p>
           </div>
         </div>
         <li>
-          現在你要面對最危險的敵人理姆斯，理姆斯在地圖北邊深處有座防禦要塞，他們在這場戰役中是可怕的對手。他們會以<nuxt-link
+          現在你要面對最危險的敵人漢斯，漢斯在地圖北邊深處有座防禦要塞，他們在這場戰役中是可怕的對手。他們會以<nuxt-link
             to="/units/Longbowman"
             >長弓兵</nuxt-link
           >、<nuxt-link to="/units/Light_Cavalry">輕騎兵</nuxt-link
           >、騎士、<nuxt-link to="/units/Mangonel">投石車</nuxt-link
-          >和巨型投石機對付玩家，不過你不去打他們就不會出來。理姆斯有<nuxt-link
+          >和巨型投石機對付玩家，不過你不去打他們就不會出來。漢斯有<nuxt-link
             to="/building/Fortified_Wall"
             >垛牆</nuxt-link
           >、<nuxt-link to="/building/Keep">大型箭塔</nuxt-link
-          >和城堡防衛基地，而且時間拉長城堡會增加到3~4座擋在城鎮中心前面，還有軍隊防禦著基地。玩家在攻擊理姆斯時會承受非常嚴重的損傷，用巨型投石機跟衝撞車攻破<nuxt-link
+          >和城堡防衛基地，而且時間拉長城堡會增加到3~4座擋在城鎮中心前面，還有軍隊防禦著基地。玩家在攻擊漢斯時會承受非常嚴重的損傷，用巨型投石機跟衝撞車攻破<nuxt-link
             to="/building/Gate"
             >城門</nuxt-link
           >，用遊俠對付其他部隊，然後用所有軍隊集中攻擊城鎮中心來獲得最後勝利。
@@ -407,6 +407,13 @@
             <p><em>為了勝利攻破城牆破壞城鎮中心！</em></p>
           </div>
         </div>
+              <h2>決定版變更點</h2>
+      <div class="hrn1"></div>
+      <div class="n1">
+        <ul>
+          <li>
+            人口上限由75改125，敵方城鎮中心被打敗後會改同盟並重建城鎮中心，但不會出兵。</li>
+        </ul></div>
       </div>
     </div>
   </div>
