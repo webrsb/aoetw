5b3c34999ce87f698442818ac5944efc00791708
harry3305 <52451150+harry3305@users.noreply.github.com>
2020-12-25 21:46:18 +0800

    Update The_Siege_of_Paris.vue
    修改至決定版用語以及變更點

--- pages/batt/The_Siege_of_Paris.vue
+++ pages/batt/The_Siege_of_Paris.vue
@@ -2,15 +2,15 @@
   <div class="container">
     <div class="row padtop">
       <div class="col-md-12">
-        <h1>巴黎攻城戰</h1>
+        <h1>圍攻巴黎</h1>
         <div class="hrn1"></div>
       </div>
       <div class="col-md-8">
         <div class="n1">
           <p>
-            <b>巴黎攻城戰</b>為<nuxt-link to="/batt/Joan_of_Arc"
+            <b>圍攻巴黎</b>（舊譯：巴黎攻城戰）為<nuxt-link to="/batt/Joan_of_Arc"
               >聖女貞德戰役</nuxt-link
-            >的第五關劇情關卡，此關任務內容為貞德與法軍前往巴黎市區解救難民，並護送至康白尼的任務。
+            >的第五關劇情關卡，此關任務內容為貞德與法軍前往巴黎市區解救難民，並護送至康比涅的任務。
           </p>
         </div>
         <h2>遊戲者</h2>
@@ -39,13 +39,28 @@
               玩家4
             </td>
             <td class="tc6" width="38%">
-              康白尼（藍綠色）<nuxt-link to="/civs/Franks">法蘭克</nuxt-link>
+              康比涅（藍綠色）<nuxt-link to="/civs/Franks">法蘭克</nuxt-link>
             </td>
             <td class="tc6" width="12%">
               <div class="p4"></div>
               玩家5
             </td>
-            <td class="tc6" width="38%">國王的臣子（黃色）法蘭克</td>
+            <td class="tc6" width="38%">國王的手下（黃色）法蘭克</td>
+          </tr>
+          <tr>
+            <td class="tc4" colspan="4" style="background:#ea6153;">中立</td>
+          </tr>
+          <tr>
+            <td class="tc6">
+              <div class="p2"></div>
+              玩家3
+            </td>
+            <td class="tc6">
+              <nuxt-link to="/civs/Britons">英格蘭人</nuxt-link>（紅色）<nuxt-link
+                to="/civs/Britons"
+                >不列顛</nuxt-link
+              >
+            </td>
           </tr>
           <tr>
             <td class="tc4" colspan="4" style="background:#ea6153;">敵對</td>
@@ -56,17 +71,7 @@
               玩家2
             </td>
             <td class="tc6">勃根地（紫色）法蘭克</td>
-            <td class="tc6">
-              <div class="p2"></div>
-              玩家3
-            </td>
-            <td class="tc6">
-              <nuxt-link to="/civs/Britons">英國</nuxt-link>（紅色）<nuxt-link
-                to="/civs/Britons"
-                >不列顛</nuxt-link
-              >
-            </td>
-          </tr>
+          </tr>          
         </table>
 
         <h2>戰役說明</h2>
@@ -105,7 +110,7 @@
                   src="~assets/Lorddegravilleicon.png"
                   alt=""
                 /><nuxt-link to="/units/hero/Lord_de_Graville"
-                  >格拉維爾領主</nuxt-link
+                  >格拉維爾‧德領主</nuxt-link
                 >
               </li>
               <li>
@@ -114,7 +119,7 @@
                   src="~assets/Jeandelorrain.png"
                   alt=""
                 /><nuxt-link to="/units/hero/Jean_de_Lorrain"
-                  >洛林的約翰</nuxt-link
+                  >約翰‧德‧洛林</nuxt-link
                 >
               </li>
               <li>
@@ -181,9 +186,9 @@
               <li>
                 <img
                   class="gg"
-                  src="~assets/MilitiaIcon.png"
+                  src="~assets/ManAtArmsIcon.png"
                   alt=""
-                /><nuxt-link to="/units/Militia">民兵</nuxt-link>*16
+                /><nuxt-link to="/units/Man-at-Arms">裝甲步兵</nuxt-link>*16
               </li>
               <li>
                 <img class="gg" src="~assets/KnightIcon.png" alt="" /><nuxt-link
@@ -214,13 +219,13 @@
             <li>貞德必須活著。</li>
             <li>
               巴黎避難的<nuxt-link to="/units/Villager">村民</nuxt-link
-              >正嘗試逃離英國人的專制統治，至少找出六位難民。
+              >正嘗試逃離英格蘭人人的專制統治，至少找出六位難民。
             </li>
             <li>
               在巴黎要能夠跟國王的增援部隊會合的地方，就只有在河道上的橋樑南邊。
             </li>
             <li>
-              護衛貞德和至少六名難民前往康白尼 (旗子中間的方塊)。
+              護衛貞德和至少六名難民前往康比涅 (旗子中間的方塊)。
             </li>
           </ol>
         </div>
@@ -228,7 +233,7 @@
         <div class="hrn1"></div>
         <div class="n1">
           <ul>
-            <li>不要嘗試擊敗每一支英國部隊。要把焦點集中在你的目標。</li>
+            <li>不要嘗試擊敗每一支英格蘭人部隊。要把焦點集中在你的目標。</li>
             <li>
               看守你的<nuxt-link to="/units/Siege_Weapons">攻城武器</nuxt-link
               >。如果你沒有攻城武器的話，敵軍的<nuxt-link
@@ -237,27 +242,28 @@
               >將會是一個棘手的問題。
             </li>
             <li>如果面對著強大的反抗時，就逃走吧！一定要逃走！</li>
-          </ul>
+          </ul>          
           <h3>細節</h3>
           <div class="hrn1"></div>
           <ul>
             <li>
-              勃艮第的預設外交立場為同盟，原因不明。但只要他們看到一個玩家的單位，他們就會改變它外交關係並進行攻擊。在這之前先與勃艮第的外交關係改成中立，這樣就可以殲滅勃根地在康白尼城外的所有單位。
-            </li>
-            <li>
-              國王的臣子在遠北的樹林裡隱藏著一位<nuxt-link to="/units/Militia"
-                >民兵</nuxt-link
-              >，這是避免其他玩家攻擊會導致國王的臣子被擊敗。
-            </li>
-          </ul>
-        </div>
+              勃艮第的預設外交立場為同盟，原因不明。但只要他們看到一個玩家的單位，他們就會改變它外交關係並進行攻擊。在這之前先與勃艮第的外交關係改成中立，這樣就可以殲滅勃根地在康比涅城外的所有單位。
+            </li>            
+          </ul>        
+                    <h3>決定版變更點</h3>
+          <div class="hrn1"></div>
+          <ul>
+            <li>和英格蘭的外交關係更改成中立，城中的大地之母軍隊由民兵變成裝甲步兵，並且都有封建時代的科技。國王的手下在樹林隱藏的民兵也沒了，但支援完玩家後仍不會被擊敗。
+            </li>            
+          </ul>        
+        </div>        
       </div>
       <div class="col-md-4">
         <table class="table-bordered" width="100%">
           <tr>
             <td class="unit" colspan="2" style="background-color:#375d96;">
               <div class="unititle2">
-                巴黎攻城戰
+                圍攻巴黎
               </div>
             </td>
           </tr>
@@ -313,7 +319,7 @@
           <tr>
             <td class="unit3">下一關</td>
             <td class="unit4">
-              <nuxt-link to="/batt/A_Perfect_Martyr">完美的烈士</nuxt-link>
+              <nuxt-link to="/batt/A_Perfect_Martyr">遺芳餘烈</nuxt-link>
             </td>
           </tr>
         </table>
