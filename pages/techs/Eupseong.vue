f10c14cc77447451246ab15ba5202ed2a415063e
singsing612 <62696812+singsing612@users.noreply.github.com>
2021-01-02 13:59:09 +0800

    Create Eupseong.vue
    韓國新特殊科技邑城

---
+++ pages/techs/Eupseong.vue
@@ -0,0 +1,167 @@
+<template>
+  <div class="container">
+    <div class="row padtop">
+      <div class="col-md-12">
+        <h1>邑城</h1>
+        <div class="hrn1"></div>
+      </div>
+      <div class="col-md-8">
+        <div class="n1">
+          <p>
+            <b>邑城</b>是<nuxt-link to="/civs/Koreans">朝鮮</nuxt-link
+            >的特殊科技，於《<nuxt-link to="/games/de"
+              >世紀帝國II：決定版</nuxt-link
+            >》引進。可於<nuxt-link to="/techs/age/Castle_Age"
+              >城堡時代</nuxt-link
+            >在<nuxt-link to="/building/Castle">城堡</nuxt-link
+            >研發。研發後<nuxt-link to="/building/Watch_Tower">箭塔系</nuxt-link
+            >射程+2。這項科技取代先前的特殊科技<nuxt-link to="/techs/Panokseon">板屋船</nuxt-link
+            >。
+          </p>
+        </div>
+        <h3>戰術要點</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <ul>
+            <li>
+              邑城與朝鮮的<nuxt-link to="/building/Watch_Tower">箭塔</nuxt-link
+            >自動升級相對應。 隨著射程的增加，朝鮮箭塔可以攻擊更遠的單位，並在敵人靠近前將其擊敗。
+            </li>
+          </ul>
+        </div>
+        <h3>團隊加分</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <ul>
+            <li>
+              <nuxt-link to="/civs/Berbers">柏柏</nuxt-link>：<nuxt-link
+                to="/techs/Kasbah"
+                >聚居堡壘</nuxt-link
+              >能夠加速邑城的研究時間(快25%)。
+            </li>
+          </ul>
+        </div>
+        <h3>更新日誌</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <h3>
+            <em><nuxt-link to="/games/aof">失落的帝國</nuxt-link></em>
+          </h3>
+          <ul>
+            <li>朝鮮可於<nuxt-link to="/techs/age/Castle_Age"
+              >城堡時代</nuxt-link
+            >研發特殊科技<nuxt-link to="/techs/Panokseon">板屋船</nuxt-link
+            >。</li>
+          </ul>  
+          <h3>
+            <em><nuxt-link to="/games/de">決定版</nuxt-link></em>
+          </h3>
+          <ul>
+            <li>
+              (42848更新) 邑城取代板屋船，朝鮮從此失去了箭塔射程加成的文明加成。
+            </li>
+          </ul>
+        </div>        
+        <h3>趣事</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <ul>
+            <li>
+              取代板屋船前，箭塔射程於城堡時代+1、帝王時代+2。邑城取代了這免費加成。儘管現在升級箭塔射程需有成本（相對以前免費加成），但它在城堡時代即可+2射程。
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <table class="table-bordered" width="100%">
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#31b0d5;">
+              <div class="unititle2">邑城</div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <div class="unititle">
+                <img src="~assets/unique_tech.png" class="img-fluid" alt="" />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#31b0d5;">
+              <div class="unititle2">
+                <nuxt-link to="/techs">科技介紹</nuxt-link>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3"><nuxt-link to="/civs">文明</nuxt-link></td>
+            <td class="unit4">
+              <nuxt-link to="/civs/Koreans">朝鮮</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">時代</td>
+            <td class="unit4">
+              <img class="gg" src="~assets/castle_age.png" alt="" /><nuxt-link
+                to="/techs/age/Castle_Age"
+                >城堡時代</nuxt-link
+              >
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">研發於</td>
+            <td class="unit4">
+              <img class="gg" src="~assets/castle.png" alt="" /><nuxt-link
+                to="/building/Castle"
+                >城堡</nuxt-link
+              >
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">研發時間</td>
+            <td class="unit4">40秒</td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#31b0d5;">
+              <div class="unititle2">成本</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">
+              <nuxt-link to="/resource/Food">食物</nuxt-link>
+            </td>
+            <td class="unit4">300</td>
+          </tr>
+          <tr>
+            <td class="unit3">
+              <nuxt-link to="/resource/Wood">木材</nuxt-link>
+            </td>
+            <td class="unit4">300</td>
+          </tr>
+
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#31b0d5;">
+              <div class="unititle2">影響</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">影響</td>
+            <td class="unit4">
+              <nuxt-link to="/building/Watch_Tower">箭塔系</nuxt-link
+            >射程+2。
+            </td>
+          </tr>
+        </table>
+
+        <div class="thanks">感謝! 此頁面由Singsing612協助編輯</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'alltech',
+  components: {}
+}
+</script>
+<style></style>
