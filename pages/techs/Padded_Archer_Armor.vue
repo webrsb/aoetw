bb311766266c5a9ef3de14700e4114f59c962896
singsing612 <62696812+singsing612@users.noreply.github.com>
2021-01-05 15:33:10 +0900

    Update Padded_Archer_Armor.vue
    保加利亞的兵工廠升級所需食物-50%相關頁面需要更新

--- pages/techs/Padded_Archer_Armor.vue
+++ pages/techs/Padded_Archer_Armor.vue
@@ -20,12 +20,18 @@
         <div class="n1">
           <ul>
             <li>
-              <nuxt-link to="/civs/Chinese">中國</nuxt-link>：該<nuxt-link
-                to="/techs"
-                >科技</nuxt-link
-              >在封建/<nuxt-link to="/techs/age/Castle_Age">城堡</nuxt-link
+              <nuxt-link to="/civs/Bulgarians">保加利亞</nuxt-link>：該科技所需食物-50%。
+            </li>
+            <li>
+              <nuxt-link to="/civs/Chinese">中國</nuxt-link>：該科技在封建/<nuxt-link to="/techs/age/Castle_Age">城堡</nuxt-link
               >/<nuxt-link to="/techs/age/Imperial_Age">帝王時代</nuxt-link
               >花費便宜10%/15%/20%。
+            </li>
+            <li>
+              <nuxt-link to="/civs/Koreans">朝鮮</nuxt-link>：該科技免費。
+            </li>
+            <li>
+              <nuxt-link to="/civs/Portuguese">葡萄牙</nuxt-link>：該科技研發速度+30%。
             </li>
           </ul>
         </div>
@@ -35,7 +41,7 @@
           <ul>
             <li>
               <nuxt-link to="/civs/Bulgarians">保加利亞</nuxt-link
-              >：弓兵護甲研發速度加快50%。
+              >：弓兵護甲研發速度加快80%。
             </li>
           </ul>
         </div>
