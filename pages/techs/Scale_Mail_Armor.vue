9ce9ca6c5389394785edabf3e3880f39adcbf296
singsing612 <62696812+singsing612@users.noreply.github.com>
2021-01-06 22:27:25 +0900

    Update Scale_Mail_Armor.vue (#591)
    * Update Scale_Mail_Armor.vue
    
    保加利亞的兵工廠升級所需食物-50%相關頁面更新
    
    * Update Scale_Mail_Armor.vue
    
    * format
    
    Co-authored-by: webrsb <webrsb@gmail.com>

--- pages/techs/Scale_Mail_Armor.vue
+++ pages/techs/Scale_Mail_Armor.vue
@@ -21,10 +21,13 @@
         <div class="n1">
           <ul>
             <li>
-              <nuxt-link to="/civs/Chinese">中國</nuxt-link>：該<nuxt-link
-                to="/techs"
-                >科技</nuxt-link
-              >在封建/<nuxt-link to="/techs/age/Castle_Age">城堡</nuxt-link
+              <nuxt-link to="/civs/Bulgarians">保加利亞</nuxt-link
+              >：該科技所需食物-50%。
+            </li>
+            <li>
+              <nuxt-link to="/civs/Chinese">中國</nuxt-link
+              >：該科技在封建/<nuxt-link to="/techs/age/Castle_Age"
+                >城堡</nuxt-link
               >/<nuxt-link to="/techs/age/Imperial_Age">帝王時代</nuxt-link
               >花費便宜10%/15%/20%。
             </li>
@@ -32,7 +35,11 @@
               <nuxt-link to="/civs/Incas">印加</nuxt-link>：<nuxt-link
                 to="/units/Villager"
                 >村民</nuxt-link
-              >也獲得+1/+1護甲
+              >也獲得+1/+1護甲。
+            </li>
+            <li>
+              <nuxt-link to="/civs/Portuguese">葡萄牙</nuxt-link
+              >：該科技研發速度+30%。
             </li>
           </ul>
         </div>
@@ -42,7 +49,7 @@
           <ul>
             <li>
               <nuxt-link to="/civs/Bulgarians">保加利亞</nuxt-link
-              >：兵工廠科技研發速度加快50%。
+              >：步兵鱗甲研發速度加快80%。
             </li>
           </ul>
         </div>
