e48413cb71f71ba0efad6564ecb3d2bed596c8b3
singsing612 <62696812+singsing612@users.noreply.github.com>
2021-01-06 13:56:45 +0900

    Update Chain_Barding_Armor.vue
    保加利亞的兵工廠升級所需食物-50%相關頁面更新

--- pages/techs/Chain_Barding_Armor.vue
+++ pages/techs/Chain_Barding_Armor.vue
@@ -86,12 +86,15 @@
         <div class="n1">
           <ul>
             <li>
-              <nuxt-link to="/civs/Chinese">中國</nuxt-link>：該<nuxt-link
-                to="/techs"
-                >科技</nuxt-link
-              >在<nuxt-link to="/techs/age/Castle_Age">城堡</nuxt-link
+              <nuxt-link to="/civs/Bulgarians">保加利亞</nuxt-link>：該科技所需食物-50%。
+            </li>
+            <li>
+              <nuxt-link to="/civs/Chinese">中國</nuxt-link>：該科技在<nuxt-link to="/techs/age/Castle_Age">城堡</nuxt-link
               >/<nuxt-link to="/techs/age/Imperial_Age">帝王時代</nuxt-link
               >花費便宜15%/20%。
+            </li>
+            <li>
+              <nuxt-link to="/civs/Portuguese">葡萄牙</nuxt-link>：該科技研發速度+30%。
             </li>
             <li>
               <nuxt-link to="/civs/Spanish">西班牙</nuxt-link
@@ -106,7 +109,7 @@
           <ul>
             <li>
               <nuxt-link to="/civs/Bulgarians">保加利亞</nuxt-link
-              >：兵工廠科技研發速度加快50%。
+              >：兵工廠科技研發速度加快80%。
             </li>
           </ul>
         </div>
