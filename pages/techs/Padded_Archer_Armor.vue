f6cb12095d5a037c7055710527eec8c95da1ff59
webrsb <webrsb@gmail.com>
2021-01-06 21:16:33 +0800

    format

--- pages/techs/Padded_Archer_Armor.vue
+++ pages/techs/Padded_Archer_Armor.vue
@@ -20,10 +20,13 @@
         <div class="n1">
           <ul>
             <li>
-              <nuxt-link to="/civs/Bulgarians">保加利亞</nuxt-link>：該科技所需食物-50%。
+              <nuxt-link to="/civs/Bulgarians">保加利亞</nuxt-link
+              >：該科技所需食物-50%。
             </li>
             <li>
-              <nuxt-link to="/civs/Chinese">中國</nuxt-link>：該科技在封建/<nuxt-link to="/techs/age/Castle_Age">城堡</nuxt-link
+              <nuxt-link to="/civs/Chinese">中國</nuxt-link
+              >：該科技在封建/<nuxt-link to="/techs/age/Castle_Age"
+                >城堡</nuxt-link
               >/<nuxt-link to="/techs/age/Imperial_Age">帝王時代</nuxt-link
               >花費便宜10%/15%/20%。
             </li>
@@ -31,7 +34,8 @@
               <nuxt-link to="/civs/Koreans">朝鮮</nuxt-link>：該科技免費。
             </li>
             <li>
-              <nuxt-link to="/civs/Portuguese">葡萄牙</nuxt-link>：該科技研發速度+30%。
+              <nuxt-link to="/civs/Portuguese">葡萄牙</nuxt-link
+              >：該科技研發速度+30%。
             </li>
           </ul>
         </div>
