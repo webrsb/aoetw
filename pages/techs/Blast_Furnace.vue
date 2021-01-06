8c370ed7a45c8447c837e9b93c15435f6df9af72
singsing612 <62696812+singsing612@users.noreply.github.com>
2021-01-06 14:18:35 +0900

    Update Blast_Furnace.vue
    保加利亞的兵工廠升級所需食物-50%相關頁面更新

--- pages/techs/Blast_Furnace.vue
+++ pages/techs/Blast_Furnace.vue
@@ -81,10 +81,10 @@
         <div class="n1">
           <ul>
             <li>
-              <nuxt-link to="/civs/Chinese">中國</nuxt-link>：該<nuxt-link
-                to="/techs"
-                >科技</nuxt-link
-              >在<nuxt-link to="/techs/age/Imperial_Age">帝王時代</nuxt-link
+              <nuxt-link to="/civs/Bulgarians">保加利亞</nuxt-link>：該科技所需食物-50%。
+            </li>
+            <li>
+              <nuxt-link to="/civs/Chinese">中國</nuxt-link>：該科技在<nuxt-link to="/techs/age/Imperial_Age">帝王時代</nuxt-link
               >花費便宜20%。
             </li>
             <li>
@@ -95,6 +95,9 @@
             </li>
             <li>
               <nuxt-link to="/civs/Magyars">馬扎爾</nuxt-link>：該科技免費。
+            </li>
+            <li>
+              <nuxt-link to="/civs/Portuguese">葡萄牙</nuxt-link>：該科技研發速度+30%。
             </li>
             <li>
               <nuxt-link to="/civs/Spanish">西班牙</nuxt-link
@@ -109,7 +112,7 @@
           <ul>
             <li>
               <nuxt-link to="/civs/Bulgarians">保加利亞</nuxt-link
-              >：兵工廠科技研發速度加快50%。
+              >：兵工廠科技研發速度加快80%。
             </li>
           </ul>
         </div>
