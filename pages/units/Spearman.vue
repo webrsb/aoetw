b5975545cf48595837f3475492826bcbad804e2f
singsing612 <62696812+singsing612@users.noreply.github.com>
2021-01-10 14:04:18 +0900

    Update Spearman.vue
    更新

--- pages/units/Spearman.vue
+++ pages/units/Spearman.vue
@@ -636,7 +636,11 @@
           <ul>
             <li>
               <nuxt-link to="/civs/Aztecs">阿茲特克</nuxt-link
-              >：長槍兵訓練速度提高18％。
+              >：長槍兵訓練速度提高11％。
+            </li>
+            <li>
+              <nuxt-link to="/civs/Bulgarians">保加利亞</nuxt-link
+              >：兵工廠長槍兵科技升級所需食物-50%。
             </li>
             <li>
               <nuxt-link to="/civs/Burmese">緬甸</nuxt-link
@@ -655,7 +659,7 @@
             </li>
             <li>
               <nuxt-link to="/civs/Chinese">中國</nuxt-link
-              >：有利於長槍兵的技術在封建/<nuxt-link to="/techs/age/Castle_Age"
+              >：有利於長槍兵的科技在封建/<nuxt-link to="/techs/age/Castle_Age"
                 >城堡</nuxt-link
               >/帝王時代便宜10％/15％/20％。
             </li>
@@ -665,7 +669,7 @@
             </li>
             <li>
               <nuxt-link to="/civs/Goths">哥德</nuxt-link
-              >：長槍兵在封建/城堡/帝王時代，減免為25%/30%/35%，對<nuxt-link
+              >：長槍兵成本在封建/城堡/帝王時代，減免為25%/30%/35%，對<nuxt-link
                 to="/armor/Armor_Class_Standard_Building"
                 >標準建築</nuxt-link
               >攻擊+1/+2/+3。研發<nuxt-link to="/techs/Perfusion"
@@ -678,6 +682,9 @@
                 to="/elements/Rate_of_Fire"
                 >攻擊速度</nuxt-link
               >快33％。
+            </li>
+            <li>
+              <nuxt-link to="/civs/Koreans">朝鮮</nuxt-link>：長槍兵木材成本減少20%。
             </li>
             <li>
               <nuxt-link to="/civs/Lithuanians">立陶宛</nuxt-link
@@ -694,6 +701,10 @@
               >：長槍兵在封建/<nuxt-link to="/techs/age/Castle_Age"
                 >城堡</nuxt-link
               >/帝王時代有 +1/+2/+3<nuxt-link to="/armor">遠程護甲</nuxt-link>。
+            </li>
+            <li>
+              <nuxt-link to="/civs/Portuguese">葡萄牙</nuxt-link
+              >：長槍兵科技研發速度+30%。
             </li>
             <li>
               <nuxt-link to="/civs/Spanish">西班牙</nuxt-link>：<nuxt-link
@@ -721,7 +732,7 @@
               >：長槍兵在封建/<nuxt-link to="/techs/age/Castle_Age"
                 >城堡</nuxt-link
               >/帝王時代擁有10％/15％/20％的<nuxt-link to="/elements/Hit_points"
-                >生命值</nuxt-link
+                >血量</nuxt-link
               >。
             </li>
           </ul>
@@ -738,23 +749,23 @@
               >、的研究時間(快25%)。
             </li>
             <li>
+              <nuxt-link to="/civs/Bulgarians">保加利亞</nuxt-link
+              >：兵工廠科技研發速度快80%。
+            </li>
+            <li>
               <nuxt-link to="/civs/Goths">哥德</nuxt-link
               >：長槍兵訓練和升級快20%。研發<nuxt-link to="/techs/Squires"
                 >護衛技術</nuxt-link
               >、<nuxt-link to="/techs/Arson">縱火</nuxt-link>科技的時間快20%。
             </li>
             <li>
-              <nuxt-link to="/civs/Teutons">條頓</nuxt-link
-              >：長槍兵更不容易被招降。
-            </li>
-            <li>
               <nuxt-link to="/civs/Lithuanians">立陶宛</nuxt-link
               >：研發忠誠信仰和異端邪說的速度快20%。
             </li>
             <li>
-              <nuxt-link to="/civs/Bulgarians">保加利亞</nuxt-link
-              >：兵工廠科技研發速度快50%。
-            </li>
+              <nuxt-link to="/civs/Teutons">條頓</nuxt-link
+              >：長槍兵更不容易被招降。
+            </li>          
           </ul>
         </div>
         <h3>更新日誌</h3>
@@ -857,6 +868,25 @@
               <nuxt-link to="/civs/Lithuanians">立陶宛</nuxt-link
               >：36906更新後<nuxt-link to="/techs/Tower_Shields">塔盾</nuxt-link
               >給予長槍兵額外+2遠程護甲。
+            </li>
+            <li>
+              (39284更新)
+              <nuxt-link to="/civs/Koreans">朝鮮</nuxt-link
+              >：除攻城武器的軍事單位減少20%的木材消耗。
+            </li>
+            <li>
+              (42848更新)
+              <nuxt-link to="/civs/Portuguese">葡萄牙</nuxt-link>:
+              長槍兵科技研發速度+30%。
+            </li>
+            <li>
+              (42848更新)
+              <nuxt-link to="/civs/Bulgarians">保加利亞</nuxt-link>:
+              團隊加分中<nuxt-link to="/building/Blacksmith">兵工廠</nuxt-link
+              >工作效率從+50%增加成+80%。
+            </li>
+            <li>
+              (42848更新) 保加利亞: 兵工廠科技升級所需食物-50%。
             </li>
           </ul>
         </div>
