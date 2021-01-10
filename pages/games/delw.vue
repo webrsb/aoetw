bc44d1c0cc499b7212668c13f1f608e0fdc0f190
singsing612 <62696812+singsing612@users.noreply.github.com>
2021-01-10 09:58:07 +0800

    Create delw.vue
    世紀帝國II：決定版 西方霸主介紹

---
+++ pages/games/delw.vue
@@ -0,0 +1,270 @@
+<template>
+  <div class="container">
+    <div class="row padtop">
+      <div class="col-md-12">
+        <h1>世紀帝國II：決定版 西方霸主</h1>
+        <div class="hrn1"></div>
+      </div>
+      <div class="col-md-8">
+        <div class="n1">
+          <p>
+            <b>《世紀帝國II：決定版 西方霸主》</b
+            >，又稱<b>西方霸主</b>，是世紀帝國II第六張資料片，也是《世紀帝國II：決定版》第一張資料片，將在2021年1月26日發行。本資料片新增了兩個新<nuxt-link
+              to="/civs"
+              >文明</nuxt-link
+            >。
+          </p>
+        </div>
+        <h2>新增內容</h2>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <ul>
+          </ul>
+        </div>
+
+        <h2>新增元素</h2>
+        <div class="hrn1"></div>
+        <h3>文明</h3>
+        <div class="n1">
+          <ul>
+            <li>
+              <strong
+                ><nuxt-link to="/civs/Burgundians">勃根地</nuxt-link></strong
+              >(西歐):以經濟實力、文化成就和運用先進的軍事科技及戰術，由單純的公國崛起成為西歐奇跡。勃根地特殊單位馬上輕裝兵，是進入戰場時會使用強大震懾攻擊的騎兵單位。
+            </li>
+            <li>
+              <strong
+                ><nuxt-link to="/civs/Sicilians">西西里</nuxt-link></strong
+              >(地中海):在您打造中世紀歐洲最令人垂涎的王國之一時，也一同體驗位於地中海交通樞紐特有的多元文化和尚武精神。西西里特殊單位薩金特衛兵，是能建造強大城樓的強健步兵單位。
+            </li>
+          </ul>
+        </div>
+        <h3>單位</h3>
+        <div class="n1"></div>
+        <h5>特殊單位</h5>
+        <div class="n1">
+          <ul>
+            <li>
+              <strong
+                ><nuxt-link to="/units/Coustillier">馬上輕裝兵</nuxt-link></strong
+              >:勃根地特殊騎兵單位。可給予高攻擊的騎兵部隊。
+            </li>
+            <li>
+              <strong><nuxt-link to="/units/Flemish_Militia">佛拉芒民兵</nuxt-link></strong
+              >:勃根地特殊步兵單位，對騎兵強勢，對弓兵劣勢。
+            </li>
+            <li>
+              <strong><nuxt-link to="/units/Serjeant">薩金特衛兵</nuxt-link></strong
+              >:西西里特殊步兵單位，可建造特殊建築城樓。
+            </li>
+          </ul>
+        </div>
+        <h5>動物</h5>
+        <div class="n1">
+          <ul>
+          </ul>
+        </div>
+        <h5>英雄</h5>
+        <div class="n1">
+          <ul>
+          </ul>
+        </div>
+        <h5>其他</h5>
+        <div class="n1">
+          <ul>
+          </ul>
+        </div>
+        <h5>作弊單位</h5>
+        <div class="n1">
+          <ul>
+          </ul>
+        </div>
+        <h3>建築</h3>
+        <div class="n1">
+          <ul>
+            <li>
+              <strong><nuxt-link to="/building/Donjon">城樓</nuxt-link></strong
+              >:西西里特殊建築。可於城堡時代由薩金特衛兵建造並生產薩金特衛兵的小城堡，但無法研發科技。
+            </li>
+          </ul>
+        <h3>科技</h3>
+        <div class="n1"></div>
+        </div>
+        <h5>特殊科技</h5>
+        <div class="n1">
+          <ul>
+            <li>
+              <strong
+                ><nuxt-link to="/techs/Burgundian_Vineyards">勃根地葡萄園</nuxt-link></strong
+              >:勃根地城堡時代特殊科技。以2:1的比例把所有食物轉換成黃金。
+            </li>
+            <li>
+              <strong
+                ><nuxt-link to="/techs/Flemish_Revolution"
+                  >佛拉芒革命</nuxt-link
+                ></strong
+              >:勃根地帝王時代特殊科技。所有村民變成佛拉芒民兵。
+            </li>
+            <li>
+              <strong><nuxt-link to="/techs/First_Crusade">第一次十字軍東征</nuxt-link></strong
+              >:西西里城堡時代特殊科技。所有城鎮中心一次性生產7個薩金特衛兵。
+            </li>
+            <li>
+              <strong><nuxt-link to="/techs/Scutage">軍役免除金</nuxt-link></strong
+              >:西西里帝王時代特殊科技。玩家和同盟立即獲得一次性的黃金，每個軍事單位獲得15黃金。
+            </li>
+          </ul>
+        <h3>戰役</h3>
+        <div class="n1"></div>
+        <div class="n1">
+          <p>
+            <em>主要文章:<nuxt-link to="/batt">戰役</nuxt-link></em>
+          </p>
+          <ul>
+            <li>
+              長腿愛德華(不列顛):身為叛亂貴族的人質而懷恨在心的長腿愛德華，誓言重新奪回自己理應繼承的一切，並帶領分裂的英格蘭獲得前所未有、至高無上的威望和堅韌地位。他既是精明的指揮官，也是狡詐的政治家，使用的手法殘酷有效，卻罔顧道義。跟隨被權力和榮耀驅使的他，一起征戰英格蘭、威爾斯、蘇格蘭，甚至踏上十字軍東征的征途吧！
+            </li>
+            <li>
+              西方大公爵(勃根地):數十年來，勃根地的公爵不斷運用謀略巧思、軍事實力和龐大財富，涉入爭奪法蘭西瘋子國王攝政權的血腥鬥爭。然而，隨著付出的鮮血代價越來越高昂，他們將重心移往北方，致力於建立屬於自己的王國。
+            </li>
+            <li>
+              歐特維爾家族(西西里):公元 1047 年，羅伯特·德·歐特維爾隨著一個小型強盜集團一同離開諾曼第。短短一百年間，他的家族成員茁壯成為義大利公爵、聖地王子，甚至是西西里國王。加入這些勇猛諾曼冒險家的陣營，一同將三種文化交織融合成歐洲最具包容性也最錯綜複雜的王國吧。
+            </li>
+          </ul>
+
+            <h3>圖庫</h3>
+            <div class="hrn1"></div>
+            <div class="n1">
+              <div class="row">
+                <div class="col-md-4">
+                  <gallery
+                    ><img
+                      class="imgn hg"
+                      width="250"
+                      src="~assets/Bulgarians.jpg"
+                      alt=""
+                  /></gallery>
+                  <p><em>勃根地預覽圖</em></p>
+                </div>
+                <div class="col-md-4">
+                  <gallery
+                    ><img
+                      class="imgn hg"
+                      width="250"
+                      src="~assets/Cumans.jpg"
+                      alt=""
+                  /></gallery>
+                  <p><em>西西里預覽圖</em></p>
+                </div>
+                <div class="col-md-4">
+                  <gallery
+                    ><img
+                      class="imgn hg"
+                      width="250"
+                      src="~assets/Lithuanians.jpg"
+                      alt=""
+                  /></gallery>
+                  <p><em>有世界奇觀布魯塞爾市政廳的勃根地預覽圖</em></p>
+                </div>
+              </div>
+          </div>
+        </div>
+       </div>
+      </div>
+      <div class="col-md-4">
+        <table class="table-bordered" width="100%">
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#356a98">
+              <div class="unititle2">
+                世紀帝國II：決定版 西方霸主
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <div class="unititle">
+                <img
+                  width="100%"
+                  src="~assets/Age-of-empires-ii_de_keyart.jpg"
+                  alt=""
+                />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#356a98">
+              <div class="unititle2" style="color: #fff;">遊戲介紹</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">開發商</td>
+            <td class="unit4">
+              Tantalus <br />Forgotten Empires<br />Wicked Witch
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">發行</td>
+            <td class="unit4">微軟</td>
+          </tr>
+          <tr>
+            <td class="unit3">遊玩平台</td>
+            <td class="unit4">Windows 10</td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#356a98;">
+              <div class="unititle2" style="color: #fff;">發行日期</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">steam版本</td>
+            <td class="unit4">2021年1月26日</td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#356a98;">
+              <div class="unititle2" style="color: #fff;">前作與擴充</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">擴充</td>
+            <td class="unit4">
+              <nuxt-link to="/games/aok">帝王世紀</nuxt-link>
+              <br /><nuxt-link to="/games/aoc">征服者入侵</nuxt-link>
+              <br /><nuxt-link to="/games/aof">失落的帝國</nuxt-link>
+              <br /><nuxt-link to="/games/ak">非洲王國</nuxt-link>
+              <br /><nuxt-link to="/games/aor">王者崛起</nuxt-link>
+              <br /><nuxt-link to="/games/de">決定版</nuxt-link>
+            </td>
+          </tr>
+        </table>
+
+        <div class="thanks">感謝! 此頁面由singsing612協助編輯</div>
+        <div class="wiki" style="margin-top: 5px;">
+          <div style="float: left;">
+            <img src="~assets/Wikipedia-logo.png" width="50" height="46" />
+          </div>
+          <div style="margin-left: 60px;">
+            百科上的 世紀帝國II：決定版
+            <div style="margin-left: 10px;">
+              <i
+                ><b
+                  ><a
+                    href="https://ageofempires.fandom.com/wiki/Age_of_Empires_II:_Definitive_Edition_-_Lords_of_the_West"
+                    target="_blank"
+                    >Age of Empires II: Definitive Edition - Lords of the West</a
+                  ></b
+                ></i
+              >
+            </div>
+          </div>                    
+        </div>
+       </div>
+      </div>
+     </div>
+</template>
+<script>
+export default {
+  layout: '',
+  components: {}
+}
+</script>
+<style></style>
