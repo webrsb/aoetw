3d868905668ac57d352915849c414636f11d0222
singsing612 <62696812+singsing612@users.noreply.github.com>
2021-01-13 22:06:41 +0900

    Update Condottiero.vue (#630)
    * Update Condottiero.vue
    
    移除追蹤技術，視野自動+2。
    
    * Update Condottiero.vue

--- pages/units/Condottiero.vue
+++ pages/units/Condottiero.vue
@@ -269,19 +269,6 @@
           </tr>
           <tr>
             <td class="tta1" style="width:10%">
-              <nuxt-link to="/elements/Line_of_Sight">視野</nuxt-link>
-            </td>
-            <td class="tta4">
-              <img
-                class="gg"
-                src="~assets/tracking.png"
-                width="30"
-                alt=""
-              /><nuxt-link to="/techs/Tracking">追蹤技術</nuxt-link>(+2)
-            </td>
-          </tr>
-          <tr>
-            <td class="tta1" style="width:10%">
               <nuxt-link to="/elements/Conversion">招降</nuxt-link>抵抗
             </td>
             <td class="tta4">
@@ -943,7 +930,7 @@
           </tr>
           <tr>
             <td class="unit3">視野</td>
-            <td class="unit4">6</td>
+            <td class="unit4">6 (+2)</td>
           </tr>
         </table>
 
