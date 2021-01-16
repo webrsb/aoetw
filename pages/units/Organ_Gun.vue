c15be0cecef3f85e8bcaac64d321a262119da523
singsing612 <62696812+singsing612@users.noreply.github.com>
2021-01-16 19:27:55 +0900

    Update Organ_Gun.vue

--- pages/units/Organ_Gun.vue
+++ pages/units/Organ_Gun.vue
@@ -574,7 +574,7 @@
             <td class="unit3">
               <nuxt-link to="/resource/Gold">黃金</nuxt-link>
             </td>
-            <td class="unit4">70</td>
+            <td class="unit4">56</td>
           </tr>
           <tr>
             <td
