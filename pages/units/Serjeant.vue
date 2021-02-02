9b7a4f602820f8aa8a0699e1ce194ac86224f930
webrsb <webrsb@gmail.com>
2021-02-02 21:51:15 +0800

    format

--- pages/units/Serjeant.vue
+++ pages/units/Serjeant.vue
@@ -8,16 +8,17 @@
       <div class="col-md-8">
         <div class="n1">
           <p>
-            <b>薩金特衛兵</b>是<nuxt-link to="/civs/Sicilians"
-              >西西里</nuxt-link
+            <b>薩金特衛兵</b>是<nuxt-link to="/civs/Sicilians">西西里</nuxt-link
             >的<nuxt-link to="/units/Unique_Unit">特殊單位</nuxt-link
             >，能夠在<nuxt-link to="/techs/age/Feudal_Age">封建時代</nuxt-link
-            >的<nuxt-link to="/building/Donjon">城樓</nuxt-link
-            >和<nuxt-link to="/building/Castle">城堡</nuxt-link
+            >的<nuxt-link to="/building/Donjon">城樓</nuxt-link>和<nuxt-link
+              to="/building/Castle"
+              >城堡</nuxt-link
             >中訓練。薩金特衛兵是個可以建造城樓的單位。薩金特衛兵可以在<nuxt-link
               to="/techs/age/Imperial_Age"
               >帝王時代</nuxt-link
-            >升級為<nuxt-link to="/units/Shotel_Warrior">精銳薩金特衛兵</nuxt-link
+            >升級為<nuxt-link to="/units/Shotel_Warrior"
+              >精銳薩金特衛兵</nuxt-link
             >。
           </p>
         </div>
@@ -26,38 +27,62 @@
         <div class="n1">
           <ul>
             <li>
-              薩金特衛兵是一個超級特殊的<nuxt-link to="/units/Infantry_Units">步兵單位</nuxt-link
+              薩金特衛兵是一個超級特殊的<nuxt-link to="/units/Infantry_Units"
+                >步兵單位</nuxt-link
               >，他們能建造<nuxt-link to="/building/Donjon">城樓</nuxt-link
-              >，並且可以在<nuxt-link to="/techs/age/Feudal_Age">封建時代</nuxt-link
-              >的城樓中訓練。封建時代的薩金特衛兵與<nuxt-link to="/units/Man-at-Arms">裝甲步兵</nuxt-link
-              >相比，他們擁有相同<nuxt-link to="/elements/Hit_points">血量</nuxt-link
+              >，並且可以在<nuxt-link to="/techs/age/Feudal_Age"
+                >封建時代</nuxt-link
+              >的城樓中訓練。封建時代的薩金特衛兵與<nuxt-link
+                to="/units/Man-at-Arms"
+                >裝甲步兵</nuxt-link
+              >相比，他們擁有相同<nuxt-link to="/elements/Hit_points"
+                >血量</nuxt-link
               >，<nuxt-link to="/armor">近防</nuxt-link
-              >和遠防比裝甲步兵高，但<nuxt-link to="/elements/Attack">攻擊力</nuxt-link
-              >和視野低。不過，薩金特衛兵成本更高，並且需要花費200<nuxt-link to="/resource/Stone">石頭</nuxt-link
-              >和75<nuxt-link to="/resource/Wood">木材</nuxt-link
-              >建造城樓訓練。
+              >和遠防比裝甲步兵高，但<nuxt-link to="/elements/Attack"
+                >攻擊力</nuxt-link
+              >和視野低。不過，薩金特衛兵成本更高，並且需要花費200<nuxt-link
+                to="/resource/Stone"
+                >石頭</nuxt-link
+              >和75<nuxt-link to="/resource/Wood">木材</nuxt-link>建造城樓訓練。
             </li>
             <li>
-              薩金特衛兵能建造城樓的能力衍生出類似<nuxt-link to="/ar/towerrush">塔攻</nuxt-link
-              >的打法-城樓攻，這打法的配置和策略大致與塔攻相似。另外薩金特衛兵建造城樓的時間和<nuxt-link to="/units/Villager">村民</nuxt-link
-              >一樣，因此村民在蓋完第一座城樓時就可以返回基地採集資源，而且城樓可以<nuxt-link to="/elements/Garrison">駐守</nuxt-link
+              薩金特衛兵能建造城樓的能力衍生出類似<nuxt-link to="/ar/towerrush"
+                >塔攻</nuxt-link
+              >的打法-城樓攻，這打法的配置和策略大致與塔攻相似。另外薩金特衛兵建造城樓的時間和<nuxt-link
+                to="/units/Villager"
+                >村民</nuxt-link
+              >一樣，因此村民在蓋完第一座城樓時就可以返回基地採集資源，而且城樓可以<nuxt-link
+                to="/elements/Garrison"
+                >駐守</nuxt-link
               >10個單位，可以配合<nuxt-link to="/units/Archer">弓兵</nuxt-link
               >增加城樓箭數，再通過城樓訓練薩金特衛兵繼續建造城樓。
             </li>
             <li>
               一旦到達<nuxt-link to="/techs/age/Castle_Age">城堡時代</nuxt-link
-              >，薩金特衛兵會獲得+15血量（總計60），+3攻擊力（總計8），+2遠防和+1近防（總計3/3護甲），與<nuxt-link to="/units/Long_Swordsman">長劍兵</nuxt-link
-              >相比，城堡時代的薩金特衛兵更能抵禦弓箭和近戰攻擊，加上西西里有減少額外攻擊的文明加成，使薩金特衛兵能應對不同類型的單位。城堡時代後，除非缺乏石頭建造城樓或城堡，薩金特衛兵基本上是首選步兵。另外，<nuxt-link to="/units/Militia">民兵</nuxt-link
-              >系仍然是對抗<nuxt-link to="/units/Eagle_Warrior">鷹勇士</nuxt-link
-              >的更好選擇，在某些情況下還是有可能出動長劍兵。<nuxt-link to="/techs/age/Imperial_Age">帝王時代</nuxt-link
-              >後，精銳薩金特衛兵仍然比<nuxt-link to="/units/Champion">劍兵勇士</nuxt-link
-              >好。其裝甲是高遠防<nuxt-link to="/units/Huskarl">哥德衛隊</nuxt-link
-              >和高近防<nuxt-link to="/units/Teutonic_Knight">條頓武士</nuxt-link
+              >，薩金特衛兵會獲得+15血量（總計60），+3攻擊力（總計8），+2遠防和+1近防（總計3/3護甲），與<nuxt-link
+                to="/units/Long_Swordsman"
+                >長劍兵</nuxt-link
+              >相比，城堡時代的薩金特衛兵更能抵禦弓箭和近戰攻擊，加上西西里有減少額外攻擊的文明加成，使薩金特衛兵能應對不同類型的單位。城堡時代後，除非缺乏石頭建造城樓或城堡，薩金特衛兵基本上是首選步兵。另外，<nuxt-link
+                to="/units/Militia"
+                >民兵</nuxt-link
+              >系仍然是對抗<nuxt-link to="/units/Eagle_Warrior"
+                >鷹勇士</nuxt-link
+              >的更好選擇，在某些情況下還是有可能出動長劍兵。<nuxt-link
+                to="/techs/age/Imperial_Age"
+                >帝王時代</nuxt-link
+              >後，精銳薩金特衛兵仍然比<nuxt-link to="/units/Champion"
+                >劍兵勇士</nuxt-link
+              >好。其裝甲是高遠防<nuxt-link to="/units/Huskarl"
+                >哥德衛隊</nuxt-link
+              >和高近防<nuxt-link to="/units/Teutonic_Knight"
+                >條頓武士</nuxt-link
               >的融合中間體。
             </li>
             <li>
-              西西里特殊科技<nuxt-link to="/techs/First_Crusade">第一次十字軍東征</nuxt-link
-              >可以使所有<nuxt-link to="/building/Town_Center">城鎮中心</nuxt-link
+              西西里特殊科技<nuxt-link to="/techs/First_Crusade"
+                >第一次十字軍東征</nuxt-link
+              >可以使所有<nuxt-link to="/building/Town_Center"
+                >城鎮中心</nuxt-link
               >（最多5座）一次性生產10個薩金特衛兵。即使已經達到人口上限仍然可以研發，可以在緊急情況下研發，從而快速增加軍隊數量突襲對手。
             </li>
           </ul>
@@ -77,29 +102,29 @@
               <nuxt-link to="/units/Villager">村民</nuxt-link>、<nuxt-link
                 to="/units/Skirmisher"
                 >矛兵</nuxt-link
-              >、<nuxt-link to="/units/Halberdier">戟兵</nuxt-link
-              >、<nuxt-link to="/units/Light_Cavalry">輕騎兵</nuxt-link
+              >、<nuxt-link to="/units/Halberdier">戟兵</nuxt-link>、<nuxt-link
+                to="/units/Light_Cavalry"
+                >輕騎兵</nuxt-link
               >和<nuxt-link to="/units/Eagle_Warrior">鷹勇士</nuxt-link>。
             </td>
           </tr>
           <tr>
             <td class="tta1" style="width:30%">劣勢(不易對抗)</td>
             <td class="tta4">
-              <nuxt-link
-                to="/units/Scorpion"
-                >弩砲</nuxt-link
-              >、<nuxt-link to="/units/Mangonel">投石車</nuxt-link>、<nuxt-link
-                to="/units/Hand_Cannoneer"
-                >火槍兵</nuxt-link
+              <nuxt-link to="/units/Scorpion">弩砲</nuxt-link>、<nuxt-link
+                to="/units/Mangonel"
+                >投石車</nuxt-link
+              >、<nuxt-link to="/units/Hand_Cannoneer">火槍兵</nuxt-link
               >、<nuxt-link to="/units/Conquistador">西班牙征服者</nuxt-link
               >、<nuxt-link to="/units/Janissary">土耳其火槍兵</nuxt-link
-              >、<nuxt-link to="/units/Slinger">投石手</nuxt-link
-              >、<nuxt-link to="/units/Cataphract">拜占庭聖騎兵</nuxt-link
+              >、<nuxt-link to="/units/Slinger">投石手</nuxt-link>、<nuxt-link
+                to="/units/Cataphract"
+                >拜占庭聖騎兵</nuxt-link
               >、<nuxt-link to="/units/Jaguar_Warrior">豹勇士</nuxt-link
-              >、<nuxt-link to="/units/Samurai">日本武士</nuxt-link
-              >、<nuxt-link to="/units/Leitis">烈堤司</nuxt-link
-              >、<nuxt-link to="/units/Paladin">遊俠</nuxt-link
-              >。
+              >、<nuxt-link to="/units/Samurai">日本武士</nuxt-link>、<nuxt-link
+                to="/units/Leitis"
+                >烈堤司</nuxt-link
+              >、<nuxt-link to="/units/Paladin">遊俠</nuxt-link>。
             </td>
           </tr>
           <tr class="tta">
@@ -207,6 +232,7 @@
             </td>
           </tr>
         </table>
+
         <h3>剋薩金特衛兵的單位與建築</h3>
         <div class="hrn1"></div>
         <div class="n1 bonuses">
@@ -353,7 +379,7 @@
               >(<nuxt-link to="/civs/Byzantines">拜占庭</nuxt-link>限定)
             </li>
           </ul>
-          </div>
+        </div>
         <h3>文明加分</h3>
         <div class="hrn1"></div>
         <div class="n1">
@@ -375,7 +401,8 @@
               >能夠提高薩金特衛兵的生產速度還有加速<nuxt-link
                 to="/techs/Conscription"
                 >徵兵技術</nuxt-link
-              >和<nuxt-link to="/techs/First_Crusade">第一次十字軍東征</nuxt-link
+              >和<nuxt-link to="/techs/First_Crusade"
+                >第一次十字軍東征</nuxt-link
               >的研究時間(快25%)。
             </li>
             <li>
@@ -385,10 +412,10 @@
               >提升薩金特衛兵護甲和怯薛攻擊的科技研發快80％。
             </li>
             <li>
-              <nuxt-link to="/civs/Goths">哥德</nuxt-link>：研發<nuxt-link to="/techs/Squires">護衛技術</nuxt-link>、<nuxt-link
-                to="/techs/Arson"
-                >縱火</nuxt-link
-              >的時間快20%。
+              <nuxt-link to="/civs/Goths">哥德</nuxt-link>：研發<nuxt-link
+                to="/techs/Squires"
+                >護衛技術</nuxt-link
+              >、<nuxt-link to="/techs/Arson">縱火</nuxt-link>的時間快20%。
             </li>
             <li>
               <nuxt-link to="/civs/Berbers">立陶宛</nuxt-link>：<nuxt-link
@@ -409,9 +436,7 @@
             <em><nuxt-link to="/games/delw">西方霸主</nuxt-link></em>
           </h3>
           <ul>
-            <li>
-              
-            </li>
+            <li></li>
           </ul>
         </div>
         <h3>趣事</h3>
@@ -478,7 +503,8 @@
             <td class="unit3">時代</td>
             <td class="unit4">
               <img class="gg" src="~assets/castle_age.png" alt="" /><nuxt-link
-                to="/techs/age/Feudal_Age">
+                to="/techs/age/Feudal_Age"
+              >
                 封建時代</nuxt-link
               >
             </td>
@@ -546,7 +572,8 @@
             <td class="unit4">
               對<nuxt-link to="/armor/Armor_Class_Eagle_Warrior"
                 >鷹勇士</nuxt-link
-              >+2/+3(精銳) <br />對<nuxt-link to="/armor/Armor_Class_Standard_Building"
+              >+2/+3(精銳) <br />對<nuxt-link
+                to="/armor/Armor_Class_Standard_Building"
                 >標準建築</nuxt-link
               >+2/+3(精銳)
             </td>
@@ -590,7 +617,11 @@
           <tr>
             <td class="unit3">升級到</td>
             <td class="unit4">
-              <img class="gg" src="~assets/ShotelIcon.png" alt="" />精銳薩金特衛兵
+              <img
+                class="gg"
+                src="~assets/ShotelIcon.png"
+                alt=""
+              />精銳薩金特衛兵
             </td>
           </tr>
           <tr>
