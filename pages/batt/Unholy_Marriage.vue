3e34f986e37576e1460d1bfeea7e839dbe34dd11
webrsb <webrsb@gmail.com>
2021-03-29 00:00:59 +0800

    Update and rename Unholy_Marriage to Unholy_Marriage.vue

---
+++ pages/batt/Unholy_Marriage.vue
@@ -0,0 +1,227 @@
+<template>
+  <div class="container">
+    <div class="row padtop">
+      <div class="col-md-12">
+        <h1>罪惡的婚姻</h1>
+        <div class="hrn1"></div>
+      </div>
+      <div class="col-md-8">
+        <div class="n1">
+          <p>
+            <b>罪惡的婚姻</b>是<nuxt-link to="/games/delw">西方霸主</nuxt-link>DLC新增的<nuxt-link to="/batt">戰役</nuxt-link>中，<nuxt-link to="/batt/Tamerlane">大公爵</nuxt-link>的第四關。本關之後的主角換成約翰的兒子：好人菲利普。
+          </p>
+        </div>
+        <h2>玩家</h2>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <h3>基本資訊</h3>
+          <table class="table-bordered" width="100%">
+            <tr>
+              <td class="tc1" width="14%">起始時代：</td>
+              <td class="tc2" width="14%">
+                <nuxt-link to="/techs/age/Castle_Age">城堡時代</nuxt-link>
+              </td>
+              <td class="tc1" width="14%">起始資源：</td>
+              <td class="tc2">
+                <nuxt-link to="/resource/Wood">木材</nuxt-link>450/<nuxt-link to="/resource/Food">食物</nuxt-link>500/<nuxt-link to="/resource/Gold">黃金</nuxt-link>450/<nuxt-link to="/resource/Stone">石料</nuxt-link>125
+              </td>
+              <td class="tc1" width="14%">人口上限：</td>
+              <td class="tc2" width="5%">175</td>
+            </tr>
+          </table>
+
+          <h3>AI</h3>
+          <table class="table-bordered" width="100%">
+            <tr>
+              <td class="tc4" colspan="4" style="background: #2980b9;">
+                玩家本身
+              </td>
+            </tr>
+            <tr>
+              <td class="tc6" width="14%">
+                <div class="p6"></div>
+                玩家1
+              </td>
+              <td class="tc6" colspan="6">
+                <nuxt-link to="/civs/Burgundians">勃根地人</nuxt-link>（紫色)
+              </td>
+            </tr>
+            <tr>
+              <td class="tc4" colspan="4" style="background: #27ae60;">同盟</td>
+            </tr>
+            <tr>
+              <td class="tc6" width="14%">
+                <div class="p4"></div>
+                玩家2
+              </td>
+              <td class="tc6">巴伐利亞公爵（黃色）<nuxt-link to="/civs/Teutons">條頓人</nuxt-link></td>
+              <td class="tc6" width="14%">
+                <div class="p5"></div>
+                玩家6
+              </td>
+              <td class="tc6">那慕爾郡（青色）條頓人</td>
+            </tr>
+            <tr>
+              <td class="tc6" width="14%">
+                <div class="p8"></div>
+                玩家6
+              </td>
+              <td class="tc6">康布雷西（橙色）<nuxt-link to="/civs/Franks">法蘭克人</nuxt-link></td>
+            </tr>
+            <td col class="tc4" colspan="4" style="background:#ea6153;">
+              敵對
+            </td>
+            <tr>
+              <td class="tc6" width="14%">
+                <div class="p2"></div>
+                玩家3
+              </td>
+              <td class="tc6">蘭卡斯特的亨弗萊（紅色）<nuxt-link to="/civs/Britons">不列顛人</nuxt-link></td>
+              <td class="tc6" width="14%">
+                <div class="p3"></div>
+                玩家4
+              </td>
+              <td colspan="3" class="tc6">埃諾的賈桂琳（綠色）勃根地人</td>
+            </tr>
+            <tr></tr>
+          </table>
+
+          <br />
+          <dl class="dl-horizontal">
+            <dt>開場：</dt>
+            <dd>
+              好人菲利普、<nuxt-link to="/units/Knight">騎士</nuxt-link>x6、馬上輕裝兵 x3、<nuxt-link to="/units/Scout_Cavalry">斥侯騎兵</nuxt-link>x1、<nuxt-link to="/units/Villager">村民</nuxt-link>x9
+            </dd>
+          </dl>
+
+          <h3>任務</h3>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ul>
+              <li>打敗埃諾的賈桂琳。</li>
+              <li>打敗蘭卡斯特的亨弗萊。</li>
+            </ul>
+            <p>次要任務</p>
+            <ul>
+              <li>
+                向那慕爾伯爵支付1000<nuxt-link to="/resource/Gold">黃金</nuxt-link>，使好人菲莉普成為那慕爾繼承人，讓他們為您而戰。
+              </li>
+              <li>
+                放置兩件<nuxt-link to="/Relic">聖物</nuxt-link>於康布雷西<nuxt-link to="/building/Monastery">修道院</nuxt-link>，確保您的親戚可以被選為主教。之後康布雷西的居民會向您進貢。
+              </li>
+              <li>
+                在康布雷西建造一座大教堂（於標記區域內），城鎮將會繼續向<nuxt-link to="/civs/Burgundians">勃根地人</nuxt-link>進貢。
+              </li>
+            </ul>
+          </div>
+          <h3>提示</h3>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ul>
+              <li>好人菲利普的人口上限為175人。</li>
+              <li>
+                戰爭的花費十分高昂。菲利普最好能夠提高影響力來贏得可以在經濟和軍事上支持他的盟友。
+              </li>
+              <li>
+                英格蘭<nuxt-link to="/units/Longbowman">長弓兵</nuxt-link>能夠造成的威脅不容忽視。訓練大量的<nuxt-link to="/units/Cavalry_Units">騎兵</nuxt-link>和<nuxt-link to="/units/Skirmisher">矛兵</nuxt-link>可能是戰勝蘭卡斯特的亨弗萊軍隊的關鍵。
+              </li>
+            </ul>
+          </div>
+          <h3>斥候</h3>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ul>
+              <li>
+                好人菲利普（1，紫色）已經在埃諾郡的西邊站穩腳跟。他的父親遇害後留給他一筆財產，所以他已經相當富有了，但他得馬上開始招募一支軍隊才能和他得敵人抗衡。
+              </li>
+              <li>
+                巴伐利亞的約翰（2，黃色）正在從東邊向埃諾進軍。他指揮著一支強力的軍隊，但單憑一己之力很難戰勝他的姪女和護國公。
+              </li>
+
+              <li>
+                賈桂琳（4，綠色）和蘭卡斯特的亨弗萊（3，紅色）的領土位於北邊和西北方向，他們會不惜一切代價進行防守。賈桂琳的主力部隊為<nuxt-link to="/units/Infantry_Units">步兵</nuxt-link>和<nuxt-link to="/armor/Armor_Class_Cavalry">騎兵</nuxt-link>，亨弗萊則仰賴他那些致命長弓兵的射程。
+              </li>
+              <li>
+                勃根地人或許可以組成其他聯盟。東南方的那慕爾郡（5，青色）沒有男性子嗣，菲利普可以利用這點來獲得優勢。康布雷西城（6，橙色）坐落於西南方。菲利普的親戚在那麼有相當大的影響力，因此勃根地人可以從中受益。
+              </li>
+            </ul>
+          </div>
+          <h3>攻略</h3>
+          <div class="hrn1"></div>
+          <ul>
+            <li>
+              在基地的東北方有一處<nuxt-link to="/resource/Gold">金礦</nuxt-link>和<nuxt-link to="/resource/Stone">石礦</nuxt-link>，先在該處起一座<nuxt-link to="/building/Town_Center">城鎮中心</nuxt-link>挖個<nuxt-link to="/resource/Stone">石礦</nuxt-link>後用<nuxt-link to="/building/Stone_Wall">石牆</nuxt-link>封住道路並蓋個<nuxt-link to="/building/Castle">城堡</nuxt-link>防守紅色，主力也調來這邊防守，同時基地的下方也不要疏忽警備，綠色會從下面攻來，至少要有20隻<nuxt-link to="/units/Pikeman">重裝長槍兵</nuxt-link>防守，並在河口那邊也補一座<nuxt-link to="/building/Castle">城堡</nuxt-link>。過了不久，青色和橙色會紛紛提出請求；青色會要我們提供1000黃金就會出兵，否則會變敵對；橙色會要我們收集<nuxt-link to="/Relic">聖物</nuxt-link>後就會進貢，但過一段時間會要我們在他家建造大教堂才會繼續進貢。因此經濟的起步先擺在黃金，並把支線任務做一做達成盟友的要求。
+            </li>
+
+            <li>
+              準備好後先集中精力打紅色免得<nuxt-link to="/units/Longbowman">長弓兵</nuxt-link>會來亂以及讓基地後方沒有後顧之憂，進攻兵力以<nuxt-link to="/units/Coustillier">馬上輕裝兵 </nuxt-link>、<nuxt-link to="/units/Elite_Skirmisher">戰矛兵</nuxt-link>、<nuxt-link to="/units/Bombard_Cannon">火砲</nuxt-link>或巨投為主，再來就攻擊綠色，對方除了騎兵外會出<nuxt-link to="/units/Halberdier">戟兵</nuxt-link>來制衡我方的<nuxt-link to="/armor/Armor_Class_Cavalry">騎兵單位</nuxt-link>反制即可。
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <table class="table-bordered" width="100%">
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#375d96;">
+              <div class="unititle2">罪惡的婚姻</div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              （地圖待補）
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#375d96;">
+              <div class="unititle2">戰役資訊</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">版本</td>
+            <td class="unit4">
+              <nuxt-link to="/games/delw">西方霸主</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">戰役</td>
+            <td class="unit4">
+              <nuxt-link to="/batt/Tamerlane">大公爵</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3"><nuxt-link to="/civs">文明</nuxt-link></td>
+            <td class="unit4">
+              <nuxt-link to="/civs/Burgundians">勃根地</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#375d96;">
+              <div class="unititle2">戰役關卡編號</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">章節</td>
+            <td class="unit4">
+              勃根地第四章
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">上一關</td>
+            <td class="unit4"><nuxt-link to="/batt/The_Cleansing_of_Paris">掃蕩巴黎</nuxt-link></td>
+          </tr>
+          <tr>
+            <td class="unit3">下一關</td>
+            <td class="unit4"><nuxt-link to="/batt/The_Hook_and_Cod_Wars">鉤子和鱈魚之戰</nuxt-link></td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'allbatt',
+  components: {}
+}
+</script>
+<style></style>
