c750bc7aef47d7dcc7eaf89112b5803dc4714bd3
harry3305 <52451150+harry3305@users.noreply.github.com>
2021-03-28 23:57:21 +0800

    Create Sultan_of_Hindustan (#688)
    * Create Sultan_of_Hindustan
    
    帖木兒 第四關

---
+++ pages/batt/Sultan_of_Hindustan.vue
@@ -0,0 +1,243 @@
+<template>
+  <div class="container">
+    <div class="row padtop">
+      <div class="col-md-12">
+        <h1>印度斯坦的蘇丹</h1>
+        <div class="hrn1"></div>
+      </div>
+      <div class="col-md-8">
+        <div class="n1">
+          <p>
+            <b>印度斯坦的蘇丹</b>是<nuxt-link to="/batt/Tamerlane">帖木兒戰役</nuxt-link>的第四關。脫脫迷失逃去<nuxt-link to="/civs/Lithuanians">立陶宛</nuxt-link>部隊，<nuxt-link to="/units/hero/Tamerlane">帖木兒</nuxt-link>已制定了迎擊的戰術。
+          </p>
+        </div>
+        <h2>玩家</h2>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <h3>基本資訊</h3>
+          <table class="table-bordered" width="100%">
+            <tr>
+              <td class="tc1" width="14%">起始時代：</td>
+              <td class="tc2" width="15%">
+                <nuxt-link to="/techs/age/Castle_Age">城堡時代</nuxt-link>
+              </td>
+              <td class="tc1" width="14%">起始資源：</td>
+              <td class="tc2">
+                無
+              </td>
+              <td class="tc1" width="14%">人口上限：</td>
+              <td class="tc2" width="5%">200</td>
+            </tr>
+          </table>
+
+          <h3>AI</h3>
+          <table class="table-bordered" width="100%">
+            <tr>
+              <td class="tc4" colspan="4" style="background: #2980b9;">
+                玩家本身
+              </td>
+            </tr>
+            <tr>
+              <td class="tc6" width="14%">
+                <div class="p6"></div>
+                玩家1
+              </td>
+              <td class="tc6" colspan="6">
+                <nuxt-link to="/civs/Tatars">韃靼人</nuxt-link>（紫色）
+              </td>
+            </tr>
+
+            <tr>
+              <td class="tc4" colspan="4" style="background: #27ae60;">同盟</td>
+            </tr>
+            <tr>
+              <td class="tc6" width="14%">
+                <div class="p2"></div>
+                玩家2
+              </td>
+              <td class="tc6">帖木兒軍隊（紅色）韃靼人</td>
+              <td class="tc6" width="14%">
+                <div class="p7"></div>
+                玩家3
+              </td>
+              <td class="tc6">德里（灰色）<nuxt-link to="/civs/Indians">印度人</nuxt-link></td>
+            </tr>
+            <tr>
+              <td class="tc6" width="14%">
+                <div class="p8"></div>
+                玩家5
+              </td>
+              <td class="tc6">沙阿圖格魯克（橙色）印度人</td>
+            </tr>
+            <tr>
+              <td col class="tc4" colspan="4" style="background:#ea6153;">
+                敵對
+              </td>
+            </tr>
+            <tr>
+              <td class="tc6" width="14%">
+                <div class="p3"></div>
+                玩家4
+              </td>
+              <td class="tc6">德里駐軍（綠色）印度人</td>
+              <td class="tc6" width="14%">
+                <div class="p5"></div>
+                玩家6
+              </td>
+              <td colspan="3" class="tc6">外圍村莊（青色）印度人</td>
+            </tr>
+
+            <tr>
+              <td class="tc6" width="14%">
+                <div class="p1"></div>
+                玩家7
+              </td>
+              <td class="tc6">
+                外圍村莊（藍色）印度人
+              </td>
+            </tr>
+          </table>
+
+          <br />
+          <dl class="dl-horizontal">
+            <dt>開場：</dt>
+            <dd>
+              <nuxt-link to="/units/Keshik">怯薛</nuxt-link>x20，<nuxt-link to="/units/Cavalry_Archer">馬弓騎兵</nuxt-link>x20，<nuxt-link to="/units/Light_Cavalry">輕騎兵</nuxt-link>x22，<nuxt-link to="/units/Pikeman">重裝長槍兵</nuxt-link>x20
+            </dd>
+          </dl>
+          <h3>任務</h3>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ul>
+              <li>等待戰鬥開始。</li>
+              <li>在德里城外打敗沙阿圖格魯克的軍隊。</li>
+              <li>在時間限制內摧毀防守德里的5座<nuxt-link to="/building/Castle">城堡</nuxt-link>。</li>
+              <li>
+                次要任務：襲擊外圍的印度村莊和<nuxt-link to="/building/Monastery">修道院</nuxt-link>，奪取他們的糧草，切斷德里的補給線。
+              </li>
+            </ul>
+          </div>
+          <h3>提示</h3>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ul>
+              <li>
+                帖木兒的勢力可以發展到<nuxt-link to="/techs/age/Imperial_Age">帝王時代</nuxt-link>。人口上限為200。
+              </li>
+              <li>
+                帖木兒的軍隊配有一支大型補給車隊，但並非永久性的。必須在時間限制內（代表剩餘糧草量）佔領德里。
+              </li>
+              <li>
+                良好的軍隊管理將決定最初戰鬥的動向。利用輕騎兵的速度從敵人側翼進攻，消滅脆弱的<nuxt-link to="/units/Archer_Units">遠程單位</nuxt-link>。
+              </li>
+              <li>
+                地圖南方和西方的山脈和叢林資源豐富，而德里周邊的平原是建造更多軍事<nuxt-link to="/building">建築</nuxt-link>的理想位置。
+              </li>
+              <li>外圍村莊和<nuxt-link to="/building/Monastery">修道院</nuxt-link>會提供德里補給。消滅他們是明智的做法。</li>
+            </ul>
+          </div>
+          <h3>斥候</h3>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ul>
+              <li>
+                帖木兒的軍隊(1，紫色)從三座軍事開始進攻德里，在城外的平原上列陣完畢，準備戰鬥。
+              </li>
+              <li>
+                沙阿圖格魯克(5，橙色)的軍隊擋在帖木兒和德里之間。沙阿圖格魯克的軍隊中有駱駝兵、<nuxt-link to="/units/Infantry_Units">步兵</nuxt-link>、<nuxt-link to="/units/Skirmisher">矛兵</nuxt-link>、<nuxt-link to="/units/Light_Cavalry">輕騎兵</nuxt-link>和<nuxt-link to="/units/War_Elephant">戰象</nuxt-link>。
+              </li>
+              <li>
+                北邊是防守嚴密的德里城(3，灰色)。城中駐軍(4，綠色)包括駱駝兵、矛兵、劍兵和野象，旨在拖延帖木兒前進的速度。
+              </li>
+              <li>
+                外圍印度村裝和修道院(青色、藍色)會向德里城輸送補給，支援德里守軍。掃蕩這些地方能夠對帖木兒的征程有很大作用。
+              </li>
+            </ul>
+          </div>
+          <h3>攻略</h3>
+          <div class="hrn1"></div>
+          <ul>
+            <li>
+              一開始將和橙色進行一場會戰。帖木兒使用<nuxt-link to="/units/Flaming_Camel">火焰駱駝</nuxt-link>讓<nuxt-link to="/units/War_Elephant">戰象</nuxt-link>倒戈回去攻擊他們士兵（控制權歸我方，橙色馬上轉敵對），其他近戰單位跟上，會戰要花點心思控兵，輕騎兵隊從兩翼包抄敵人的<nuxt-link to="/units/Elite_Skirmisher">戰矛兵</nuxt-link>免得對我方馬弓造成極大損傷。贏得會戰後會得到<nuxt-link to="/resource/Wood">木材</nuxt-link><nuxt-link to="/resource/Gold">黃金</nuxt-link><nuxt-link to="/resource/Food">食物</nuxt-link>各2000以及500石頭，基地的<nuxt-link to="/units/Villager">村民</nuxt-link>也歸我控制。利用這些資源和村民開始發展經濟。
+            </li>
+            <li>
+              任務目標要在90分內攻破德里城的五座城堡，如果在85分內完成可以獲得成就「野火燎原」，支線任務則是消滅青色和藍色以斷絕城內補給。由於敵人完全不會主動攻擊，因此可以專心發展。剩餘的軍隊治療一下後就派快速部隊去掃蕩周邊。
+            </li>
+            <li>
+              西側和西南側的礦點有兩座修道院，每摧毀一座就會有五隻精銳<nuxt-link to="/units/Mameluke">阿拉伯奴隸兵</nuxt-link>從西北方支援（共十隻），順手把<nuxt-link to="/Relic">遺跡</nuxt-link>撿回家。正南方有一個小據點，摧毀後可以獲得木材和黃金各500。
+            </li>
+            <li>
+              東側的基地則投入這一戰獲得的戰象，搭配<nuxt-link to="/units/Light_Cavalry">輕騎兵</nuxt-link>和馬弓即可輕鬆踏平村莊。摧毀<nuxt-link to="/building/Town_Center">城鎮中心</nuxt-link>可獲得食物和木材各1000，摧毀每個<nuxt-link to="/building/Dock">碼頭</nuxt-link>可各得500黃金共1500金。
+            </li>
+            <li>
+              最後就剩摧毀德里的五座城堡了。兵種投入大量的重裝馬弓騎兵、搭配<nuxt-link to="/units/Hussar">匈牙利輕騎兵</nuxt-link>、<nuxt-link to="/units/Halberdier">戟兵</nuxt-link>、<nuxt-link to="/units/Monk">僧侶</nuxt-link>以及<nuxt-link to="/units/Siege_Ram">重型衝撞車</nuxt-link>和<nuxt-link to="/units/Trebuchet">巨型投石機</nuxt-link>。先利用19射程的<nuxt-link to="/units/Trebuchet">巨型投石機</nuxt-link>摧毀外圍的城堡，再摧毀裡面的城堡，搭配<nuxt-link to="/armor/Armor_Class_Ram">衝撞車</nuxt-link>好幫我軍開出空地。敵人有大量的<nuxt-link to="/units/Elephant_Archer">象弓騎兵</nuxt-link>、<nuxt-link to="/units/Heavy_Camel">重裝駱駝騎兵</nuxt-link>、<nuxt-link to="/units/Two-Handed_Swordsman">雙手劍兵</nuxt-link>、<nuxt-link to="/units/Scorpion">弩砲</nuxt-link>和戰矛兵，還會有少量戰象和<nuxt-link to="/units/Monk">僧侶</nuxt-link>。輕騎兵的目標是遠程部隊、<nuxt-link to="/units/Scorpion">弩砲</nuxt-link>和<nuxt-link to="/units/Monk">僧侶</nuxt-link>，<nuxt-link to="/units/Halberdier">戟兵</nuxt-link>則以<nuxt-link to="/armor/Armor_Class_Camel">駱駝騎兵</nuxt-link>為目標，配合馬弓攻擊敵人的守軍。如果有事先摧毀外圍的營地，德里城很快就物資告罄，再也生不出守軍了。只要在限時內將五座城堡摧毀本關即勝利。
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <table class="table-bordered" width="100%">
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#375d96;">
+              <div class="unititle2">印度斯坦的蘇丹</div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              （地圖待補）
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#375d96;">
+              <div class="unititle2">戰役資訊</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">版本</td>
+            <td class="unit4">
+              <nuxt-link to="/games/de">決定版</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">戰役</td>
+            <td class="unit4">
+              <nuxt-link to="/batt/Tamerlane">帖木兒</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3"><nuxt-link to="/civs">文明</nuxt-link></td>
+            <td class="unit4">
+              <nuxt-link to="/civs/Tatars">韃靼</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#375d96;">
+              <div class="unititle2">戰役關卡編號</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">章節</td>
+            <td class="unit4">
+              帖木兒第四章
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">上一關</td>
+            <td class="unit4"><nuxt-link to="/batt/Harbinger_of_Destruction">毀滅的先兆</nuxt-link></td>
+          </tr>
+          <tr>
+            <td class="unit3">下一關</td>
+            <td class="unit4"><nuxt-link to="/batt/Scourge_of_the_Levant">黎凡特的災禍</nuxt-link></td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'allbatt',
+  components: {}
+}
+</script>
+<style></style>
