a3747655c982b9b4cd21c298e1a3704a11a80aa1
webrsb <webrsb@gmail.com>
2021-03-29 00:03:58 +0800

    Update Catalaunian_Fields.vue

--- pages/batt/Catalaunian_Fields.vue
+++ pages/batt/Catalaunian_Fields.vue
@@ -172,7 +172,7 @@
               >和一些<nuxt-link to="/units/Cavalry_Units">騎兵</nuxt-link
               >，而奄蔡人的軍隊主要就由<nuxt-link to="/units/Knight"
                 >騎士</nuxt-link
-              >和<nuxt-link to="/units/Cavalry_Archer">馬騎弓兵</nuxt-link>組成。
+              >和<nuxt-link to="/units/Cavalry_Archer">馬弓騎兵</nuxt-link>組成。
             </li>
           </ul>
           <h2>芝麻小事</h2>
