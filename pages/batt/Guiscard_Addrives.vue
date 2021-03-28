27b136c48e886c101d26ce781619eabd0c964da2
harry3305 <52451150+harry3305@users.noreply.github.com>
2021-03-28 23:50:37 +0800

    Create Guiscard_Addrives (#703)
    * Create Guiscard_Addrives
    
    歐特維爾 第一關
    
    * Update Guiscard_Addrives
    
    增加快速型過關卡
    版面亂掉重牌

---
+++ pages/batt/Guiscard_Addrives.vue
@@ -0,0 +1,293 @@
+<template>
+  <div class="container">
+    <div class="row padtop">
+      <div class="col-md-12">
+        <h1>吉斯卡爾的到來</h1>
+        <div class="hrn1"></div>
+      </div>
+      <div class="col-md-8">
+        <div class="n1">
+          <p>
+            <b>吉斯卡爾的到來</b>是<nuxt-link to="/games/delw">西方霸主</nuxt-link>DLC新增的<nuxt-link to="/batt">戰役</nuxt-link>中，<nuxt-link to="/batts/The_Hautevilles">歐特維爾家族</nuxt-link>的第一關。該系列<nuxt-link to="/batt">戰役</nuxt-link>的劇情會串聯起其他相關<nuxt-link to="/batt">戰役</nuxt-link>的關聯。
+          </p>
+        </div>
+        <h2>玩家</h2>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <h3>基本資訊</h3>
+          <table class="table-bordered" width="100%">
+            <tr>
+              <td class="tc1" width="14%">起始時代：</td>
+              <td class="tc2" width="28%">
+                <nuxt-link to="/techs/age/Castle_Age">城堡時代</nuxt-link>
+              </td>
+              <td class="tc1" width="14%">起始資源：</td>
+              <td class="tc2">
+                無
+              </td>
+              <td class="tc1" width="14%">人口上限：</td>
+              <td class="tc2" width="5%">160</td>
+            </tr>
+          </table>
+
+          <h3>AI</h3>
+          <table class="table-bordered" width="100%">
+            <tr>
+              <td class="tc4" colspan="4" style="background: #2980b9;">
+                玩家本身
+              </td>
+            </tr>
+            <tr>
+              <td class="tc6">
+                <div class="p1"></div>
+                玩家1
+              </td>
+              <td class="tc6" colspan="3">
+                諾曼人［<nuxt-link to="/civs/Sicilians">西西里人</nuxt-link>］（藍色）
+              </td>
+            </tr>
+
+            <tr>
+              <td class="tc4" colspan="4" style="background: #27ae60;">同盟</td>
+            </tr>
+            <tr>
+              <td class="tc6">
+                <div class="p2"></div>
+                玩家2
+              </td>
+              <td class="tc6">諾曼男爵（紅色）諾曼人［西西里人］</td>
+            </tr>
+            <tr>
+              <td class="tc6">
+                <div class="p4"></div>
+                玩家4
+              </td>
+              <td class="tc6">倫巴底的瓜伊馬爾（黃色）<nuxt-link to="/civs/Italians">義大利人</nuxt-link></td>
+            </tr>
+            <tr>
+              <td class="tc6">
+                <div class="p6"></div>
+                玩家5
+              </td>
+              <td class="tc6">拜占庭的阿爾吉魯斯（紫色）<nuxt-link to="/civs/Byzantines">拜占庭人</nuxt-link></td>
+            </tr>
+            <tr>
+              <td class="tc4" colspan="4" style="background:#ea6153;">敵對</td>
+            </tr>
+            <tr>
+              <td class="tc6">
+                <div class="p5"></div>
+                玩家3
+              </td>
+              <td class="tc6">
+                德倫戈家族（青色）諾曼人［西西里人］
+              </td>
+            </tr>
+            <tr>
+              <td class="tc6">
+                <div class="p3"></div>
+                玩家6
+              </td>
+              <td class="tc6">
+                薩拉森海盜（綠色）<nuxt-link to="/civs/Saracens">薩拉森人</nuxt-link>
+              </td>
+            </tr>
+          </table>
+
+          <br />
+          <dl class="dl-horizontal">
+            <dt>開場：</dt>
+            <dd>
+              羅伯特‧吉斯卡爾
+            </dd>
+          </dl>
+
+          <h3>任務</h3>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ul>
+              <li>與德羅戈‧德‧歐特維爾交談。</li>
+              <li>將羅伯特帶至他的追隨者處。</li>
+              <li>請從以下任務中選擇一項：</li>
+              <ol>
+                <li>
+                  背叛並透過擊殺駐軍來佔領第一塊諾曼男爵領地。若要背叛一名玩家，請前往外交介面並將您對該玩家的立場改為中立或敵對。
+                </li>
+                <li>前往薩勒諾為倫巴底人效力。</li>
+                <li>前往巴里為拜占庭人效力。</li>
+              </ol>
+              <li>控制3(4)個諾曼男爵領地。（依難度要求不同）</li>
+              <li>摧毀倫巴底或拜占庭<nuxt-link to="/building/Castle">城堡</nuxt-link>。</li>
+            </ul>
+            <p>次要任務</p>
+            <ul>
+              <li>使用您的<nuxt-link to="/units/hero">英雄</nuxt-link>戰鬥，藉由戰鬥提升他們的實力。</li>
+              <li>
+                每占領一個諾曼男爵領地能為人口上限增加15人，並為您提供額外的諾曼士兵。
+              </li>
+              <li>每佔領一個村莊能為人口上限增加5人，並為您提供<nuxt-link to="/units/Villager">村民</nuxt-link>和資源。</li>
+              <li>
+                前往倫巴底或拜占庭的城堡，為相應的勢力效力。您只能效忠於一個領主，所以得謹慎選擇！
+              </li>
+            </ul>
+          </div>
+          <h3>提示</h3>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ul>
+              <li>羅伯特‧德‧歐特維爾只能發展到<nuxt-link to="/techs/age/Castle_Age">城堡時代</nuxt-link>。</li>
+              <li>
+                您的人口上限為160人，但您不能建造<nuxt-link to="/building/House">居住房舍</nuxt-link>，必須佔領諾曼男爵領地和村莊來提升人口上限。
+              </li>
+              <li>
+                您有幾種方式可以建立自己的勢力。前往倫巴底或<nuxt-link to="/civs/Byzantines">拜占庭</nuxt-link>城市，向君主許諾今後向他們進貢以換取土地。或者您可以透過背叛宗親來占領一個諾曼男爵領地，以確保您的獨力。
+              </li>
+              <li>
+                殺死防守諾曼男爵領地和敵方村莊的單位，可以讓您佔領這些地盤。佔領諾曼男爵領地可以獲得軍事<nuxt-link to="/building">建築</nuxt-link>和部隊；佔領村莊可以獲得<nuxt-link to="/units/Villager">村民</nuxt-link>和經濟<nuxt-link to="/building">建築</nuxt-link>。
+              </li>
+              <li>
+                如果您選擇為倫巴底人或拜占庭人效力，您可能會發現持續為他們服務的代價十分昂貴。您可以隨時轉變您的外交力場來反抗他們。
+              </li>
+              <li>
+                諾曼人只尊重軍事實力，所以您的歐特維爾英雄們必須在前線領導他們。您的英雄們可以透過與敵方作戰來提升自身的實力，所以您應該讓他們頻繁地投身戰場。
+              </li>
+            </ul>
+          </div>
+          <h3>斥候</h3>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ul>
+              <li>
+                羅伯特‧德‧歐特維爾（1，藍色）已經抵達了義大利南部，尋求他身為諾曼第小貴族的次子所無法獲得的土地和財富。
+              </li>
+
+              <li>
+                其他諾曼人（2，紅色）幾十年前就到了這裡，利用義大利人的分裂在山區奪取了自己的男爵領地。佔領這些男爵領地至關重要，因為諾曼人會為羅伯特的戰團提供支援。諾曼人的部隊由<nuxt-link to="/units/Knight">騎士</nuxt-link>、<nuxt-link to="/units/Serjeant">薩金特衛兵</nuxt-link>以及一些輔助兵種組成。
+              </li>
+
+              <li>
+                諾曼人德侖戈家族（3，青色）在義大利是歐特維爾家族的死對頭。他們的<nuxt-link to="/techs/Marauders">掠奪者</nuxt-link>###會從海上發起進攻，然後肆意地在諾曼男爵領地區域進行掠奪。他們的主要戰力是<nuxt-link to="/units/Knight">騎士</nuxt-link>和<nuxt-link to="/units/Light_Cavalry">輕騎兵</nuxt-link>。
+              </li>
+
+              <li>
+                拜占庭人（5，紫色）曾經統治著整個義大利南部。一個名叫梅勒斯的倫巴底親王在第一批到達的諾曼人支持底下發起了一次叛亂，雖然最終失敗了，但仍然削弱了拜占庭的統治。儘管如此，在梅勒斯之子阿爾吉魯斯的領導下，拜占庭人又憑藉著其多樣化的軍隊和軍團兵重新在東部和南部站穩了腳跟。
+              </li>
+
+              <li>
+                瓜伊馬爾親王（4，黃色）統治著倫巴底人，他們是北方日耳曼移民的後裔。倫巴底人的部隊主要由<nuxt-link to="/units/Infantry_Units">步兵</nuxt-link>和<nuxt-link to="/units/Archer">弓兵</nuxt-link>趕出義大利，即便這樣做有其風險，可能導致更危險的諾曼人到來。
+              </li>
+
+              <li>
+                最後，薩拉森海盜（6，綠色）以<nuxt-link to="/civs/Sicilians">西西里</nuxt-link>為據點威脅著整個義大利，當地的<nuxt-link to="/maps/Arabia">阿拉伯</nuxt-link>埃米爾保護著他們。海盜主要在海上用<nuxt-link to="/units/Naval_Vessels">戰船</nuxt-link>作戰，但也會運載掠奪者登陸義大利的海岸。
+              </li>
+            </ul>
+          </div>
+          <h3>攻略</h3>
+          <div class="hrn1"></div>
+          <ul>
+            <li>
+              開場劇情拿到跟隨者後，可選擇占領一個領地或選擇為領主效力。即使選擇了占領一個領地，之後還是可以選擇效力一個領主。選擇後發展經濟的同時去掠奪敵人的領地。隨著發展會有幾波援軍，而讓英雄去戰鬥可以提升能力值直到上限為止，但如果被打敗也不會算失敗。要注意的是如果選擇任何一個領主效力，則一旦蓋城堡或是進攻另一方的領地時，效力的領主會把玩家視為背叛者改敵對。
+            </li>
+
+            <li>
+              附帶一提，如果一開始放置一小時仍沒有拿到諾曼男爵領地也沒有為領主效力，則會有拜占庭的間諜殺了戈爾德，並拿到該處的領地和軍隊。該領地也算在任務要求要拿下的領地之一。
+            </li>
+
+            <li>
+              如果不選擇為任何一個領主效力，通關後可以獲得成就「白手起家」。建議用成就方式，直接選一個諾曼領地後將敵人調中立（避免誤傷村民和建築），對剩下兩勢力也一樣。將兵力補一下直接打爆在<nuxt-link to="/building/Town_Center">城鎮中心</nuxt-link>的軍隊還可以直接拿到城鎮中心和<nuxt-link to="/units/Villager">村民</nuxt-link>，比起為領主效力還要擔心被對方捅一刀好多了。然後就把剩下的小村莊和領地拿一拿，因為這關要提升人口上限的方式要靠拿領地和村莊。
+            </li>
+
+            <li>
+              這段期間內薩拉森海盜會不斷用船和運駱駝兵過來騷擾，有多的步兵就用<nuxt-link to="/units/Transport_Ship">運輸船</nuxt-link>送過去把<nuxt-link to="/building/Dock">碼頭</nuxt-link>和生兵建築打掉就沒有後顧之憂了，還有個<nuxt-link to="/Relic">遺跡</nuxt-link>可以拿。青色的德倫戈家則是出現時間會比較晚，但難度低加上打的快的話有可能整場都沒看到。陸續戰鬥後過一段時間會有幾波<nuxt-link to="/units/Transport_Ship">運輸船</nuxt-link>的援軍，最後一波還附帶兩台巨投，直接用巨投把其中一座城堡炸毀後，加上自己有拿下三或四個領地就過關（依難度有所不同）。
+            </li>
+
+            <li>
+              放置型快速過關法如下：先放置一個小時後直接拿到大批軍隊和初始據點，期間也有幾波運輸船的軍隊讓其合流。拿到軍隊後，載著巨投的運輸船差不多也到了。派少量軍隊守護巨投打掉黃色城堡，騎士團和步兵團攻擊紅色領地，並派<nuxt-link to="/units/Monk">僧侶</nuxt-link>治療傷兵。打下黃色城堡加上規定的領地數量後直接獲勝，連發展都不用。
+            </li>
+          </ul>
+          <h2>芝麻小事</h2>
+          <div class="hrn1"></div>
+          <div class="n1">
+            <ul>
+              <li>
+                如果透過聊天清單或是點選玩家、紅色、青色的單位，則玩家的種族會顯示為諾曼人而不是西西里人。但諾曼人是西西里人的前身故<nuxt-link to="/tree/vie">科技樹</nuxt-link>亦是西西里人。第二關到第四關玩家亦是諾曼人，直到第五關才是西西里人。
+              </li>
+              <li>
+                在戰役中諾曼人會聊到威廉公爵，當時他還沒征服英格蘭，一直被當私生子笑話。
+              </li>
+              <li>
+                吉斯卡爾在包圍巴里三年之後攻下城池，就是在<nuxt-link to="/batt/Baria">巴里戰役</nuxt-link>的最後一關的主要敵人。
+              </li>
+              <li>
+                阿爾吉魯斯就是巴里戰役裡叛變的梅勒斯的兒子。這點在斥侯的敘述中也有提及。
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <table class="table-bordered" width="100%">
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#375d96;">
+              <div class="unititle2">吉斯卡爾的到來</div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              （地圖待補）
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#375d96;">
+              <div class="unititle2">戰役資訊</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">版本</td>
+            <td class="unit4">
+              <nuxt-link to="/games/delw">西方霸主</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">戰役</td>
+            <td class="unit4">
+              <nuxt-link to="/batts/The_Hautevilles">歐特維爾家族</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3"><nuxt-link to="/civs">文明</nuxt-link></td>
+            <td class="unit4">
+              <nuxt-link to="/civs/Sicilians">西西里</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#375d96;">
+              <div class="unititle2">戰役關卡編號</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">章節</td>
+            <td class="unit4">
+              歐特維爾家族第一章
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">上一關</td>
+            <td class="unit4">N/A</td>
+          </tr>
+          <tr>
+            <td class="unit3">下一關</td>
+            <td class="unit4"> <nuxt-link to="/batt/Roger_in_Sicily">西西里的羅傑</nuxt-link></td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'allbatt',
+  components: {}
+}
+</script>
+<style></style>
