f34f1c230a6dff42c6b54751077360f7b9a05eaf
zhaoleftstop <35518174+zhaoleftstop@users.noreply.github.com>
2021-06-19 03:31:06 +0800

    Update editor.vue
    修正研發於學院的連結，上一版連去射箭場了

--- pages/editor.vue
+++ pages/editor.vue
@@ -1,21 +1,260 @@
 <template>
-  <play />
+  <div class="container">
+    <div class="row padtop">
+      <div class="col-md-12">
+        <h1>磨坊水車</h1>
+        <div class="hrn1"></div>
+      </div>
+      <div class="col-md-8">
+        <div class="n1">
+          <p>
+            <b>磨坊水車</b>(2013版稱<b>滑輪起重機</b>)可於<nuxt-link
+              to="/techs/age/Castle_Age"
+              >城堡時代</nuxt-link
+            >在<nuxt-link to="/building/University">學院</nuxt-link
+            >研發，研發後<nuxt-link to="/building">建築物</nuxt-link
+            >建造速度快20%。
+          </p>
+          <p>
+            <nuxt-link to="/civs/Spanish">西班牙</nuxt-link
+            >雖沒有磨坊水車<nuxt-link to="/techs">科技</nuxt-link
+            >，他們的<nuxt-link to="/civs">文明</nuxt-link>效益可讓<nuxt-link
+              to="/units/Villager"
+              >村民</nuxt-link
+            >工作速率快30%，但是不會影響<nuxt-link to="/building/Wonder"
+              >世界奇觀</nuxt-link
+            >的建造時間因為所有文明建造奇觀的時間都是一樣的
+          </p>
+        </div>
+        <h3>文明可研發與否</h3>
+        <div class="hrn1"></div>
+        <div class="row">
+          <div class="col-md-12">
+            <div class="n1">
+              <h4>
+                <strong
+                  ><i class="fas fa-check" style="color: green;"></i
+                  ><span style="margin-left: 2px;">可研發</span></strong
+                >
+              </h4>
+              <nuxt-link to="/civs/Aztecs">阿茲特克</nuxt-link>、<nuxt-link
+                to="/civs/Berbers"
+                >柏柏</nuxt-link
+              >、<nuxt-link to="/civs/Burmese">緬甸</nuxt-link>、<nuxt-link
+                to="/civs/Celts"
+                >塞爾特</nuxt-link
+              >、<nuxt-link to="/civs/Franks">法蘭克</nuxt-link>、<nuxt-link
+                to="/civs/Incas"
+                >印加</nuxt-link
+              >、<nuxt-link to="/civs/Italians">義大利</nuxt-link>、<nuxt-link
+                to="/civs/Japanese"
+                >日本</nuxt-link
+              >、<nuxt-link to="/civs/Khmer">高棉</nuxt-link>、<nuxt-link
+                to="/civs/Koreans"
+                >朝鮮</nuxt-link
+              >、<nuxt-link to="/civs/Magyars">馬扎爾</nuxt-link>、<nuxt-link
+                to="/civs/Malians"
+                >馬利</nuxt-link
+              >、<nuxt-link to="/civs/Mayans">馬雅</nuxt-link>、<nuxt-link
+                to="/civs/Portuguese"
+                >葡萄牙</nuxt-link
+              >、<nuxt-link to="/civs/Saracens">薩拉森</nuxt-link>、<nuxt-link
+                to="/civs/Slavs"
+                >斯拉夫</nuxt-link
+              >、<nuxt-link to="/civs/Teutons">條頓</nuxt-link>。<nuxt-link
+                to="/civs/Turks"
+                >土耳其</nuxt-link
+              >、<nuxt-link to="/civs/Vietnamese">越南</nuxt-link>、<nuxt-link
+                to="/civs/Vikings"
+                >維京</nuxt-link
+              >。
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <div class="n1">
+              <h4>
+                <strong
+                  ><i class="fas fa-times" style="color: #d9534f;"></i
+                  ><span style="margin-left: 2px;">不可研發</span></strong
+                >
+              </h4>
+              <nuxt-link to="/civs/Britons">不列顛</nuxt-link>、<nuxt-link
+                to="/civs/Byzantines"
+                >拜占庭</nuxt-link
+              >、<nuxt-link to="/civs/Chinese">中國</nuxt-link>、<nuxt-link
+                to="/civs/Ethiopians"
+                >衣索比亞</nuxt-link
+              >、<nuxt-link to="/civs/Goths">哥德</nuxt-link>、<nuxt-link
+                to="/civs/Huns"
+                >匈人</nuxt-link
+              >、<nuxt-link to="/civs/Indians">印度</nuxt-link>、<nuxt-link
+                to="/civs/Malay"
+                >馬來</nuxt-link
+              >、<nuxt-link to="/civs/Mongols">蒙古</nuxt-link>、<nuxt-link
+                to="/civs/Persians"
+                >波斯</nuxt-link
+              >、<nuxt-link to="/civs/Spanish">西班牙</nuxt-link>。
+            </div>
+          </div>
+        </div>
+        <h3>團隊加分</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <ul>
+            <li>
+              <nuxt-link to="/civs/Malians">馬利</nuxt-link
+              >：該科技研發時間快80%
+            </li>
+          </ul>
+        </div>
+        <h3>更新日誌</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <h3>
+            <em><nuxt-link to="/games/aok">帝王世紀</nuxt-link></em>
+          </h3>
+          <ul>
+            <li>成本為200木材300石頭。</li>
+            <li>
+              <nuxt-link to="/civs/Goths">哥德</nuxt-link>:可以研發磨坊水車。
+            </li>
+            <li>
+              <nuxt-link to="/civs/Japanese">日本</nuxt-link
+              >:不可以研發磨坊水車。
+            </li>
+            <li>
+              <nuxt-link to="/civs/Vikings">維京</nuxt-link
+              >:不可以研發磨坊水車。
+            </li>
+          </ul>
+          <h3>
+            <em><nuxt-link to="/games/aoc">征服者入侵</nuxt-link></em>
+          </h3>
+          <ul>
+            <li>1.0c改版後成本現在為300食物200木材。</li>
+            <li>
+              <nuxt-link to="/civs/Huns">匈人</nuxt-link>:可以研發磨坊水車。
+            </li>
+          </ul>
+          <h3>
+            <em><nuxt-link to="/games/aof">失落的帝國</nuxt-link></em>
+          </h3>
+          <ul>
+            <li><nuxt-link to="/civs/Goths">哥德</nuxt-link>:移除磨坊水車。</li>
+            <li><nuxt-link to="/civs/Huns">匈人</nuxt-link>:移除磨坊水車。</li>
+            <li>
+              <nuxt-link to="/civs/Vikings">維京</nuxt-link>:可以研發磨坊水車。
+            </li>
+          </ul>
+          <h3>
+            <em><nuxt-link to="/games/ak">非洲王國</nuxt-link></em>
+          </h3>
+          <ul>
+            <li>
+              <nuxt-link to="/civs/Japanese">日本</nuxt-link
+              >:4.8改版後從<nuxt-link to="/tree/vie">科技樹</nuxt-link
+              >移除磨坊水車。
+            </li>
+          </ul>
+        </div>
+        <h3>歷史</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <p>
+            城堡，大教堂和其他主要建築項目的建設需要發明新技術，以及重新運用古代技術。起重機是古人所知的一個重要工具，但是卻被黑暗時代的歐洲人所遺忘。簡單的起重機是一個帶有滑輪和滑車裝置的長桿，用於將負載拉到一定高度。踏輪機起重機是簡單版本的改進。它使用了一台大型的圓形跑步機讓人們可以走在上面。齒輪將水平圓周運動轉換為垂直轉動，為鼓周圍的風繩提供動力並拉起重物。踏輪機起重機的一個變體採用了一個男人可以推動的絞盤，就像在帆船上用來升起錨桿的絞盤。
+          </p>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <table class="table-bordered" width="100%">
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#31b0d5;">
+              <div class="unititle2">
+                磨坊水車
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <div class="unititle">
+                <img
+                  src="~assets/treadmill_crane.png"
+                  class="img-fluid"
+                  alt=""
+                />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#31b0d5;">
+              <div class="unititle2">
+                <nuxt-link to="/techs">科技介紹</nuxt-link>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">文明</td>
+            <td class="unit4">請看左方分類</td>
+          </tr>
+          <tr>
+            <td class="unit3">時代</td>
+            <td class="unit4">
+              <img class="gg" src="~assets/castle_age.png" alt="" /><nuxt-link
+                to="/techs/age/Castle_Age"
+                >城堡時代</nuxt-link
+              >
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">研發於</td>
+            <td class="unit4">
+              <img class="gg" src="~assets/university.png" alt="" /><nuxt-link
+                to="/building/University"
+                >學院</nuxt-link
+              >
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">研發時間</td>
+            <td class="unit4">50秒</td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#31b0d5;">
+              <div class="unititle2">成本</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">
+              <nuxt-link to="/resource/Wood">木材</nuxt-link>
+            </td>
+            <td class="unit4">200</td>
+          </tr>
+          <tr>
+            <td class="unit3">
+              <nuxt-link to="/resource/Food">食物</nuxt-link>
+            </td>
+            <td class="unit4">300</td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#31b0d5;">
+              <div class="unititle2">影響</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">影響</td>
+            <td class="unit4">建築物建造速度快20%。</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
 </template>
-
 <script>
-import play from '~/components/play'
-
 export default {
-  layout: 'nochat',
-  components: {
-    play
-  },
-  head() {
-    return {
-      title: '線上編輯器 - Aoetw'
-    }
-  }
+  layout: 'alltech',
+  components: {}
 }
 </script>
-
 <style></style>
